a018fc49eae49710aa20d60bad229c36011b520a
alfonsobries <alfonsobries@users.noreply.github.com>
2022-05-31 18:03:37 +0000

    style: resolve style guide violations

--- src/domains/transaction/hooks/use-send-transfer-form.ts
+++ src/domains/transaction/hooks/use-send-transfer-form.ts
@@ -129,7 +129,7 @@ export const useSendTransferForm = (wallet?: Contracts.IReadWriteWallet) => {
 		register("suppressWarning");
 
 		if (onlyHasOneNetwork) {
-			console.log(":D")
+			console.log(":D");
 			setValue("network", networks[0], { shouldDirty: true, shouldValidate: true });
 		}
 	}, [register, sendTransferValidation, commonValidation, fees, wallet, remainingBalance, amount, senderAddress]);
