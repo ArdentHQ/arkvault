10f854edf69b63fd0d713d624171a83ce974e412
Edgar Goetzendorff <hello@dated.fun>
2022-06-01 05:14:52 +0200

    refactor: always show mnemonic skeleton indices (#14)

--- src/domains/wallet/components/MnemonicList/MnemonicList.tsx
+++ src/domains/wallet/components/MnemonicList/MnemonicList.tsx
@@ -1,17 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Skeleton } from "@/app/components/Skeleton";
 
-interface Properties {
+interface MnemonicListProperties {
 	mnemonic: string;
 }
 
-const skeletons = [...Array.from({ length: 24 }).keys()].map((index) => {
-	const min = 50;
-	const max = 70;
-	return [index + 1, Math.floor(Math.random() * (max - min + 1) + min)];
-});
-
-export function MnemonicList({ mnemonic }: Properties) {
+export const MnemonicList: React.VFC<MnemonicListProperties> = ({ mnemonic }) => {
 	let mnemonicWords: string[];
 
 	// Check for Japanese "space"
@@ -34,23 +28,31 @@ export function MnemonicList({ mnemonic }: Properties) {
 			))}
 		</ul>
 	);
-}
+};
 
-export function MnemonicListSkeleton() {
+export const MnemonicListSkeleton: React.VFC = () => {
+	const skeletons = useMemo(
+		() =>
+			[...Array.from({ length: 24 })].map(() => {
+				const [min, max] = [50, 70];
+
+				return Math.floor(Math.random() * (max - min + 1) + min);
+			}),
+		[],
+	);
+
 	return (
 		<ul className="grid grid-cols-2 gap-x-3 gap-y-5 sm:grid-cols-4">
-			{skeletons.map(([index, width]) => (
+			{skeletons.map((width, index) => (
 				<li
 					data-testid="MnemonicList__item_skeleton"
 					key={index}
 					className="relative flex items-center rounded border border-theme-secondary-300 p-2 dark:border-theme-secondary-700 sm:p-4"
 				>
 					<span className="absolute top-0 left-0 hidden translate-x-2 -translate-y-2 bg-theme-background px-1 text-xs text-theme-secondary-700 sm:block">
-						{index}
+						{index + 1}
 					</span>
-					<div className="mr-4 block text-xs sm:hidden">
-						<Skeleton width={20} height={20} />
-					</div>
+					<div className="ml-1 mr-4 block text-xs text-theme-secondary-700 sm:hidden">{index + 1}</div>
 					<div>
 						<Skeleton width={width} height={20} />
 					</div>
@@ -58,4 +60,4 @@ export function MnemonicListSkeleton() {
 			))}
 		</ul>
 	);
-}
+};
