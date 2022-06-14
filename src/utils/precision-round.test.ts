ad3e11d7e57a72c78be0952929c34a6d5e3e9bd8
alfonsobries <alfonsobries@gmail.com>
2022-06-14 16:46:22 -0500

    remove unnecessary async

--- src/utils/precision-round.test.ts
+++ src/utils/precision-round.test.ts
@@ -7,7 +7,7 @@ describe("Precision round", () => {
 		[9.892_000_000_000_001, 2, 9.89],
 		[9.892_000_000_000_001, 15, 9.892_000_000_000_001],
 		[1, 10, 1],
-	])("rounds a number according to precision", async (number, precision, result) => {
+	])("rounds a number according to precision", (number, precision, result) => {
 		expect(precisionRound(number, precision)).toBe(result);
 	});
 });
