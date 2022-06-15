9b4a1179056ed21bdd3c2acf0cfbc2d0b276a8d0
goga-m <goga-m@users.noreply.github.com>
2022-06-15 09:02:08 +0000

    style: resolve style guide violations

--- src/domains/exchange/components/ExchangeForm/FormStep.tsx
+++ src/domains/exchange/components/ExchangeForm/FormStep.tsx
@@ -289,7 +289,7 @@ export const FormStep = ({ profile }: FormStepProperties) => {
 				>
 					{currency.coin.toUpperCase()}
 				</span>
-				<span className="text-sm text-theme-secondary-500 dark:theme-text-secondary-700">{currency.name}</span>
+				<span className="dark:theme-text-secondary-700 text-sm text-theme-secondary-500">{currency.name}</span>
 			</div>
 		);
 	};
