13cee38848899aae93160df03017ac1ebc210205
goga-m <goga.mamoulasvili@gmail.com>
2022-06-20 23:18:40 +0200

    wip

--- src/app/hooks/env.ts
+++ src/app/hooks/env.ts
@@ -1,11 +1,8 @@
-import { Networks } from "@ardenthq/sdk";
 import { Contracts } from "@ardenthq/sdk-profiles";
 import { useMemo } from "react";
 import { useHistory, useParams } from "react-router-dom";
 
 import { useEnvironmentContext } from "@/app/contexts/Environment";
-import { profileEnabledNetworkIds } from "@/utils/network-utils";
-import { sortWallets } from "@/utils/wallet-utils";
 
 export const useActiveProfile = (): Contracts.IProfile => {
 	const history = useHistory();
@@ -29,24 +26,6 @@ export const useActiveWallet = (): Contracts.IReadWriteWallet => {
 	const { walletId } = useParams<{ walletId: string }>();
 
 	return useMemo(() => profile.wallets().findById(walletId), [profile, walletId]);
-};
-
-export const useNetworks = (profile: Contracts.IProfile) => {
-	const isProfileRestored = profile.status().isRestored();
-	return useMemo<Networks.Network[]>(() => {
-		const networks: Record<string, Networks.Network> = {};
-
-		const wallets = sortWallets(profile.wallets().values());
-
-		for (const wallet of wallets) {
-			if (profileEnabledNetworkIds(profile).includes(wallet.networkId())) {
-				networks[wallet.networkId()] = wallet.network();
-			}
-		}
-
-		return Object.values(networks);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [profile, isProfileRestored]);
 };
 
 export const useActiveWalletWhenNeeded = (isRequired: boolean) => {
