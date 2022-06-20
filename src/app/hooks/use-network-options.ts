ce55f327d7d4f9fb7ef2b47e9bbf400c61c9bdba
goga-m <goga-m@users.noreply.github.com>
2022-06-20 21:25:29 +0000

    style: resolve style guide violations

--- src/app/hooks/use-network-options.ts
+++ src/app/hooks/use-network-options.ts
@@ -1,8 +1,8 @@
 import { useCallback } from "react";
 import { Contracts } from "@ardenthq/sdk-profiles";
 import { Networks } from "@ardenthq/sdk";
-import { networksAsOptions } from "@/utils/network-utils";
 import { useNetworks } from "./use-networks";
+import { networksAsOptions } from "@/utils/network-utils";
 
 export const useNetworkOptions = ({ profile }: { profile: Contracts.IProfile }) => {
 	const networks = useNetworks({ profile });
