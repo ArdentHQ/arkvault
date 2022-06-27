42d558152cda582a6977be477edf1854754dee85
goga-m <goga.mamoulasvili@gmail.com>
2022-06-27 09:30:23 +0200

    wip

--- src/domains/transaction/components/TransactionExportModal/TransactionExportForm/BasicSettings.tsx
+++ src/domains/transaction/components/TransactionExportModal/TransactionExportForm/BasicSettings.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cn from "classnames";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { useTransactionTypeOptions, useDateRangeOptions } from "./hooks";
@@ -7,65 +8,83 @@ import { CollapseToggleButton } from "@/app/components/Collapse";
 import { Dropdown } from "@/app/components/Dropdown";
 import { FormField } from "@/app/components/Form";
 import { ListDivided } from "@/app/components/ListDivided";
+import { useBreakpoint } from "@/app/hooks";
 
-export const BasicSettings = () => {
-	const { t } = useTranslation();
-
+const TranctionTypeOptions = () => {
+	const { isXs } = useBreakpoint();
 	const form = useFormContext();
 
 	const { options: transactionTypeOptions } = useTransactionTypeOptions({
 		selectedValue: form.watch("transactionType"),
 	});
 
-	const { options: dateRangeOptions, selected: selectedDate } = useDateRangeOptions({
+	return (
+		<div>
+			<ButtonGroup
+				className={cn({
+					"mt-4 flex flex-col space-y-3": isXs,
+					"space-x-3": !isXs,
+				})}
+			>
+				{transactionTypeOptions.map(({ label, value, active }) => (
+					<ButtonGroupOption
+						key={value}
+						tooltipContent={label}
+						isSelected={() => active}
+						setSelectedValue={() => form.setValue("transactionType", value)}
+						value={value}
+						variant="modern"
+					>
+						<div className="px-2">{label}</div>
+					</ButtonGroupOption>
+				))}
+			</ButtonGroup>
+		</div>
+	);
+};
+
+const DateRangeOptions = () => {
+	const form = useFormContext();
+
+	const { options, selected } = useDateRangeOptions({
 		selectedValue: form.watch("dateRange"),
 	});
+
+	return (
+		<FormField name="dateRange">
+			<Dropdown
+				data-testid="TransactionExportForm--daterange-options"
+				options={options}
+				onSelect={(option) => form.setValue("dateRange", option.value)}
+				toggleContent={(isOpen: boolean) => (
+					<CollapseToggleButton
+						isOpen={isOpen}
+						className="w-full cursor-pointer justify-between space-x-4 overflow-hidden rounded-xl border border-theme-success-100 p-3 dark:border-theme-secondary-800 sm:p-6 md:w-auto md:space-x-2 md:rounded md:border-0 md:border-none md:py-2 md:px-0"
+						label={<div className="whitespace-nowrap leading-tight">{selected?.label}</div>}
+					/>
+				)}
+			/>
+		</FormField>
+	);
+};
+
+export const BasicSettings = () => {
+	const { t } = useTranslation();
+
+	const { isXs } = useBreakpoint();
 
 	return (
 		<ListDivided
 			items={[
 				{
 					label: t("TRANSACTION.EXPORT.FORM.TRANSACTIONS"),
-					value: (
-						<div>
-							<ButtonGroup className="space-x-3">
-								{transactionTypeOptions.map(({ label, value, active }) => (
-									<ButtonGroupOption
-										key={value}
-										tooltipContent={label}
-										isSelected={() => active}
-										setSelectedValue={() => form.setValue("transactionType", value)}
-										value={value}
-										variant="modern"
-									>
-										<div className="px-2">{label}</div>
-									</ButtonGroupOption>
-								))}
-							</ButtonGroup>
-						</div>
-					),
+					content: isXs && <TranctionTypeOptions />,
+					value: !isXs && <TranctionTypeOptions />,
 					wrapperClass: "py-4",
 				},
 				{
 					label: t("TRANSACTION.EXPORT.FORM.DATE_RANGE"),
-					value: (
-						<FormField name="dateRange">
-							<Dropdown
-								data-testid="TransactionExportForm--daterange-options"
-								options={dateRangeOptions}
-								onSelect={(option) => form.setValue("dateRange", option.value)}
-								toggleContent={(isOpen: boolean) => (
-									<CollapseToggleButton
-										isOpen={isOpen}
-										className="w-full cursor-pointer justify-between space-x-4 overflow-hidden rounded-xl border border-theme-success-100 p-3 dark:border-theme-secondary-800 sm:p-6 md:w-auto md:space-x-2 md:rounded md:border-0 md:border-none md:py-2 md:px-0"
-										label={
-											<div className="whitespace-nowrap leading-tight">{selectedDate?.label}</div>
-										}
-									/>
-								)}
-							/>
-						</FormField>
-					),
+					value: <DateRangeOptions />,
 					wrapperClass: "pt-4",
 				},
 			]}
