3de7fdaaca3c86680e0b50dca658c4d7c46c181e
Edgar Goetzendorff <hello@dated.fun>
2022-07-04 15:33:54 +0200

    fix: add migration to update outdated ark network data

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,3 +1,4 @@
+import { Networks } from "@ardenthq/sdk";
 import { ARK } from "@ardenthq/sdk-ark";
 import { Environment } from "@ardenthq/sdk-profiles";
 import { connectedTransport as ledgerTransportFactory } from "@/app/contexts/Ledger/transport";
@@ -38,6 +39,21 @@ export const initializeEnvironment = (): Environment => {
 				profile.networks().fill(initialNetworks);
 
 				await env.persist();
+			},
+			"1.0.0": function ({ data }) {
+				if (typeof data.networks === "object" && !data.networks.ark) {
+					return;
+				}
+
+				for (const manifest of Object.values<Networks.NetworkManifest>(data.networks.ark)) {
+					if (manifest.constants.epoch === undefined) {
+						manifest.constants.epoch = ARK.manifest.networks[manifest.id].constants.epoch;
+					}
+
+					if (manifest.hosts.some((host: Networks.NetworkHost) => host.host.includes("payvo.com"))) {
+						manifest.hosts = [...ARK.manifest.networks[manifest.id].hosts];
+					}
+				}
 			},
 		},
 		"1.0.0",
