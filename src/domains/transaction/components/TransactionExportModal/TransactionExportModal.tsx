7e77f7a161d5eae42ddb53925502a1052fdb3ea5
Edgar Goetzendorff <hello@dated.fun>
2022-07-06 11:24:41 +0200

    refactor: show actual filename in success toast (#80)

--- src/domains/transaction/components/TransactionExportModal/TransactionExportModal.tsx
+++ src/domains/transaction/components/TransactionExportModal/TransactionExportModal.tsx
@@ -56,12 +56,9 @@ export const TransactionExportModal = ({
 					<TransactionExportSuccess
 						file={file}
 						onCancel={onClose}
-						onDownload={() => {
+						onDownload={(filename: string) => {
 							toasts.success(
-								<Trans
-									i18nKey="COMMON.SAVE_FILE.SUCCESS"
-									values={{ filePath: `${file.name}.${file.extension}` }}
-								/>,
+								<Trans i18nKey="COMMON.SAVE_FILE.SUCCESS" values={{ filePath: filename }} />,
 							);
 							onClose?.();
 						}}
