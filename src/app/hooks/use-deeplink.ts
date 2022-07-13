cd01e33a870c19e9be7383f1de070f5cf1c5d7e2
goga-m <goga-m@users.noreply.github.com>
2022-07-13 16:23:49 +0000

    style: resolve style guide violations

--- src/app/hooks/use-deeplink.ts
+++ src/app/hooks/use-deeplink.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { matchPath, useHistory, useLocation } from "react-router-dom";
 
