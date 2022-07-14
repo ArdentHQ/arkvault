b627f6e431cdc3a87110f36b2a342f7ca707b06c
Edgar Goetzendorff <hello@dated.fun>
2022-07-14 17:14:32 +0200

    wip

--- src/domains/transaction/components/QrModal/QrModal.tsx
+++ src/domains/transaction/components/QrModal/QrModal.tsx
@@ -1,15 +1,14 @@
 import { Modal } from "@/app/components/Modal";
 import { QRCameraReader } from "@/app/components/QRCameraReader";
+import React from "react";
 
 interface QrModalProperties {
 	isOpen: boolean;
 	onCancel: () => void;
 }
 
-export const QrModal = ({ isOpen, onCancel }: QrModalProperties) => {
-	return (
-		<Modal isOpen={isOpen} title="QR" size="lg" onClose={() => onCancel()}>
-			<QRCameraReader />
-		</Modal>
-	);
-};
+export const QrModal = ({ isOpen, onCancel }: QrModalProperties) => (
+  <Modal isOpen={isOpen} title="QR" size="lg" onClose={() => onCancel()}>
+    <QRCameraReader />
+  </Modal>
+);
