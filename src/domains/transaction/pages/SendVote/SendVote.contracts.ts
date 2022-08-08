5a40bd8cc45a5ab4f0f7cadf63e8f0c3794af2d7
Goga Mamoulasvili <goga.mamoulasvili@gmail.com>
2022-08-08 06:37:42 +0200

    wip

--- src/domains/transaction/pages/SendVote/SendVote.contracts.ts
+++ src/domains/transaction/pages/SendVote/SendVote.contracts.ts
@@ -2,7 +2,7 @@ import { Networks } from "@ardenthq/sdk";
 import { Contracts } from "@ardenthq/sdk-profiles";
 
 export interface SendVoteStepProperties {
-	wallet?: Contracts.IReadWriteWallet;
+	wallet: Contracts.IReadWriteWallet;
 	votes: Contracts.VoteRegistryItem[];
 	unvotes: Contracts.VoteRegistryItem[];
 	network: Networks.Network;
