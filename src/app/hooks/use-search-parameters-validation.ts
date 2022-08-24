1e448e28edee6ae18eb63cdadc8a380227d9cdb6
dated <dated@users.noreply.github.com>
2022-08-24 13:50:41 +0000

    style: resolve style guide violations

--- src/app/hooks/use-search-parameters-validation.ts
+++ src/app/hooks/use-search-parameters-validation.ts
@@ -116,7 +116,7 @@ export const useSearchParametersValidation = () => {
 
 		await env.delegates().sync(profile, network.coin(), network.id());
 
-		const delegate = delegateFromSearchParameters({ env, network, profile, parameters });
+		const delegate = delegateFromSearchParameters({ env, network, parameters, profile });
 
 		const delegatePublicKey =
 			publicKey &&
@@ -158,7 +158,7 @@ export const useSearchParametersValidation = () => {
 				const network = findNetworkFromSearchParameters(profile, parameters);
 				assertNetwork(network);
 
-				const delegate = delegateFromSearchParameters({ env, network, profile, parameters });
+				const delegate = delegateFromSearchParameters({ env, network, parameters, profile });
 
 				parameters.set("vote", delegate?.address() as string);
 
