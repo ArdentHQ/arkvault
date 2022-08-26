0d3fa9dc365849475db055df302c3c4b1f6289b8
alfonsobries <alfonsobries@gmail.com>
2022-08-26 10:29:07 -0500

    add test for sign path

--- src/app/hooks/use-search-parameters-validation.test.ts
+++ src/app/hooks/use-search-parameters-validation.test.ts
@@ -215,6 +215,27 @@ describe("useSearchParametersValidation", () => {
 		await expect(result.current.validateSearchParameters(profile, env, parameters)).resolves.not.toThrow();
 	});
 
+	it("should generate sign path", () => {
+		const parameters = new URLSearchParams(
+			"coin=ARK&nethash=2a44f340d76ffc3df204c5f38cd355b7496c9065a1ade2ef92071436bd72e867&method=sign&message=test",
+		);
+
+		const { result } = renderHook(() => useSearchParametersValidation());
+
+		expect(
+			result.current.methods.sign.path({
+				env,
+				network: profile.wallets().first().network(),
+				profile,
+				searchParameters: parameters,
+			}),
+		).toBe(
+			`/profiles/${profile.id()}/sign-message?coin=ARK&nethash=${
+				profile.wallets().first().network().meta().nethash
+			}&method=sign&message=test`,
+		);
+	});
+
 	it("should validate vote", async () => {
 		const mockFindDelegateByName = jest
 			.spyOn(env.delegates(), "findByUsername")
