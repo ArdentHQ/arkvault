87c931ecc7d9d62a36ebd0a73a243f1be628dcfc
goga-m <goga.mamoulasvili@gmail.com>
2022-09-02 19:09:37 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
 	build: {
 		target: "esnext",
