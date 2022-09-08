51573078365e873916c5622d32b53c4b59dc6e4c
goga-m <goga.mamoulasvili@gmail.com>
2022-09-08 11:20:15 +0200

    wip

---
+++ src/app/hooks/use-input-focus.ts
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+export const useInputFocus = () => {
+	const [isInputElementFocused, setIsInputElementFocused] = useState(false);
+
+	const handleFocusIn = (event) => {
+		if (!["textarea", "input"].includes(event.target.tagName.toLowerCase())) {
+			return;
+		}
+
+		console.log("focusing", event.target);
+		setIsInputElementFocused(true);
+	};
+
+	const handleFocusOut = (event) => {
+		if (!["textarea", "input"].includes(event.target.tagName.toLowerCase())) {
+			return;
+		}
+
+		console.log("focusout", event);
+		setIsInputElementFocused(false);
+	};
+
+	useEffect(() => {
+		document.addEventListener("focusin", handleFocusIn);
+		document.addEventListener("focusout", handleFocusOut);
+
+		return () => {
+			document.removeEventListener("focusin", handleFocusIn);
+			document.removeEventListener("focusout", handleFocusIn);
+		};
+	}, []);
+
+	return {
+		isInputElementFocused,
+	};
+};
