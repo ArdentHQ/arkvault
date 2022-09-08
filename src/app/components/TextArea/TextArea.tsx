328fcb5e6ed4484c4c59d95a528ddea2250879fe
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2022-09-08 16:37:01 +0200

    refactor: adjust textarea resizer UI (#164)

--- src/app/components/TextArea/TextArea.tsx
+++ src/app/components/TextArea/TextArea.tsx
@@ -21,14 +21,19 @@ export const TextArea = React.forwardRef(
 		}, [reference, properties.value, initialHeight]);
 
 		return (
-			<Input
-				data-testid="TextArea"
-				as="textarea"
-				isTextArea
-				ref={reference}
-				{...properties}
-				innerClassName={cn({ "resize-none": properties.disabled })}
-			/>
+			<div className="relative inline-block">
+				<div className="pointer-events-none absolute bottom-4 right-3 z-10 hidden h-0 w-0 -rotate-45 border-x-8 border-t-8 border-x-transparent border-x-transparent border-t-theme-secondary-400 dark:border-t-theme-secondary-700 md:block" />
+				<Input
+					data-testid="TextArea"
+					as="textarea"
+					isTextArea
+					ref={reference}
+					{...properties}
+					innerClassName={cn("resize-none min-h-[8rem] md:min-h-auto md:resize-y", {
+						"resize-none": properties.disabled,
+					})}
+				/>
+			</div>
 		);
 	},
 );
