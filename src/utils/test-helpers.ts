a596064b5feafb7b09006aed4407a6a08e7c4d00
goga-m <goga.mamoulasvili@gmail.com>
2022-09-09 07:57:43 +0200

    wip

--- src/utils/test-helpers.ts
+++ src/utils/test-helpers.ts
@@ -57,3 +57,8 @@ export const bootEnvironmentWithProfileFixtures = async ({
 export const isE2E = () => !!["true", "1"].includes(process.env.REACT_APP_IS_E2E?.toLowerCase() || "");
 
 export const isUnit = () => !!["true", "1"].includes(process.env.REACT_APP_IS_UNIT?.toLowerCase() || "");
+
+export const isPreview = () =>
+	isE2E() ||
+	process.env.NODE_ENV === "development" ||
+	["development", "preview"].includes(String(import.meta.env.VITE_VERCEL_ENV));
