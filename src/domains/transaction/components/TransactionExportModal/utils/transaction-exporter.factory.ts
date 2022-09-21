f29d4f96013765e4408bb5f0a39139426573fbf6
dated <dated@users.noreply.github.com>
2022-09-21 13:39:05 +0000

    style: resolve style guide violations

--- src/domains/transaction/components/TransactionExportModal/utils/transaction-exporter.factory.ts
+++ src/domains/transaction/components/TransactionExportModal/utils/transaction-exporter.factory.ts
@@ -1,9 +1,9 @@
 import { Contracts, DTO } from "@ardenthq/sdk-profiles";
 import { Services } from "@ardenthq/sdk";
+import { BigNumber } from "@ardenthq/sdk-helpers";
 import { convertToCsv } from "./transaction-to-csv-converter";
 import { CsvSettings } from "@/domains/transaction/components/TransactionExportModal";
 import { assertString } from "@/utils/assertions";
-import { BigNumber } from "@ardenthq/sdk-helpers";
 
 interface TransactionExporterFetchProperties {
 	type: "all" | "received" | "sent";
