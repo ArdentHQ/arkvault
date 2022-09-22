5f16e16d8801345bc5144d25e6aa04f8d727464b
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2022-09-22 18:07:37 +0200

    Update src/domains/transaction/utils.ts
    Co-authored-by: Edgar Goetzendorff <hello@dated.fun>

--- src/domains/transaction/utils.ts
+++ src/domains/transaction/utils.ts
@@ -25,10 +25,8 @@ export const handleBroadcastError = ({ errors }: Services.BroadcastResponse) => 
 	throw new Error(allErrors[0]);
 };
 
-export const getTransferType = ({ recipients }: { recipients: RecipientItem[] }): "multiPayment" | "transfer" => {
-	const isMultiPayment = recipients.length > 1;
-	return isMultiPayment ? "multiPayment" : "transfer";
-};
+export const getTransferType = ({ recipients }: { recipients: RecipientItem[] }): "multiPayment" | "transfer" =>
+	recipients.length > 1 ? "multiPayment" : "transfer";
 
 export const withAbortPromise =
 	(signal?: AbortSignal, callback?: () => void) =>
