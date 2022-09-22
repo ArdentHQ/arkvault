235478dfec7da3cf94ce2c884e22c64d17932abd
goga-m <goga.mamoulasvili@gmail.com>
2022-09-22 09:20:05 +0200

    wip

--- src/domains/transaction/pages/SendTransfer/SendTransfer.MultiPayment.test.tsx
+++ src/domains/transaction/pages/SendTransfer/SendTransfer.MultiPayment.test.tsx
@@ -11,12 +11,9 @@ import { Route } from "react-router-dom";
 import { SendTransfer } from "./SendTransfer";
 import { LedgerProvider } from "@/app/contexts";
 import { translations as transactionTranslations } from "@/domains/transaction/i18n";
-import transactionFixture from "@/tests/fixtures/coins/ark/devnet/transactions/transfer.json";
-import transactionMultipleFixture from "@/tests/fixtures/coins/ark/devnet/transactions/transfer-multiple.json";
 import {
 	env,
 	getDefaultProfileId,
-	getDefaultWalletMnemonic,
 	render,
 	screen,
 	syncFees,
@@ -25,30 +22,7 @@ import {
 	mockProfileWithPublicAndTestNetworks,
 } from "@/utils/testing-library";
 
-const passphrase = getDefaultWalletMnemonic();
 const fixtureProfileId = getDefaultProfileId();
-
-jest.setTimeout(1_000);
-
-const createTransactionMultipleMock = (wallet: Contracts.IReadWriteWallet) =>
-	jest.spyOn(wallet.transaction(), "transaction").mockReturnValue({
-		amount: () => +transactionMultipleFixture.data.amount / 1e8,
-		data: () => ({ data: () => transactionMultipleFixture.data }),
-		explorerLink: () => `https://test.arkscan.io/transaction/${transactionFixture.data.id}`,
-		fee: () => +transactionMultipleFixture.data.fee / 1e8,
-		id: () => transactionMultipleFixture.data.id,
-		isMultiSignatureRegistration: () => false,
-		recipient: () => transactionMultipleFixture.data.recipient,
-		recipients: () => [
-			{
-				address: transactionMultipleFixture.data.recipient,
-				amount: +transactionMultipleFixture.data.amount / 1e8,
-			},
-		],
-		sender: () => transactionMultipleFixture.data.sender,
-		type: () => "multiPayment",
-		usesMultiSignature: () => false,
-	} as any);
 
 let profile: Contracts.IProfile;
 let wallet: Contracts.IReadWriteWallet;
@@ -58,14 +32,10 @@ const selectFirstRecipient = () => userEvent.click(screen.getByTestId("Recipient
 const selectRecipient = () =>
 	userEvent.click(within(screen.getByTestId("recipient-address")).getByTestId("SelectRecipient__select-recipient"));
 const continueButton = () => screen.getByTestId("StepNavigation__continue-button");
-const sendButton = () => screen.getByTestId("StepNavigation__send-button");
 
-const reviewStepID = "SendTransfer__review-step";
 const formStepID = "SendTransfer__form-step";
 
 const history = createHashHistory();
-
-jest.setTimeout(3_000);
 
 jest.mock("@/utils/delay", () => ({
 	delay: (callback: () => void) => callback(),
@@ -107,7 +77,7 @@ describe("SendTransfer MultiPayment", () => {
 		mockProfileNetworkReset();
 	});
 
-	it.skip("should send a multi payment", async () => {
+	it("should select two recipients", async () => {
 		const transferURL = `/profiles/${fixtureProfileId}/wallets/${wallet.id()}/send-transfer`;
 
 		history.push(transferURL);
@@ -158,53 +128,12 @@ describe("SendTransfer MultiPayment", () => {
 			expect(screen.getAllByTestId("SelectDropdown__input")[1]).toHaveValue(profile.wallets().first().address()),
 		);
 
-		// userEvent.paste(screen.getByTestId("AddRecipient__amount"), "1");
-		// await waitFor(() => expect(screen.getByTestId("AddRecipient__amount")).toHaveValue("1"));
-		//
-		// userEvent.click(screen.getByTestId("AddRecipient__add-button"));
-		// await waitFor(() => expect(screen.getAllByTestId("AddRecipientItem")).toHaveLength(2));
+		userEvent.paste(screen.getByTestId("AddRecipient__amount"), "1");
+		await waitFor(() => expect(screen.getByTestId("AddRecipient__amount")).toHaveValue("1"));
 
-		// // Fee
-		// userEvent.click(within(screen.getByTestId("InputFee")).getByText(transactionTranslations.FEES.SLOW));
-		// await waitFor(() => expect(screen.getAllByRole("radio")[0]).toBeChecked());
-		//
-		// expect(screen.getAllByRole("radio")[0]).toHaveTextContent("0.1");
-		//
-		// // Step 2
+		userEvent.click(screen.getByTestId("AddRecipient__add-button"));
+		await waitFor(() => expect(screen.getAllByTestId("AddRecipientItem")).toHaveLength(2));
+
 		expect(continueButton()).not.toBeDisabled();
-		//
-		// userEvent.click(continueButton());
-		//
-		// await expect(screen.findByTestId(reviewStepID)).resolves.toBeVisible();
-		//
-		// // Step 3
-		// expect(continueButton()).not.toBeDisabled();
-		//
-		// userEvent.click(continueButton());
-		//
-		// await expect(screen.findByTestId("AuthenticationStep")).resolves.toBeVisible();
-
-		// const passwordInput = screen.getByTestId("AuthenticationStep__mnemonic");
-		// userEvent.paste(passwordInput, passphrase);
-		// await waitFor(() => expect(passwordInput).toHaveValue(passphrase));
-		//
-		// const signMock = jest
-		// 	.spyOn(wallet.transaction(), "signMultiPayment")
-		// 	.mockReturnValue(Promise.resolve(transactionMultipleFixture.data.id));
-		// const broadcastMock = jest.spyOn(wallet.transaction(), "broadcast").mockResolvedValue({
-		// 	accepted: [transactionFixture.data.id],
-		// 	errors: {},
-		// 	rejected: [],
-		// });
-		// const transactionMock = createTransactionMultipleMock(wallet);
-		//
-		// await waitFor(() => expect(sendButton()).not.toBeDisabled());
-		// userEvent.click(sendButton());
-		//
-		// await expect(screen.findByTestId("TransactionSuccessful")).resolves.toBeVisible();
-
-		// signMock.mockRestore();
-		// broadcastMock.mockRestore();
-		// transactionMock.mockRestore();
 	});
 });
