5280bbe561a28c203fea79441d847058570efe0d
Edgar Goetzendorff <hello@dated.fun>
2022-10-05 12:49:01 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path'
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -16,11 +15,6 @@ export default defineConfig({
 		environment: "jsdom",
 		setupFiles: "@testing-library/jest-dom",
 	},
-	resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
-    },
-  },
 	build: {
 		target: "esnext",
 		rollupOptions: {
