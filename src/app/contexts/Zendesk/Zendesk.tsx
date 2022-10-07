bb97b96f5d8f1638f599353aa8ebb6838e919d5b
goga-m <goga.mamoulasvili@gmail.com>
2022-10-07 16:44:28 +0200

    wip

--- src/app/contexts/Zendesk/Zendesk.tsx
+++ src/app/contexts/Zendesk/Zendesk.tsx
@@ -2,6 +2,7 @@ import { useAccentColor, useTheme } from "@/app/hooks";
 import React from "react";
 import Zendesk, { ZendeskAPI } from "react-zendesk";
 const ZENDESK_KEY = "0e4c4d37-9d38-4be4-925d-e659dd4d12bd";
+import ZendeskStyles from "/src/styles/zendesk.css";
 
 interface Properties {
 	children: React.ReactNode;
@@ -41,51 +42,14 @@ export const useZendesk = () => {
 		// @ts-ignore
 		window.$zopim?.livechat?.window?.show?.();
 
-		// TODO: Cleanup.
 		setTimeout(() => {
 			// @ts-ignore
 			const widget = window.document.getElementById("webWidget").contentWindow.document;
 
-			widget.getElementsByTagName("H1")[0].style.color = "#eef3f5";
-			widget.getElementsByTagName("FOOTER")[0].style.setProperty("border", "none", "important");
-			widget.getElementsByTagName("a")[0].style.visibility = "hidden";
-			widget.getElementsByTagName("BUTTON")[2].style.setProperty("color", "#eef3f5", "important");
-		}, 400);
-
-		if (!isDarkMode) {
-			return;
-		}
-
-		// TODO: Cleanup
-		setTimeout(() => {
-			// @ts-ignore
-			const widget = window.document.getElementById("webWidget").contentWindow.document;
-
-			widget.getElementsByTagName("H1")[0].style.color = "#eef3f5";
-			widget.getElementsByTagName("BUTTON")[0].style.color = "#eef3f5";
-
-			widget.getElementsByTagName("FORM")[0].style.backgroundColor = "#212225";
-
-			widget.getElementsByTagName("INPUT")[0].style.backgroundColor = "#212225";
-			widget.getElementsByTagName("INPUT")[0].style.borderColor = "#637282";
-
-			widget.getElementsByTagName("INPUT")[1].style.backgroundColor = "#212225";
-			widget.getElementsByTagName("INPUT")[1].style.borderColor = "#637282";
-
-			widget.getElementsByTagName("INPUT")[2].style.backgroundColor = "#212225";
-			widget.getElementsByTagName("INPUT")[2].style.borderColor = "#637282";
-
-			widget.getElementsByTagName("TEXTAREA")[0].style.backgroundColor = "#212225";
-			widget.getElementsByTagName("TEXTAREA")[0].style.borderColor = "#637282";
-
-			widget.getElementsByTagName("LABEL")[0].style.color = "#a5adb9";
-			widget.getElementsByTagName("LABEL")[1].style.color = "#a5adb9";
-			widget.getElementsByTagName("LABEL")[2].style.color = "#a5adb9";
-			widget.getElementsByTagName("LABEL")[3].style.color = "#a5adb9";
-
-			widget.getElementsByTagName("BUTTON")[1].style.background = "#3c4249";
-			widget.getElementsByTagName("BUTTON")[1].style.border = "none";
-		}, 200);
+			widget.body.classList.add("widget");
+			widget.body.classList.add(isDarkMode ? "widget-dark" : "widget-light");
+			widget.body.insertAdjacentHTML("afterend", `<style>${ZendeskStyles}</style>`);
+		}, 300);
 	};
 
 	const hideSupportChat = () => {
