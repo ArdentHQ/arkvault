488471750bb99d8738b2532b2db0ec9b90449761
dated <dated@users.noreply.github.com>
2022-10-12 13:41:40 +0000

    style: resolve style guide violations

--- src/tests/mocks/handlers/news.ts
+++ src/tests/mocks/handlers/news.ts
@@ -11,9 +11,9 @@ export const newsHandlers = [
 	rest.get("https://news.arkvault.io/api", (request, response, context) => {
 		const page = request.url.searchParams.get("page") as string;
 
-    if (!!page) {
-      return response(context.status(200), context.json(require(`../../fixtures/news/page-${page}.json`)));
-    }
+		if (!!page) {
+			return response(context.status(200), context.json(require(`../../fixtures/news/page-${page}.json`)));
+		}
 
 		return response(context.status(200), context.json(require("../../fixtures/news/page-1.json")));
 	}),
