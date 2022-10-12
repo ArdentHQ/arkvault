d31fbd0f4c68fc128f1b34f39b14fe541408e2a5
dated <dated@users.noreply.github.com>
2022-10-12 12:50:07 +0000

    style: resolve style guide violations

--- src/tests/mocks/handlers/misc.ts
+++ src/tests/mocks/handlers/misc.ts
@@ -2,11 +2,14 @@ import fs from "fs";
 import { rest } from "msw";
 
 export const miscHandlers = [
-  rest.get("https://api.pwnedpasswords.com/range/:range", (request, response, context) => {
-    if (request.params.range === "f3f69") {
-      return response(context.status(200), context.json(fs.readFileSync("src/tests/fixtures/haveibeenpwned/range-f3f69.txt", "utf8")));
-    }
+	rest.get("https://api.pwnedpasswords.com/range/:range", (request, response, context) => {
+		if (request.params.range === "f3f69") {
+			return response(
+				context.status(200),
+				context.json(fs.readFileSync("src/tests/fixtures/haveibeenpwned/range-f3f69.txt", "utf8")),
+			);
+		}
 
-    return response(context.status(200), context.json(""));
-  }),
+		return response(context.status(200), context.json(""));
+	}),
 ];
