e03047c485e5e2cf9c205397feaec7317bf2489b
Edgar Goetzendorff <hello@dated.fun>
2022-12-14 11:57:01 +0100

    wip

--- src/domains/migration/i18n.ts
+++ src/domains/migration/i18n.ts
@@ -2,7 +2,7 @@ export const translations = {
 	PAGE_MIGRATION: {
 		NEW_MIGRATION: "New Migration",
 		NO_MIGRATIONS: "You don't have any migration transactions yet.",
-		SUBTITLE: 'ARK is Moving to Polygon. Click "New Migration" to get started.',
+		SUBTITLE: 'ARK is moving to Polygon. Click "New Migration" to get started.',
 		TITLE: "Migrate to Polygon",
 	},
 };
