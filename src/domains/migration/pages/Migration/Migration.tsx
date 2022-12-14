53f3b4eee9a02c48a8bba025d55634b7b89511ff
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-12-14 17:37:46 -0600

    Add migration disclaimer modal

--- src/domains/migration/pages/Migration/Migration.tsx
+++ src/domains/migration/pages/Migration/Migration.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { MigrationEmpty, MigrationHeader, MigrationNewMigrationMobileButton } from "./Migration.blocks";
 import { Page } from "@/app/components/Layout";
-
-const newMigrationHandler = () => {
-	// @TODO: newMigrationHandler
+import { MigrationDisclaimer } from "@/domains/migration/components/MigrationDisclaimer";
+const confirmHandler = () => {
+	// @TODO: Start migration
 };
 
 export const Migration = () => {
 	const { t } = useTranslation();
+
+	const [isDisclaimerOpen, setIsDisclaimerOpen] = useState(false);
 
 	const migrations = [];
 
@@ -22,11 +24,18 @@ export const Migration = () => {
 	return (
 		<>
 			<Page pageTitle={t("MIGRATION.PAGE_MIGRATION.TITLE")} isBackDisabled={true} data-testid="Migration">
-				<MigrationHeader onNewMigration={newMigrationHandler} />
+				<MigrationHeader onNewMigration={() => setIsDisclaimerOpen(true)} />
 
 				{renderMigrations()}
 
-				<MigrationNewMigrationMobileButton onNewMigration={newMigrationHandler} />
+				<MigrationNewMigrationMobileButton onNewMigration={() => setIsDisclaimerOpen(true)} />
+
+				<MigrationDisclaimer
+					isOpen={isDisclaimerOpen}
+					onClose={() => setIsDisclaimerOpen(false)}
+					onCancel={() => setIsDisclaimerOpen(false)}
+					onConfirm={confirmHandler}
+				/>
 			</Page>
 		</>
 	);
