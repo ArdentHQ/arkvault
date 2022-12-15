362cca350974293817a5fac22a7b05d24874a89f
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-12-15 13:20:18 -0600

    feat: add migration disclaimer (#273)

--- src/domains/migration/i18n.ts
+++ src/domains/migration/i18n.ts
@@ -1,4 +1,12 @@
 export const translations = {
+	DISCLAIMER_MODAL: {
+		DISCLAIMER:
+			"I agree with the rules and with something else <linkTerms>Terms of Service</linkTerms> & <linkPrivacyPolicy>Privacy Policy</linkPrivacyPolicy>",
+		TITLE: "Migration Disclaimer",
+		// @TBD
+		WARNING:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla doloribus, mollitia natus est, voluptas eligendi nihil dolorum illum accusamus obcaecati itaque corrupti tenetur, accusantium perferendis officiis hic modi neque aspernatur!",
+	},
 	PAGE_MIGRATION: {
 		NEW_MIGRATION: "New Migration",
 		NO_MIGRATIONS: "You don't have any migration transactions yet.",
