3fc3d1d6ce5a8c4f775e004994c9338bbdcb90a1
Edgar Goetzendorff <hello@dated.fun>
2023-01-05 15:29:18 +0100

    refactor: adjust step indicator

--- src/app/components/StepIndicatorAlt/StepIndicatorAlt.tsx
+++ src/app/components/StepIndicatorAlt/StepIndicatorAlt.tsx
@@ -33,21 +33,21 @@ export const StepIndicatorAlt: React.FC<Properties> = ({
 					data-testid="StepIndicatorAlt__step"
 					key={index}
 					className={classNames(
-						"relative z-0 flex h-6 w-6 items-center justify-center rounded-full border-2 bg-white dark:bg-theme-secondary-900",
+						"relative z-0 flex h-6 w-6 items-center justify-center rounded-full border-2 dark:bg-theme-secondary-900",
 						{
-							"border-theme-primary-600": index < activeIndex,
-							"border-theme-secondary-300 dark:border-theme-secondary-800": index >= activeIndex,
+							"border-theme-primary-600 bg-theme-navy-100": index < activeIndex,
+							"border-theme-secondary-300 dark:border-theme-secondary-800 bg-white": index >= activeIndex,
 						},
 					)}
 				>
-					{index === activeIndex - 1 && (
+					{index === activeIndex - 1 && activeIndex !== length && (
 						<div
 							data-testid="StepIndicatorAlt__activestep"
 							className=" h-2 w-2 rounded-full bg-theme-primary-600"
 						/>
 					)}
 
-					{index < activeIndex - 1 && (
+					{(index < activeIndex - 1 || activeIndex === length) && (
 						<Icon
 							data-testid="StepIndicatorAlt__prevstep"
 							name="CheckmarkSmall"
