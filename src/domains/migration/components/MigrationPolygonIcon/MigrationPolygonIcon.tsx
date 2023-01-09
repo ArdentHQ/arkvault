3846c6d3a3070e6e473655061aa3bc18f14e23df
Edgar Goetzendorff <hello@dated.fun>
2023-01-09 11:06:32 +0100

    feat: implement `MigrationPendingStep` (#283)

--- src/domains/migration/components/MigrationPolygonIcon/MigrationPolygonIcon.tsx
+++ src/domains/migration/components/MigrationPolygonIcon/MigrationPolygonIcon.tsx
@@ -4,12 +4,10 @@ import { Image } from "@/app/components/Image";
 
 export const MigrationPolygonIcon = () => (
 	<>
-		<div className="justify-center-white absolute top-0 right-8 bottom-0 flex w-8 items-center">
+		<div className="absolute inset-0">
 			<Image name="Hexagon" width={44} height={44} useAccentColor={false} />
 		</div>
 
-		<div className="absolute top-0 right-8 bottom-0 flex w-8 items-center justify-center text-theme-hint-600">
-			<Icon name="Received" size="md" />
-		</div>
+		<Icon className="relative text-theme-hint-600" name="Received" size="lg" />
 	</>
 );
--- src/domains/migration/i18n.ts
+++ src/domains/migration/i18n.ts
@@ -10,6 +10,8 @@ export const translations = {
 	},
 
 	MIGRATION_ADD: {
+		FROM_ARK_ADDRESS: "From ARK Address",
+
 		STEP_CONNECT: {
 			DESCRIPTION: "Fill in address details below to start the migration process.",
 			FORM: {
@@ -38,19 +40,27 @@ export const translations = {
 			},
 			TITLE: "Migration",
 		},
+
+		STEP_PENDING: {
+			DESCRIPTION: "Migration is processing, you will be notified once the process is complete.",
+			MIGRATION_INFO:
+				"Migration time <strong>≈ 24 hours</strong>.\nUpon completion you will receive a notification.",
+			TITLE: "Migration Pending",
+		},
+
 		STEP_REVIEW: {
 			AMOUNT_MIGRATED: "Amount Migrated",
 			AMOUNT_SEND: "Amount You Send",
 			DESCRIPTION: "Review the migration transaction details below.",
-			FROM_ARK_ADDRESS: "From ARK Address",
 			TITLE: "Review Migration Transaction",
-			TO_POLYGON_ADDRESS: "To Polygon Address",
 		},
 
 		STEP_SUCCESS: {
 			DESCRIPTION: "Your migration was successful.\nWelcome to Polygon!",
 			TITLE: "Successfully Migrated",
 		},
+
+		TO_POLYGON_ADDRESS: "To Polygon Address",
 	},
 
 	PAGE_MIGRATION: {
