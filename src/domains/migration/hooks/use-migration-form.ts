4656d35491c6a3d61ea36ee62ad19ca37c5162c1
goga-m <goga.mamoulasvili@gmail.com>
2023-01-09 20:37:22 +0100

    wip

--- src/domains/migration/hooks/use-migration-form.ts
+++ src/domains/migration/hooks/use-migration-form.ts
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useForm, useFormContext } from "react-hook-form";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 import { RecipientItem } from "@/domains/transaction/components/RecipientList/RecipientList.contracts";
 
@@ -21,8 +21,6 @@ export interface MigrationForm {
 const TRANSACTION_FEE = Number.parseFloat(import.meta.env.VITE_POLYGON_MIGRATION_TRANSACTION_FEE || 0.05);
 
 export const useMigrationForm = () => {
-	const [isSending, setIsSending] = useState(false);
-
 	const form = useForm<MigrationForm>({
 		defaultValues: {
 			fee: TRANSACTION_FEE,
@@ -30,7 +28,7 @@ export const useMigrationForm = () => {
 			migrationAddress: "0x080de88aE69Bc02eB8csr34E863B7F428699bb20",
 			recipients: [
 				{
-					amount: 10,
+					amount: 1,
 					address: "DNBURNBURNBURNBRNBURNBURNBURKz8StY",
 				},
 			],
