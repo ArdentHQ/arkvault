c4006d6b9d1c58bf101c3e94c3b75eda9d2498c4
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2023-01-11 19:37:21 +0100

    wip

--- src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRow.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRow.tsx
@@ -13,6 +13,8 @@ import { Link } from "@/app/components/Link";
 import { TableCell, TableRow } from "@/app/components/Table";
 import { useTimeFormat } from "@/app/hooks/use-time-format";
 import { useBreakpoint } from "@/app/hooks";
+import { useTransaction } from "@/domains/transaction/hooks";
+import { TransactionRowMigrationDetails } from "./TransactionRowMigrationDetails";
 
 export const TransactionRow = memo(
 	({
@@ -27,6 +29,7 @@ export const TransactionRow = memo(
 		const { isXs, isSm, isMd } = useBreakpoint();
 		const { t } = useTranslation();
 		const timeFormat = useTimeFormat();
+		const { isMigrationTransaction } = useTransaction();
 
 		const isCompact = useMemo(
 			() => !profile.appearance().get("useExpandedTables") || isSm || isXs || isMd,
@@ -77,7 +80,13 @@ export const TransactionRow = memo(
 				</TableCell>
 
 				<TableCell innerClassName="space-x-4" isCompact={isCompact}>
-					<TransactionRowRecipient transaction={transaction} profile={profile} isCompact={isCompact} />
+					{!isMigrationTransaction(transaction) && (
+						<TransactionRowRecipient transaction={transaction} profile={profile} isCompact={isCompact} />
+					)}
+
+					{isMigrationTransaction(transaction) && (
+						<TransactionRowMigrationDetails transaction={transaction} isCompact={isCompact} />
+					)}
 				</TableCell>
 
 				<TableCell innerClassName="justify-end" isCompact={isCompact}>
