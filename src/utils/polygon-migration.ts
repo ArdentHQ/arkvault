d955b646df22844d4906684c8e64d7bc00a6d980
goga-m <goga-m@users.noreply.github.com>
2023-01-12 09:21:35 +0000

    style: resolve style guide violations

--- src/utils/polygon-migration.ts
+++ src/utils/polygon-migration.ts
@@ -1,16 +1,11 @@
 import { isPreview } from "./test-helpers";
 
-export const migrationTransactionFee = () => {
-	return Number.parseFloat(import.meta.env.VITE_POLYGON_MIGRATION_TRANSACTION_FEE || 0.05);
-};
+export const migrationTransactionFee = () =>
+	Number.parseFloat(import.meta.env.VITE_POLYGON_MIGRATION_TRANSACTION_FEE || 0.05);
 
-export const migrationGuideUrl = () => {
-	return import.meta.env.MIGRATION_GUIDE_URL || "https://arkvault.io/docs";
-};
+export const migrationGuideUrl = () => import.meta.env.MIGRATION_GUIDE_URL || "https://arkvault.io/docs";
 
-export const metamaskUrl = () => {
-	return import.meta.env.METAMASK_URL || "https://metamask.io/";
-};
+export const metamaskUrl = () => import.meta.env.METAMASK_URL || "https://metamask.io/";
 
 export const migrationNetwork = () => {
 	if (isPreview()) {
