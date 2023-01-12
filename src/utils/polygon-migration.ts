5fb0b47d6a39f1186d6c485d6f95e878d7b4c590
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2023-01-12 09:34:38 +0100

    wip

---
+++ src/utils/polygon-migration.ts
@@ -0,0 +1,30 @@
+import { isPreview } from "./test-helpers";
+
+export const migrationTransactionFee = () => {
+	return Number.parseFloat(import.meta.env.VITE_POLYGON_MIGRATION_TRANSACTION_FEE || 0.05);
+};
+
+export const migrationGuideUrl = () => {
+	return process.env.MIGRATION_GUIDE_URL || "https://arkvault.io/docs";
+};
+
+export const metamaskUrl = () => {
+	return process.env.METAMASK_URL || "https://metamask.io/";
+};
+
+export const migrationNetwork = () => {
+	if (isPreview()) {
+		return "ark.devnet";
+	}
+
+	return "ark.mainnet";
+};
+
+export const migrationWalletAddress = () => {
+	if (isPreview()) {
+		return "DNBURNBURNBURNBRNBURNBURNBURKz8StY";
+	}
+
+	// @TBD
+	return "DNBURNBURNBURNBRNBURNBURNBURKz8StY";
+};
