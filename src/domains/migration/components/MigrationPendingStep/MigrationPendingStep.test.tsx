da2f708137c78f919bab99c51760d2fe5c86d187
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2023-01-13 11:52:48 +0100

    wip

--- src/domains/migration/components/MigrationPendingStep/MigrationPendingStep.test.tsx
+++ src/domains/migration/components/MigrationPendingStep/MigrationPendingStep.test.tsx
@@ -1,24 +1,51 @@
 import React from "react";
 import { createHashHistory } from "history";
+import { DTO } from "@ardenthq/sdk-profiles";
 import userEvent from "@testing-library/user-event";
 import { Route } from "react-router-dom";
 import { MigrationPendingStep } from "./MigrationPendingStep";
-import { renderResponsiveWithRoute, getDefaultProfileId, screen } from "@/utils/testing-library";
+import { renderResponsiveWithRoute, getDefaultProfileId, screen, env } from "@/utils/testing-library";
 import { useTheme } from "@/app/hooks/use-theme";
 
 const history = createHashHistory();
 let migrationUrl: string;
+let transactionFixture;
 
 describe("MigrationPendingStep", () => {
-	beforeAll(() => {
+	beforeAll(async () => {
+		const profile = env.profiles().findById(getDefaultProfileId());
+		const wallet = profile.wallets().first();
+
 		migrationUrl = `/profiles/${getDefaultProfileId()}/migration/add`;
 		history.push(migrationUrl);
+
+		transactionFixture = new DTO.ExtendedSignedTransactionData(
+			await wallet
+				.coin()
+				.transaction()
+				.transfer({
+					data: {
+						amount: 1,
+						to: wallet.address(),
+					},
+					fee: 1,
+					nonce: "1",
+					signatory: await wallet
+						.coin()
+						.signatory()
+						.multiSignature({
+							min: 2,
+							publicKeys: [wallet.publicKey()!, profile.wallets().last().publicKey()!],
+						}),
+				}),
+			wallet,
+		);
 	});
 
 	it.each(["xs", "sm"])("should render in %s", (breakpoint) => {
 		const { asFragment } = renderResponsiveWithRoute(
 			<Route path="/profiles/:profileId/migration/add">
-				<MigrationPendingStep />
+				<MigrationPendingStep migrationTransaction={transactionFixture} />
 			</Route>,
 			breakpoint,
 			{
@@ -26,8 +53,6 @@ describe("MigrationPendingStep", () => {
 				route: migrationUrl,
 			},
 		);
-
-		userEvent.click(screen.getByTestId("BackToDashboard__button"));
 
 		expect(asFragment()).toMatchSnapshot();
 	});
@@ -37,7 +62,7 @@ describe("MigrationPendingStep", () => {
 
 		const { asFragment } = renderResponsiveWithRoute(
 			<Route path="/profiles/:profileId/migration/add">
-				<MigrationPendingStep />
+				<MigrationPendingStep migrationTransaction={transactionFixture} />
 			</Route>,
 			breakpoint,
 			{
@@ -45,8 +70,6 @@ describe("MigrationPendingStep", () => {
 				route: migrationUrl,
 			},
 		);
-
-		userEvent.click(screen.getByTestId("BackToDashboard__button"));
 
 		expect(asFragment()).toMatchSnapshot();
 	});
