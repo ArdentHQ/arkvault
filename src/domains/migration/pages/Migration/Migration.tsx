aefa24625aef65be033529126c1482ddb7bf8fcc
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-01-17 10:46:04 -0600

    add message on migrations page when contract is paused

--- src/domains/migration/pages/Migration/Migration.tsx
+++ src/domains/migration/pages/Migration/Migration.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { generatePath, useHistory } from "react-router-dom";
 import { MigrationHeader, MigrationNewMigrationMobileButton } from "./Migration.blocks";
 import { Page, Section } from "@/app/components/Layout";
@@ -8,6 +8,8 @@ import { useActiveProfile, useBreakpoint } from "@/app/hooks";
 import { MigrationTransactionsTable } from "@/domains/migration/components/MigrationTransactionsTable";
 import { ProfilePaths } from "@/router/paths";
 import { useMigrations } from "@/app/contexts";
+import { Alert } from "@/app/components/Alert";
+import { Link } from "@/app/components/Link";
 
 export const Migration = () => {
 	const { t } = useTranslation();
@@ -32,6 +34,19 @@ export const Migration = () => {
 	return (
 		<>
 			<Page pageTitle={t("MIGRATION.PAGE_MIGRATION.TITLE")} isBackDisabled={true} data-testid="Migration">
+				{contractIsPaused && (
+					<>
+						<Alert layout="horizontal">
+							<Trans
+								i18nKey="MIGRATION.CONTRACT_PAUSED_MESSAGE"
+								components={{
+									linkTwitter: <Link to="https://twitter.com/arkecosystem" isExternal />,
+								}}
+							/>
+						</Alert>
+					</>
+				)}
+
 				<MigrationHeader onNewMigration={onNewMigrationHandler} contractIsPaused={contractIsPaused} />
 
 				<Section className="mt-4">
