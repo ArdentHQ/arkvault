5ab2dbeb93b56dac157850351e728f30f9bdcdc5
goga-m <goga-m@users.noreply.github.com>
2023-01-17 18:36:48 +0000

    style: resolve style guide violations

--- src/domains/migration/pages/Migration/Migration.tsx
+++ src/domains/migration/pages/Migration/Migration.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { generatePath, useHistory } from "react-router-dom";
 import { MigrationHeader, MigrationNewMigrationMobileButton } from "./Migration.blocks";
