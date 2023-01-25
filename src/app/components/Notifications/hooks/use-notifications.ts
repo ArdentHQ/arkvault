8efc7295a613388e30b78841e7bc2b09af75a39f
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-01-24 20:11:17 -0600

    refactor use-notificatons

--- src/app/components/Notifications/hooks/use-notifications.ts
+++ src/app/components/Notifications/hooks/use-notifications.ts
@@ -1,29 +1,33 @@
 import { Contracts } from "@ardenthq/sdk-profiles";
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Migration, MigrationTransactionStatus } from "@/domains/migration/migration.contracts";
 import { useMigrations } from "@/app/contexts";
 export const useNotifications = ({ profile }: { profile: Contracts.IProfile }) => {
 	const isSyncing = profile.notifications().transactions().isSyncing();
 
 	const { migrations, markMigrationAsRead } = useMigrations();
-	const [confirmedMigrations, setConfirmedMigrations] = useState<Migration[]>([]);
+	const [readMigrationsIds, setReadMigrationsIds] = useState<string[]>([]);
 
-	useEffect(() => {
+	const storeReadMigrationsAndMarkAsRead = useCallback(
+		(migration: Migration) => {
+			markMigrationAsRead(migration);
+
+			setReadMigrationsIds((readMigrationsIds) => [...readMigrationsIds, migration.id]);
+		},
+		[markMigrationAsRead],
+	);
+
+	const confirmedMigrations = useMemo<Migration[]>(() => {
 		if (migrations === undefined) {
-			return;
+			return [];
 		}
 
-		setConfirmedMigrations((confirmedMigrations) =>
-			migrations.filter(
-				(migration) =>
-					migration.status === MigrationTransactionStatus.Confirmed &&
-					(migration.readAt === undefined ||
-						// If were loaded on the session we should kept it to avoid
-						// immediately hide the notification
-						confirmedMigrations.some((confirmedMigration) => confirmedMigration.id === migration.id)),
-			),
+		return migrations.filter(
+			(migration) =>
+				migration.status === MigrationTransactionStatus.Confirmed &&
+				(migration.readAt === undefined || readMigrationsIds.includes(migration.id)),
 		);
-	}, [migrations]);
+	}, [migrations, readMigrationsIds]);
 
 	const { markAllTransactionsAsRead, markAsRead, releases, transactions, migrationTransactions } = useMemo(() => {
 		const markAllTransactionsAsRead = (isVisible: boolean) => {
@@ -59,7 +63,7 @@ export const useNotifications = ({ profile }: { profile: Contracts.IProfile }) =
 		hasUnread: (releases.length > 0 || transactions.length > 0) && profile.notifications().hasUnread(),
 		markAllTransactionsAsRead,
 		markAsRead,
-		markMigrationAsRead,
+		markMigrationAsRead: storeReadMigrationsAndMarkAsRead,
 		migrationTransactions,
 		releases,
 		transactions,
