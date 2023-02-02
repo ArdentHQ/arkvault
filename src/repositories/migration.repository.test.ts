35316f14973ab94c2819d42f83930a24477b1c38
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-02-02 12:04:56 -0600

    add migration repository tests

---
+++ src/repositories/migration.repository.test.ts
@@ -0,0 +1,35 @@
+import { Contracts } from "@ardenthq/sdk-profiles";
+import { MigrationRepository } from "@/repositories/migration.repository";
+
+import { env, getDefaultProfileId } from "@/utils/testing-library";
+
+let profile: Contracts.IProfile;
+let profile2: Contracts.IProfile;
+
+describe("MigrationRepository", () => {
+	beforeAll(async () => {
+		profile = env.profiles().findById(getDefaultProfileId());
+
+		profile2 = await env.profiles().create("new profile 2");
+	});
+
+	it("return an undefined value for a new repository", () => {
+		const repository = new MigrationRepository(profile, env.data());
+
+		expect(repository.get()).toBeUndefined();
+	});
+
+	it("stores migrations for one profile", () => {
+		const repository = new MigrationRepository(profile, env.data());
+		const repository2 = new MigrationRepository(profile2, env.data());
+
+		repository.set([{ id: "1" }, { id: "2" }], 2);
+
+		const data = repository.get();
+		const data2 = repository2.get();
+
+		expect(data.migrations).toHaveLength(2);
+		expect(data.page).toBe(2);
+		expect(data2).toBeUndefined();
+	});
+});
