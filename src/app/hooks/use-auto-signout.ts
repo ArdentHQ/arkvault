37c96152e68275ac4f48b64996a5f582e9046385
goga-m <goga.mamoulasvili@gmail.com>
2023-07-27 14:09:04 +0200

    wip

--- src/app/hooks/use-auto-signout.ts
+++ src/app/hooks/use-auto-signout.ts
@@ -6,7 +6,7 @@ import { useCallback } from "react";
 export const useAutoSignOut = (profile?: Contracts.IProfile) => {
 	const history = useHistory();
 
-	const idleTimeout = 1000 * 60 * (profile?.settings().get(Contracts.ProfileSetting.AutomaticSignOutPeriod, 15) ?? 1);
+	const timeout = 1000 * 60 * (profile?.settings().get(Contracts.ProfileSetting.AutomaticSignOutPeriod, 15) ?? 0);
 
 	const onIdle = useCallback(() => {
 		if (history.location.pathname === "/") {
@@ -18,7 +18,7 @@ export const useAutoSignOut = (profile?: Contracts.IProfile) => {
 
 	const { start, pause } = useIdleTimer({
 		onIdle,
-		timeout: idleTimeout,
+		timeout,
 		debounce: 500,
 		startManually: true,
 		stopOnIdle: false,
