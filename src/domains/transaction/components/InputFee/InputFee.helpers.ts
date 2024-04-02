44222860cd76fef8408391e606c21a63f6ddfc8b
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-04-02 17:48:31 -0600

    use bignumber to handle step increments/decrements

--- src/domains/transaction/components/InputFee/InputFee.helpers.ts
+++ src/domains/transaction/components/InputFee/InputFee.helpers.ts
@@ -1,4 +1,6 @@
-export const useStepMath = (step: number, value: number) => ({
-	decrement: () => +(value - step).toFixed(12),
-	increment: () => +(value + step).toFixed(12),
+import { BigNumber } from "@ardenthq/sdk-helpers";
+
+export const useStepMath = (step: number, value: number | string) => ({
+	decrement: () => BigNumber.make(value).minus(step).toFixed(),
+	increment: () => BigNumber.make(value).plus(step).toFixed(),
 });
