1880b0f2b23d5450bfc2b1935fb48e16ffaffbfa
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-04-08 12:14:38 -0600

    add tests for handling small values

--- src/domains/transaction/components/InputFee/InputFee.helpers.ts
+++ src/domains/transaction/components/InputFee/InputFee.helpers.ts
@@ -1,6 +1,14 @@
 import { BigNumber } from "@ardenthq/sdk-helpers";
 
 export const useStepMath = (step: number, value: number | string) => ({
-	decrement: () => BigNumber.make(value).minus(step).toFixed(0),
-	increment: () => BigNumber.make(value).plus(step).toFixed(0),
+	decrement: () =>
+		BigNumber.make(value)
+			.minus(BigNumber.make(step))
+			.toFixed(12)
+			.replace(/\.?0+$/, ""),
+	increment: () =>
+		BigNumber.make(value)
+			.plus(step)
+			.toFixed(12)
+			.replace(/\.?0+$/, ""),
 });
