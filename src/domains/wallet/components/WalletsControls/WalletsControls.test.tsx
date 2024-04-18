4306d8932cf834a2d896bcc35eaf2a392a413d24
goga-m <goga.mamoulasvili@gmail.com>
2024-04-18 15:21:50 +0200

    wip

--- src/domains/wallet/components/WalletsControls/WalletsControls.test.tsx
+++ src/domains/wallet/components/WalletsControls/WalletsControls.test.tsx
@@ -3,9 +3,14 @@ import React from "react";
 import userEvent from "@testing-library/user-event";
 import { WalletsControls } from "./WalletsControls";
 import { FilterWalletsHookProperties } from "@/domains/dashboard/components/FilterWallets";
-import { render, renderResponsive, screen } from "@/utils/testing-library";
+import { render, renderResponsive, screen, getDefaultProfileId } from "@/utils/testing-library";
+import { createHashHistory } from "history";
+import { renderResponsiveWithRoute, waitFor } from "../../../../utils/testing-library";
+const history = createHashHistory();
 
 describe("WalletsControls", () => {
+	const dashboardURL = `/profiles/${getDefaultProfileId()}/dashboard`;
+
 	const filterProperties: FilterWalletsHookProperties = {
 		defaultConfiguration: {
 			selectedNetworkIds: [],
@@ -19,37 +24,61 @@ describe("WalletsControls", () => {
 		walletsDisplayType: "all",
 	};
 
+	beforeAll(() => {
+		history.push(dashboardURL);
+	});
+
 	it("should render", () => {
 		const { container } = render(
 			<WalletsControls onCreateWallet={vi.fn()} onImportWallet={vi.fn()} filterProperties={filterProperties} />,
+			{
+				history,
+				withProfileSynchronizer: true,
+			},
 		);
 
 		expect(container).toMatchSnapshot();
 	});
 
-	it("should render for incompatible ledger wallet", () => {
+	it("should render for incompatible ledger wallet", async () => {
 		process.env.REACT_APP_IS_UNIT = undefined;
 
 		const { container } = render(
 			<WalletsControls onCreateWallet={vi.fn()} onImportWallet={vi.fn()} filterProperties={filterProperties} />,
+			{
+				history,
+				withProfileSynchronizer: true,
+			},
 		);
 
 		expect(container).toMatchSnapshot();
 	});
 
 	it.each(["xs", "sm", "md", "lg", "xl"])("should render responsive", (breakpoint) => {
-		const { container } = renderResponsive(
+		const { container } = renderResponsiveWithRoute(
 			<WalletsControls onCreateWallet={vi.fn()} onImportWallet={vi.fn()} filterProperties={filterProperties} />,
 			breakpoint,
+			{
+				history,
+				withProfileSynchronizer: true,
+			},
 		);
 
 		expect(container).toMatchSnapshot();
 	});
 
-	it("should execute onCreateWallet callback", () => {
+	it("should execute onCreateWallet callback", async () => {
 		const onCreateWallet = vi.fn();
 
-		render(<WalletsControls onCreateWallet={onCreateWallet} onImportWallet={vi.fn()} filterProperties={{}} />);
+		render(<WalletsControls onCreateWallet={onCreateWallet} onImportWallet={vi.fn()} filterProperties={{}} />, {
+			history,
+			withProviders: true,
+			withProfileSynchronizer: true,
+		});
+
+		await waitFor(() => {
+			expect(screen.getByTestId("WalletControls__create-wallet")).toBeInTheDocument();
+		});
 
 		userEvent.click(screen.getByTestId("WalletControls__create-wallet"));
 
@@ -59,9 +88,13 @@ describe("WalletsControls", () => {
 	it("should execute onCreateWallet callback when responsive", () => {
 		const onCreateWallet = vi.fn();
 
-		renderResponsive(
+		renderResponsiveWithRoute(
 			<WalletsControls onCreateWallet={onCreateWallet} onImportWallet={vi.fn()} filterProperties={{}} />,
 			"xs",
+			{
+				history,
+				withProfileSynchronizer: true,
+			},
 		);
 
 		userEvent.click(screen.getAllByTestId("dropdown__toggle")[1]);
@@ -76,7 +109,10 @@ describe("WalletsControls", () => {
 	it("should execute onImportWallet callback", () => {
 		const onImportWallet = vi.fn();
 
-		render(<WalletsControls onCreateWallet={vi.fn()} onImportWallet={onImportWallet} filterProperties={{}} />);
+		render(<WalletsControls onCreateWallet={vi.fn()} onImportWallet={onImportWallet} filterProperties={{}} />, {
+			history,
+			withProfileSynchronizer: true,
+		});
 
 		userEvent.click(screen.getByTestId("WalletControls__import-wallet"));
 
@@ -89,6 +125,10 @@ describe("WalletsControls", () => {
 		renderResponsive(
 			<WalletsControls onCreateWallet={vi.fn()} onImportWallet={onImportWallet} filterProperties={{}} />,
 			"xs",
+			{
+				history,
+				withProfileSynchronizer: true,
+			},
 		);
 
 		userEvent.click(screen.getAllByTestId("dropdown__toggle")[1]);
@@ -107,6 +147,10 @@ describe("WalletsControls", () => {
 				onImportWallet={vi.fn()}
 				filterProperties={filterProperties as any}
 			/>,
+			{
+				history,
+				withProfileSynchronizer: true,
+			},
 		);
 
 		expect(container).toMatchSnapshot();
