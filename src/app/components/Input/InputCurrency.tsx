a0192a2174ab031cdf0faa9c93c9a71e37746eb2
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-04-22 10:30:48 -0600

    prevent sanitizing issues on currency input

--- src/app/components/Input/InputCurrency.tsx
+++ src/app/components/Input/InputCurrency.tsx
@@ -14,22 +14,32 @@ type InputCurrencyProperties = {
 	noShadow?: boolean;
 } & Omit<React.InputHTMLAttributes<any>, "onChange" | "defaultValue">;
 
-const sanitize = (value?: string) => Currency.fromString(value || "").display;
+const sanitize = (value?: string, magnitude?: number) => Currency.fromString(value || "", magnitude).display;
 
 export const InputCurrency = React.forwardRef<HTMLInputElement, InputCurrencyProperties>(
-	({ onChange, value, ...properties }: InputCurrencyProperties, reference) => {
+	({ onChange, value, onBlur, ...properties }: InputCurrencyProperties, reference) => {
 		const [amount, setAmount] = useState<string>(sanitize(value?.toString()));
 
 		useEffect(() => {
 			// when value is changed outside, update amount as well
-			setAmount(sanitize(value?.toString()));
+			setAmount(sanitize(value?.toString(), 999));
 		}, [value]);
 
-		const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-			const sanitizedValue = sanitize(event.target.value);
+		const parseValue = (value: string, limitLength = true) => {
+			const sanitizedValue = sanitize(value, limitLength ? undefined : 999);
 
 			setAmount(sanitizedValue);
 			onChange?.(sanitizedValue);
+		};
+
+		const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+			parseValue(event.target.value, false);
+		};
+
+		const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+			parseValue(event.target.value);
+
+			onBlur?.(event);
 		};
 
 		return (
@@ -37,6 +47,7 @@ export const InputCurrency = React.forwardRef<HTMLInputElement, InputCurrencyPro
 				<Input
 					data-testid="InputCurrency"
 					onChange={handleInput}
+					onBlur={handleBlur}
 					ref={reference}
 					type="text"
 					value={amount}
