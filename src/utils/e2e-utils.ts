2368fe4efc7cdffceb6161ba58f4822e0d9931b6
goga-m <goga.mamoulasvili@gmail.com>
2024-04-26 13:18:26 +0200

    wip

--- src/utils/e2e-utils.ts
+++ src/utils/e2e-utils.ts
@@ -46,6 +46,7 @@ const pingServerUrls = new Set([
 	"https://ark-test.arkvault.io",
 	"https://ark-live.arkvault.io/api/wallets?limit=1&nonce=0",
 	"https://ark-test.arkvault.io/api/wallets?limit=1&nonce=0",
+	"https://dwallets.mainsailhq.com/",
 ]);
 
 const pingMusigServerUrls = new Set(["https://ark-live-musig.arkvault.io", "https://ark-test-musig.arkvault.io"]);
