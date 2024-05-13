fefe1335099683e80d1dde31f75f53d9124dcd4c
Alfonso Bribiesca <alfonsobries@gmail.com>
2024-05-13 15:49:51 -0600

    accept mainsail as coin

--- src/app/hooks/use-search-parameters-validation.tsx
+++ src/app/hooks/use-search-parameters-validation.tsx
@@ -266,7 +266,7 @@ export const useSearchParametersValidation = () => {
 			return { error: { type: SearchParametersError.CoinMismatch } };
 		}
 
-		if (!allEnabledNetworks.some((item) => item.coin() === coin)) {
+		if (!allEnabledNetworks.some((item) => item.coin().toUpperCase() === coin)) {
 			return { error: { type: SearchParametersError.CoinNotSupported, value: coin } };
 		}
 
