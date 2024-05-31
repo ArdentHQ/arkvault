f8c0d93db4d06de3b645754add687d2cf78a6a77
goga-m <goga.mamoulasvili@gmail.com>
2024-05-31 12:32:31 +0200

    wip

--- src/domains/transaction/pages/SendRegistration/SendRegistration.test.tsx
+++ src/domains/transaction/pages/SendRegistration/SendRegistration.test.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable max-lines-per-function */
-/* eslint-disable max-lines */
 import { Contracts } from "@ardenthq/sdk-profiles";
 import { Observer } from "@ledgerhq/hw-transport";
 import React from "react";
