e00090783f484f7093aa6e5fe9702cac76069a5e
goga-m <goga-m@users.noreply.github.com>
2024-06-03 14:19:47 +0000

    style: resolve style guide violations

--- src/utils/migrations/initialize-ark-networks.test.ts
+++ src/utils/migrations/initialize-ark-networks.test.ts
@@ -1,6 +1,5 @@
 import { ARK } from "@ardenthq/sdk-ark";
 import { initializeArkNetworks } from "./initialize-ark-networks";
-import * as testHelpers from "@/utils/test-helpers";
 
 describe("initializeArkNetworks", () => {
 	it("should initialize the ARK mainnet network", () => {
