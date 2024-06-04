0dfeed0fdd5830bb5ccccb2081a7ba5dd372b155
Shahin Safaraliyev <shahin@ardenthq.com>
2024-06-04 15:21:43 +0400

    wip

--- src/utils/migrations/initialize-mainsail-networks.ts
+++ src/utils/migrations/initialize-mainsail-networks.ts
@@ -1,10 +1,6 @@
 import { Mainsail } from "@ardenthq/sdk-mainsail";
 
 export const initializeMainsailNetworks = ({ data }) => {
-	// if (isE2E()) {
-	// 	return;
-	// }
-
 	if (typeof data.networks === "object" && !!data.networks.mainsail) {
 		// Networks already assigned to profile, skipping migration
 		return;
