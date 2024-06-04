92f4290bcaffc11b1e5ab410490673eb8f8d7203
Shahin Safaraliyev <shahin@ardenthq.com>
2024-06-04 14:52:13 +0400

    wip

--- src/utils/migrations/initialize-mainsail-networks.test.ts
+++ src/utils/migrations/initialize-mainsail-networks.test.ts
@@ -19,18 +19,6 @@ describe("initializeMainsailNetworks", () => {
 		expect(data.networks.mainsail.devnet).toEqual(Mainsail.manifest.networks["mainsail.devnet"]);
 	});
 
-	it("should not initialize the mainsail devnet network if is e2e", () => {
-		const isE2EMock = vi.spyOn(testHelpers, "isE2E").mockReturnValue(true);
-
-		const data = { networks: {} };
-
-		initializeMainsailNetworks({ data });
-
-		expect(data.networks.mainsail).toBeUndefined();
-
-		isE2EMock.mockRestore();
-	});
-
 	it("should not initialize the mainsail devnet network if already defined", () => {
 		const data = {
 			networks: {
