9af14d424d50872773dccda71f4a9ca515125881
goga-m <goga.mamoulasvili@gmail.com>
2024-06-05 11:45:33 +0200

    wip

--- src/utils/migrations/initialize-ark-networks.ts
+++ src/utils/migrations/initialize-ark-networks.ts
@@ -2,6 +2,11 @@ import { ARK } from "@ardenthq/sdk-ark";
 import { isE2E, isPreview } from "@/utils/test-helpers";
 
 export const initializeArkNetworks = ({ data }) => {
+	if (typeof data.networks === "object" && !!data.networks.ark) {
+		// Networks already assigned to profile, skipping migration
+		return;
+	}
+
 	if (!data.networks) {
 		data.networks = {};
 	}
