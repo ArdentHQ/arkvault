fddeefdf7a87d6850377cc169b9bef1e0fbaad4b
Shahin Safaraliyev <shahin@ardenthq.com>
2024-06-11 17:31:17 +0400

    refactor: add public key validation

--- src/domains/transaction/validations/ValidatorRegistration.ts
+++ src/domains/transaction/validations/ValidatorRegistration.ts
@@ -1,4 +1,8 @@
 import { Contracts } from "@ardenthq/sdk-profiles";
+import {Networks} from "@ardenthq/sdk";
+import {validatePattern} from "@/utils/validations";
+import {debounceAsync} from "@/utils/debounce";
+import {ValidateResult} from "react-hook-form";
 
 export const validatorRegistration = (t: any) => ({
 	validatorPublicKey: (wallet: Contracts.IReadWriteWallet) => ({
@@ -12,14 +16,44 @@ export const validatorRegistration = (t: any) => ({
 		required: t("COMMON.VALIDATION.FIELD_REQUIRED", {
 			field: t("TRANSACTION.VALIDATOR_PUBLIC_KEY"),
 		}),
-		validate: async (publicKey: string) => {
-			const isValid = await wallet.coin().publicKey().verifyPublicKeyWithBLS(publicKey);
 
-			if (!isValid) {
-				return t("COMMON.INPUT_PUBLIC_KEY.VALIDATION.INVALID_BLS_PUBLIC_KEY");
-			}
+		validate: {
+			pattern: async (publicKey: string) => {
+				const isValid = await wallet.coin().publicKey().verifyPublicKeyWithBLS(publicKey);
 
-			return true;
+				if (!isValid) {
+					return t("COMMON.INPUT_PUBLIC_KEY.VALIDATION.INVALID_BLS_PUBLIC_KEY");
+				}
+
+				return true;
+			},
+			unique: debounceAsync<ValidateResult>(async (publicKey: string) => {
+				try {
+					await publicKeyExists(wallet.network(), publicKey);
+				} catch {
+					return t("COMMON.INPUT_PUBLIC_KEY.VALIDATION.PUBLIC_KEY_ALREADY_EXISTS", { publicKey });
+				}
+			}, 300),
 		},
 	}),
 });
+
+
+const publicKeyExists = async (network: Networks.Network, publicKey: string) => {
+	const endpoints = {
+		"mainsail.devnet": "https://dwallets.mainsailhq.com/api/wallets/",
+		"mainsail.mainnet": "https://wallets.mainsailhq.com/api/wallets/",
+	};
+
+	if (publicKey.length === 0) {
+		return;
+	}
+
+	const response = await fetch(`${endpoints[network.id()]}?attributes.validatorPublicKey=${publicKey}`);
+
+	const data = await response.json();
+
+	if (data.meta.count > 0) {
+		throw new Error("Public key has used!");
+	}
+};
