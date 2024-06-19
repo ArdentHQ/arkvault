ba7aadc3b346539bf7f97b7bb7fde8e3fc94e110
goga-m <goga-m@users.noreply.github.com>
2024-06-19 12:18:19 +0000

    style: resolve style guide violations

--- vite.config.ts
+++ vite.config.ts
@@ -60,12 +60,12 @@ export default defineConfig(() => {
 				},
 				plugins: [
 					process.env.ANALYZE_BUNDLE &&
-					visualizer({
-						open: true,
-						brotliSize: true,
-						gzipSize: true,
-						template: "treemap",
-					}),
+						visualizer({
+							open: true,
+							brotliSize: true,
+							gzipSize: true,
+							template: "treemap",
+						}),
 				],
 			},
 		},
