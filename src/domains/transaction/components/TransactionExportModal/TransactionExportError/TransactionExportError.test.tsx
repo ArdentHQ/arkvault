4e34f70da221a57d803efc5682496b9443bf7e70
shahin-hq <132887516+shahin-hq@users.noreply.github.com>
2024-06-24 17:02:26 +0400

    refactor: make partial exports downloadable (#532)

--- src/domains/transaction/components/TransactionExportModal/TransactionExportError/TransactionExportError.test.tsx
+++ src/domains/transaction/components/TransactionExportModal/TransactionExportError/TransactionExportError.test.tsx
@@ -2,12 +2,14 @@ import { createHashHistory } from "history";
 import React from "react";
 
 import { TransactionExportError } from "./TransactionExportError";
-import { getDefaultProfileId, screen, renderResponsive } from "@/utils/testing-library";
+import { getDefaultProfileId, screen, renderResponsive, render } from "@/utils/testing-library";
 
 const history = createHashHistory();
 
 const fixtureProfileId = getDefaultProfileId();
 let dashboardURL: string;
+
+const downloadButton = () => screen.getByTestId("TransactionExportError__download");
 
 describe("TransactionExportError", () => {
 	beforeEach(() => {
@@ -22,5 +24,19 @@ describe("TransactionExportError", () => {
 
 		expect(screen.getByTestId("TransactionExportError__retry-button")).toBeInTheDocument();
 		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should show the Download button if export has processed partially", () => {
+		const function_ = vi.fn();
+
+		const file = {
+			content: "",
+			extension: "csv",
+			name: "name",
+		};
+
+		render(<TransactionExportError count={1} file={file} onDownload={function_} onBack={function_} />);
+
+		expect(downloadButton()).toBeInTheDocument();
 	});
 });
