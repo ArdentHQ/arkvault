05da10d3d49db350a58783e05b82e430b48fe110
goga-m <goga.mamoulasvili@gmail.com>
2024-06-26 11:44:48 +0200

    wip

--- src/domains/transaction/cucumber/multiple-transfer.ts
+++ src/domains/transaction/cucumber/multiple-transfer.ts
@@ -49,7 +49,7 @@ cucumber(
 		"Then the transaction is sent successfully": async (t: TestController) => {
 			await t
 				.expect(Selector("h1").withText(translations.TRANSACTION.SUCCESS.TITLE).exists)
-				.ok({ timeout: 10_000 });
+				.ok({ timeout: 5000 });
 		},
 	},
 	[
