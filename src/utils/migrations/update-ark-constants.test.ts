ffc0c6536644e5200730932796e614bb09aa3ff9
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2024-06-26 17:34:06 +0200

    chore: merge `develop` into `feat/mainsail` (#536)

--- src/utils/migrations/update-ark-constants.test.ts
+++ src/utils/migrations/update-ark-constants.test.ts
@@ -53,18 +53,6 @@ describe("updateArkConstants", () => {
 		expect(data.networks.ark[arkMainnet].constants.epoch).toEqual("2023-01-01T00:00:00.000Z");
 	});
 
-	it("should update hosts if any host contains 'payvo.com'", () => {
-		data.networks.ark[arkMainnet].hosts = [{ host: "example.payvo.com" }];
-		updateArkConstants({ data });
-		expect(data.networks.ark[arkMainnet].hosts).toEqual(ARK.manifest.networks[arkMainnet].hosts);
-	});
-
-	it("should not update hosts if no host contains 'payvo.com'", () => {
-		data.networks.ark[arkMainnet].hosts = [{ host: "safehost.com" }];
-		updateArkConstants({ data });
-		expect(data.networks.ark[arkMainnet].hosts).toEqual([{ host: "safehost.com" }]);
-	});
-
 	it("should leave hosts unchanged if they are already correct", () => {
 		const expectedHosts = [...data.networks.ark[arkMainnet].hosts];
 		updateArkConstants({ data });
