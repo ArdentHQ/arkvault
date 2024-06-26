4cb0a0f3a2286598184d1a51fbe99b54ebfec8ef
goga-m <goga.mamoulasvili@gmail.com>
2024-06-26 16:46:12 +0200

    wip

--- src/utils/migrations/update-ark-constants.ts
+++ src/utils/migrations/update-ark-constants.ts
@@ -2,7 +2,7 @@ import { Networks } from "@ardenthq/sdk";
 import { ARK } from "@ardenthq/sdk-ark";
 
 export const updateArkConstants = ({ data }) => {
-	for (const manifest of Object.values<Networks.NetworkManifest>(data.networks.ark)) {
+	for (const manifest of Object.values<Networks.NetworkManifest>(data?.networks?.ark ?? {})) {
 		if (manifest.constants.epoch === undefined) {
 			manifest.constants.epoch = ARK.manifest.networks[manifest.id].constants.epoch;
 		}
--- src/utils/migrations/update-ark-nethashes.ts
+++ src/utils/migrations/update-ark-nethashes.ts
@@ -2,7 +2,7 @@ import { ARK } from "@ardenthq/sdk-ark";
 import { Networks } from "@ardenthq/sdk";
 
 export const updateArkNethashes = ({ data }) => {
-	for (const manifest of Object.values<Networks.NetworkManifest>(data.networks.ark)) {
+	for (const manifest of Object.values<Networks.NetworkManifest>(data?.networks?.ark ?? {})) {
 		if (["ark.devnet", "ark.mainnet"].includes(manifest.id)) {
 			if (!manifest.meta) {
 				manifest.meta = {};
