b0b38b91c176ee149e572d42b7683e0b005583a0
Shahin Safaraliyev <shahin@ardenthq.com>
2024-07-01 16:24:51 +0400

    chore: dependencies major version update #1

--- src/app/i18n/react-i18next.contracts.ts
+++ src/app/i18n/react-i18next.contracts.ts
@@ -1,4 +1,5 @@
+import {TFunction as TFunction18n} from "i18next";
 import { buildTranslations } from "./helpers";
 
 export type TranslationSet = ReturnType<typeof buildTranslations>;
-export type TFunction = (key: string, options?: object) => string;
+export type TFunction =  TFunction18n<"translation", undefined>
