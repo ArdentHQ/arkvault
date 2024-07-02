cc19f3292b750412b3bdeb70a7b5acdd6d123d66
goga-m <goga.mamoulasvili@gmail.com>
2024-07-02 19:22:22 +0200

    wip

--- src/app/i18n/helpers/index.ts
+++ src/app/i18n/helpers/index.ts
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-import { translations as COMMON } from "@/app/i18n/common/i18n";
-import { translations as CONTACTS } from "@/domains/contact/i18n";
-import { translations as DASHBOARD } from "@/domains/dashboard/i18n";
-import { translations as ERROR } from "@/domains/error/i18n";
-import { translations as EXCHANGE } from "@/domains/exchange/i18n";
-import { translations as MESSAGE } from "@/domains/message/i18n";
-import { translations as PROFILE } from "@/domains/profile/i18n";
-import { translations as SETTINGS } from "@/domains/setting/i18n";
-import { translations as TRANSACTION } from "@/domains/transaction/i18n";
-import { translations as VOTE } from "@/domains/vote/i18n";
-import { translations as WALLETS } from "@/domains/wallet/i18n";
-=======
 import { translations as CONTACTS } from "../../../domains/contact/i18n";
 import { translations as DASHBOARD } from "../../../domains/dashboard/i18n";
 import { translations as ERROR } from "../../../domains/error/i18n";
@@ -22,7 +9,6 @@ import { translations as TRANSACTION } from "../../../domains/transaction/i18n";
 import { translations as VOTE } from "../../../domains/vote/i18n";
 import { translations as WALLETS } from "../../../domains/wallet/i18n";
 import { translations as COMMON } from "../common/i18n";
->>>>>>> Stashed changes
 
 export const buildTranslations = () => ({
 	COMMON,
