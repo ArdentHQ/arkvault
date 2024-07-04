839a390c49bf32fba8ebd79dd212a327dffb0ea4
shahin-hq <132887516+shahin-hq@users.noreply.github.com>
2024-07-04 17:43:51 +0400

    chore: dependencies major version update - part 6 (#567)

--- src/app/assets/images/index.ts
+++ src/app/assets/images/index.ts
@@ -1,60 +1,44 @@
-import { ReactComponent as ARKVaultLogo } from "./arkvault-logo.svg";
-import { ReactComponent as DeleteBannerDarkGreen } from "./delete-banner-dark-green.svg";
-import { ReactComponent as DeleteBannerDarkNavy } from "./delete-banner-dark-navy.svg";
-import { ReactComponent as DeleteBannerLightGreen } from "./delete-banner-light-green.svg";
-import { ReactComponent as DeleteBannerLightNavy } from "./delete-banner-light-navy.svg";
-import { ReactComponent as EmptyNotificationsDarkGreen } from "./empty-notifications-dark-green.svg";
-import { ReactComponent as EmptyNotificationsDarkNavy } from "./empty-notifications-dark-navy.svg";
-import { ReactComponent as EmptyNotificationsLightGreen } from "./empty-notifications-light-green.svg";
-import { ReactComponent as EmptyNotificationsLightNavy } from "./empty-notifications-light-navy.svg";
-import { ReactComponent as ErrorDark } from "./error-dark.svg";
-import { ReactComponent as ErrorLight } from "./error-light.svg";
-import { ReactComponent as ErrorSmall } from "./error-small.svg";
-import { ReactComponent as GenericWarningDarkGreen } from "./generic-warning-dark-green.svg";
-import { ReactComponent as GenericWarningDarkNavy } from "./generic-warning-dark-navy.svg";
-import { ReactComponent as GenericWarningLightGreen } from "./generic-warning-light-green.svg";
-import { ReactComponent as GenericWarningLightNavy } from "./generic-warning-light-navy.svg";
-import { ReactComponent as SuccessDarkGreen } from "./success-dark-green.svg";
-import { ReactComponent as SuccessDarkNavy } from "./success-dark-navy.svg";
-import { ReactComponent as SuccessLightGreen } from "./success-light-green.svg";
-import { ReactComponent as SuccessLightNavy } from "./success-light-navy.svg";
-import { ReactComponent as NoResultsDarkGreen } from "./no-results-dark-green.svg";
-import { ReactComponent as NoResultsDarkNavy } from "./no-results-dark-navy.svg";
-import { ReactComponent as NoResultsLightGreen } from "./no-results-light-green.svg";
-import { ReactComponent as NoResultsLightNavy } from "./no-results-light-navy.svg";
-import { ReactComponent as WelcomeBannerDark } from "./welcome-banner-dark.svg";
-import { ReactComponent as WelcomeBannerLight } from "./welcome-banner-light.svg";
-import {
-	ReactComponent as IOsInstructionsStep1DarkBlue,
-	ReactComponent as IOsInstructionsStep1DarkGreen,
-} from "./ios-instructions-step-1-dark.svg";
-import {
-	ReactComponent as IOsInstructionsStep1LightBlue,
-	ReactComponent as IOsInstructionsStep1LightGreen,
-} from "./ios-instructions-step-1-light.svg";
-import {
-	ReactComponent as IOsInstructionsStep2DarkBlue,
-	ReactComponent as IOsInstructionsStep2DarkGreen,
-} from "./ios-instructions-step-2-dark.svg";
-import {
-	ReactComponent as IOsInstructionsStep2LightBlue,
-	ReactComponent as IOsInstructionsStep2LightGreen,
-} from "./ios-instructions-step-2-light.svg";
-import {
-	ReactComponent as IOsInstructionsStep3DarkBlue,
-	ReactComponent as IOsInstructionsStep3DarkGreen,
-} from "./ios-instructions-step-3-dark.svg";
-import {
-	ReactComponent as IOsInstructionsStep3LightBlue,
-	ReactComponent as IOsInstructionsStep3LightGreen,
-} from "./ios-instructions-step-3-light.svg";
-import { ReactComponent as TrashLight } from "./trash-light.svg";
-import { ReactComponent as TrashDark } from "./trash-dark.svg";
-import { ReactComponent as InfoLight } from "./info-light.svg";
-import { ReactComponent as InfoDark } from "./info-dark.svg";
-import { ReactComponent as WarningLight } from "./warning-light.svg";
-import { ReactComponent as WarningDark } from "./warning-dark.svg";
-import { ReactComponent as WorldMap } from "./world-map.svg";
+/// <reference types="vite-plugin-svgr/client" />
+
+import ARKVaultLogo from "./arkvault-logo.svg?react";
+import DeleteBannerDarkGreen from "./delete-banner-dark-green.svg?react";
+import DeleteBannerDarkNavy from "./delete-banner-dark-navy.svg?react";
+import DeleteBannerLightGreen from "./delete-banner-light-green.svg?react";
+import DeleteBannerLightNavy from "./delete-banner-light-navy.svg?react";
+import EmptyNotificationsDarkGreen from "./empty-notifications-dark-green.svg?react";
+import EmptyNotificationsDarkNavy from "./empty-notifications-dark-navy.svg?react";
+import EmptyNotificationsLightGreen from "./empty-notifications-light-green.svg?react";
+import EmptyNotificationsLightNavy from "./empty-notifications-light-navy.svg?react";
+import ErrorDark from "./error-dark.svg?react";
+import ErrorLight from "./error-light.svg?react";
+import ErrorSmall from "./error-small.svg?react";
+import GenericWarningDarkGreen from "./generic-warning-dark-green.svg?react";
+import GenericWarningDarkNavy from "./generic-warning-dark-navy.svg?react";
+import GenericWarningLightGreen from "./generic-warning-light-green.svg?react";
+import GenericWarningLightNavy from "./generic-warning-light-navy.svg?react";
+import SuccessDarkGreen from "./success-dark-green.svg?react";
+import SuccessDarkNavy from "./success-dark-navy.svg?react";
+import SuccessLightGreen from "./success-light-green.svg?react";
+import SuccessLightNavy from "./success-light-navy.svg?react";
+import NoResultsDarkGreen from "./no-results-dark-green.svg?react";
+import NoResultsDarkNavy from "./no-results-dark-navy.svg?react";
+import NoResultsLightGreen from "./no-results-light-green.svg?react";
+import NoResultsLightNavy from "./no-results-light-navy.svg?react";
+import WelcomeBannerDark from "./welcome-banner-dark.svg?react";
+import WelcomeBannerLight from "./welcome-banner-light.svg?react";
+import IOsInstructionsStep1Dark from "./ios-instructions-step-1-dark.svg?react";
+import IOsInstructionsStep1Light from "./ios-instructions-step-1-light.svg?react";
+import IOsInstructionsStep2Dark from "./ios-instructions-step-2-dark.svg?react";
+import IOsInstructionsStep2Light from "./ios-instructions-step-2-light.svg?react";
+import IOsInstructionsStep3Dark from "./ios-instructions-step-3-dark.svg?react";
+import IOsInstructionsStep3Light from "./ios-instructions-step-3-light.svg?react";
+import TrashLight from "./trash-light.svg?react";
+import TrashDark from "./trash-dark.svg?react";
+import InfoLight from "./info-light.svg?react";
+import InfoDark from "./info-dark.svg?react";
+import WarningLight from "./warning-light.svg?react";
+import WarningDark from "./warning-dark.svg?react";
+import WorldMap from "./world-map.svg?react";
 import { wallet } from "@/domains/wallet/images";
 import { transaction } from "@/domains/transaction/images";
 import { profile } from "@/domains/profile/images";
@@ -79,18 +63,18 @@ export const images = {
 		GenericWarningDarkNavy,
 		GenericWarningLightGreen,
 		GenericWarningLightNavy,
-		IOsInstructionsStep1DarkBlue,
-		IOsInstructionsStep1DarkGreen,
-		IOsInstructionsStep1LightBlue,
-		IOsInstructionsStep1LightGreen,
-		IOsInstructionsStep2DarkBlue,
-		IOsInstructionsStep2DarkGreen,
-		IOsInstructionsStep2LightBlue,
-		IOsInstructionsStep2LightGreen,
-		IOsInstructionsStep3DarkBlue,
-		IOsInstructionsStep3DarkGreen,
-		IOsInstructionsStep3LightBlue,
-		IOsInstructionsStep3LightGreen,
+		IOsInstructionsStep1DarkBlue: IOsInstructionsStep1Dark,
+		IOsInstructionsStep1DarkGreen: IOsInstructionsStep1Dark,
+		IOsInstructionsStep1LightBlue: IOsInstructionsStep1Light,
+		IOsInstructionsStep1LightGreen: IOsInstructionsStep1Light,
+		IOsInstructionsStep2DarkBlue: IOsInstructionsStep2Dark,
+		IOsInstructionsStep2DarkGreen: IOsInstructionsStep2Dark,
+		IOsInstructionsStep2LightBlue: IOsInstructionsStep2Light,
+		IOsInstructionsStep2LightGreen: IOsInstructionsStep2Light,
+		IOsInstructionsStep3DarkBlue: IOsInstructionsStep3Dark,
+		IOsInstructionsStep3DarkGreen: IOsInstructionsStep3Dark,
+		IOsInstructionsStep3LightBlue: IOsInstructionsStep3Light,
+		IOsInstructionsStep3LightGreen: IOsInstructionsStep3Light,
 		InfoDark,
 		InfoDarkGreen: InfoDark,
 		InfoDarkNavy: InfoDark,
