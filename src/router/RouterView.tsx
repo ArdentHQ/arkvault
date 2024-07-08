8e6a05af76263f7ec4a2a8262aa7b1886b0e8837
Shahin Safaraliyev <shahin@ardenthq.com>
2024-07-08 15:29:01 +0400

    chore: update `react-router-dom` package

--- src/router/RouterView.tsx
+++ src/router/RouterView.tsx
@@ -1,5 +1,5 @@
-import React, { createElement, FC, useEffect, useMemo, useRef } from "react";
-import { Redirect, Route, Switch, useHistory, useLocation } from "react-router-dom";
+import React, { createElement, FC, useMemo, } from "react";
+import {Route, Routes, useLocation,} from "react-router-dom";
 import { styled } from "twin.macro";
 
 import { useEnvironmentContext } from "@/app/contexts";
@@ -16,45 +16,46 @@ const Wrapper = styled.div();
 
 export const RouterView: React.VFC<Properties> = ({ routes, middlewares = [] }) => {
 	const location = useLocation();
-	const history = useHistory();
+
 	const { env } = useEnvironmentContext();
-	const [redirectUrl, setRedirectUrl] = React.useState<string | undefined>(undefined);
+	const [_redirectUrl, setRedirectUrl] = React.useState<string | undefined>(undefined);
 
-	const previousPath = useRef("");
+	// const previousPath = useRef("");
 
-	useEffect(() => {
-		history.listen((route) => {
-			// @ts-ignore
-			if (!previousPath.current || route.location?.pathname !== previousPath.current) {
-				// @ts-ignore
-				previousPath.current = route.location?.pathname;
-				window.scrollTo(0, 0);
-			}
-		});
-	}, [history]);
+	// useEffect(() => {
+	// 	history.listen((route) => {
+	// 		// @ts-ignore
+	// 		if (!previousPath.current || route.location?.pathname !== previousPath.current) {
+	// 			// @ts-ignore
+	// 			previousPath.current = route.location?.pathname;
+	// 			window.scrollTo(0, 0);
+	// 		}
+	// 	});
+	// }, [history]);
 
 	const canActivate = useMemo(
 		() =>
 			// @ts-ignore
-			middlewares.every((middleware) => middleware.handler({ env, history, location, redirect: setRedirectUrl })),
-		[location, middlewares, env, history],
+			middlewares.every((middleware) => middleware.handler({ env, location, redirect: setRedirectUrl })),
+		[location, middlewares, env],
 	);
 
 	return (
-		<Switch>
+		<Routes>
 			{routes.map((route, index) => (
-				<Route key={index} path={route.path} exact={route.exact}>
+				<Route key={index} path={route.path}>
 					<RouteSuspense skeleton={route.skeleton} path={route.path}>
 						{canActivate ? (
 							<Wrapper data-testid="RouterView__wrapper">
 								{createElement(route.component as PreloadableComponent<FC<unknown>>)}
 							</Wrapper>
 						) : (
-							<Redirect to={redirectUrl ?? "/"} />
+							<></>
+							// <Redirect to={redirectUrl ?? "/"} />
 						)}
 					</RouteSuspense>
 				</Route>
 			))}
-		</Switch>
+		</Routes>
 	);
 };
