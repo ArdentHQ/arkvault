f0a2695d6e3bfd704551315247a8204fd97274c2
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-05 09:38:45 -0500

    fix: unit tests for receiver mobile item

--- src/app/components/NavigationBar/NavigationBar.test.tsx
+++ src/app/components/NavigationBar/NavigationBar.test.tsx
@@ -330,7 +330,7 @@ describe("NavigationBar", () => {
 
 		await expect(screen.findByTestId("Modal__inner")).resolves.toHaveTextContent("Select Address");
 
-		userEvent.click(screen.getAllByTestId("WalletListItemMobile")[0]);
+		userEvent.click(screen.getAllByTestId("ReceiverItemMobile")[0]);
 
 		await expect(screen.findByTestId("ReceiveFunds__name")).resolves.toBeVisible();
 		await expect(screen.findByTestId("ReceiveFunds__address")).resolves.toBeVisible();
