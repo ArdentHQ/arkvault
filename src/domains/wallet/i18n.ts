52717a64f35498950d2796ea4b6d1e247158cb0d
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-12 20:48:42 -0500

    chore: update copy

--- src/domains/wallet/i18n.ts
+++ src/domains/wallet/i18n.ts
@@ -138,10 +138,11 @@ export const translations = {
 
 		LEDGER_SCAN_STEP: {
 			ADD_NEW_ADDRESS: "Add New Address",
+			SHOW_ALL: "Show All ({{count}})",
 			LOADED_SINGLE_WALLET: "Loaded <strong>1</strong> Wallet",
 			LOADED_WALLETS: "Loaded <strong>{{count}}</strong> Wallets",
 			SUBTITLE: "Select the addresses that you want to import.",
-			TITLE: "Accounts",
+			TITLE: "Addresses",
 		},
 
 		METHOD_STEP: {
