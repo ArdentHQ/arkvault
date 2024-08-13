3cee095ba01e571bcf5838b37fc3738f38992c1e
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-12 20:49:10 -0500

    fix: icon color

--- src/app/assets/svg/index.ts
+++ src/app/assets/svg/index.ts
@@ -53,6 +53,7 @@ import MagnifyingGlassId from "./magnifying-glass-id.svg?react";
 import Menu from "./menu.svg?react";
 import MenuOpen from "./menu-open.svg?react";
 import MoneyCoinSwap from "./money-coin-swap.svg?react";
+import NoteCheck from "./note-check.svg?react";
 import Pencil from "./pencil.svg?react";
 import PencilRuler from "./pencil-ruler.svg?react";
 import Server from "./server.svg?react";
@@ -147,6 +148,7 @@ export const SvgCollection: Record<string, FC<SVGProps<SVGSVGElement>>> = {
 	Menu,
 	MenuOpen,
 	MoneyCoinSwap,
+	NoteCheck,
 	Pencil,
 	PencilRuler,
 	Plus,
