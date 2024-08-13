fc46ef67b5382e03ee72d61214b8c9fab6d16bc5
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-12 19:06:59 -0500

    test: loader

---
+++ src/app/components/Loader/Loader.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { render, screen } from "@/utils/testing-library";
+import { Loader } from '@/app/components/Loader';
+
+describe("Loader", () => {
+	it("should render with custom text", () => {
+		const { asFragment } = render(<Loader text="Testing" />);
+
+		expect(screen.queryByTestId("Loader__wrapper")).toBeInTheDocument();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+    it("should render with custom class names", () => {
+        const { asFragment } = render(<Loader text="Testing" className="custom-class" />);
+
+        expect(screen.queryByTestId("Loader__wrapper")).toHaveClass("custom-class");
+        expect(asFragment()).toMatchSnapshot();
+    })
+
+    it("should render custom text", () => {
+        render(<Loader text="Testing" />);
+        expect(screen.getByText("Testing")).toBeInTheDocument();
+    })
+});
