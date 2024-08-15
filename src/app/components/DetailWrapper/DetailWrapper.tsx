4fb7fd017a6d9b974ed571067be0cb878d17ef73
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-08-15 19:38:43 +0000

    style: resolve style guide violations

--- src/app/components/DetailWrapper/DetailWrapper.tsx
+++ src/app/components/DetailWrapper/DetailWrapper.tsx
@@ -37,7 +37,7 @@ export const DetailWrapper = ({
 		{label && <DetailLabel>{label}</DetailLabel>}
 		<div
 			className={cn(
-				"w-full rounded-lg border-theme-secondary-300 p-3 dark:border-theme-secondary-800 sm:border sm:px-6 sm:py-5 break-words",
+				"w-full break-words rounded-lg border-theme-secondary-300 p-3 dark:border-theme-secondary-800 sm:border sm:px-6 sm:py-5",
 				{
 					"mt-0 sm:mt-2": !!label,
 				},
