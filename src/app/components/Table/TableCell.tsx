a5a13356a70f2fef47e60ac2d57dbc7a9d674f6c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-22 19:52:34 -0500

    refactor: table cell

--- src/app/components/Table/TableCell.tsx
+++ src/app/components/Table/TableCell.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import { styled, TwStyle } from "twin.macro";
-
 import { getStyles } from "./TableCell.styles";
+import { twMerge } from "tailwind-merge";
 
 type TableCellProperties = {
 	variant?: "start" | "middle" | "end";
 	size?: "sm" | "base";
 	className?: string;
-	innerClassName?: TwStyle | string;
+	innerClassName?: string;
 	isCompact?: boolean;
 	children: React.ReactNode;
 } & Omit<React.HTMLProps<any>, "size">;
 
-const TableCellInnerWrapper = styled.div<TableCellProperties>`
-	${({ className }) => className}
-	${getStyles}
-`;
+const TableCellInnerWrapper = ({...props}) => <div {...props} />;
 
 export const TableCell = ({
 	variant = "middle",
@@ -28,11 +24,7 @@ export const TableCell = ({
 }: TableCellProperties) => (
 	<td className={className} {...properties}>
 		<TableCellInnerWrapper
-			variant={variant}
-			size={size}
-			css={typeof innerClassName === "string" ? "" : innerClassName}
-			className={typeof innerClassName === "string" ? innerClassName : ""}
-			isCompact={isCompact}
+			className={twMerge(getStyles({variant, isCompact, size}), innerClassName)}
 		>
 			{children}
 		</TableCellInnerWrapper>
