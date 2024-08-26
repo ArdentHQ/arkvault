a955d63cbf411a8bf662f55430a78b132cd77f0e
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-25 22:32:36 -0500

    chore: format

--- src/domains/exchange/components/ExchangeTransactionsTable/ExchangeTransactionsRowMobile.tsx
+++ src/domains/exchange/components/ExchangeTransactionsTable/ExchangeTransactionsRowMobile.tsx
@@ -126,7 +126,10 @@ export const ExchangeTransactionsRowMobile: React.VFC<ExchangeTransactionsRowMob
 	return (
 		<TableRow onClick={() => onClick(exchangeTransaction.provider(), exchangeTransaction.orderId())} border={false}>
 			<MobileCard className="mb-3">
-				<div className="flex h-11 w-full items-center justify-between bg-theme-secondary-100 px-4 dark:bg-black" data-testid="TableRow__mobile">
+				<div
+					className="flex h-11 w-full items-center justify-between bg-theme-secondary-100 px-4 dark:bg-black"
+					data-testid="TableRow__mobile"
+				>
 					<div className="text-sm font-semibold">
 						{exchangeTransaction.orderId() ? (
 							<TruncateMiddle
@@ -150,7 +153,12 @@ export const ExchangeTransactionsRowMobile: React.VFC<ExchangeTransactionsRowMob
 						</div>
 						<ExchangeTransactionsRowStatusIcon status={exchangeTransaction.status()} />
 						<Divider type="vertical" />
-						<TableRemoveButton className="cursor-pointer !p-0" isCompact={true} onClick={handleRemove} data-testid="TableRow__mobile-remove-button" />
+						<TableRemoveButton
+							className="cursor-pointer !p-0"
+							isCompact={true}
+							onClick={handleRemove}
+							data-testid="TableRow__mobile-remove-button"
+						/>
 					</div>
 				</div>
 
