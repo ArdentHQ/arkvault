036e17aa34d1096d0bf68ebde0501afe781d6b14
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2024-08-27 10:10:25 -0500

    refactor: exchange initial page (#628)
    Co-authored-by: patricio0312rev <patricio0312rev@users.noreply.github.com>

--- src/domains/exchange/components/ExchangeCard/ExchangeCard.tsx
+++ src/domains/exchange/components/ExchangeCard/ExchangeCard.tsx
@@ -12,7 +12,7 @@ interface ExchangeCardProperties {
 export const ExchangeCard = ({ exchange, onClick }: ExchangeCardProperties) => (
 	<div data-testid={`ExchangeCard--${exchange.slug}`}>
 		<Card onClick={exchange.isActive ? onClick : undefined}>
-			<div className="flex flex-col items-center space-y-3 md:flex-row md:space-x-3 md:space-y-0">
+			<div className="flex flex-col items-center gap-3 md:flex-row">
 				<div className="h-11 w-11 shrink-0 overflow-hidden rounded-lg">
 					<img
 						src={exchange.logo.thumbnail}
@@ -24,7 +24,7 @@ export const ExchangeCard = ({ exchange, onClick }: ExchangeCardProperties) => (
 				<div className="flex flex-col truncate">
 					<div
 						className={cn(
-							"truncate text-lg font-semibold",
+							"truncate text-sm font-semibold md:text-lg",
 							exchange.isActive ? "link" : "text-theme-primary-100 dark:text-theme-secondary-800",
 						)}
 					>
--- src/domains/exchange/components/ExchangeGrid/ExchangeGrid.tsx
+++ src/domains/exchange/components/ExchangeGrid/ExchangeGrid.tsx
@@ -17,8 +17,8 @@ export const ExchangeGrid = ({ exchanges, isLoading, onClick }: ExchangeGridProp
 	if (isLoading) {
 		return (
 			<div data-testid="ExchangeGrid">
-				<div className="grid w-full grid-cols-2 gap-4.5 sm:grid-cols-3">
-					{Array.from({ length: 3 }).map((_, index) => (
+				<div className="grid w-full grid-cols-2 gap-2 sm:grid-cols-4 sm:gap-5">
+					{Array.from({ length: 4 }).map((_, index) => (
 						<ExchangeCardSkeleton key={index} />
 					))}
 				</div>
@@ -35,7 +35,10 @@ export const ExchangeGrid = ({ exchanges, isLoading, onClick }: ExchangeGridProp
 	}
 
 	return (
-		<div data-testid="ExchangeGrid" className="grid w-full grid-cols-2 gap-4.5 sm:grid-cols-3">
+		<div
+			data-testid="ExchangeGrid"
+			className="grid w-full grid-cols-2 gap-2 sm:grid-cols-4 md:grid-cols-3 md:gap-5 xl:grid-cols-4"
+		>
 			{exchanges.map((exchange: Exchange) => (
 				<ExchangeCard key={exchange.slug} exchange={exchange} onClick={() => onClick(exchange.slug)} />
 			))}
