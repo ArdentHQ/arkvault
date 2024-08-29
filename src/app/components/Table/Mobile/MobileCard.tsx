57355779490fec117ca9b660cf131b7883a79270
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-29 07:19:36 -0500

    refactor: mobile card

--- src/app/components/Table/Mobile/MobileCard.tsx
+++ src/app/components/Table/Mobile/MobileCard.tsx
@@ -6,15 +6,13 @@ interface MobileCardProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export const MobileCard: FC<MobileCardProps> = ({ className, children, ...props }) => (
-	<td>
-		<div
-			className={cn(
-				"w-full cursor-pointer overflow-hidden rounded border border-theme-secondary-300 bg-white dark:border-theme-secondary-800 dark:bg-theme-secondary-900",
-				className,
-			)}
-			{...props}
-		>
-			{children}
-		</div>
-	</td>
+	<div
+		className={cn(
+			"w-full cursor-pointer overflow-hidden rounded border border-theme-secondary-300 bg-white dark:border-theme-secondary-800 dark:bg-theme-secondary-900",
+			className,
+		)}
+		{...props}
+	>
+		{children}
+	</div>
 );
