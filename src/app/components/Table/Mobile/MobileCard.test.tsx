db68f6f4687c8a6c46e34adfc42aef59b9c8b12d
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-08-29 11:59:02 +0000

    style: resolve style guide violations

--- src/app/components/Table/Mobile/MobileCard.test.tsx
+++ src/app/components/Table/Mobile/MobileCard.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@/utils/testing-library";
-import { MobileCard } from './MobileCard';
+import { MobileCard } from "./MobileCard";
 describe("MobileCard", () => {
 	it("should render", () => {
 		render(<MobileCard data-testid="MobileCard__wrapper">Content</MobileCard>);
