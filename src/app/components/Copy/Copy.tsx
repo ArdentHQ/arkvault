36551a8422d990872efbd9341d0752e38b5b2439
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-08-28 21:59:44 -0500

    chore: run prettier format

--- src/app/components/Copy/Copy.tsx
+++ src/app/components/Copy/Copy.tsx
@@ -1,46 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { Clipboard } from "@/app/components/Clipboard";
 import { Icon } from "@/app/components/Icon";
 
 export const Copy = ({ address }: { address: string }) => {
-    const { t } = useTranslation();
-    const [isClicked, setIsClicked] = useState<boolean>(false);
+	const { t } = useTranslation();
+	const [isClicked, setIsClicked] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (isClicked) {
-            const timer = setTimeout(() => {
-                setIsClicked(false);
-            }, 1000);
+	useEffect(() => {
+		if (isClicked) {
+			const timer = setTimeout(() => {
+				setIsClicked(false);
+			}, 1000);
 
-            return () => clearTimeout(timer);
-        }
-    }, [isClicked, 1000]);
+			return () => clearTimeout(timer);
+		}
+	}, [isClicked, 1000]);
 
-    return (
-            <Clipboard
-                variant="icon"
-                data={address}
-                tooltip={t("COMMON.COPY_ADDRESS")}
-                tooltipDarkTheme
-                >
-                    {
-                        isClicked ? (
-                            <Icon
-                                name="CopySuccess"
-                                className="text-theme-primary-600"
-                                data-testid="Copy__icon_success"
-                            />
-                        ) : (
-                            <Icon
-                                name="Copy"
-                                onClick={() => setIsClicked(true)}
-                                className="hover:text-theme-secondary-500"
-                                data-testid="Copy__icon"
-                            />
-                        )
-                    }
-            </Clipboard>
-        
-    );
+	return (
+		<Clipboard variant="icon" data={address} tooltip={t("COMMON.COPY_ADDRESS")} tooltipDarkTheme>
+			{isClicked ? (
+				<Icon name="CopySuccess" className="text-theme-primary-600" data-testid="Copy__icon_success" />
+			) : (
+				<Icon
+					name="Copy"
+					onClick={() => setIsClicked(true)}
+					className="hover:text-theme-secondary-500"
+					data-testid="Copy__icon"
+				/>
+			)}
+		</Clipboard>
+	);
 };
