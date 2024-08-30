2f531a50ebf36856761e51d128557bf6aef8a67b
shahin-hq <shahin-hq@users.noreply.github.com>
2024-08-30 17:00:20 +0000

    style: resolve style guide violations

--- src/domains/vote/components/DelegateTable/DelegateRow/DelegateRowMobileSkeleton.tsx
+++ src/domains/vote/components/DelegateTable/DelegateRow/DelegateRowMobileSkeleton.tsx
@@ -8,12 +8,16 @@ export const DelegateRowMobileSkeleton = () => {
 
 	return (
 		<tr data-testid="DelegateRowMobileSkeleton">
-			<td  className="pt-3">
-				<div className="overflow-hidden w-full rounded-xl border border-theme-secondary-300 dark:border-theme-secondary-800">
+			<td className="pt-3">
+				<div className="w-full overflow-hidden rounded-xl border border-theme-secondary-300 dark:border-theme-secondary-800">
 					<div className="overflow-hidden border-b border-theme-secondary-300 p-4 dark:border-theme-secondary-800">
 						<div className="flex items-center justify-between">
-							<div><Skeleton height={17} width={nameWidth} /></div>
-							<div><Skeleton height={17} width={60} /></div>
+							<div>
+								<Skeleton height={17} width={nameWidth} />
+							</div>
+							<div>
+								<Skeleton height={17} width={60} />
+							</div>
 						</div>
 					</div>
 
