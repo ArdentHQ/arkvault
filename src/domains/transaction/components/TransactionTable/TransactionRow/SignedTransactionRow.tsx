50a3e2be2caf0f6b99a46d60ff76afdfa64d22a4
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-09-03 18:24:51 -0500

    refactor: signed transaction row - 768px

--- src/domains/transaction/components/TransactionTable/TransactionRow/SignedTransactionRow.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionRow/SignedTransactionRow.tsx
@@ -1,21 +1,22 @@
 import { Contracts, DTO } from "@ardenthq/sdk-profiles";
-import cn from "classnames";
 import React, { MouseEvent, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-
-import { BaseTransactionRowAmount } from "./TransactionRowAmount";
-import { BaseTransactionRowMode } from "./TransactionRowMode";
-import { BaseTransactionRowRecipientLabel } from "./TransactionRowRecipientLabel";
 import { Button } from "@/app/components/Button";
-import { Icon } from "@/app/components/Icon";
 import { TableCell, TableRow } from "@/app/components/Table";
 import { TableRemoveButton } from "@/app/components/TableRemoveButton";
 import { Tooltip } from "@/app/components/Tooltip";
 import { useTimeFormat } from "@/app/hooks/use-time-format";
 import { useMultiSignatureStatus } from "@/domains/transaction/hooks";
-import { Dropdown, DropdownOption } from "@/app/components/Dropdown";
+import { DropdownOption } from "@/app/components/Dropdown";
 import { getMultiSignatureInfo } from "@/domains/transaction/components/MultiSignatureDetail/MultiSignatureDetail.helpers";
 import { assertString } from "@/utils/assertions";
+import { TimeAgo } from "@/app/components/TimeAgo";
+import { DateTime } from "@ardenthq/sdk-intl";
+import { TruncateMiddle } from "@/app/components/TruncateMiddle";
+import { useTransactionTypes } from "@/domains/transaction/hooks/use-transaction-types";
+import { Label } from "@/app/components/Label";
+import { AmountLabel } from "@/app/components/Amount";
+import { Divider } from "@/app/components/Divider";
 
 interface SignedTransactionRowProperties {
 	transaction: DTO.ExtendedSignedTransactionData;
@@ -46,7 +47,7 @@ export const SignButton = ({
 	const { t } = useTranslation();
 
 	if (!canBeSigned && !isAwaitingOurFinalSignature) {
-		return <></>;
+		return <span className="text-sm text-theme-secondary-500 dark:text-theme-secondary-600 font-semibold">{t("COMMON.WAITING")}...</span>;
 	}
 
 	const ButtonContent = () => {
@@ -54,14 +55,12 @@ export const SignButton = ({
 			return (
 				<>
 					<span>{t("COMMON.SEND")}</span>
-					<Icon name="DoubleArrowRight" />
 				</>
 			);
 		}
 
 		return (
 			<>
-				<Icon name="Pencil" />
 				<span>{t("COMMON.SIGN")}</span>
 			</>
 		);
@@ -73,7 +72,7 @@ export const SignButton = ({
 				size="sm"
 				data-testid="TransactionRow__sign"
 				variant="transparent"
-				className={`text-theme-primary-600 hover:text-theme-primary-700 ${className}`}
+				className={`text-theme-primary-600 hover:text-theme-primary-700 p-0 ${className}`}
 				onClick={onClick}
 			>
 				<ButtonContent />
@@ -103,6 +102,7 @@ export const SignedTransactionRow = ({
 }: SignedTransactionRowProperties) => {
 	const { t } = useTranslation();
 	const timeFormat = useTimeFormat();
+	const { getLabel } = useTransactionTypes();
 	const recipient = transaction.get<string>("recipientId");
 	const { canBeSigned, isAwaitingFinalSignature, isAwaitingOurFinalSignature, status } = useMultiSignatureStatus({
 		transaction,
@@ -159,70 +159,71 @@ export const SignedTransactionRow = ({
 		return options;
 	}, [canBeSigned]);
 
-	const handleDropdownSelection = ({ value }) => {
-		if (value === "remove") {
-			handleRemove();
-		} else {
-			onSign?.(transaction);
-		}
-	};
-
 	return (
-		<TableRow onClick={() => onRowClick?.(transaction)}>
+		<TableRow className="relative">
 			<TableCell variant="start" isCompact={isCompact}>
-				<Tooltip content={transaction.id()}>
-					<span className="text-theme-secondary-300 dark:text-theme-secondary-800">
-						<Icon name="MagnifyingGlassId" />
+				<div className="flex flex-col gap-1 font-semibold">
+					<span className="text-sm">
+						<TruncateMiddle
+							className="text-theme-primary-600 cursor-pointer"
+							text={transaction.id()}
+							maxChars={14}
+							onClick={() => onRowClick?.(transaction)} 
+						/>
 					</span>
-				</Tooltip>
+					<span className="text-xs text-theme-secondary-700">
+						<TimeAgo date={DateTime.fromUnix(transaction.timestamp().toUNIX()).toISOString()} />
+					</span>
+				</div>
 			</TableCell>
 
 			<TableCell
-				className="hidden lg:table-cell"
+				className="hidden xl:table-cell"
 				innerClassName="text-theme-secondary-text"
 				isCompact={isCompact}
 			>
 				<span data-testid="TransactionRow__timestamp">{transaction.timestamp().format(timeFormat)}</span>
 			</TableCell>
 
-			<TableCell innerClassName="space-x-4" isCompact={isCompact}>
-				<BaseTransactionRowMode
-					isSent={true}
-					type={transaction.type()}
-					address={recipient}
-					isCompact={isCompact}
-				/>
-
-				<BaseTransactionRowRecipientLabel type={transaction.type()} recipient={recipient} />
+			<TableCell
+				isCompact={isCompact}
+			>
+				<Label color="secondary" size="xs" noBorder className="p-1 rounded">
+					{getLabel(transaction.type())}
+				</Label>
 			</TableCell>
-
-			<TableCell className="w-16" innerClassName="justify-center truncate" isCompact={isCompact}>
-				<Tooltip content={status.label}>
-					<span className={`p-1 ${status.className}`}>
-						<Icon name={status.icon} size="lg" />
-					</span>
-				</Tooltip>
-			</TableCell>
-
-			<TableCell innerClassName="justify-end" isCompact={isCompact}>
-				<BaseTransactionRowAmount
-					isCompact={isCompact}
-					isSent={true}
-					total={transaction.amount() + transaction.fee()}
-					wallet={wallet}
-				/>
-			</TableCell>
-
-			<TableCell variant="end" innerClassName="justify-end space-x-2" isCompact={isCompact}>
-				<div className="hidden space-x-3 xl:flex">
-					<SignButton
-						isCompact={isCompact}
-						canBeSigned={canBeSigned}
-						isAwaitingFinalSignature={isAwaitingFinalSignature}
-						isAwaitingOurFinalSignature={isAwaitingOurFinalSignature}
-						onClick={() => onSign?.(transaction)}
+			
+			<TableCell innerClassName="space-x-2" isCompact={isCompact}>
+				<Label color="danger-bg" size="xs" noBorder className="p-1 rounded">
+					{t("COMMON.TO")}
+				</Label>
+				<span className="text-sm">
+					<TruncateMiddle
+						className="text-theme-primary-600 cursor-pointer"
+						text={recipient || ""}
+						maxChars={10}
+						data-testId="TransactionRowRecipientLabel"
 					/>
+				</span>
+			</TableCell>
 
+			<TableCell isCompact={isCompact} innerClassName="justify-end">
+				<div className="flex flex-col gap-1 items-end">
+					<AmountLabel value={transaction.amount() + transaction.fee()} isNegative={true} ticker={wallet.currency()} isCompact />
+					<span className="text-theme-secondary-700 text-xs font-semibold">{transaction.fee()}</span>
+				</div>
+			</TableCell>
+
+			<TableCell isCompact={isCompact}>
+				<div className="flex flex-row items-center">
+					<SignButton
+							isCompact={isCompact}
+							canBeSigned={canBeSigned}
+							isAwaitingFinalSignature={isAwaitingFinalSignature}
+							isAwaitingOurFinalSignature={isAwaitingOurFinalSignature}
+							onClick={() => onSign?.(transaction)}
+						/>
+					<Divider type="vertical" className="m-0 border-theme-secondary-300 dark:border-theme-secondary-800" />
 					<Tooltip
 						content={
 							canBeDeleted
@@ -235,32 +236,11 @@ export const SignedTransactionRow = ({
 								isCompact={isCompact}
 								isDisabled={!canBeDeleted}
 								onClick={handleRemove}
+								className="p-0 m-0"
 							/>
 						</div>
 					</Tooltip>
 				</div>
-
-				{dropdownOptions.length > 0 && (
-					<div className="xl:hidden">
-						<Dropdown
-							data-testid="SignedTransactionRow--dropdown"
-							toggleContent={
-								<Button
-									variant={isCompact ? "transparent" : "secondary"}
-									size="icon"
-									className={cn("px-2", {
-										"-mr-1.5 text-theme-primary-300 hover:text-theme-primary-600": isCompact,
-										"flex-1 bg-theme-primary-600 text-white hover:bg-theme-primary-700": !isCompact,
-									})}
-								>
-									<Icon name="EllipsisVertical" size="lg" />
-								</Button>
-							}
-							options={dropdownOptions}
-							onSelect={handleDropdownSelection}
-						/>
-					</div>
-				)}
 			</TableCell>
 		</TableRow>
 	);
