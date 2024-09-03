cdd34d7b183eba0a38ffe4e2446227632fcb1418
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-09-03 18:24:06 -0500

    feat: label secondary style

--- src/app/components/Label/Label.styles.tsx
+++ src/app/components/Label/Label.styles.tsx
@@ -5,7 +5,7 @@ import { Size } from "@/types";
 
 const baseStyle = tw`inline-block font-semibold overflow-hidden`;
 
-export type ColorType = "primary" | "success" | "danger" | "warning" | "neutral" | "success-bg" | "danger-bg";
+export type ColorType = "primary" | "success" | "danger" | "warning" | "neutral" | "success-bg" | "danger-bg" | "secondary";
 
 const getColor = (color?: ColorType, variant?: string) => {
 	if (variant === "solid") {
@@ -31,6 +31,7 @@ const getColor = (color?: ColorType, variant?: string) => {
 		success: () => tw`text-theme-success-600 border-theme-success-200 dark:border-theme-success-600`,
 		"success-bg": () =>
 			tw`bg-theme-success-100 text-theme-success-700 dark:border dark:border-theme-success-700 dark:bg-transparent dark:text-theme-success-500`,
+		secondary: () => tw`text-theme-secondary-700 bg-theme-secondary-200 border-theme-secondary-200 dark:border-theme-secondary-800 dark:text-theme-secondary-500 dark:bg-transparent`
 	};
 
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
@@ -44,6 +45,10 @@ const getSize = (size?: Size) => {
 
 	if (size === "sm") {
 		return tw`text-sm`;
+	}
+
+	if (size === "xs") {
+		return tw`text-xs`;
 	}
 
 	return tw`text-base`;
