ab2d66c1fffd12026f76e1bb09ba654e252d421a
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-09-05 19:24:03 +0000

    style: resolve style guide violations

--- src/app/components/Table/TableWrapper.tsx
+++ src/app/components/Table/TableWrapper.tsx
@@ -4,7 +4,7 @@ import cn from "classnames";
 export const TableWrapper = ({ className, children }: { className?: string; children: React.ReactNode }) => (
 	<div
 		className={cn(
-			"hidden rounded-xl border-transparent outline outline-1 outline-transparent sm:block md:border-b-[5px] md:border-b-theme-secondary-200 md:outline-theme-secondary-300 dark:md:border-theme-secondary-800 dark:md:outline-theme-secondary-800 pb-2",
+			"hidden rounded-xl border-transparent pb-2 outline outline-1 outline-transparent sm:block md:border-b-[5px] md:border-b-theme-secondary-200 md:outline-theme-secondary-300 dark:md:border-theme-secondary-800 dark:md:outline-theme-secondary-800",
 			className,
 		)}
 		data-testid="TableWrapper"
