1370f67d3c489cdb6af0c585ad6f0db4e2ab0d29
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-09-12 15:54:50 -0500

    test: transaction row addressing

---
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowAddressing.test.tsx
@@ -0,0 +1,51 @@
+import { Contracts } from "@ardenthq/sdk-profiles";
+import React from 'react';
+import { env, getDefaultProfileId, render, screen } from "@/utils/testing-library";
+import { TransactionRowAddressing } from './TransactionRowAddressing';
+import { TransactionFixture } from "@/tests/fixtures/transactions";
+
+describe("TransactionRowAddressing", () => {
+    let profile: Contracts.IProfile;
+    const fixture = {...TransactionFixture, wallet: () => ({...TransactionFixture.wallet(), currency: () => "DARK",}),};
+
+    beforeAll(() => {
+        profile = env.profiles().findById(getDefaultProfileId());
+    });
+    it("should render", () => {
+        render(<TransactionRowAddressing transaction={fixture as any} profile={profile} />);
+
+        expect(screen.getByTestId("TransactionRowAddressing__container")).toBeTruthy();
+    })
+
+    it("should render vote variant if transaction is vote or unvote", () => {
+        const voteFixture = {...fixture, isVote: () => true};
+        render(<TransactionRowAddressing transaction={voteFixture as any} profile={profile} />);
+
+        expect(screen.getByTestId("TransactionRowAddressing__vote")).toBeTruthy();
+
+        const unvoteFixture = {...fixture, isVote: () => false, isUnvote: () => true};
+        render(<TransactionRowAddressing transaction={unvoteFixture as any} profile={profile} />);
+    });
+
+    it("should render multipayment variant", () => {
+        const multiPaymentFixture = {...fixture, isMultiPayment: () => true};
+        render(<TransactionRowAddressing transaction={multiPaymentFixture as any} profile={profile} />);
+
+        expect(screen.getByTestId("TransactionRowAddressing__multipayment")).toBeTruthy();
+    })
+
+    it("should render the danger label if transaction is sent", () => {
+        const sentFixture = {...fixture, isSent: () => true};
+        render(<TransactionRowAddressing transaction={sentFixture as any} profile={profile} />);
+
+        expect(screen.getByTestId("TransactionRowAddressing__label")).toHaveTextContent("To");
+    });
+
+    it("should render the success label if transaction is not sent", () => {
+        const notSentFixture = {...fixture, isSent: () => false};
+        render(<TransactionRowAddressing transaction={notSentFixture as any} profile={profile} />);
+
+        expect(screen.getByTestId("TransactionRowAddressing__label")).toHaveTextContent("From");
+    });
+
+})
