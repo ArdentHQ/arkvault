c859b59a3dcab35a3f049a1e2a30972e173d0aba
Shahin Safaraliyev <shahin@ardenthq.com>
2024-09-12 18:09:57 +0400

    refactor: adjust review step for delegate registration

--- src/domains/transaction/components/DelegateRegistrationForm/ReviewStep.tsx
+++ src/domains/transaction/components/DelegateRegistrationForm/ReviewStep.tsx
@@ -4,12 +4,11 @@ import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
 import { TotalAmountBox } from "@/domains/transaction/components/TotalAmountBox";
-import {
-	TransactionDetail,
-	TransactionNetwork,
-	TransactionSender,
-} from "@/domains/transaction/components/TransactionDetail";
 import { StepHeader } from "@/app/components/StepHeader";
+import {DetailLabel, DetailTitle, DetailWrapper} from "@/app/components/DetailWrapper";
+import {Address} from "@/app/components/Address";
+import {Divider} from "@/app/components/Divider";
+import {ThemeIcon} from "@/app/components/Icon";
 
 export const ReviewStep = ({ wallet }: { wallet: Contracts.IReadWriteWallet }) => {
 	const { t } = useTranslation();
@@ -25,20 +24,59 @@ export const ReviewStep = ({ wallet }: { wallet: Contracts.IReadWriteWallet }) =
 	}, [unregister]);
 
 	return (
-		<section data-testid="DelegateRegistrationForm__review-step">
+		<section data-testid="DelegateRegistrationForm__review-step" className="space-y-3 sm:space-y-4">
 			<StepHeader
 				title={t("TRANSACTION.REVIEW_STEP.TITLE")}
 				subtitle={t("TRANSACTION.REVIEW_STEP.DESCRIPTION")}
+				titleIcon={
+					<ThemeIcon dimensions={[24, 24]} lightIcon="SendTransactionLight" darkIcon="SendTransactionDark" />
+				}
 			/>
 
-			<TransactionNetwork network={wallet.network()} border={false} />
+			<DetailWrapper label={t("TRANSACTION.ADDRESSING")}>
+				<div className="flex w-full items-center justify-between gap-4 space-x-2 sm:justify-start sm:space-x-0">
+					<DetailTitle className="w-auto sm:min-w-28">{t("COMMON.FROM")}</DetailTitle>
+					<Address
+						address={wallet.address()}
+						walletName={wallet.alias()}
+						walletNameClass="text-theme-text text-sm leading-[17px] sm:leading-5 sm:text-base"
+						addressClass="text-theme-secondary-500 dark:text-theme-secondary-700 text-sm leading-[17px] sm:leading-5 sm:text-base"
+						wrapperClass="justify-end sm:justify-start"
+						showCopyButton
+					/>
+				</div>
+			</DetailWrapper>
 
-			<TransactionSender address={wallet.address()} network={wallet.network()} />
 
-			<TransactionDetail label={t("TRANSACTION.DELEGATE_NAME")}>{username}</TransactionDetail>
+			<DetailWrapper label={t("TRANSACTION.TRANSACTION_TYPE")}>
+				<div className="space-y-3 sm:space-y-0">
+					<div className="flex w-full items-center justify-between gap-4 sm:justify-start">
+						<DetailTitle className="w-auto sm:min-w-28">{t("COMMON.CATEGORY")}</DetailTitle>
+						<div className="flex items-center rounded bg-theme-secondary-200 px-1 py-[3px] dark:border dark:border-theme-secondary-800 dark:bg-transparent">
+						<span className="text-[12px] font-semibold leading-[15px] text-theme-secondary-700 dark:text-theme-secondary-500">
+							{ t("TRANSACTION.TRANSACTION_TYPES.DELEGATE_REGISTRATION") }
+						</span>
+						</div>
+					</div>
 
-			<div className="mt-2">
-				<TotalAmountBox amount={0} fee={fee} ticker={wallet.currency()} />
+					<div className="hidden sm:block">
+						<Divider dashed />
+					</div>
+
+					<div className="flex w-full items-center justify-between gap-4 sm:justify-start">
+						<DetailTitle className="w-auto sm:min-w-28">{t("TRANSACTION.DELEGATE_NAME")}</DetailTitle>
+						<div className="no-ligatures truncate text-sm font-semibold leading-[17px] text-theme-secondary-900 dark:text-theme-secondary-200 sm:text-base sm:leading-5">
+							{username}
+						</div>
+					</div>
+				</div>
+			</DetailWrapper>
+
+			<div data-testid="DetailWrapper">
+				<DetailLabel>{t("COMMON.TRANSACTION_SUMMARY")}</DetailLabel>
+				<div className="mt-0 p-3 sm:mt-2 sm:p-0">
+					<TotalAmountBox amount={0} fee={fee} ticker={wallet.currency()}/>
+				</div>
 			</div>
 		</section>
 	);
