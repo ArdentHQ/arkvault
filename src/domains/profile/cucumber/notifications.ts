ec373f48fda63f3de678744b67b744dfd3d181b0
goga-m <goga.mamoulasvili@gmail.com>
2024-09-22 19:47:51 +0200

    wip

--- src/domains/profile/cucumber/notifications.ts
+++ src/domains/profile/cucumber/notifications.ts
@@ -64,9 +64,42 @@ cucumber(
 	},
 	[
 		mockRequest(
+			"https://ark-test.arkvault.io/api/blocks/e6d4760c50419d8665731d3391ba209767f3073d4b2b6661078ff220034d197e",
+			{
+				"data": {
+					"id": "e6d4760c50419d8665731d3391ba209767f3073d4b2b6661078ff220034d197e",
+					"version": 0,
+					"height": 19362468,
+					"previous": "ee54abc37bac91358cbcdfe725347268bd6ee1c5f0b3a48b3ba3025ee7cfca11",
+					"forged": {
+						"reward": "200000000",
+						"fee": "6206028",
+						"amount": "100000",
+						"total": "206206028"
+					},
+					"payload": {
+						"hash": "2cf134885471ae6c9ea44f43a7b5f8a1ab78078cd102615d56344f6a43a1ff6f",
+						"length": 32
+					},
+					"generator": {
+						"username": "genesis_27",
+						"address": "DKY5eyQUKKYyaCfPp6MUv3Y4FW6EbNF53A",
+						"publicKey": "03508436f55577f406be58a5e7e59307cea823943c5312d62f4e3f3c63966f6e7c"
+					},
+					"signature": "30450221009b5d4312dc7e2169aa36c35ef33e95fc02d9033faeef822e4e2ef60e53f5609802207614d9a9d2442a426fe01d505b4546c3d8f6e9581d263d873c41c673e4e8f92a",
+					"confirmations": 28467,
+					"transactions": 1,
+					"timestamp": {
+						"epoch": 236656104,
+						"unix": 1726757304,
+						"human": "2024-09-19T14:48:24.000Z"
+					}
+				}
+			}
+		),
+		mockRequest(
 			"https://ark-test.arkvault.io/api/transactions?page=1&limit=12&address=D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD",
 			"coins/ark/devnet/transactions/byAddress/D8rr7B1d6TL6pf14LgMz4sKp1VBMs6YUYD-1-10",
-			"https://ark-test.arkvault.io/api/blocks/e6d4760c50419d8665731d3391ba209767f3073d4b2b6661078ff220034d197e"
 		),
 		mockRequest(
 			"https://ark-test.arkvault.io/api/transactions?page=1&limit=12&address=D5sRKWckH4rE1hQ9eeMeHAepgyC3cvJtwb",
