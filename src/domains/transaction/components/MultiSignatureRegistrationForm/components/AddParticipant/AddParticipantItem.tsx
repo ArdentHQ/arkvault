a0af977561a8631907061897d1a5f3adfb14a23f
Shahin Safaraliyev <shahin@ardenthq.com>
2024-09-23 15:31:13 +0400

    refactor: musig transaction step 1 adjustments

--- src/domains/transaction/components/MultiSignatureRegistrationForm/components/AddParticipant/AddParticipantItem.tsx
+++ src/domains/transaction/components/MultiSignatureRegistrationForm/components/AddParticipant/AddParticipantItem.tsx
@@ -23,18 +23,45 @@ export const AddParticipantItem: VFC<{
 	return (
 		<div
 			data-testid="AddParticipantItem"
-			className="pt-3 border-t border-dashed border-theme-secondary-300 last:pb-0 pb-3 dark:border-theme-secondary-800"
+			className="sm:pt-3 mb-3 last:mb-0 bg-white rounded overflow-hidden sm:rounded-none sm:bg-transparent border sm:border-x-0 sm:border-b-0 sm:border-dashed border-theme-secondary-300  dark:border-theme-secondary-800"
 		>
 			<div className="flex flex-col items-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-				<div className="flex max-w-full flex-1 flex-row items-center space-x-4 overflow-auto sm:flex-col sm:items-start sm:space-x-0 sm:space-y-1">
-					<div className="whitespace-nowrap text-sm font-semibold text-theme-secondary-500 dark:text-theme-secondary-700">
-						{t("TRANSACTION.MULTISIGNATURE.PARTICIPANT_#", { count: index + 1 })}
+				<div className="flex w-full flex-1 sm:w-auto flex-col sm:items-start sm:space-x-0 sm:space-y-1">
+					<div className="flex flex-row items-center justify-between flex-1 w-full px-4 py-3 sm:p-0 bg-theme-secondary-100 sm:bg-transparent">
+						<div
+							className="whitespace-nowrap text-sm font-semibold text-theme-secondary-700 sm:text-theme-secondary-500 dark:text-theme-secondary-700">
+							{t("TRANSACTION.MULTISIGNATURE.PARTICIPANT_#", {count: index + 1})}
+						</div>
+						<div className="sm:hidden">
+							<Tooltip
+								content={t("TRANSACTION.MULTISIGNATURE.REMOVE_NOT_ALLOWED")}
+								disabled={!deleteButtonIsDisabled}
+							>
+								<Button
+									disabled={deleteButtonIsDisabled}
+									onClick={() => onDelete(index)}
+									data-testid="AddParticipantItem--mobile-deleteButton"
+									size="icon"
+									sizeClassName="p-0"
+									className="text-theme-secondary-700 dark:text-theme-secondary-500"
+									variant="transparent"
+								>
+									<Icon name="Trash" size="lg" />
+								</Button>
+							</Tooltip>
+						</div>
 					</div>
-					<div className="max-w-full overflow-auto sm:w-full">
-						<Address address={address} walletName={alias} />
+					<div className="max-w-full overflow-auto sm:w-full pt-3 pb-4 px-4 sm:p-0">
+						<div className="text-sm leading-[17px] sm:hidden mb-2 font-semibold text-theme-secondary-700">{t("COMMON.ADDRESS")}</div>
+						<Address
+							address={address}
+							addressClass="text-sm leading-[17px] sm:text-base leading-5  text-theme-secondary-500 dark:text-theme-secondary-700"
+							walletName={alias}
+							walletNameClass="text-sm leading-[17px] sm:text-base leading-5 text-theme-text"
+						/>
 					</div>
 				</div>
-				<div className="w-full sm:w-auto">
+				<div className="hidden sm:block">
 					<Tooltip
 						content={t("TRANSACTION.MULTISIGNATURE.REMOVE_NOT_ALLOWED")}
 						disabled={!deleteButtonIsDisabled}
@@ -45,7 +72,7 @@ export const AddParticipantItem: VFC<{
 							size="icon"
 							onClick={() => onDelete(index)}
 							data-testid="AddParticipantItem--deleteButton"
-							className="w-full sm:w-auto p-3.5"
+							className="p-3.5"
 						>
 							<Icon name="Trash" />
 						</Button>
