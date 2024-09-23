81284e358e9504ccf0a14033502898b8f28cd63e
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-09-23 07:21:41 -0500

    refactor: adjust show all addresses page

--- src/domains/wallet/pages/WalletGroupPage/WalletGroupPage.tsx
+++ src/domains/wallet/pages/WalletGroupPage/WalletGroupPage.tsx
@@ -32,21 +32,21 @@ export const WalletGroupPage: React.VFC = () => {
 	return (
 		<Page pageTitle={network.coin()}>
 			<Section>
-				<div className="flex flex-col rounded-xl border-theme-secondary-300 dark:border-theme-secondary-800 dark:bg-theme-background md:border-2">
+				<div className="flex flex-col rounded-xl border-theme-navy-100 dark:border-theme-secondary-800 dark:bg-theme-background md:border-2">
 					<WalletsGroupHeader
 						network={network}
 						wallets={wallets}
 						isExpanded={false}
 						className="px-0 md:px-8"
 					/>
-				</div>
 
 				<WalletsList
 					wallets={wallets}
 					itemsPerPage={MAX_WALLETS_ON_SINGLE_PAGE_LIST}
 					showPagination={isMdAndAbove}
-					className="px-0 md:px-0"
+					className="px-0 md:px-0 border-t border-t-theme-secondary-300 dark:border-t-theme-secondary-800"
 				/>
+				</div>
 			</Section>
 		</Page>
 	);
