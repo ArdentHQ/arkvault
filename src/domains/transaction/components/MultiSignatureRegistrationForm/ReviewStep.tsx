e37bdda9a11bb867b6baeba46e69b12e60fa8cf8
Shahin Safaraliyev <shahin@ardenthq.com>
2024-09-25 17:35:10 +0400

    refactor: musig transaction review step adjustments

--- src/domains/transaction/components/MultiSignatureRegistrationForm/ReviewStep.tsx
+++ src/domains/transaction/components/MultiSignatureRegistrationForm/ReviewStep.tsx
@@ -82,7 +82,9 @@ export const ReviewStep = ({ wallet, profile }: { wallet: Contracts.IReadWriteWa
 						</div>
 						<div className="px-3 sm:px-6 mt-3 mb-2">
 							{participants.map(participiant => (
-								<div className="px-4 sm:px-0 py-3 mb-3 last:mb-0 sm:mb-0 bg-theme-secondary-100 sm:bg-transparent overflow-hidden sm:last:border-none border sm:border-t-0 sm:border-x-0 rounded sm:rounded-none sm:border-dashed border-theme-secondary-300">
+								<div
+									key={participant.address}
+									className="px-4 sm:px-0 py-3 mb-3 last:mb-0 sm:mb-0 bg-theme-secondary-100 sm:bg-transparent overflow-hidden sm:last:border-none border sm:border-t-0 sm:border-x-0 rounded sm:rounded-none sm:border-dashed border-theme-secondary-300">
 									<Address address={participiant.address}
 											 addressClass="text-theme-navy-600 text-sm leading-[17px]"
 											 showCopyButton/>
