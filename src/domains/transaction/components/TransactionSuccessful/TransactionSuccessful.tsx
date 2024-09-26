69a719eba63c41bd3e56e3b540e0e18cc045573d
goga-m <goga.mamoulasvili@gmail.com>
2024-09-26 17:55:55 +0200

    wip

--- src/domains/transaction/components/TransactionSuccessful/TransactionSuccessful.tsx
+++ src/domains/transaction/components/TransactionSuccessful/TransactionSuccessful.tsx
@@ -4,18 +4,9 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 import { useConfirmedTransaction } from "./hooks/useConfirmedTransaction";
-import {
-	TransactionAddresses,
-	TransactionConfirmations,
-	TransactionMusigParticipants,
-	TransactionType,
-} from "@/domains/transaction/components/TransactionDetail";
 import { StepHeader } from "@/app/components/StepHeader";
 import { Icon } from "@/app/components/Icon";
-import { DetailLabel, DetailPadded } from "@/app/components/DetailWrapper";
-import { TransactionId } from "@/domains/transaction/components/TransactionDetail/TransactionId";
-import { RecipientItem } from "@/domains/transaction/components/RecipientList/RecipientList.contracts";
-import { transactionPublicKeys } from "@/domains/transaction/components/MultiSignatureDetail/MultiSignatureDetail.helpers";
+import { TransactionDetailContent } from "../TransactionDetailModal";
 
 interface TransactionSuccessfulProperties {
 	transaction: DTO.ExtendedSignedTransactionData;
@@ -23,19 +14,16 @@ interface TransactionSuccessfulProperties {
 	title?: string;
 	description?: string;
 	children?: React.ReactNode;
-	recipients?: RecipientItem[];
 }
 
 export const TransactionSuccessful = ({
 	transaction,
 	senderWallet,
 	title,
-	children,
-	recipients = [],
 }: TransactionSuccessfulProperties) => {
 	const { t } = useTranslation();
 
-	const { isConfirmed, confirmations } = useConfirmedTransaction({
+	const { isConfirmed } = useConfirmedTransaction({
 		transactionId: transaction.id(),
 		wallet: senderWallet,
 	});
@@ -59,51 +47,7 @@ export const TransactionSuccessful = ({
 				}
 			/>
 
-			<div className="mt-8">
-				<TransactionId transaction={transaction} />
-			</div>
-
-			<div className="mt-6 space-y-8">
-				<DetailPadded>
-					<TransactionAddresses
-						network={senderWallet.network()}
-						senderAddress={senderWallet.address()}
-						recipients={recipients}
-						profile={senderWallet.profile()}
-					/>
-				</DetailPadded>
-
-				{!transaction.isVote() && (
-					<DetailPadded>
-						<TransactionType transaction={transaction} />
-					</DetailPadded>
-				)}
-
-				{children}
-
-				{senderWallet.transaction().isAwaitingConfirmation(transaction.id()) && (
-					<DetailPadded>
-						<DetailLabel>{t("TRANSACTION.CONFIRMATIONS")}</DetailLabel>
-						<div className="mt-2">
-							<TransactionConfirmations isConfirmed={isConfirmed} confirmations={confirmations} transaction={transaction} />
-						</div>
-					</DetailPadded>
-				)}
-
-				{transaction.usesMultiSignature() && (
-					<DetailPadded>
-						<DetailLabel>{t("TRANSACTION.PARTICIPANTS")}</DetailLabel>
-						<div className="mt-2">
-							<TransactionMusigParticipants
-								publicKeys={transactionPublicKeys(transaction).publicKeys}
-								profile={senderWallet.profile()}
-								network={senderWallet.network()}
-								useExplorerLinks
-							/>
-						</div>
-					</DetailPadded>
-				)}
-			</div>
+			<TransactionDetailContent transactionItem={transaction} profile={senderWallet.profile()} />
 		</section>
 	);
 };
