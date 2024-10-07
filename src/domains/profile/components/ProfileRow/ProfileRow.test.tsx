cd817374abcd75dec6e433539215381b4f05f537
goga-m <goga.mamoulasvili@gmail.com>
2024-10-07 19:27:57 +0200

    wip

--- src/domains/profile/components/ProfileRow/ProfileRow.test.tsx
+++ src/domains/profile/components/ProfileRow/ProfileRow.test.tsx
@@ -1,7 +1,7 @@
 import { Contracts } from "@ardenthq/sdk-profiles";
 import React from "react";
 import { env, getDefaultProfileId, render, screen } from "@/utils/testing-library";
-import { ProfileRow, ProfileRowSkeleton } from "./ProfileRow";
+import { ProfileRow, ProfileRowSkeleton, ProfilesSliderSkeleton } from "./ProfileRow";
 
 let profile: Contracts.IProfile;
 
@@ -32,5 +32,23 @@ describe("ProfileRowSkeleton", () => {
 		render(<ProfileRowSkeleton />);
 
 		expect(screen.getByTestId("ProfileRowSkeleton")).toBeInTheDocument();
+	});
+});
+
+describe("ProfileSliderSkeleton", () => {
+	beforeAll(() => {
+		profile = env.profiles().findById(getDefaultProfileId());
+	});
+
+	it("should render", () => {
+		const { asFragment } = render(<ProfilesSliderSkeleton maxProfilesPerPage={5} length={3} />);
+
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it("should render with dots", () => {
+		const { asFragment } = render(<ProfilesSliderSkeleton maxProfilesPerPage={3} />);
+
+		expect(asFragment()).toMatchSnapshot();
 	});
 });
