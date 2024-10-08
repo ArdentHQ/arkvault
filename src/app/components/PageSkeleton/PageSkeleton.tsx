f4561369da0aaff6e3083a48a6e5195e7bb1a153
goga-m <goga.mamoulasvili@gmail.com>
2024-10-08 12:23:15 +0200

    wip

--- src/app/components/PageSkeleton/PageSkeleton.tsx
+++ src/app/components/PageSkeleton/PageSkeleton.tsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { Trans, useTranslation } from "react-i18next";
-import cn from "classnames";
 import { ThemeIcon } from "@/app/components/Icon";
 import { Page, Section } from "@/app/components/Layout";
 import { WelcomeSlider } from "@/domains/profile/components/WelcomeSlider/WelcomeSlider";
-import { ProfilesSliderSkeleton } from "@/domains/profile/components/ProfileRow/ProfileRow";
+import { ProfileRowSkeleton, ProfilesSliderSkeleton } from "@/domains/profile/components/ProfileRow/ProfileRow";
 import { Skeleton } from "@/app/components/Skeleton";
-import { useEnvironmentContext } from "@/app/contexts";
 
 export const PageSkeleton = () => {
-	const { env } = useEnvironmentContext();
+
 	const { t } = useTranslation();
-
-	const hasProfiles = env.profiles().count() > 0;
-
-	if (!hasProfiles) {
-		return <div data-testid="PageSkeleton" />;
-	}
-
 	return (
 		<div data-testid="PageSkeleton">
 			<Page navbarVariant="logo-only" pageTitle={t("COMMON.WELCOME")} title={<Trans i18nKey="COMMON.APP_NAME" />}>
@@ -28,14 +19,7 @@ export const PageSkeleton = () => {
 				>
 					<div className="flex flex-col gap-3 lg:flex-row">
 						<div
-							className={cn(
-								"min-w-0 basis-1/2 rounded-xl border border-theme-navy-100 bg-theme-navy-50 bg-[url('/welcome-bg-white.svg')] dark:border-theme-secondary-800 dark:bg-theme-secondary-800 dark:bg-[url('/welcome-bg-dark.svg')]",
-								{
-									"hidden sm:block": hasProfiles,
-									"mb-6 sm:mb-0": !hasProfiles,
-								},
-							)}
-						>
+							className="min-w-0 basis-1/2 rounded-xl border border-theme-navy-100 bg-theme-navy-50 bg-[url('/welcome-bg-white.svg')] dark:border-theme-secondary-800 dark:bg-theme-secondary-800 dark:bg-[url('/welcome-bg-dark.svg')] hidden sm:block" >
 							<WelcomeSlider />
 						</div>
 						<div className="min-w-0 basis-1/2 rounded-xl border-theme-navy-100 dark:border-theme-secondary-800 sm:border sm:p-6">
@@ -47,25 +31,15 @@ export const PageSkeleton = () => {
 											lightIcon="PersonLight"
 											dimensions={[24, 24]}
 										/>
-
-										<h2 className="mx-4 text-2xl font-bold">
-											{hasProfiles
-												? t("PROFILE.PAGE_WELCOME.WITH_PROFILES.TITLE")
-												: t("PROFILE.PAGE_WELCOME.WITHOUT_PROFILES.TITLE")}
-										</h2>
-
-										<p className="text-base leading-7 text-theme-secondary-text">
-											{hasProfiles
-												? t("PROFILE.PAGE_WELCOME.WITH_PROFILES.DESCRIPTION")
-												: t("PROFILE.PAGE_WELCOME.WITHOUT_PROFILES.DESCRIPTION")}
-										</p>
 									</div>
 
-									<div className="mt-4 flex w-full flex-col justify-center">
-										<ProfilesSliderSkeleton length={env.profiles().count()} />
-
-										<div className={cn({ "mt-3": hasProfiles })}>
-											<Skeleton className="h-[2.75rem]" />
+									<div className="mt-4 space-y-4 w-full flex flex-col justify-center items-center">
+										<Skeleton className="h-8" width={140} />
+										<Skeleton className="h-4 block" width={240} />
+										<Skeleton className="h-4 block" width={160} />
+										<div className="w-full space-y-3">
+											<ProfileRowSkeleton />
+											<ProfileRowSkeleton />
 										</div>
 									</div>
 								</div>
@@ -81,5 +55,5 @@ export const PageSkeleton = () => {
 				</Section>
 			</Page>
 		</div>
-	);
+	)
 };
