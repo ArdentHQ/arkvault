e5496c5d1bf4268e96d86f934c3c92b2a378625e
Shahin Safaraliyev <shahin@ardenthq.com>
2024-10-11 19:29:34 +0400

    refactor: add Pagination

--- src/domains/wallet/components/WalletsGroup/WalletsGroup.tsx
+++ src/domains/wallet/components/WalletsGroup/WalletsGroup.tsx
@@ -1,24 +1,14 @@
-import React, { useCallback } from "react";
-import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router-dom";
-import { Button } from "@/app/components/Button";
-import { useActiveProfile, useAccordion } from "@/app/hooks";
+import React from "react";
 import { WalletsGroupProperties } from "@/domains/wallet/components/WalletsGroup/WalletsGroup.contracts";
 import { WalletsGroupHeader } from "@/domains/wallet/components/WalletsGroup/WalletsGroupHeader";
 import { WalletsList } from "@/domains/wallet/components/WalletsList";
 import { AccordionWrapper } from "@/app/components/Accordion";
+import { useAccordion } from "@/app/hooks";
 
-const MAX_WALLETS_ON_DASHBOARD_LIST = 1
+const MAX_WALLETS_ON_DASHBOARD_LIST = 2
 
 export const WalletsGroup: React.VFC<WalletsGroupProperties> = ({ network, wallets, maxWidthReferences }) => {
-	const { t } = useTranslation();
-	const history = useHistory();
-	const profile = useActiveProfile();
 	const { isExpanded, handleHeaderClick } = useAccordion();
-
-	const goToCoinWallets = useCallback(() => {
-		history.push(`/profiles/${profile.id()}/network/${network.id()}`);
-	}, [history, network, profile]);
 
 	return (
 		<AccordionWrapper data-testid="WalletsGroup" isCollapsed={!isExpanded} className="md:!mb-3">
@@ -34,17 +24,6 @@ export const WalletsGroup: React.VFC<WalletsGroupProperties> = ({ network, walle
 			{isExpanded && (
 				<WalletsList wallets={wallets} itemsPerPage={MAX_WALLETS_ON_DASHBOARD_LIST} showPagination={wallets.length > MAX_WALLETS_ON_DASHBOARD_LIST} />
 			)}
-
-			{/*{isExpanded && wallets.length > MAX_WALLETS_ON_DASHBOARD_LIST && (*/}
-			{/*	<Button*/}
-			{/*		variant="secondary"*/}
-			{/*		className="mx-4 mb-4 mt-1"*/}
-			{/*		data-testid="WalletsList__ShowAll"*/}
-			{/*		onClick={goToCoinWallets}*/}
-			{/*	>*/}
-			{/*		{`${t("COMMON.SHOW_ALL")} (${wallets.length})`}*/}
-			{/*	</Button>*/}
-			{/*)}*/}
 		</AccordionWrapper>
 	);
 };
