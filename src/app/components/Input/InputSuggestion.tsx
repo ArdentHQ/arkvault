3ed675f75bb3f36a0b49419384f63393855b1ca3
Shahin Safaraliyev <shahin@ardenthq.com>
2024-10-15 19:08:00 +0400

    wip

--- src/app/components/Input/InputSuggestion.tsx
+++ src/app/components/Input/InputSuggestion.tsx
@@ -31,7 +31,7 @@ export const InputSuggestion: VFC<InputSuggestionProperties> = ({ hiddenReferenc
 		<span
 			data-testid="Input__suggestion"
 			className={cn(
-				"pointer-events-none absolute inset-y-0 flex w-full items-center font-normal opacity-50",
+				"pointer-events-none absolute inset-y-0 text-sm sm:text-base flex w-full items-center font-normal opacity-50",
 				{ invisible: hideSuggestion() },
 				innerClassName,
 			)}
