b47383d5fcf80e0d18de45a2aa1ec0e6158a302b
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2024-10-16 11:23:33 +0200

    fix: adjust transaction review step alignment (#734)

--- src/domains/transaction/pages/SendVote/ReviewStep.tsx
+++ src/domains/transaction/pages/SendVote/ReviewStep.tsx
@@ -23,7 +23,7 @@ export const ReviewStep = ({ unvotes, votes, wallet }: SendVoteStepProperties) =
 	const category = getVoteCategory(votes, unvotes);
 
 	return (
-		<section data-testid="SendVote__review-step" className="space-y-3 sm:space-y-4">
+		<section data-testid="SendVote__review-step">
 			<StepHeader
 				title={t("TRANSACTION.REVIEW_STEP.TITLE")}
 				titleIcon={
@@ -31,34 +31,35 @@ export const ReviewStep = ({ unvotes, votes, wallet }: SendVoteStepProperties) =
 				}
 				subtitle={t("TRANSACTION.REVIEW_STEP.DESCRIPTION")}
 			/>
+			<div className="-mx-3 space-y-3 sm:mx-0 sm:space-y-4">
+				<DetailWrapper label={t("TRANSACTION.ADDRESSING")}>
+					<div className="flex w-full items-center justify-between gap-4 space-x-2 sm:justify-start sm:space-x-0">
+						<DetailTitle
+							className={cn("w-auto", {
+								"sm:min-w-20": category !== "swap",
+								"sm:min-w-28": category === "swap",
+							})}
+						>
+							{t("COMMON.FROM")}
+						</DetailTitle>
+						<Address
+							address={wallet.address()}
+							walletName={wallet.alias()}
+							walletNameClass="text-theme-text text-sm leading-[17px] sm:leading-5 sm:text-base"
+							addressClass="text-theme-secondary-500 dark:text-theme-secondary-700 text-sm leading-[17px] sm:leading-5 sm:text-base"
+							wrapperClass="justify-end sm:justify-start"
+							showCopyButton
+						/>
+					</div>
+				</DetailWrapper>
 
-			<DetailWrapper label={t("TRANSACTION.ADDRESSING")}>
-				<div className="flex w-full items-center justify-between gap-4 space-x-2 sm:justify-start sm:space-x-0">
-					<DetailTitle
-						className={cn("w-auto", {
-							"sm:min-w-20": category !== "swap",
-							"sm:min-w-28": category === "swap",
-						})}
-					>
-						{t("COMMON.FROM")}
-					</DetailTitle>
-					<Address
-						address={wallet.address()}
-						walletName={wallet.alias()}
-						walletNameClass="text-theme-text text-sm leading-[17px] sm:leading-5 sm:text-base"
-						addressClass="text-theme-secondary-500 dark:text-theme-secondary-700 text-sm leading-[17px] sm:leading-5 sm:text-base"
-						wrapperClass="justify-end sm:justify-start"
-						showCopyButton
-					/>
-				</div>
-			</DetailWrapper>
+				<VoteTransactionType votes={votes} unvotes={unvotes} />
 
-			<VoteTransactionType votes={votes} unvotes={unvotes} />
-
-			<div data-testid="DetailWrapper">
-				<DetailLabel>{t("COMMON.TRANSACTION_SUMMARY")}</DetailLabel>
-				<div className="mt-0 p-3 sm:mt-2 sm:p-0">
-					<TotalAmountBox amount={0} fee={fee} ticker={wallet.currency()} />
+				<div data-testid="DetailWrapper">
+					<DetailLabel>{t("COMMON.TRANSACTION_SUMMARY")}</DetailLabel>
+					<div className="mt-0 p-3 sm:mt-2 sm:p-0">
+						<TotalAmountBox amount={0} fee={fee} ticker={wallet.currency()} />
+					</div>
 				</div>
 			</div>
 		</section>
