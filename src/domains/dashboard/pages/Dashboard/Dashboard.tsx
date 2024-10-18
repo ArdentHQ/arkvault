0b4e92bf937e15242ad18480456baf817a01b308
shahin-hq <shahin-hq@users.noreply.github.com>
2024-10-18 09:59:38 +0000

    style: resolve style guide violations

--- src/domains/dashboard/pages/Dashboard/Dashboard.tsx
+++ src/domains/dashboard/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { uniq } from "@ardenthq/sdk-helpers";
 import { Contracts, DTO } from "@ardenthq/sdk-profiles";
-import React, {useEffect, useMemo, useState} from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { EmptyBlock } from "@/app/components/EmptyBlock";
 import { useWalletFilters } from "@/domains/dashboard/components/FilterWallets";
@@ -16,8 +16,8 @@ import { useConfiguration, useEnvironmentContext } from "@/app/contexts";
 import { useActiveProfile } from "@/app/hooks";
 import { WelcomeModal } from "@/domains/profile/components/WelcomeModal";
 import { TransactionDetailModal } from "@/domains/transaction/components/TransactionDetailModal";
-import {generatePath} from "react-router-dom";
-import {ProfilePaths} from "@/router/paths";
+import { generatePath } from "react-router-dom";
+import { ProfilePaths } from "@/router/paths";
 
 export const Dashboard: React.VFC = () => {
 	const activeProfile = useActiveProfile();
@@ -39,7 +39,7 @@ export const Dashboard: React.VFC = () => {
 	});
 
 	useEffect(() => {
-		generatePath(ProfilePaths.SendTransfer, { profileId: activeProfile.id() })
+		generatePath(ProfilePaths.SendTransfer, { profileId: activeProfile.id() });
 	}, []);
 
 	const liveNetworkIds = useMemo(
