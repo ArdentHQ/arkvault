ffa2b5c8d523902a4d0d4926482bd4e2225e9b0c
shahin-hq <shahin-hq@users.noreply.github.com>
2024-10-21 11:40:46 +0000

    style: resolve style guide violations

--- src/app/components/MultiEntryItem/MultiEntryItem.tsx
+++ src/app/components/MultiEntryItem/MultiEntryItem.tsx
@@ -10,7 +10,8 @@ interface Properties {
 export const MultiEntryItem = ({ titleSlot, bodySlot, rightSlot, dataTestId }: Properties): JSX.Element => (
 	<div
 		data-testid={dataTestId}
-		className="mb-3 overflow-hidden rounded border border-theme-secondary-300 bg-white last:mb-0 dark:border-theme-secondary-800 dark:bg-black sm:rounded-none sm:border-x-0 sm:border-b-0 sm:border-dashed sm:bg-transparent sm:pt-3 sm:dark:bg-transparent">
+		className="mb-3 overflow-hidden rounded border border-theme-secondary-300 bg-white last:mb-0 dark:border-theme-secondary-800 dark:bg-black sm:rounded-none sm:border-x-0 sm:border-b-0 sm:border-dashed sm:bg-transparent sm:pt-3 sm:dark:bg-transparent"
+	>
 		<div className="flex flex-col items-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
 			<div className="flex w-full min-w-0 flex-1 flex-col sm:w-auto sm:items-start sm:space-x-0 sm:space-y-1">
 				<div className="flex w-full flex-1 flex-row items-center justify-between bg-theme-secondary-100 px-4 py-3 dark:bg-theme-secondary-900 sm:bg-transparent sm:p-0 dark:sm:bg-transparent">
