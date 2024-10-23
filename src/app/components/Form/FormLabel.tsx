647b77f78d0d464b8bd14039a2c8574c81aa88bd
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-10-23 18:22:29 -0500

    refactor: custom class names for form label

--- src/app/components/Form/FormLabel.tsx
+++ src/app/components/Form/FormLabel.tsx
@@ -3,16 +3,17 @@ import { useTranslation } from "react-i18next";
 
 import { useFormField } from "./useFormField";
 import { Tooltip } from "@/app/components/Tooltip";
+import cn from 'classnames';
 
 type FormLabelProperties = {
 	label?: string;
 	optional?: boolean;
+	className?: string;
 } & React.LabelHTMLAttributes<any>;
 
 export function FormLabel(properties: FormLabelProperties) {
 	const fieldContext = useFormField();
-
-	const labelProperties = { ...properties };
+    const { className, ...labelProperties } = properties;
 
 	for (const property of ["label", "optional"]) {
 		// @ts-ignore
@@ -24,7 +25,7 @@ export function FormLabel(properties: FormLabelProperties) {
 	return (
 		<label
 			data-testid="FormLabel"
-			className="FormLabel mb-2 flex text-sm font-semibold leading-[17px] text-theme-secondary-text transition-colors duration-100"
+			className={cn("FormLabel mb-2 flex text-sm font-semibold leading-[17px] text-theme-secondary-text transition-colors duration-100", className)}
 			htmlFor={fieldContext?.name}
 			{...labelProperties}
 		>
