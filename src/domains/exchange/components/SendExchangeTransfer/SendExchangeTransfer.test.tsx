30ab9fe80cf3818970df74cd721f7250151567c0
goga-m <goga.mamoulasvili@gmail.com>
2024-10-25 14:57:17 +0200

    wip

--- src/domains/exchange/components/SendExchangeTransfer/SendExchangeTransfer.test.tsx
+++ src/domains/exchange/components/SendExchangeTransfer/SendExchangeTransfer.test.tsx
@@ -7,6 +7,7 @@ import {
 	getDefaultProfileId,
 	getDefaultWalletMnemonic,
 	mockNanoXTransport,
+	mockLedgerTransportError,
 	render,
 	screen,
 	syncFees,
@@ -277,15 +278,6 @@ describe("SendExchangeTransfer", () => {
 			"0335a27397927bfa1704116814474d39c2b933aabb990e7226389f022886e48deb",
 		);
 
-		vi.spyOn(wallet.transaction(), "signTransfer").mockReturnValue(Promise.resolve(transactionFixture.data.id));
-
-		vi.spyOn(wallet.coin().ledger(), "scan").mockImplementation(({ onProgress }) => {
-			onProgress(wallet);
-			return {
-				"m/44'/1'/0'/0/0": wallet.toData(),
-			};
-		});
-
 		mockNanoXTransport();
 
 		createTransactionMock(wallet);
@@ -301,12 +293,31 @@ describe("SendExchangeTransfer", () => {
 		await selectSender();
 	});
 
+	it("should show error if unable to detect ledger device", async () => {
+		const { result } = renderHook(() => useTranslation());
+		const { t } = result.current;
+
+		vi.spyOn(wallet, "isLedger").mockImplementation(() => true);
+
+		const ledgerErrorMock = mockLedgerTransportError("Access denied to use Ledger device");
+		vi.spyOn(profile.wallets(), "findByCoinWithNetwork").mockReturnValue([wallet])
+
+		renderComponent();
+
+		await expect(
+			screen.findByText(
+				t("WALLETS.MODAL_LEDGER_WALLET.DEVICE_NOT_AVAILABLE"),
+			),
+		).resolves.toBeVisible();
+
+		ledgerErrorMock.mockRestore();
+	});
+
 	it("should show browser compatibility error ledger is not supported", async () => {
 		const { result } = renderHook(() => useTranslation());
 		const { t } = result.current;
 
 		vi.spyOn(wallet, "isLedger").mockImplementation(() => true);
-		vi.spyOn(wallet.coin(), "__construct").mockImplementation(vi.fn());
 		vi.spyOn(wallet.ledger(), "isNanoX").mockResolvedValue(true);
 
 		vi.spyOn(wallet.coin().ledger(), "getPublicKey").mockResolvedValue(
@@ -325,23 +336,16 @@ describe("SendExchangeTransfer", () => {
 		vi.spyOn(profile.wallets(), "findByCoinWithNetwork").mockReturnValue([wallet])
 		mockNanoXTransport();
 
-
-		//
-		vi.spyOn(wallet.transaction(), "broadcast").mockResolvedValue({
-			accepted: [transactionFixture.data.id],
-			errors: {},
-			rejected: [],
-		});
-
-
 		process.env.REACT_APP_IS_UNIT = null
 		window.navigator.usb = undefined
 
 		renderComponent();
+
 		await expect(
 			screen.findByText(
 				t("WALLETS.MODAL_LEDGER_WALLET.COMPATIBILITY_ERROR"),
 			),
 		).resolves.toBeVisible();
 	});
+
 });
