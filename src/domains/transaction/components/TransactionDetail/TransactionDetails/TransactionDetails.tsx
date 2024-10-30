252f9e5d8cb74283bde4f371b0076ac718b09546
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-10-30 16:01:07 +0000

    style: resolve style guide violations

--- src/domains/transaction/components/TransactionDetail/TransactionDetails/TransactionDetails.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionDetails/TransactionDetails.tsx
@@ -25,15 +25,13 @@ export const TransactionDetails = ({
 	const nonce = useCallback(() => {
 		try {
 			const data = transaction.data().data;
-			const nonceValue = typeof data === "function" 
-				? transaction.data().data().nonce 
-				: data?.nonce;
-			
+			const nonceValue = typeof data === "function" ? transaction.data().data().nonce : data?.nonce;
+
 			return typeof nonceValue === "string" ? nonceValue : "";
 		} catch {
 			return "";
 		}
-	}, [transaction]);	
+	}, [transaction]);
 
 	return (
 		<DetailWrapper label={t("TRANSACTION.TRANSACTION_DETAILS")}>
