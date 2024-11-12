cbc3946969bb71bbc871f378efaf55cdd2617c1d
Shahin Safaraliyev <shahin@ardenthq.com>
2024-11-12 20:43:33 +0400

    wip

--- src/domains/transaction/components/TransactionDetail/TransactionDetails/TransactionDetails.test.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionDetails/TransactionDetails.test.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { screen, renderResponsive, render } from "@/utils/testing-library";
+import { screen, renderResponsive, render, env, getDefaultProfileId } from "@/utils/testing-library";
 import { TransactionDetails } from "./TransactionDetails";
 import { TransactionFixture } from "@/tests/fixtures/transactions";
+import { Contracts } from "@ardenthq/sdk-profiles";
 
 describe("TransactionDetails", () => {
+	let wallet: Contracts.IReadWriteWallet;
+
+	beforeEach(async () => {
+		const profile = env.profiles().findById(getDefaultProfileId());
+		wallet = profile.wallets().first();
+	});
+
 	it.each(["sm", "md", "lg"])("should render in %s", (breakpoint: string) => {
-		renderResponsive(<TransactionDetails transaction={{ ...TransactionFixture }} />, breakpoint);
+		renderResponsive(<TransactionDetails transaction={{ ...TransactionFixture, wallet: () => wallet }} />, breakpoint);
 
 		expect(screen.getAllByTestId("DetailLabelText")).toHaveLength(4);
 	});
