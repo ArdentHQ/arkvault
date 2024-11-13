449f343718462d92e8e9a511563a3794cb5a4130
patricio0312rev <patricio0312rev@users.noreply.github.com>
2024-11-13 16:13:24 +0000

    style: resolve style guide violations

--- src/app/components/Accordion/Accordion.test.tsx
+++ src/app/components/Accordion/Accordion.test.tsx
@@ -98,9 +98,9 @@ describe("Button", () => {
 
 	it("should toggle the accordion on click", async () => {
 		const Accordion = () => {
-			useAccordion('accordion');
+			useAccordion("accordion");
 
-			const { isExpanded, handleHeaderClick } = useAccordion('accordion');
+			const { isExpanded, handleHeaderClick } = useAccordion("accordion");
 
 			return (
 				<AccordionWrapper>
