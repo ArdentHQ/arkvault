c2eb0b9d11576f6f920f19900b39d6ec7b569a01
shahin-hq <shahin-hq@users.noreply.github.com>
2024-11-15 12:39:35 +0000

    style: resolve style guide violations

--- src/domains/transaction/components/FilterTransactions/FilterTransactions.tsx
+++ src/domains/transaction/components/FilterTransactions/FilterTransactions.tsx
@@ -30,7 +30,7 @@ const FilterOption = ({
 }) => (
 	<span
 		data-testid="FilterOption"
-		className={classNames("-my-4 -ml-9 -mr-8 flex w-screen items-center space-x-2 px-4 py-3 group transition-all", {
+		className={classNames("group -my-4 -ml-9 -mr-8 flex w-screen items-center space-x-2 px-4 py-3 transition-all", {
 			"border-b border-theme-secondary-300 dark:border-theme-secondary-600": hasDivider,
 			"font-normal hover:text-theme-secondary-900 dark:hover:text-theme-secondary-200": !isChecked,
 			"font-semibold hover:text-theme-navy-600": isChecked,
@@ -43,7 +43,7 @@ const FilterOption = ({
 			data-testid={`FilterOption__checkbox`}
 			className={classNames("transition-all", {
 				"group-hover:bg-theme-navy-700": isChecked,
-				"group-hover:border-theme-navy-600 dark:group-hover:border-theme-navy-600": !isChecked
+				"group-hover:border-theme-navy-600 dark:group-hover:border-theme-navy-600": !isChecked,
 			})}
 		/>
 		<span data-testid={`FilterOption__${label}`}>{label}</span>
