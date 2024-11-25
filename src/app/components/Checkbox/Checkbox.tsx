632fd8aa291ff5cc289da22f4e8103d7585a2491
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-25 18:01:25 -0500

    fix: checkbox styles

--- src/app/components/Checkbox/Checkbox.tsx
+++ src/app/components/Checkbox/Checkbox.tsx
@@ -7,12 +7,12 @@ type CheckboxProperties = {
     color?: Color;
 } & React.InputHTMLAttributes<HTMLInputElement> & React.RefAttributes<HTMLInputElement>;
 
-export const Checkbox =	forwardRef<HTMLInputElement, CheckboxProperties>(({ color = "success", ...props }, ref) => <input type="checkbox" ref={ref} className={twMerge("w-5 h-5 bg-transparent !rounded cursor-pointer !border-2 transition duration-150 ease-in-out focus:!ring-offset-0 focus:!ring-theme-primary-400 disabled:bg-theme-secondary-200 disabled:border-theme-secondary-300 disabled:cursor-not-allowed disabled:dark:bg-theme-secondary-800 disabled:dark:border-theme-secondary-600 [:not(:checked)]:border-theme-secondary-300 [:not(:checked)]:dark:border-theme-secondary-600 custom-checkbox", cn({
-		"text-theme-danger-400 hover:border-theme-danger-400 checked:hover:text-theme-danger-500": color === "danger",
-		"text-theme-hint-500 hover:border-theme-hint-500": color === "hint",
-		"text-theme-primary-600 hover:border-theme-primary-600": color === "info",
-		"text-theme-primary-600 hover:border-theme-primary-600 checked:hover:text-theme-primary-700": color === "success",
-		"text-theme-warning-600 hover:border-theme-warning-600": color === "warning",
+export const Checkbox =	forwardRef<HTMLInputElement, CheckboxProperties>(({ color = "success", ...props }, ref) => <input type="checkbox" ref={ref} className={twMerge("w-5 h-5 bg-transparent !rounded cursor-pointer !border-2 transition duration-150 ease-in-out focus:!ring-offset-0 focus:!ring-theme-primary-400 disabled:bg-theme-secondary-200 disabled:border-theme-secondary-300 disabled:cursor-not-allowed disabled:dark:bg-theme-secondary-800 disabled:dark:border-theme-secondary-600 [&:not(:checked)]:border-theme-secondary-300 [&:not(:checked)]:dark:border-theme-secondary-600 custom-checkbox", cn({
+		"text-theme-danger-400 [&:not(:checked)]:hover:border-theme-danger-400 checked:hover:text-theme-danger-500": color === "danger",
+		"text-theme-hint-500 [&:not(:checked)]:hover:border-theme-hint-500": color === "hint",
+		"text-theme-primary-600 [&:not(:checked)]:hover:border-theme-primary-600": color === "info",
+		"text-theme-primary-600 [&:not(:checked)]:hover:border-theme-primary-600 checked:hover:text-theme-primary-700": color === "success",
+		"text-theme-warning-600 [&:not(:checked)]:hover:border-theme-warning-600": color === "warning",
 	}) , props.className)} {...props} />)
 
 Checkbox.displayName = "Checkbox";
