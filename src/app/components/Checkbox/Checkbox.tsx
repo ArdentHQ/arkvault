24f0e86f154f12d18aefe489990ef50dc1bac7b1
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-25 17:46:51 -0500

    fix: checkbox styles

--- src/app/components/Checkbox/Checkbox.tsx
+++ src/app/components/Checkbox/Checkbox.tsx
@@ -8,7 +8,7 @@ type CheckboxProperties = {
 } & React.InputHTMLAttributes<any>;
 
 export const Checkbox = ({ color = "success", ...props }: CheckboxProperties) => {
-	return <input type="checkbox" className={twMerge("w-5 h-5 bg-transparent !rounded cursor-pointer border-2 transition duration-150 ease-in-out focus:ring-offset-0 focus:ring-theme-primary-400 disabled:bg-theme-secondary-200 disabled:border-theme-secondary-300 disabled:cursor-not-allowed disabled:dark:bg-theme-secondary-800 disabled:dark:border-theme-secondary-600 [:not(:checked)]:border-theme-secondary-300 [:not(:checked)]:dark:border-theme-secondary-600 custom-checkbox", cn({
+	return <input type="checkbox" className={twMerge("w-5 h-5 bg-transparent !rounded cursor-pointer !border-2 transition duration-150 ease-in-out focus:!ring-offset-0 focus:!ring-theme-primary-400 disabled:bg-theme-secondary-200 disabled:border-theme-secondary-300 disabled:cursor-not-allowed disabled:dark:bg-theme-secondary-800 disabled:dark:border-theme-secondary-600 [:not(:checked)]:border-theme-secondary-300 [:not(:checked)]:dark:border-theme-secondary-600 custom-checkbox", cn({
 		"text-theme-danger-400 hover:border-theme-danger-400 checked:hover:text-theme-danger-500": color === "danger",
 		"text-theme-hint-500 hover:border-theme-hint-500": color === "hint",
 		"text-theme-primary-600 hover:border-theme-primary-600": color === "info",
