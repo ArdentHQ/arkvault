4aab04ffed2be3fb53d24e1a9ada99746723745a
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-25 17:15:16 -0500

    fix: badge component

--- src/app/components/Badge/Badge.tsx
+++ src/app/components/Badge/Badge.tsx
@@ -25,25 +25,25 @@ export const Wrapper = forwardRef<HTMLSpanElement, BadgeStyleProperties>(
 	({ position, size, noShadow, ...props }, ref) => (
 		<span
 			ref={ref}
+			{...props}
 			className={twMerge(
 				"absolute bottom-0 right-0 h-5 w-5 translate-x-1/2 translate-y-1/2 transform",
 				cn({
 					"bottom-0 left-0 -translate-x-1/2 translate-y-1/2": position === "bottom-left",
 					"bottom-0 right-0 translate-x-1/2 translate-y-1/2": position === "bottom-right",
 					"bottom-1 translate-y-full": position === "bottom",
+					"h-2 w-2": size === "sm",
+					"h-6 w-6": size === "lg",
+					"left-0 top-0 -translate-x-1/2 -translate-y-1/2": position === "top-left",
 					"left-1 -translate-x-full": position === "left",
+					"right-0 top-0 -translate-y-1/2 translate-x-1/2": position === "top-right",
 					"right-1 translate-x-full": position === "right",
 					"shadow-[0_0_0_3px_theme-background]": size === "sm" && !noShadow,
 					"shadow-[0_0_0_5px_theme-background]": size !== "sm" && !noShadow,
-					"left-0 top-0 -translate-x-1/2 -translate-y-1/2": position === "top-left",
-					"right-0 top-0 -translate-y-1/2 translate-x-1/2": position === "top-right",
 					"top-1 -translate-y-full": position === "top",
-					"h-2 w-2": size === "sm",
-					"h-6 w-6": size === "lg",
 				}),
 				props.className,
 			)}
-			{...props}
 		/>
 	),
 );
