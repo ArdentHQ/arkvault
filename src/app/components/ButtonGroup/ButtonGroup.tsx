0ed0adbf33cb5e2fafe064f9dd4446f484ed48b0
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-25 17:15:23 -0500

    fix: button group component

--- src/app/components/ButtonGroup/ButtonGroup.tsx
+++ src/app/components/ButtonGroup/ButtonGroup.tsx
@@ -15,20 +15,19 @@ interface ButtonGroupOptionStyledProperties extends React.ButtonHTMLAttributes<H
 	variant: ButtonGroupOptionVariant;
 }
 
-const ButtonGroupOptionStyled = ({variant, ...props}: ButtonGroupOptionStyledProperties) => {
-	return (
-		<button className={
-			twMerge(
-				"flex items-center justify-center w-full h-full transition-colors duration-300 p-3 font-semibold text-theme-secondary-700 border-2 border-theme-primary-100 dark:border-theme-secondary-800 dark:text-theme-secondary-500 focus:outline-none focus:ring-2 focus:ring-theme-primary-400 disabled:border disabled:border-theme-secondary-300 disabled:text-theme-secondary-500 disabled:cursor-not-allowed disabled:dark:text-theme-secondary-700 disabled:dark:border-theme-secondary-700 hover:border-theme-primary-100 hover:text-theme-primary-700 hover:bg-theme-primary-100 dark:hover:border-theme-secondary-800 dark:hover:text-theme-secondary-200 dark:hover:bg-theme-secondary-800 checked:text-theme-primary-700 checked:border-theme-primary-600 checked:bg-theme-primary-50 dark:checked:bg-theme-primary-900 dark:checked:text-theme-primary-50 dark:checked:cursor-default",
-				cn({
-					"rounded": variant === "default",
-					"h-14 rounded-xl": variant === "modern",
-				}),
-				props.className
-			)
-		} {...props} />
-	)
-}
+const ButtonGroupOptionStyled = ({ variant, ...props }: ButtonGroupOptionStyledProperties) => (
+	<button
+		{...props}
+		className={twMerge(
+			"flex h-full w-full items-center justify-center border-2 border-theme-primary-100 p-3 font-semibold text-theme-secondary-700 transition-colors duration-300 checked:border-theme-primary-600 checked:bg-theme-primary-50 checked:text-theme-primary-700 hover:border-theme-primary-100 hover:bg-theme-primary-100 hover:text-theme-primary-700 focus:outline-none focus:ring-2 focus:ring-theme-primary-400 disabled:cursor-not-allowed disabled:border disabled:border-theme-secondary-300 disabled:text-theme-secondary-500 dark:border-theme-secondary-800 dark:text-theme-secondary-500 dark:checked:cursor-default dark:checked:bg-theme-primary-900 dark:checked:text-theme-primary-50 dark:hover:border-theme-secondary-800 dark:hover:bg-theme-secondary-800 dark:hover:text-theme-secondary-200 disabled:dark:border-theme-secondary-700 disabled:dark:text-theme-secondary-700",
+			cn({
+				"h-14 rounded-xl": variant === "modern",
+				rounded: variant === "default",
+			}),
+			props.className,
+		)}
+	/>
+);
 
 interface ButtonGroupOptionProperties {
 	children: React.ReactNode;
