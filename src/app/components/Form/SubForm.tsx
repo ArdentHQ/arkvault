07485378a4242583c38302add291f057c6e2fc33
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-25 20:44:11 -0500

    refactor: sub form component

--- src/app/components/Form/SubForm.tsx
+++ src/app/components/Form/SubForm.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import tw, { css, styled } from "twin.macro";
+import { twMerge } from "tailwind-merge";
+import cn from 'classnames';
 
-const SubFormWrapper = styled.div<{ noBackground?: boolean; noPadding?: boolean }>`
-	${tw`space-y-4 rounded-lg`};
-	${({ noPadding }) => !noPadding && tw`p-4 -mx-4`};
-	${({ noBackground }) =>
-		!noBackground &&
-		css`
-			& {
-				${tw`bg-theme-secondary-background`}
-			}
-		`};
-`;
+const SubFormWrapper = ({noPadding, noBackground, ...props}: React.HTMLAttributes<HTMLDivElement> & { noBackground?: boolean; noPadding?: boolean }) => (
+		<div
+			{...props}
+			className={twMerge("space-y-4 rounded-lg", cn({
+				"bg-theme-secondary-background": !noBackground,
+				"p-4 -mx-4": !noPadding,
+			}),props.className)}
+		/>
+	)
 
 export const SubForm = ({
 	className,
