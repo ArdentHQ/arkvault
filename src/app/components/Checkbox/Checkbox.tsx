10f0f73993da26831e4a40c2635d1d95cca7ed87
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-26 10:29:46 -0500

    fix: checkbox component

--- src/app/components/Checkbox/Checkbox.tsx
+++ src/app/components/Checkbox/Checkbox.tsx
@@ -7,7 +7,7 @@ type CheckboxProperties = {
     color?: Color;
 } & React.InputHTMLAttributes<HTMLInputElement> & React.RefAttributes<HTMLInputElement>;
 
-export const Checkbox =	forwardRef<HTMLInputElement, CheckboxProperties>(({ color = "success", ...props }, ref) => <input type="checkbox" ref={ref} className={twMerge("!w-5 !h-5 bg-transparent !rounded cursor-pointer !border-2 transition duration-150 ease-in-out focus:!ring-offset-0 focus:!ring-theme-primary-400 disabled:bg-theme-secondary-200 disabled:border-theme-secondary-300 disabled:cursor-not-allowed disabled:dark:bg-theme-secondary-800 disabled:dark:border-theme-secondary-600 [&:not(:checked)]:border-theme-secondary-300 [&:not(:checked)]:dark:border-theme-secondary-600 custom-checkbox", cn({
+export const Checkbox =	forwardRef<HTMLInputElement, CheckboxProperties>(({ color = "success", ...props }, ref) => <input type="checkbox" ref={ref} className={twMerge("!w-5 !h-5 !rounded cursor-pointer !border-2 transition duration-150 ease-in-out focus:!ring-offset-0 focus:!ring-theme-primary-400 disabled:bg-theme-secondary-200 disabled:border-theme-secondary-300 disabled:cursor-not-allowed disabled:dark:bg-theme-secondary-800 disabled:dark:border-theme-secondary-600 [&:not(:checked)]:!bg-transparent [&:not(:checked)]:border-theme-secondary-300 [&:not(:checked)]:dark:border-theme-secondary-600 custom-checkbox", cn({
 		"checked:text-theme-danger-400 [&:not(:checked)]:hover:border-theme-danger-400 checked:hover:text-theme-danger-500": color === "danger",
 		"checked:text-theme-hint-500 [&:not(:checked)]:hover:border-theme-hint-500": color === "hint",
 		"checked:text-theme-primary-600 [&:not(:checked)]:hover:border-theme-primary-600": color === "info",
