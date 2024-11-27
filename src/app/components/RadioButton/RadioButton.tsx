c0bb5621c0b271ba06a2a2610f8c03f40293522c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-11-27 07:40:36 -0500

    refactor: radio input

--- src/app/components/RadioButton/RadioButton.tsx
+++ src/app/components/RadioButton/RadioButton.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { styled } from "twin.macro";
-
-import { getStyles } from "./RadioButton.styles";
 import { Color } from "@/types";
+import { twMerge } from "tailwind-merge";
+import cn from 'classnames';
 
 type RadioButtonProperties = {
 	color?: Color;
 } & React.InputHTMLAttributes<any>;
 
-export const RadioButton = styled.input<RadioButtonProperties>(getStyles);
-
-RadioButton.defaultProps = {
-	color: "success",
-	type: "radio",
-};
+export const RadioButton = ({ color = "success", ...props }: RadioButtonProperties & React.InputHTMLAttributes<any>) => {
+	return (
+		<input {...props} type="radio" className={twMerge("w-4 h-4 transition duration-150 ease-in-out cursor-pointer focus:ring-offset-0 border-theme-secondary-300", cn({
+			"text-theme-danger-400": color === "danger",
+			"text-theme-hint-500": color === "hint",
+			"text-theme-primary-600": color === "info",
+			"text-theme-success-600": color === "success",
+			"text-theme-warning-600": color === "warning",
+		}), props.className)} />
+	)
+}
