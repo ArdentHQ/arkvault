d02236b103e1b0ad5d6514e1384115ca0224194e
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-12-02 12:17:31 -0500

    refactor: settings group component

--- src/domains/setting/pages/General/General.blocks.tsx
+++ src/domains/setting/pages/General/General.blocks.tsx
@@ -1,6 +1,12 @@
-import tw, { styled } from "twin.macro";
+import React from "react";
+import { twMerge } from "tailwind-merge";
 
-export const SettingsGroup = styled.div(
-	() =>
-		tw`relative -mx-8 mt-8 pt-6 sm:pt-0 px-8 border-t sm:border-0 border-theme-secondary-300 dark:border-theme-secondary-800`,
+export const SettingsGroup = ({ ...props }: React.HTMLProps<HTMLDivElement>) => (
+	<div
+		{...props}
+		className={twMerge(
+			"relative -mx-8 mt-8 border-t border-theme-secondary-300 px-8 pt-6 dark:border-theme-secondary-800 sm:border-0 sm:pt-0",
+			props.className,
+		)}
+	/>
 );
