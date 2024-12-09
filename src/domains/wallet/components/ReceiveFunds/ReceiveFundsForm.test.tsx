5adae2190ce1c604974be3d28c0ddbcced3efafa
goga-m <goga.mamoulasvili@gmail.com>
2024-12-09 10:57:53 +0100

    wip

--- src/domains/wallet/components/ReceiveFunds/ReceiveFundsForm.test.tsx
+++ src/domains/wallet/components/ReceiveFunds/ReceiveFundsForm.test.tsx
@@ -19,7 +19,6 @@ describe("ReceiveFundsForm", () => {
 		const { asFragment } = renderWithForm(<ReceiveFundsForm network={network} />);
 
 		await waitFor(() => expect(screen.getByTestId("ReceiveFundsForm__amount")).not.toHaveValue());
-		await waitFor(() => expect(screen.getByTestId("ReceiveFundsForm__memo")).not.toHaveValue());
 
 		expect(asFragment()).toMatchSnapshot();
 	});
@@ -35,33 +34,5 @@ describe("ReceiveFundsForm", () => {
 		await waitFor(() => expect(form()?.getValues("amount")).toBe("10"));
 
 		expect(asFragment()).toMatchSnapshot();
-	});
-
-	it("should emit memo onChange event", async () => {
-		const { asFragment, form } = renderWithForm(<ReceiveFundsForm network={network} />);
-		await waitFor(() => expect(screen.getByTestId("ReceiveFundsForm__memo")).not.toHaveValue());
-
-		await userEvent.clear(screen.getByTestId("ReceiveFundsForm__memo"));
-		await userEvent.type(screen.getByTestId("ReceiveFundsForm__memo"), "test");
-		await waitFor(() => expect(form()?.getValues("memo")).toBe("test"));
-		await waitFor(() => expect(screen.getByTestId("ReceiveFundsForm__memo")).toHaveValue("test"));
-
-		expect(asFragment()).toMatchSnapshot();
-	});
-
-	it("should not show memo if is not supported by network", async () => {
-		const memo = Array.from({ length: 256 }).fill("x").join("");
-
-		const memoMock = vi.spyOn(network, "usesMemo").mockReturnValue(false);
-
-		const { asFragment } = renderWithForm(<ReceiveFundsForm network={network} />, {
-			defaultValues: { memo },
-		});
-
-		expect(screen.queryByTestId("ReceiveFundsForm__memo")).not.toBeInTheDocument();
-
-		expect(asFragment()).toMatchSnapshot();
-
-		memoMock.mockRestore();
 	});
 });
