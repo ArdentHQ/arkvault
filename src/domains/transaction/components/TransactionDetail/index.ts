8e63fe0f3acdc3ef2375939ef02292f38614d0e2
Shahin Safaraliyev <shahin@ardenthq.com>
2024-12-10 19:53:52 +0400

    rm transaction vote components

--- src/domains/transaction/components/TransactionDetail/index.ts
+++ src/domains/transaction/components/TransactionDetail/index.ts
@@ -9,7 +9,6 @@ export * from "./TransactionSender";
 export * from "./TransactionStatus";
 export * from "./TransactionTimestamp";
 export * from "./TransactionType";
-export * from "./TransactionVotes";
 export * from "./TransactionAddresses";
 export * from "./TransactionDetails";
 export * from "./TransactionSummary";
