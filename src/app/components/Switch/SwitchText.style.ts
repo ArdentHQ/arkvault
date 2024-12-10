e00df77f7efa857f5d8bd0694afe43f8017d366f
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2024-12-09 23:49:34 -0500

    refactor: switch input

--- src/app/components/Switch/SwitchText.style.ts
+++ src/app/components/Switch/SwitchText.style.ts
@@ -1,26 +1,24 @@
-import tw from "twin.macro";
-
 import { Size } from "@/types";
 
-const baseStyle = tw`font-semibold select-none text-theme-secondary-500 dark:text-theme-secondary-700`;
+const baseStyle = 'font-semibold select-none text-theme-secondary-500 dark:text-theme-secondary-700';
 
 const getSelected = (selected: boolean) => {
 	if (selected) {
-		return tw`text-theme-secondary-700 dark:text-theme-secondary-200`;
+		return 'text-theme-secondary-700 dark:text-theme-secondary-200';
 	}
 };
 
 const getVariant = (disabled: boolean) => {
 	if (disabled) {
-		return tw`cursor-default`;
+		return 'cursor-default';
 	}
 };
 
 const getSize = (size?: Size) => {
 	const sizes = {
-		default: () => tw`text-base first:mr-3 last:ml-3`,
-		lg: () => tw`text-lg first:mr-4 last:ml-4`,
-		sm: () => tw`text-sm first:mr-2 last:ml-2`,
+		default: () => 'text-base first:mr-3 last:ml-3',
+		lg: () => 'text-lg first:mr-4 last:ml-4',
+		sm: () => 'text-sm first:mr-2 last:ml-2',
 	};
 
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
@@ -32,9 +30,9 @@ export interface SwitchTextType {
 	selected?: boolean;
 	disabled?: boolean;
 }
-export const getSwitchTextStyles = ({ size, selected, disabled }: SwitchTextType) => [
+export const getSwitchTextStyles = ({ size, selected, disabled }: SwitchTextType): string[] => [
 	baseStyle,
 	getSize(size),
-	getSelected(selected!),
-	getVariant(disabled!),
+	getSelected(selected!) || '',
+	getVariant(disabled!) || '',
 ];
