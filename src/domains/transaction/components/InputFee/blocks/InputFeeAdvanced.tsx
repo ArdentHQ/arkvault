da34fa5c1fa8ac09fc49169d5d03eb4a000793be
Shahin Safaraliyev <shahin@ardenthq.com>
2025-01-08 18:10:44 +0400

    refactor: implement `gasPrice` and `gasLimit`

--- src/domains/transaction/components/InputFee/blocks/InputFeeAdvanced.tsx
+++ src/domains/transaction/components/InputFee/blocks/InputFeeAdvanced.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 
 import { InputFeeAdvancedAddon } from "./InputFeeAdvancedAddon";
 import { useFormField } from "@/app/components/Form/useFormField";
