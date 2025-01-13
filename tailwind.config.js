b802a9fce3562fed768dee2aa8ce32304985d4bf
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-01-13 16:11:59 -0300

    feat: new md-lg breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
 			xs: "375px",
 			sm: "640px",
 			md: "768px",
+			"md-lg": "960px",
 			lg: "1024px",
 			xl: "1280px",
 			"not-all-hover-none": { raw: "not all and (hover: none)" },
