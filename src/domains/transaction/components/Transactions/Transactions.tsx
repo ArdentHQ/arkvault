3f65962600a678a3a2b258b3be065fd2356fec13
goga-m <goga.mamoulasvili@gmail.com>
2025-01-16 13:35:10 +0100

    wip

--- src/domains/transaction/components/Transactions/Transactions.tsx
+++ src/domains/transaction/components/Transactions/Transactions.tsx
@@ -263,7 +263,6 @@ export const Transactions = memo(function Transactions({
 						skeletonRowsLimit={8}
 						onRowClick={setTransactionModalItem}
 						profile={profile}
-						coinName={wallets.at(0)?.currency()}
 					/>
 				)}
 
