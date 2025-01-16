af7044029c248fbb99464d0fc9ecac64ad0ff1a4
goga-m <goga.mamoulasvili@gmail.com>
2025-01-16 16:51:29 +0100

    Revert "refactor: redirect to first wallet while entering dashboard"
    This reverts commit 15e16979017540d3472fab65928d64237e89e3a6.

--- src/domains/wallet/middleware.ts
+++ src/domains/wallet/middleware.ts
@@ -3,31 +3,26 @@ import { generatePath } from "react-router";
 import { Middleware, MiddlewareParameters } from "@/router/router.types";
 import { ProfilePaths } from "@/router/paths";
 
-// TODO: Define and implement the criteria for selecting the initial active wallet
-// (e.g., profile settings, last used wallet, active wallet)
-const navigateToActiveWallet = async (env, profileId, history) => {
-	const profile = env.profiles().findById(profileId)
-
-	const password = profile.usesPassword() ? profile.password().get() : undefined;
-	await env.profiles().restore(profile, password)
-
-	const wallet = profile.wallets().first()
-
-	if (wallet) {
-		history.replace(generatePath(ProfilePaths.WalletDetails, { profileId, walletId: wallet.id() }));
-	}
-}
-
 export class WalletMiddleware implements Middleware {
-	handler({ location, env, history }: MiddlewareParameters): boolean {
-
-		const match = matchPath<{ profileId: string }>(location.pathname, {
-			path: ProfilePaths.Dashboard,
+	handler({ location, redirect, env }: MiddlewareParameters): boolean {
+		const match = matchPath<{ profileId: string; walletId: string }>(location.pathname, {
+			path: ProfilePaths.WalletDetails,
 		});
 
 		if (match) {
-			const { profileId } = match.params;
-			navigateToActiveWallet(env, profileId, history);
+			const { profileId, walletId } = match.params;
+
+			if (["create", "import"].includes(walletId)) {
+				return true;
+			}
+
+			try {
+				const wallet = env.profiles().findById(profileId).wallets().findById(walletId);
+				return !!wallet;
+			} catch {
+				redirect(generatePath(ProfilePaths.Dashboard, { profileId }));
+				return false;
+			}
 		}
 
 		return true;
