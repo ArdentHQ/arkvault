a238e3c5cbb4217077d9ed835048a65d793201c2
goga-m <goga.mamoulasvili@gmail.com>
2025-01-16 16:51:20 +0100

    Revert "style: resolve style guide violations"
    This reverts commit aa65e8a15b32dddb1aaef48766aea84cc0c55cd8.

--- src/domains/wallet/middleware.ts
+++ src/domains/wallet/middleware.ts
@@ -6,20 +6,21 @@ import { ProfilePaths } from "@/router/paths";
 // TODO: Define and implement the criteria for selecting the initial active wallet
 // (e.g., profile settings, last used wallet, active wallet)
 const navigateToActiveWallet = async (env, profileId, history) => {
-	const profile = env.profiles().findById(profileId);
+	const profile = env.profiles().findById(profileId)
 
 	const password = profile.usesPassword() ? profile.password().get() : undefined;
-	await env.profiles().restore(profile, password);
+	await env.profiles().restore(profile, password)
 
-	const wallet = profile.wallets().first();
+	const wallet = profile.wallets().first()
 
 	if (wallet) {
 		history.replace(generatePath(ProfilePaths.WalletDetails, { profileId, walletId: wallet.id() }));
 	}
-};
+}
 
 export class WalletMiddleware implements Middleware {
 	handler({ location, env, history }: MiddlewareParameters): boolean {
+
 		const match = matchPath<{ profileId: string }>(location.pathname, {
 			path: ProfilePaths.Dashboard,
 		});
