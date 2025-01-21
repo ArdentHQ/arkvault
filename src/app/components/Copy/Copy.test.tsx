81cd8082ef9b9e3f5150e8399ad06861e1808935
goga-m <goga.mamoulasvili@gmail.com>
2025-01-21 13:42:10 +0100

    wip

--- src/app/components/Copy/Copy.test.tsx
+++ src/app/components/Copy/Copy.test.tsx
@@ -12,23 +12,45 @@ describe("Copy", () => {
 	});
 
 	it("should change the icon when clicked", async () => {
-		render(<Copy copyData="test" icon={(isClicked) => isClicked ? <Icon name="Copy" data-testid="Copy__icon_success" /> : <Icon name="Copy" data-testid="Copy__icon" />} />);
+		render(
+			<Copy
+				copyData="test"
+				icon={(isClicked) =>
+					isClicked ? (
+						<Icon name="Copy" data-testid="Copy__icon_success" />
+					) : (
+						<Icon name="Copy" data-testid="Copy__icon" />
+					)
+				}
+			/>,
+		);
 		await userEvent.click(screen.getByTestId("Copy__icon"));
 
 		await waitFor(() => {
 			expect(screen.getByTestId("Copy__icon_success")).toBeInTheDocument();
-		})
+		});
 	});
 
 	it("should return to the original icon after 1 second", async () => {
 		vi.useFakeTimers({ shouldAdvanceTime: true });
 
-		render(<Copy copyData="test" icon={(isClicked) => isClicked ? <Icon name="Copy" data-testid="Copy__icon_success" /> : <Icon name="Copy" data-testid="Copy__icon" />} />);
+		render(
+			<Copy
+				copyData="test"
+				icon={(isClicked) =>
+					isClicked ? (
+						<Icon name="Copy" data-testid="Copy__icon_success" />
+					) : (
+						<Icon name="Copy" data-testid="Copy__icon" />
+					)
+				}
+			/>,
+		);
 		await userEvent.click(screen.getByTestId("Copy__icon"));
 
 		await waitFor(() => {
 			expect(screen.getByTestId("Copy__icon_success")).toBeInTheDocument();
-		})
+		});
 		act(() => {
 			vi.advanceTimersByTime(1000);
 		});
