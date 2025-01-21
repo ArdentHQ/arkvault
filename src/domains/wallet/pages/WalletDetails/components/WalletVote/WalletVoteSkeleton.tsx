061711a96f45bf3cfb970159593abfe980857e09
goga-m <goga.mamoulasvili@gmail.com>
2025-01-21 12:13:37 +0100

    wip

--- src/domains/wallet/pages/WalletDetails/components/WalletVote/WalletVoteSkeleton.tsx
+++ src/domains/wallet/pages/WalletDetails/components/WalletVote/WalletVoteSkeleton.tsx
@@ -7,20 +7,17 @@ export const WalletVoteSkeleton = () => (
 		data-testid="WalletVote__skeleton"
 		className="flex flex-col rounded-xl border border-theme-secondary-300 bg-theme-secondary-100 dark:border-theme-secondary-800 dark:bg-black md:border-0 md:bg-transparent md:dark:bg-transparent"
 	>
-		<div className="flex w-full items-center px-6 py-4 md:p-0">
-			<div className="mr-4 hidden md:block">
-				<Skeleton circle height={44} width={44} />
-			</div>
-
-			<div className="flex h-10 flex-col justify-between">
-				<Skeleton height={14} width={100} />
-				<Skeleton height={16} width={150} />
+		<div className="flex w-full items-center px-8 py-4 md:p-0 h-[1.688rem]">
+			<div className="flex space-x-2">
+				<Skeleton height={16} width={250} />
+				<Skeleton height={16} width={50} />
 			</div>
 
 			<div className="ml-auto">
-				<Skeleton height={44} width={100} />
+				<Skeleton height={22} width={62} />
 			</div>
 		</div>
+
 		<div className="flex justify-center border-t border-theme-secondary-300 px-6 py-4 dark:border-theme-secondary-800 md:hidden">
 			<div className="flex h-10 flex-col items-center justify-between">
 				<Skeleton height={16} width={150} />
