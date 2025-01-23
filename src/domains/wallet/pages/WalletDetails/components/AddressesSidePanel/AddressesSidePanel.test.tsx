3da4239cea46e07905f52526eecad0f049ff71de
shahin-hq <132887516+shahin-hq@users.noreply.github.com>
2025-01-23 20:19:35 +0400

    feat: implement addresses side panel (#900)

---
+++ src/domains/wallet/pages/WalletDetails/components/AddressesSidePanel/AddressesSidePanel.test.tsx
@@ -0,0 +1,315 @@
+import React from "react";
+import { Contracts } from "@ardenthq/sdk-profiles";
+import { env, getMainsailProfileId, render, screen } from "@/utils/testing-library";
+import { expect } from "vitest";
+import { AddressesSidePanel } from "./AddressesSidePanel";
+import userEvent from "@testing-library/user-event";
+import { waitFor } from "@testing-library/react";
+
+describe("AddressesSidePanel", () => {
+	let profile: Contracts.IProfile;
+	let wallets: Contracts.IWalletRepository;
+
+	beforeAll(async () => {
+		profile = env.profiles().findById(getMainsailProfileId());
+
+		await env.profiles().restore(profile);
+
+		await profile.sync();
+
+		wallets = profile.wallets();
+	});
+
+	it("should render", () => {
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		expect(screen.getByTestId("AddressesSidePanel")).toBeInTheDocument();
+		expect(screen.getAllByTestId("AddressRow").length).toBe(wallets.count());
+	});
+
+	it("should select an address when AddressRow is clicked", async () => {
+		const onSelectedAddressChange = vi.fn();
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={onSelectedAddressChange}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.click(screen.getAllByTestId("AddressRow")[0]);
+
+		expect(onSelectedAddressChange).toHaveBeenCalledWith([wallets.first().address()]);
+	});
+
+	it("should deselect an address when AddressRow is clicked", async () => {
+		const onSelectedAddressChange = vi.fn();
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[wallets.first().address()]}
+				open={true}
+				onSelectedAddressesChange={onSelectedAddressChange}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.click(screen.getAllByTestId("AddressRow")[0]);
+
+		expect(onSelectedAddressChange).toHaveBeenCalledWith([]);
+	});
+
+	it("should select all displayed addresses when `select all` clicked", async () => {
+		const onSelectedAddressChange = vi.fn();
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={onSelectedAddressChange}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.click(screen.getByTestId("SelectAllAddresses"));
+
+		expect(onSelectedAddressChange).toHaveBeenCalledWith(wallets.values().map((w) => w.address()));
+	});
+
+	it("should deselect displayed addresses if all already selected when `select all` clicked", async () => {
+		const onSelectedAddressChange = vi.fn();
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={wallets.values().map((w) => w.address())}
+				open={true}
+				onSelectedAddressesChange={onSelectedAddressChange}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.click(screen.getByTestId("SelectAllAddresses"));
+
+		expect(onSelectedAddressChange).toHaveBeenCalledWith([]);
+	});
+
+	it("should show delete buttons when `manage` clicked", async () => {
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.click(screen.getByTestId("ManageAddresses"));
+
+		expect(screen.getByTestId("CancelDelete")).toBeInTheDocument();
+		expect(screen.getByTestId("ConfirmDelete")).toBeInTheDocument();
+	});
+
+	it("should disable `select all` when delete mode enabled", async () => {
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.click(screen.getByTestId("ManageAddresses"));
+
+		expect(screen.getByTestId("SelectAllAddresses_Checkbox")).toBeDisabled();
+	});
+
+	it("should delete an address when `done` clicked", async () => {
+		const onDelete = vi.fn();
+		const onSelectedAddressesChange = vi.fn();
+
+		const firstWallet = wallets.first();
+		const addressesExcludingFirstWallet = wallets
+			.values()
+			.slice(1)
+			.map((wallet) => wallet.address());
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[firstWallet.address(), ...addressesExcludingFirstWallet]}
+				open={true}
+				onSelectedAddressesChange={onSelectedAddressesChange}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={onDelete}
+			/>,
+		);
+
+		// go into delete mode
+		await userEvent.click(screen.getByTestId("ManageAddresses"));
+
+		// click on the delete button
+		await userEvent.click(screen.getByTestId(`AddressRow--delete-${wallets.first().address()}`));
+
+		// confirm address deletion
+		await userEvent.click(screen.getByTestId("ConfirmDelete"));
+
+		expect(onDelete).toHaveBeenCalledWith(wallets.first().address());
+
+		expect(onSelectedAddressesChange).toHaveBeenCalledWith([...addressesExcludingFirstWallet]);
+
+		// should reset back to select mode
+		expect(screen.getByTestId("ManageAddresses")).toBeInTheDocument();
+	});
+
+	it("should reset delete state when `cancel` clicked", async () => {
+		const onDelete = vi.fn();
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={onDelete}
+			/>,
+		);
+
+		// go into delete mode
+		await userEvent.click(screen.getByTestId("ManageAddresses"));
+
+		// click on the delete button
+		await userEvent.click(screen.getByTestId(`AddressRow--delete-${wallets.first().address()}`));
+
+		await userEvent.click(screen.getByTestId("CancelDelete"));
+
+		// should reset back to select mode
+		expect(screen.getByTestId("ManageAddresses")).toBeInTheDocument();
+
+		expect(onDelete).not.toHaveBeenCalled();
+	});
+
+	it("should filter wallets by address", async () => {
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.type(screen.getByTestId("AddressesPanel--SearchInput"), wallets.first().address());
+
+		expect(screen.getAllByTestId("AddressRow").length).toBe(1);
+	});
+
+	it("should filter wallets by displayName", async () => {
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await userEvent.type(screen.getByTestId("AddressesPanel--SearchInput"), "WALLET 2");
+
+		expect(screen.getAllByTestId("AddressRow").length).toBe(1);
+	});
+
+	it("should show a hint for `manage` button", async () => {
+		const getItemSpy = vi.spyOn(Storage.prototype, "getItem").mockReturnValue(undefined);
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await expect(screen.findByText(/You can manage and remove your addresses here./)).resolves.toBeVisible();
+
+		getItemSpy.mockRestore();
+	});
+
+	it("should not show a hint for `manage` button if already shown", async () => {
+		const getItemSpy = vi.spyOn(Storage.prototype, "getItem").mockReturnValue("1");
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await waitFor(() => {
+			expect(screen.queryByText(/You can manage and remove your addresses here./)).not.toBeInTheDocument();
+		});
+
+		getItemSpy.mockRestore();
+	});
+
+	it("should persist state for shown `manage` button hint", async () => {
+		const getItemSpy = vi.spyOn(Storage.prototype, "getItem").mockReturnValue(undefined);
+
+		render(
+			<AddressesSidePanel
+				wallets={wallets}
+				selectedAddresses={[]}
+				open={true}
+				onSelectedAddressesChange={vi.fn()}
+				onOpenChange={vi.fn()}
+				onDeleteAddress={vi.fn()}
+			/>,
+		);
+
+		await expect(screen.findByText(/You can manage and remove your addresses here./)).resolves.toBeVisible();
+
+		const setItemSpy = vi.spyOn(Storage.prototype, "setItem");
+
+		await userEvent.click(screen.getByTestId("HideManageHint"));
+
+		expect(setItemSpy).toHaveBeenCalledWith("manage-hint", "true");
+
+		setItemSpy.mockRestore();
+		getItemSpy.mockRestore();
+	});
+});
