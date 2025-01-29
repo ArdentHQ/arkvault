5ae4ca695450ac3b9061dc9dcce7a6331cd88fd5
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-01-29 11:08:33 -0500

    test: transaction row addressing

--- src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowAddressing.test.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowAddressing.test.tsx
@@ -1,14 +1,14 @@
 import { Contracts } from "@ardenthq/sdk-profiles";
 import React from "react";
 import { env, getDefaultProfileId, render, screen } from "@/utils/testing-library";
-import { TransactionRowAddressing } from "./TransactionRowAddressing";
+import { TransactionRowAddressing, TransactionRowLabel } from "./TransactionRowAddressing";
 import { TransactionFixture } from "@/tests/fixtures/transactions";
 
 describe("TransactionRowAddressing", () => {
 	let profile: Contracts.IProfile;
 	const fixture = {
 		...TransactionFixture,
-		wallet: () => ({ ...TransactionFixture.wallet(), currency: () => "DARK" }),
+		wallet: () => ({ ...TransactionFixture.wallet(), coin: () => ({ link: () => ({ wallet: () => ({ address: () => "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib" }) }) }), currency: () => "DARK" }),
 	};
 
 	beforeAll(() => {
@@ -25,6 +25,7 @@ describe("TransactionRowAddressing", () => {
 		render(<TransactionRowAddressing transaction={voteFixture as any} profile={profile} />);
 
 		expect(screen.queryAllByTestId("TransactionRowAddressing__vote")).toHaveLength(1);
+		expect(screen.getByText("Contract")).toBeInTheDocument();
 
 		const unvoteFixture = { ...fixture, isUnvote: () => true, isVote: () => false };
 		render(<TransactionRowAddressing transaction={unvoteFixture as any} profile={profile} />);
@@ -36,28 +37,18 @@ describe("TransactionRowAddressing", () => {
 		const registrationFixture = { ...fixture, isDelegateRegistration: () => true, username: () => "test" };
 		render(<TransactionRowAddressing transaction={registrationFixture as any} profile={profile} />);
 
-		expect(screen.getByTestId("TransactionRowAddressing__validator_registration")).toBeInTheDocument();
+		expect(screen.getByTestId("TransactionRowAddressing__vote")).toBeInTheDocument();
 	});
 
 	it("should render resignation variant if transaction is validator resignation", () => {
 		const resignationFixture = {
 			...fixture,
 			isDelegateResignation: () => true,
-			wallet: () => ({ ...TransactionFixture.wallet(), username: () => "test" }),
+			wallet: () => ({ ...TransactionFixture.wallet(), coin: () => ({ link: () => ({ wallet: () => ({ address: () => "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib" }) }) }), username: () => "test" }),
 		};
 		render(<TransactionRowAddressing transaction={resignationFixture as any} profile={profile} />);
 
-		expect(screen.getByTestId("TransactionRowAddressing__validator_resignation")).toBeInTheDocument();
-	});
-
-	it("should render as to-contract if transaction is musig registration", () => {
-		const resignationFixture = {
-			...fixture,
-			isMultiSignatureRegistration: () => true,
-		};
-		render(<TransactionRowAddressing transaction={resignationFixture as any} profile={profile} />);
-
-		expect(screen.getByTestId("TransactionRowAddressing__musig_registration")).toBeInTheDocument();
+		expect(screen.getByTestId("TransactionRowAddressing__vote")).toBeInTheDocument();
 	});
 
 	it("should render multipayment variant", () => {
@@ -106,5 +97,62 @@ describe("TransactionRowAddressing", () => {
 		render(<TransactionRowAddressing transaction={fixture as any} profile={profile} />);
 
 		expect(screen.getByTestId("TransactionRowAddressing__address-container")).not.toHaveClass("w-30");
+	});
+
+	it("should render advanced sender variant if the props isAdvanced is true and variant is start", () => {
+		render(<TransactionRowAddressing transaction={fixture as any} profile={profile} isAdvanced={true} variant="sender" />);
+
+		expect(screen.getByTestId("TransactionRowAddressing__container_advanced_sender")).toBeInTheDocument();
+	});
+
+	it("should render the sender address if the transaction is sent", () => {
+		const sentFixture = { ...fixture, isSent: () => true, sender: () => "DMFzWa3nHt9T1ChXdMwFrBZRTfKMjDyNss" };
+		render(<TransactionRowAddressing transaction={sentFixture as any} profile={profile} isAdvanced={true} variant="sender" />);
+
+		expect(screen.getByTestId("TransactionRowAddressing__container_advanced_sender")).toHaveTextContent("DMFzWa3nHt9T1ChXdMwFrBZRTfKMjDyNss");
+	});
+
+	it("should render advanced recipient variant if the props isAdvanced is true and variant is recipient", () => {
+		render(<TransactionRowAddressing transaction={fixture as any} profile={profile} isAdvanced={true} variant="recipient" />);
+
+		expect(screen.getByTestId("TransactionRowAddressing__container_advanced_recipient")).toBeInTheDocument();
+	});
+
+	it("should render multipayment variant with return style if the variant is recipient and isAdvanced is true", () => {
+		render(<TransactionRowAddressing transaction={fixture as any} profile={profile} isAdvanced variant="recipient" />);
+
+		expect(screen.getByTestId("TransactionRowAddressing__label")).toHaveClass("bg-theme-secondary-200");
+		expect(screen.getByTestId("TransactionRowAddressing__label")).toHaveTextContent("To");
+	});
+
+	it("should render multipayment variant with default styles if isAdvanced is false", () => {
+		render(<TransactionRowAddressing transaction={fixture as any} profile={profile} isAdvanced={false}  />);
+
+		expect(screen.getByTestId("TransactionRowAddressing__label")).not.toHaveClass("bg-theme-secondary-200");
+		expect(screen.getByTestId("TransactionRowAddressing__label")).toHaveTextContent("To");
+	});
+
+	it("should set direction as 'return' when transaction isReturn is true", () => {
+		const returnFixture = { ...fixture, isReturn: () => true };
+		render(<TransactionRowAddressing transaction={returnFixture as any} profile={profile} />);
+	
+		expect(screen.getByTestId("TransactionRowAddressing__label")).toHaveTextContent("Return");
+	});
+	
+	it("should render vote advanced variant if transaction is a contract transaction and isAdvanced is true", () => {
+		const voteFixture = { ...fixture, isVote: () => true };
+		render(<TransactionRowAddressing transaction={voteFixture as any} profile={profile} isAdvanced={true} variant="recipient" />);
+
+		expect(screen.getByTestId("TransactionRowAddressing__vote_advanced_recipient")).toBeInTheDocument();
+		expect(screen.getByText("Contract")).toBeInTheDocument();
+	});
+});
+
+
+describe("TransactionRowLabel", () => {
+	it("should render prioritizing style prop over direction prop", () => {
+		render(<TransactionRowLabel direction="received" style="return" />);
+
+		expect(screen.getByTestId("TransactionRowAddressing__label")).toHaveClass("bg-theme-secondary-200");
 	});
 });
