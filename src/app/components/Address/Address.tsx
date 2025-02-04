5b0fc891cdf40ad36785403a1c8b194ec896704a
patricio0312rev <patricio0312rev@users.noreply.github.com>
2025-02-04 22:43:30 +0000

    style: resolve style guide violations

--- src/app/components/Address/Address.tsx
+++ src/app/components/Address/Address.tsx
@@ -87,7 +87,9 @@ export const Address = ({
 				/* istanbul ignore next -- @preserve */
 				return (
 					width -
-					(walletName && aliasReference.current ? aliasReference.current.getBoundingClientRect().width + 8 : 0) -
+					(walletName && aliasReference.current
+						? aliasReference.current.getBoundingClientRect().width + 8
+						: 0) -
 					(showCopyButton ? 22 : 0)
 				);
 			} else {
