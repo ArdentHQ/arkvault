01dec763dc247972696ac4563975c3d53b1435d8
goga-m <goga.mamoulasvili@gmail.com>
2025-02-04 17:35:47 +0100

    wip

--- src/app/components/Table/Mobile/MobileCard.tsx
+++ src/app/components/Table/Mobile/MobileCard.tsx
@@ -8,7 +8,7 @@ interface MobileCardProps extends HTMLAttributes<HTMLDivElement> {
 export const MobileCard: FC<MobileCardProps> = ({ className, children, ...props }) => (
 	<div
 		className={cn(
-			"w-full cursor-pointer overflow-hidden rounded border border-theme-secondary-300 bg-white dark:border-theme-secondary-800 dark:bg-theme-secondary-900",
+			"w-full cursor-pointer overflow-hidden rounded border border-theme-secondary-300 bg-white dark:border-theme-secondary-800 dark:bg-theme-kackground",
 			className,
 		)}
 		{...props}
