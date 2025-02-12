0c34e50b8446ffe492ca4d9e848c495aac430f17
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-02-12 04:40:17 -0500

    refactor: hide balance

--- src/app/components/NavigationBar/components/HideBalance/HideBalance.tsx
+++ src/app/components/NavigationBar/components/HideBalance/HideBalance.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useBalanceVisibilityContext } from "@/app/contexts/BalanceVisibility";
 import { Icon } from "@/app/components/Icon";
 import { twMerge } from "tailwind-merge";
 import { Button } from "@/app/components/Button";
+import { useBalanceVisibility } from "@/app/hooks/use-balance-visibility";
+import { Contracts } from "@ardenthq/sdk-profiles";
 
-export const HideBalance = ({ className }: { className?: string }) => {
-	const { hideBalance, setHideBalance } = useBalanceVisibilityContext();
+export const HideBalance = ({ profile, className }: { profile: Contracts.IProfile; className?: string }) => {
+	const { hideBalance, setHideBalance } = useBalanceVisibility({ profile });
 
 	return (
 		<div className={twMerge("m-0 flex items-center gap-2 space-x-0", className)}>
