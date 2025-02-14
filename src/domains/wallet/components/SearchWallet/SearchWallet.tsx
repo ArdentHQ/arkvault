e0fc005d74d47d86dc7ba20304812612f6418ea3
goga-m <goga.mamoulasvili@gmail.com>
2025-02-14 17:48:57 +0100

    wip

--- src/domains/wallet/components/SearchWallet/SearchWallet.tsx
+++ src/domains/wallet/components/SearchWallet/SearchWallet.tsx
@@ -51,7 +51,7 @@ const SearchWalletListItem = ({
 		}
 
 		return (
-			<Tooltip content={t("TRANSACTION.TRANSACTION_TYPE_NOT_AVAILABLE")}>
+			<Tooltip content={t("TRANSACTION.TRANSACTION_TYPE_NOT_AVAILABLE")} disabled={!disabled}>
 				<div>
 					<Button
 						data-testid={`SearchWalletListItem__select-${index}`}
