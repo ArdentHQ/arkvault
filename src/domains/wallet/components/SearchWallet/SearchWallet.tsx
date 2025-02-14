77b1fa0509dde0cc34033a48643eddb3c05fa135
goga-m <goga.mamoulasvili@gmail.com>
2025-02-14 17:39:12 +0100

    wip

--- src/domains/wallet/components/SearchWallet/SearchWallet.tsx
+++ src/domains/wallet/components/SearchWallet/SearchWallet.tsx
@@ -51,7 +51,7 @@ const SearchWalletListItem = ({
 		}
 
 		return (
-			<Tooltip content={isLedgerWalletCompatible(wallet) ? "" : t("COMMON.LEDGER_COMPATIBILITY_ERROR")}>
+			<Tooltip content={t("TRANSACTION.TRANSACTION_TYPE_NOT_AVAILABLE")} >
 				<div>
 					<Button
 						data-testid={`SearchWalletListItem__select-${index}`}
