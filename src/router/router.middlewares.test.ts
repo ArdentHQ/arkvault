8deb9a2175aadfb503a19d0d4f462ea847cc8334
Shahin Safaraliyev <shahin@ardenthq.com>
2025-02-17 15:07:17 +0400

    cleanup

--- src/router/router.middlewares.test.ts
+++ src/router/router.middlewares.test.ts
@@ -4,7 +4,7 @@ import { UrlValidationMiddleware } from "./UrlValidationMiddleware";
 
 describe("Router middlewares", () => {
 	it("should have PreloadMiddleware and WalletMiddleware", () => {
-		expect(middlewares).toHaveLength(3);
+		expect(middlewares).toHaveLength(2);
 		expect(middlewares[0]).toBeInstanceOf(UrlValidationMiddleware);
 		expect(middlewares[1]).toBeInstanceOf(PreloadMiddleware);
 	});
