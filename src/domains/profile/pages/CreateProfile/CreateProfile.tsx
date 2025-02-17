693dbabc6b06f1a30603c64e5797058756ffd2e0
goga-m <goga.mamoulasvili@gmail.com>
2025-02-17 11:43:52 +0100

    wip

--- src/domains/profile/pages/CreateProfile/CreateProfile.tsx
+++ src/domains/profile/pages/CreateProfile/CreateProfile.tsx
@@ -4,7 +4,7 @@ import { Trans, useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 
 import { Header } from "@/app/components/Header";
-import { Page, Section } from "@/app/components/Layout";
+import { Page } from "@/app/components/Layout";
 import { useEnvironmentContext } from "@/app/contexts";
 import { useLocaleCurrency, useTheme } from "@/app/hooks";
 
@@ -51,8 +51,8 @@ export const CreateProfile = () => {
 			navbarVariant="logo-only"
 			title={<Trans i18nKey="COMMON.APP_NAME" />}
 		>
-			<Section className="flex flex-1 flex-col sm:justify-center">
-				<div className="mx-auto max-w-lg" data-testid="CreateProfile">
+			<div className="flex h-page items-center" data-testid="CreateProfile">
+				<div className="mx-auto max-w-lg">
 					<Header
 						title={t("PROFILE.PAGE_CREATE_PROFILE.TITLE")}
 						titleClassName="text-lg leading-[21px] sm:text-2xl sm:leading-[29px]"
@@ -61,7 +61,7 @@ export const CreateProfile = () => {
 						className="block"
 					/>
 
-					<div className="mb-16 mt-4 rounded-lg border-theme-secondary-300 bg-theme-background pt-1 dark:border-theme-secondary-800 sm:mb-0 sm:border sm:p-6">
+					<div className="rounded-lg border-theme-secondary-300 bg-theme-background pt-1 dark:border-theme-secondary-800 sm:mb-0 sm:border sm:p-6">
 						<ProfileForm
 							defaultValues={{
 								currency: defaultCurrency,
@@ -74,7 +74,7 @@ export const CreateProfile = () => {
 						/>
 					</div>
 				</div>
-			</Section>
+			</div>
 		</Page>
 	);
 };
