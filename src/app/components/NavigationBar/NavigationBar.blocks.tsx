86111c39684b82c060d357b1d7af38a35c4f5cf9
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-02-18 07:59:34 -0500

    refactor: prefill sender on navigation bar

--- src/app/components/NavigationBar/NavigationBar.blocks.tsx
+++ src/app/components/NavigationBar/NavigationBar.blocks.tsx
@@ -304,7 +304,10 @@ export const NavigationBarFull: React.FC<NavigationBarFullProperties> = ({
 	const handleCloseReceiveFunds = useCallback(() => setSelectedWallet(undefined), [setSelectedWallet]);
 
 	const sendButtonClickHandler = useCallback(() => {
-		const sendTransferPath = `/profiles/${profile.id()}/send-transfer`;
+		let sendTransferPath = `/profiles/${profile.id()}/send-transfer`;
+		if (wallets.length === 1) {
+			sendTransferPath = `/profiles/${profile.id()}/wallets/${wallets[0].id()}/send-transfer`;
+		}
 
 		// add query param reset = 1 if already on send transfer page
 		/* istanbul ignore next: tested in e2e -- @preserve */
