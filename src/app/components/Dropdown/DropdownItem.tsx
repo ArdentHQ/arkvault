b535dff2b84a611fc7ac0fcd65205abd3c853aa7
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-02-19 21:10:39 -0600

    wip

--- src/app/components/Dropdown/DropdownItem.tsx
+++ src/app/components/Dropdown/DropdownItem.tsx
@@ -11,13 +11,14 @@ export const DropdownItem = ({
 	<li
 		{...props}
 		className={twMerge(
-			"my-1 flex items-center space-x-2 whitespace-nowrap border-l-4 px-6 py-4 text-left text-base font-semibold transition-colors-shadow duration-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-theme-primary-400",
+			"my-1 flex items-center space-x-2 whitespace-nowrap px-6 py-4 text-left text-base font-semibold transition-colors-shadow duration-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-theme-primary-400",
 			cn({
+				"border-l-4": variant !== "navbar",
 				"border-theme-primary-600 bg-theme-primary-50 text-theme-primary-600 dark:bg-black": isActive,
-				"cursor-pointer border-transparent text-theme-secondary-700 hover:bg-theme-secondary-200 hover:text-theme-secondary-900 dark:text-theme-secondary-200 hover:dark:bg-theme-secondary-900 hover:dark:text-theme-secondary-200":
+				"cursor-pointer border-transparent text-theme-secondary-700 hover:bg-theme-secondary-200 hover:text-theme-secondary-900 hover:dark:bg-theme-secondary-900":
 					!isActive,
-				"rounded-lg": !isActive && variant !== "navbar",
-				"sm:rounded-lg": !isActive && variant === "navbar",
+				"dark:text-theme-dark-200 sm:rounded-lg": !isActive && variant === "navbar",
+				"rounded-lg dark:text-theme-secondary-200": !isActive && variant !== "navbar",
 			}),
 			props.className,
 		)}
