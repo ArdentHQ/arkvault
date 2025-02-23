4b580a7b606f1f4c73d833c2f7bda5f6cd1eb9f4
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-02-23 13:00:45 -0500

    fix: adjust transaction row spacings

--- src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRow.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRow.tsx
@@ -55,7 +55,7 @@ export const TransactionRow = memo(
 			<TableRow onClick={onClick} className={twMerge("relative", className)} {...properties}>
 				<TableCell
 					variant="start"
-					innerClassName={cn("items-start pr-0 lg:pr-3 xl:min-h-11 xl:max-h-11 xl:pt-2.5", {
+					innerClassName={cn("items-start pr-0 lg:pr-3 xl:min-h-11 xl:max-h-11 xl:pt-2.5 lg:min-w-36", {
 						"min-h-14 my-1 pt-1": hideSender,
 						"min-h-[66px] py-1 my-0 md-lg:min-h-14": !hideSender,
 					})}
@@ -78,6 +78,7 @@ export const TransactionRow = memo(
 						"text-sm text-theme-secondary-900 dark:text-theme-secondary-200 font-semibold items-start my-1 min-h-11 xl:max-h-11 xl:pt-3",
 						{
 							"xl:min-w-32": !hideSender,
+							"lg:min-w-40": hideSender,
 						},
 					)}
 					data-testid="TransactionRow__timestamp"
@@ -138,7 +139,7 @@ export const TransactionRow = memo(
 						"hidden md-lg:table-cell": !hideSender,
 					})}
 					innerClassName={cn("space-x-4 items-start px-0 lg:px-3 xl:pt-3 xl:min-h-11", {
-						"min-h-16 my-1 py-2": !hideSender,
+						"min-h-16 my-1 py-2 lg:min-w-36": !hideSender,
 					})}
 				>
 					<TransactionRowAddressing
