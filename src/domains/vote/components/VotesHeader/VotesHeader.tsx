5a133a6acd68e7bfa14b2f1adfcd8c92a7e14cea
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-03-03 14:41:08 -0600

    votes filter adjustment

--- src/domains/vote/components/VotesHeader/VotesHeader.tsx
+++ src/domains/vote/components/VotesHeader/VotesHeader.tsx
@@ -1,16 +1,8 @@
-import cn from "classnames";
-import { Contracts } from "@ardenthq/sdk-profiles";
 import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
-import { Icon, ThemeIcon } from "@/app/components//Icon";
-import { ControlButton } from "@/app/components/ControlButton";
-import { Dropdown } from "@/app/components/Dropdown";
+import { ThemeIcon } from "@/app/components//Icon";
 import { PageHeader } from "@/app/components/Header";
-import { HeaderSearchBar } from "@/app/components/Header/HeaderSearchBar";
-import { FilterWallets } from "@/domains/dashboard/components/FilterWallets";
-import { FilterOption, VotesFilter } from "@/domains/vote/components/VotesFilter";
-import { Divider } from "@/app/components/Divider";
 import { useBreakpoint } from "@/app/hooks";
 
 interface VotesHeaderProperties {
@@ -21,32 +13,6 @@ export const VotesHeader = ({ isSelectDelegateStep }: VotesHeaderProperties) => 
 	const { t } = useTranslation();
 
 	const { isMdAndAbove } = useBreakpoint();
-
-	// const renderPlaceholder = () => {
-	// 	if (selectedAddress) {
-	// 		return t("VOTE.VOTES_PAGE.SEARCH_VALIDATOR_PLACEHOLDER");
-	// 	}
-
-	// 	return t("VOTE.VOTES_PAGE.SEARCH_WALLET_PLACEHOLDER");
-	// };
-
-	// const headerExtra = () => {
-	// 	if (profile.wallets().count()) {
-	// 		return (
-	// 			<div className="flex items-center text-theme-primary-200">
-	// 				{selectedAddress && (
-	// 					<>
-	// 						<VotesFilter
-	// 							totalCurrentVotes={totalCurrentVotes}
-	// 							selectedOption={selectedFilter}
-	// 							onChange={setSelectedFilter}
-	// 						/>
-	// 					</>
-	// 				)}
-	// 			</div>
-	// 		);
-	// 	}
-	// };
 
 	const headerTitle = useMemo(() => {
 		if (isSelectDelegateStep) {
