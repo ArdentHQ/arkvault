766a23ac9895b20a2fc64e2d1fac57f6c5ef90d9
Patricio Marroquin <patricio@Patricios-MacBook-Pro-2.local>
2025-03-04 09:29:59 -0500

    fix: input currency component

--- src/app/components/Input/InputCurrency.tsx
+++ src/app/components/Input/InputCurrency.tsx
@@ -1,5 +1,5 @@
 import { Currency } from "@ardenthq/sdk-intl";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import { Input } from "./Input";
 import { Networks } from "@ardenthq/sdk";
@@ -24,28 +24,33 @@ export const InputCurrency = React.forwardRef<HTMLInputElement, InputCurrencyPro
 
 		useEffect(() => {
 			// when value is changed outside, update amount as well
-			setAmount(sanitize(value?.toString(), 999));
+			const newValue = sanitize(value?.toString(), 999);
+			setAmount((prev) => (prev !== newValue ? newValue : prev));
 		}, [value]);
 
-		const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-			const sanitizedValue = sanitize(event.target.value, 999);
+		const handleInput = useCallback(
+			(event: React.ChangeEvent<HTMLInputElement>) => {
+				const sanitizedValue = sanitize(event.target.value, 999);
+				if (amount !== sanitizedValue) {
+					setAmount(sanitizedValue);
+					onChange?.(sanitizedValue);
+				}
+			},
+			[amount, onChange],
+		);
 
-			setAmount(sanitizedValue);
-
-			onChange?.(sanitizedValue);
-		};
-
-		const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-			const sanitizedValue = sanitize(event.target.value, network?.toObject().currency.decimals ?? 18);
-
-			setAmount(sanitizedValue);
-
-			if (value !== sanitizedValue) {
-				onChange?.(sanitizedValue);
-			}
-
-			onBlur?.(event);
-		};
+		const handleBlur = useCallback(
+			(event: React.FocusEvent<HTMLInputElement>) => {
+				const decimals = network?.toObject().currency.decimals ?? 18;
+				const sanitizedValue = sanitize(event.target.value, decimals);
+				if (amount !== sanitizedValue) {
+					setAmount(sanitizedValue);
+					onChange?.(sanitizedValue);
+				}
+				onBlur?.(event);
+			},
+			[amount, network, onChange, onBlur],
+		);
 
 		return (
 			<div className="relative">
