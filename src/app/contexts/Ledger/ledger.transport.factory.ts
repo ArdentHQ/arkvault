be303c5655dd0aaba36bf2d115edb71d5051b1dd
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2025-03-04 11:51:50 +0100

    fix: temporarily disable ledger dependencies (#1023)

--- src/app/contexts/Ledger/ledger.transport.factory.ts
+++ src/app/contexts/Ledger/ledger.transport.factory.ts
@@ -1,40 +1,38 @@
 // Based on https://github.com/near/near-ledger-js/blob/master/supportedTransports.js
-import LedgerHID from "@ledgerhq/hw-transport-webhid";
-import LedgerUSB from "@ledgerhq/hw-transport-webusb";
-import platform from "platform";
+// import LedgerHID from "@ledgerhq/hw-transport-webhid";
+// import LedgerUSB from "@ledgerhq/hw-transport-webusb";
+// import { NotImplemented } from "@/app/lib";
+// import platform from "platform";
 
 export class LedgerTransportFactory {
-	public async supportedTransport(): Promise<typeof LedgerHID | typeof LedgerUSB> {
-		const [supportsHID, supportsUSB] = await Promise.all([this.#supportsHID(), this.#supportsUSB()]);
+	public supportedTransport(): any {
+		// const [supportsHID, supportsUSB] = await Promise.all([this.#supportsHID(), this.#supportsUSB()]);
 
-		if (supportsHID) {
-			return LedgerHID;
-		}
-
-		if (supportsUSB) {
-			return LedgerUSB;
-		}
-
+		// if (supportsHID) {
+		// 	return LedgerHID;
+		// }
+		//
+		// if (supportsUSB) {
+		// 	return LedgerUSB;
+		// }
+		//
 		throw new Error("No transports appear to be supported.");
 	}
-
-	async #supportsHID(): Promise<boolean> {
-		try {
-			return await LedgerHID.isSupported();
-		} catch {
-			return false;
-		}
-	}
-
-	async #supportsUSB(): Promise<boolean> {
-		try {
-			if (await LedgerUSB.isSupported()) {
-				return platform.os?.family !== "Windows" && platform.name !== "Opera";
-			}
-
-			return false;
-		} catch {
-			return false;
-		}
-	}
+	//
+	// #supportsHID(): Promise<boolean> {
+	// 	throw new NotImplemented(this.constructor.name, this.#supportsHID.name);
+	// }
+	//
+	// #supportsUSB(): Promise<boolean> {
+	// 	throw new NotImplemented(this.constructor.name, this.#supportsUSB.name);
+	// 	// try {
+	// 	// 	if (await LedgerUSB.isSupported()) {
+	// 	// 		return platform.os?.family !== "Windows" && platform.name !== "Opera";
+	// 	// 	}
+	// 	//
+	// 	// 	return false;
+	// 	// } catch {
+	// 	// 	return false;
+	// 	// }
+	// }
 }
