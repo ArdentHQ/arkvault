4d076ea2ebe6bf8d69f10f4ba9abe6c00c9c2835
goga-m <goga.mamoulasvili@gmail.com>
2025-03-04 11:23:09 +0100

    wip

--- src/app/contexts/Ledger/ledger.transport.factory.ts
+++ src/app/contexts/Ledger/ledger.transport.factory.ts
@@ -1,6 +1,7 @@
 // Based on https://github.com/near/near-ledger-js/blob/master/supportedTransports.js
 // import LedgerHID from "@ledgerhq/hw-transport-webhid";
 // import LedgerUSB from "@ledgerhq/hw-transport-webusb";
+import { NotImplemented } from "@/app/lib";
 import platform from "platform";
 
 export class LedgerTransportFactory {
