e9b59f4d84f8e4169b23b39057edf97dd4956313
goga-m <goga.mamoulasvili@gmail.com>
2025-03-07 13:54:46 +0100

    wip

--- src/app/contexts/Ledger/ledger.service.ts
+++ src/app/contexts/Ledger/ledger.service.ts
@@ -1,85 +1,139 @@
-/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/require-await */
+import { Buffer } from "buffer";
+import { Contracts, IoC, Services } from "@ardenthq/sdk";
+import { BIP44, HDKey } from "@ardenthq/sdk-cryptography";
+import Eth from "@ledgerhq/hw-app-eth";
+import { chunk, createRange, formatLedgerDerivationPath } from "./ledger.service.helpers";
+
 import { NotImplemented } from "@/app/lib";
-import { LedgerService, LedgerWalletList } from "./Ledger.contracts";
 
-export class AbstractLedgerService implements LedgerService {
-	readonly #dataTransferObjectService: any;
+export class LedgerService extends Services.AbstractLedgerService {
+	readonly #clientService!: Services.ClientService;
+	readonly #addressService!: Services.AddressService;
+	#ledger!: Services.LedgerTransport;
+	#transport!: InstanceType<typeof Eth>;
 
-	// protected readonly ledgerTransportFactory: LedgerTransportFactory;
+	public constructor(container: IoC.IContainer) {
+		super(container);
 
-	public constructor() {
-		//
+		this.#clientService = container.get(IoC.BindingType.ClientService);
+		this.#addressService = container.get(IoC.BindingType.AddressService);
 	}
 
-	public async onPreDestroy(): Promise<void> {
+	public override async onPreDestroy(): Promise<void> {
 		return this.disconnect();
 	}
 
-	public async connect(): Promise<void> {
-		throw new NotImplemented(this.constructor.name, this.connect.name);
+	public override async connect(): Promise<void> {
+		console.log("connecting")
+		this.#ledger = await this.ledgerTransportFactory();
+		this.#transport = new Eth(this.#ledger);
+		console.log("[connect]", { transport: this.#transport });
 	}
 
-	public async disconnect(): Promise<void> {
-		//
+	public override async disconnect(): Promise<void> {
+		if (this.#ledger) {
+			await this.#ledger.close();
+		}
 	}
 
-	public async getVersion(): Promise<string> {
-		throw new NotImplemented(this.constructor.name, this.getVersion.name);
+	public override async getVersion(): Promise<string> {
+		return "1"
 	}
 
-	public async getPublicKey(path: string): Promise<string> {
-		throw new NotImplemented(this.constructor.name, this.getPublicKey.name);
+	public override async getPublicKey(path: string): Promise<string> {
+		const result = await this.#transport.getAddress(path);
+		console.log("[getPublicKey]", result);
+		return result.publicKey;
 	}
 
-	public async getExtendedPublicKey(path: string): Promise<string> {
+	public override async getExtendedPublicKey(path: string): Promise<string> {
 		throw new NotImplemented(this.constructor.name, this.getExtendedPublicKey.name);
 	}
 
-	public async signTransaction(path: string, payload: Buffer): Promise<string> {
+	public override async signTransaction(path: string, payload: Buffer): Promise<string> {
 		throw new NotImplemented(this.constructor.name, this.signTransaction.name);
 	}
 
-	public async signMessage(path: string, payload: string): Promise<string> {
+	public override async signMessage(path: string, payload: string): Promise<string> {
 		throw new NotImplemented(this.constructor.name, this.signMessage.name);
 	}
 
-	public async scan(options?: { useLegacy: boolean }): Promise<LedgerWalletList> {
-		throw new NotImplemented(this.constructor.name, this.scan.name);
-	}
+	public override async scan(options?: {
+		useLegacy: boolean;
+		startPath?: string;
+		onProgress?: (wallet: Contracts.WalletData) => void;
+	}): Promise<Services.LedgerWalletList> {
+		const pageSize = 5;
+		let page = 0;
+		const slip44 = this.configRepository.get<number>("network.constants.slip44");
 
-	public async isNanoS(): Promise<boolean> {
-		return false;
-	}
+		const addressCache: Record<string, { address: string; publicKey: string }> = {};
 
-	public async isNanoX(): Promise<boolean> {
-		return false;
-	}
+		let hasMore = true;
 
-	protected mapPathsToWallets(
-		addressCache: Record<string, { address: string; publicKey: string }>,
-		wallets: any[],
-	): LedgerWalletList {
-		let foundFirstCold = false;
-		const ledgerWallets: LedgerWalletList = {};
+		const walletsCollection: Contracts.WalletData[] = [];
 
-		for (const [path, { address, publicKey }] of Object.entries(addressCache)) {
-			const matchingWallet = wallets.find((wallet: any) => wallet.address() === address);
+		do {
+			const addresses: string[] = [];
 
-			if (matchingWallet === undefined) {
-				if (foundFirstCold) {
-					continue;
-				}
-				foundFirstCold = true;
+			const path = `m/44'/${slip44}'/0'`;
+			let initialAddressIndex = 0;
 
-				ledgerWallets[path] = this.#dataTransferObjectService.wallet({
-					address,
-					balance: 0,
-					publicKey,
-				});
-			} else {
-				ledgerWallets[path] = matchingWallet;
+			if (options?.startPath) {
+				// Get the address index from expected format `m/purpose'/coinType'/account'/change/addressIndex`
+				initialAddressIndex = BIP44.parse(options.startPath).addressIndex + 1;
 			}
-		}
-		return ledgerWallets;
+
+			/**
+			 * @remarks
+			 * This is the new BIP44 compliant derivation which will be used by default.
+			 */
+			const compressedPublicKey = await this.getExtendedPublicKey(path);
+
+			for (const addressIndexIterator of createRange(page, pageSize)) {
+				const addressIndex = initialAddressIndex + addressIndexIterator;
+				const publicKey: string = HDKey.fromCompressedPublicKey(compressedPublicKey)
+					.derive(`m/0/${addressIndex}`)
+					.publicKey.toString("hex");
+
+				const { address } = await this.#addressService.fromPublicKey(publicKey);
+
+				addresses.push(address);
+
+				addressCache[`${path}/0/${addressIndex}`] = { address, publicKey };
+			}
+
+			const collections = await Promise.all(
+				chunk(addresses, 50).map((addresses: string[]) =>
+					this.#clientService.wallets({
+						identifiers: [...addresses].map((address: string) => ({ type: "address", value: address })),
+					}),
+				),
+			);
+
+			const batchWalletsCollection = collections.flatMap((collection) => collection.items());
+
+			if (options?.onProgress !== undefined) {
+				for (const item of batchWalletsCollection) {
+					options.onProgress(item);
+				}
+			}
+
+			walletsCollection.push(...batchWalletsCollection);
+
+			hasMore = collections.some((collection) => collection.isNotEmpty());
+
+			page++;
+		} while (hasMore);
+
+		return this.mapPathsToWallets(addressCache, walletsCollection);
+	}
+
+	public override async isNanoS(): Promise<boolean> {
+		return this.#ledger.deviceModel?.id === "nanoS";
+	}
+
+	public override async isNanoX(): Promise<boolean> {
+		return this.#ledger.deviceModel?.id === "nanoX";
 	}
 }
