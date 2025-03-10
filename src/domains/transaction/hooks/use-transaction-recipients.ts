4fcd5646117e6680ffffd1af9322e6f4e86ab7e7
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-03-10 12:12:46 -0500

    chore: remove unused import

--- src/domains/transaction/hooks/use-transaction-recipients.ts
+++ src/domains/transaction/hooks/use-transaction-recipients.ts
@@ -1,7 +1,6 @@
 import { useWalletAlias, WalletAliasResult } from "@/app/hooks";
 import { Contracts, DTO } from "@ardenthq/sdk-profiles";
 import { useMemo } from "react";
-import { isContractTransaction } from "@/domains/transaction/utils";
 import { useActiveNetwork } from "@/app/hooks/use-active-network";
 
 export const useTransactionRecipients = ({
