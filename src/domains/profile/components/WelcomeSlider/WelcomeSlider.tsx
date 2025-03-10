d5cf216865b3699c363ab5107dd97c3682bcfb2f
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2025-03-10 11:42:30 -0400

    refactor: update welcome images (#1040)

--- src/domains/profile/components/WelcomeSlider/WelcomeSlider.tsx
+++ src/domains/profile/components/WelcomeSlider/WelcomeSlider.tsx
@@ -11,7 +11,7 @@ interface SlideProperties {
 
 const Slide = ({ title, subtitle, imageName, imageNameSm }: SlideProperties) => (
 	<div className="flex w-full flex-row md:w-auto lg:flex-col">
-		<div className="w-full p-4 sm:flex-shrink-0 sm:p-6 md:w-[360px] md:pb-0 md:pr-0 lg:w-[430px]">
+		<div className="w-full p-4 sm:flex-shrink-0 sm:p-6 md:w-[360px] md:pb-0 md:pr-0 lg:w-[450px]">
 			<h3 className="mb-2 mt-7 text-base leading-7 xs:leading-5 md:font-bold lg:text-2xl lg:leading-[29px]">
 				{title}
 			</h3>
