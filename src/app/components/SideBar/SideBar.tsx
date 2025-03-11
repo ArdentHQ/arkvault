1880f5478b6c755ccaf553356be6d21783f27ee4
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-03-11 11:35:53 -0600

    adjust borders

--- src/app/components/SideBar/SideBar.tsx
+++ src/app/components/SideBar/SideBar.tsx
@@ -34,14 +34,14 @@ export const SideBar: React.FC<Properties> = ({ activeItem, handleActiveItem, it
 
 	return (
 		<>
-			<div className="relative -mx-6 -mt-4 bg-theme-secondary-200 px-6 py-2 dark:bg-theme-dark-950 md:m-0 md:bg-transparent md:p-0 dark:md:bg-transparent lg:hidden">
+			<div className="relative -mx-6 -mt-4 border-t border-theme-secondary-300 bg-theme-secondary-200 px-6 py-2 dark:border-theme-dark-700 dark:bg-black md:m-0 md:border-t-0 md:bg-transparent md:p-0 dark:md:bg-transparent lg:hidden">
 				<Dropdown
 					placement="bottom-start"
 					wrapperClass="sm:w-full px-6 sm:px-6 md:px-10 -mt-2"
 					options={options}
 					onSelect={({ value }) => handleActiveItem(String(value))}
 					toggleContent={(isOpen) => (
-						<div className="flex cursor-pointer items-center space-x-4 overflow-hidden rounded border border-transparent bg-white px-4 py-3 dark:border dark:border-theme-dark-700 dark:bg-transparent md:border-theme-secondary-300">
+						<div className="flex cursor-pointer items-center space-x-4 overflow-hidden rounded border border-transparent bg-white px-4 py-3 dark:border dark:border-theme-dark-700 dark:bg-theme-dark-900 md:border-theme-secondary-300">
 							<span className="flex-1 font-semibold leading-tight">{selectedLabel}</span>
 
 							<Icon
