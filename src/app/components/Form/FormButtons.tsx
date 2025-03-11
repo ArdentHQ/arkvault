682cf7e807726e6c50df439616aed360d3381160
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-03-11 10:43:54 -0500

    fix: set z-index for form buttons wrapper

--- src/app/components/Form/FormButtons.tsx
+++ src/app/components/Form/FormButtons.tsx
@@ -11,7 +11,7 @@ const FormButtonsWrapper = ({
 	<div
 		{...props}
 		className={twMerge(
-			"fixed inset-x-0 bottom-0 flex gap-3 bg-theme-background px-8 py-3 shadow-footer-smooth dark:bg-black dark:shadow-footer-smooth-dark sm:relative sm:inset-auto sm:mt-6 sm:justify-end sm:bg-transparent sm:p-0 sm:shadow-none sm:dark:bg-transparent sm:dark:shadow-none [&>button]:flex-1 sm:[&>button]:flex-none",
+			"fixed inset-x-0 bottom-0 z-50 flex gap-3 bg-theme-background px-8 py-3 shadow-footer-smooth dark:bg-black dark:shadow-footer-smooth-dark sm:relative sm:inset-auto sm:mt-6 sm:justify-end sm:bg-transparent sm:p-0 sm:shadow-none sm:dark:bg-transparent sm:dark:shadow-none [&>button]:flex-1 sm:[&>button]:flex-none",
 			cn({
 				"mb-14 sm:mb-0": showMobileNavigation,
 				"pb-3 sm:pb-0": !showMobileNavigation,
--- src/domains/wallet/components/CreateAddressSidePanel/CreateAddressSidePanel.tsx
+++ src/domains/wallet/components/CreateAddressSidePanel/CreateAddressSidePanel.tsx
@@ -261,7 +261,7 @@ export const CreateAddressesSidePanel = ({
 					</div>
 				</Tabs>
 
-				<div className="fixed inset-x-0 bottom-0 mr-[5px] flex items-center justify-end bg-theme-background p-2 px-4 sm:justify-between sm:px-6 sm:py-6 md:px-8">
+				<div className="fixed inset-x-0 bottom-0 z-50 mr-[5px] flex items-center justify-end bg-theme-background p-2 px-4 sm:justify-between sm:px-6 sm:py-6 md:px-8">
 					<div className="hidden w-[136px] sm:block">
 						<StepIndicator steps={allSteps} activeIndex={activeTab} showTitle={false} />
 					</div>
