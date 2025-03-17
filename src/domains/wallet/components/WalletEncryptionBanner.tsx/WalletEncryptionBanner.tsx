502e0cbda72ed6551194216e117498bbe0746ecf
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-03-17 10:44:51 -0500

    feat: wallet encryption banner

---
+++ src/domains/wallet/components/WalletEncryptionBanner.tsx/WalletEncryptionBanner.tsx
@@ -0,0 +1,86 @@
+import React, { ChangeEventHandler } from "react";
+import cn from "classnames";
+import { Tooltip } from "@/app/components/Tooltip";
+import { useTranslation } from "react-i18next";
+import { ImportOption } from "@/domains/wallet/hooks/use-import-options";
+import { Toggle } from "@/app/components/Toggle";
+import { Checkbox } from "@/app/components/Checkbox";
+
+export const WalletEncryptionBanner = ({
+	importOption,
+	toggleOnChange,
+	toggleChecked,
+	checkboxChecked,
+	checkboxOnChange,
+}: {
+	importOption?: ImportOption;
+	toggleOnChange: ChangeEventHandler<HTMLInputElement>;
+	toggleChecked: boolean;
+	checkboxChecked: boolean;
+	checkboxOnChange: ChangeEventHandler<HTMLInputElement>;
+}) => {
+	const { t } = useTranslation();
+
+	return (
+		<div
+			className={cn("flex w-full flex-col overflow-hidden rounded-xl border", {
+				"border-theme-secondary-300 dark:border-theme-dark-700": !toggleChecked,
+				"border-theme-warning-300 dark:border-theme-warning-700": toggleChecked,
+			})}
+		>
+			<div className="flex flex-row justify-between px-6 py-4">
+				<div className="flex max-w-96 flex-col gap-1">
+					<span className="text-base font-semibold leading-5 text-theme-secondary-900 dark:text-theme-dark-50">
+						{t("WALLETS.PAGE_IMPORT_WALLET.IMPORT_DETAIL_STEP.ENCRYPTION.TITLE")}
+					</span>
+					<p className="text-sm leading-[21px] text-theme-secondary-700 dark:text-theme-dark-200">
+						{t("WALLETS.PAGE_IMPORT_WALLET.IMPORT_DETAIL_STEP.ENCRYPTION.DESCRIPTION")}
+					</p>
+				</div>
+
+				<Tooltip
+					className="-ml-3 mb-1"
+					content={t("WALLETS.PAGE_IMPORT_WALLET.IMPORT_DETAIL_STEP.ENCRYPTION.NOT_AVAILABLE")}
+					disabled={importOption && !importOption?.canBeEncrypted}
+				>
+					<span data-testid="WalletEncryptionBanner__encryption">
+						<Toggle
+							data-testid="WalletEncryptionBanner__encryption-toggle"
+							disabled={importOption && !importOption?.canBeEncrypted}
+							checked={toggleChecked}
+							onChange={toggleOnChange}
+						/>
+					</span>
+				</Tooltip>
+			</div>
+
+			<div
+				className={cn(
+					"flex flex-col gap-3 bg-theme-warning-50 transition-all duration-300 dark:bg-theme-dark-950",
+					{
+						"h-0 opacity-0": !toggleChecked,
+						"min-h-[120px] px-6 pb-5 pt-3 opacity-100": toggleChecked,
+					},
+				)}
+			>
+				<p className="text-sm font-normal leading-[21px] text-theme-secondary-900 dark:text-theme-dark-50">
+					{t("WALLETS.PAGE_IMPORT_WALLET.IMPORT_DETAIL_STEP.ENCRYPTION.WARNING")}
+				</p>
+
+				<hr className="border border-dashed border-theme-warning-300 dark:border-theme-dark-700" />
+
+				<label className="inline-flex cursor-pointer items-center space-x-3">
+					<Checkbox
+						data-testid="WalletEncryptionBanner__checkbox"
+						checked={checkboxChecked}
+						onChange={checkboxOnChange}
+						color="warning"
+					/>
+					<span className="text-sm font-normal leading-[21px] text-theme-secondary-900 dark:text-theme-dark-50">
+						{t("WALLETS.PAGE_IMPORT_WALLET.IMPORT_DETAIL_STEP.ENCRYPTION.CHECKBOX")}
+					</span>
+				</label>
+			</div>
+		</div>
+	);
+};
