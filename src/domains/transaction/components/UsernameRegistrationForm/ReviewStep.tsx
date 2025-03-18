eb570fa582e33c4a9d0e8db38cec588553e8ac35
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-03-18 10:10:25 -0600

    adjust username registration

--- src/domains/transaction/components/UsernameRegistrationForm/ReviewStep.tsx
+++ src/domains/transaction/components/UsernameRegistrationForm/ReviewStep.tsx
@@ -41,7 +41,7 @@ export const ReviewStep = ({
 
 			<div className="-mx-3 mt-6 space-y-3 sm:mx-0 sm:mt-4 sm:space-y-4">
 				<TransactionAddresses
-					labelClassName="w-auto sm:min-w-36"
+					labelClassName="w-auto sm:min-w-24 sm:pr-6"
 					senderAddress={wallet.address()}
 					recipients={[]}
 					profile={profile}
@@ -51,7 +51,7 @@ export const ReviewStep = ({
 				<DetailWrapper label={t("TRANSACTION.TRANSACTION_TYPE")}>
 					<div className="space-y-3 sm:space-y-0">
 						<div className="flex w-full items-center justify-between gap-4 sm:justify-start">
-							<DetailTitle className="w-auto sm:min-w-32">{t("COMMON.CATEGORY")}</DetailTitle>
+							<DetailTitle className="w-auto sm:min-w-[84px]">{t("COMMON.CATEGORY")}</DetailTitle>
 							<div className="flex items-center rounded bg-theme-secondary-200 px-1 py-[3px] dark:border dark:border-theme-secondary-800 dark:bg-transparent">
 								<span className="text-[12px] font-semibold leading-[15px] text-theme-secondary-700 dark:text-theme-secondary-500">
 									{t("TRANSACTION.TRANSACTION_TYPES.USERNAME_REGISTRATION")}
@@ -67,7 +67,7 @@ export const ReviewStep = ({
 						</div>
 
 						<div className="flex w-full items-center justify-between gap-4 sm:justify-start">
-							<DetailTitle className="w-auto sm:min-w-32">{t("COMMON.USERNAME")}</DetailTitle>
+							<DetailTitle className="w-auto sm:min-w-[84px]">{t("COMMON.USERNAME")}</DetailTitle>
 							<div className="no-ligatures truncate text-sm font-semibold leading-[17px] text-theme-secondary-900 dark:text-theme-secondary-200 sm:text-base sm:leading-5">
 								{username}
 							</div>
