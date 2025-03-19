c99c4709d1bd7816d21697331e5a3e51cadfe575
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-03-19 15:52:11 -0600

    wip

--- src/app/hooks/use-server-health-status.test.tsx
+++ src/app/hooks/use-server-health-status.test.tsx
@@ -5,6 +5,7 @@ import { useServerHealthStatus } from "@/app/hooks";
 import { render, screen, getDefaultProfileId } from "@/utils/testing-library";
 import { ServerStatus } from "@/utils/peers";
 import { ServerHealthStatus } from "@/domains/setting/pages/Servers/Servers.contracts";
+import { Route } from "react-router-dom";
 
 const history = createHashHistory();
 const dashboardURL = `/profiles/${getDefaultProfileId()}/dashboard`;
@@ -26,25 +27,33 @@ describe("useServerHealthStatus", () => {
 	};
 
 	const ServerHealthStatusWrapper = ({ status }: { status: ServerStatus }) => (
-		<ConfigurationProvider>
-			<Component serverStatus={status} />
-		</ConfigurationProvider>
+		<Route path="/profiles/:profileId/votes">
+			<ConfigurationProvider>
+				<Component serverStatus={status} />
+			</ConfigurationProvider>
+		</Route>
 	);
 
 	it("should render as healthy", async () => {
-		render(<ServerHealthStatusWrapper status={{ "ark.devnet": { up: true } }} />);
+		render(<ServerHealthStatusWrapper status={{ "ark.devnet": { up: true } }} />, {
+			route: `/profiles/${getDefaultProfileId()}/votes`,
+		});
 
 		await expect(screen.findByTestId("ServerHealthStatus--0")).resolves.toBeVisible();
 	});
 
 	it("should render as downgraded", async () => {
-		render(<ServerHealthStatusWrapper status={{ "ark.devnet": { down: false, up: true } }} />);
+		render(<ServerHealthStatusWrapper status={{ "ark.devnet": { down: false, up: true } }} />, {
+			route: `/profiles/${getDefaultProfileId()}/votes`,
+		});
 
 		await expect(screen.findByTestId("ServerHealthStatus--1")).resolves.toBeVisible();
 	});
 
 	it("should render as unavailable", async () => {
-		render(<ServerHealthStatusWrapper status={{ "ark.devnet": { down: false } }} />);
+		render(<ServerHealthStatusWrapper status={{ "ark.devnet": { down: false } }} />, {
+			route: `/profiles/${getDefaultProfileId()}/votes`,
+		});
 
 		await expect(screen.findByTestId("ServerHealthStatus--2")).resolves.toBeVisible();
 	});
