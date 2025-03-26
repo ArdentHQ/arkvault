ddd333bd9a527476bb0900c8299376e4430c7df4
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-03-26 15:31:07 -0600

    Update TransactionTable.helpers.tsx

--- src/domains/transaction/components/TransactionTable/TransactionTable.helpers.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionTable.helpers.tsx
@@ -1,6 +1,5 @@
 import { Column } from "react-table";
 import { DTO } from "@ardenthq/sdk-profiles";
-import { PendingTransaction } from "@/domains/transaction/components/TransactionTable/PendingTransactionsTable/PendingTransactionsTable.contracts";
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -64,59 +63,3 @@ export const useTransactionTableColumns = ({ coin, hideSender }: { coin?: string
 	}, [t, coinLabel, hideSender]);
 };
 
-export const usePendingTransactionTableColumns = ({ coin }: { coin: string }) => {
-	const { t } = useTranslation();
-
-	const coinLabel = coin ? `(${coin})` : "";
-
-	return useMemo<Column<PendingTransaction>[]>(
-		() => [
-			{
-				Header: t("COMMON.TX_ID"),
-				cellWidth: "min-w-32",
-				headerClassName: "no-border",
-			},
-			{
-				Header: t("COMMON.AGE"),
-				accessor: () => "timestamp",
-				headerClassName: "hidden xl:table-cell no-border",
-				sortDescFirst: true,
-			},
-			{
-				Header: t("COMMON.TYPE"),
-				cellWidth: "w-16",
-				headerClassName: "no-border",
-			},
-			{
-				Header: t("COMMON.ADDRESSING"),
-				cellWidth: "min-w-32",
-				headerClassName: "no-border",
-			},
-			{
-				Header: t("COMMON.STATUS"),
-				className: "justify-center",
-				headerClassName: "hidden lg:table-cell no-border",
-				minimumWidth: true,
-			},
-			{
-				Header: `${t("COMMON.VALUE")} ${coinLabel}`,
-				accessor: () => "amount",
-				className: "justify-end",
-				headerClassName: "no-border",
-			},
-			{
-				Header: t("COMMON.FIAT_VALUE"),
-				accessor: () => "fiatValue",
-				className: "justify-end",
-				headerClassName: "no-border hidden lg:table-cell",
-			},
-			{
-				Header: t("COMMON.ACTION"),
-				className: "hidden",
-				headerClassName: "no-border",
-				minimumWidth: true,
-			},
-		],
-		[t, coinLabel],
-	);
-};
