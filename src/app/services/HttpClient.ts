7b71c32535a8d4b2b417d7918acb7dea46aece3e
goga-m <goga.mamoulasvili@gmail.com>
2025-03-27 10:52:18 +0100

    wip

--- src/app/services/HttpClient.ts
+++ src/app/services/HttpClient.ts
@@ -37,6 +37,8 @@ export class HttpClient extends Http.AbstractRequest {
 			url = `${url}?${new URLSearchParams(data.query as any)}`;
 		}
 
+		console.log({ url })
+
 		const cacheKey: string = hash(`${method}.${url}.${JSON.stringify(data)}`).toString();
 
 		return this.cache.remember(
@@ -65,6 +67,52 @@ export class HttpClient extends Http.AbstractRequest {
 						body: JSON.stringify(data?.data),
 						method: "POST",
 					};
+				}
+
+
+				if (url === "https://dwallets-evm.mainsailhq.com/api/wallets/0xB9Af96a9e5972503E1a0930744fe1ad985ae03b2") {
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+					console.log("=======================================")
+
+					const response = {
+						"data": {
+							"address": "0xB9Af96a9e5972503E1a0930744fe1ad985ae03b2",
+							"attributes": {
+								"validatorApproval": 0.0094,
+								"validatorLastBlock": {
+									"height": 8948,
+									"id": "56bb1277512914e42fca9e4387d6de9456753e5f031894882a0af0ae68e287a0",
+									"timestamp": 1_743_066_238_333
+								},
+								"validatorProducedBlocks": 169,
+								"validatorPublicKey": "a6dc6f406b2bbd1ab93d0a2d903b1952a7f6e753bcc18c109646d392a2e4414f38ff25f2ed23d8191cd3e60521a32ec2",
+								"validatorRank": 53,
+								"validatorResigned": false,
+								"validatorVoteBalance": "2348490565932735849056603",
+								"validatorVotersCount": 1,
+								"vote": "0x12361f0Bd5f95C3Ea8BF34af48F5484b811B5CCe"
+							},
+							"balance": "2358490566037735849056603",
+							"nonce": "2",
+							"publicKey": "03ff2e9b086a5137c8425b5cfcb0d65720270030a50a16f6ab237c5876c9b2323a",
+							"updated_at": "8948"
+						}
+					}
+
+					return new Http.Response({
+						body: JSON.stringify(response),
+						headers: {},
+						statusCode: 200,
+					});
 				}
 
 				const response = await fetch(url, options);
