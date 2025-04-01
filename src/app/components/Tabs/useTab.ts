2f8072d5c024cb751564ca3245bb3c6be1017327
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-04-01 18:55:04 -0500

    refactor: support for disabled prop in use tab hook

--- src/app/components/Tabs/useTab.ts
+++ src/app/components/Tabs/useTab.ts
@@ -2,12 +2,12 @@ import React from "react";
 
 export type TabId = string | number | undefined;
 
-export function useTab({ initialId }: { initialId: TabId }) {
+export function useTab({ initialId, disabled }: { initialId: TabId, disabled?: boolean }) {
 	const [currentId, setCurrentId] = React.useState<TabId>(initialId);
 
 	const isIdActive = React.useCallback((id: TabId) => currentId === id, [currentId]);
 
-	return { currentId, isIdActive, setCurrentId };
+	return { currentId, isIdActive, setCurrentId, disabled };
 }
 
 export const TabContext = React.createContext<ReturnType<typeof useTab> | undefined>(undefined);
