014a06c96893d626f6e1bff8db39047177384e46
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-04-03 07:52:38 -0500

    feat: import missing helpers & tests

---
+++ src/app/lib/helpers/set.test.ts
@@ -0,0 +1,19 @@
+import { set } from "./set";
+
+describe("set", () => {
+	it("should not do anything if the target is not an object", () => {
+		expect(set(undefined, "a.b.c", 4)).toBe(false);
+	});
+
+	it("should work with a string or array as path", () => {
+		const object = { a: { b: { c: 3 } } };
+
+		set(object, "a.b.c", 4);
+
+		expect(object.a.b.c).toBe(4);
+
+		set(object, "x.y.z", 5);
+
+		expect(object.x.y.z).toBe(5);
+	});
+});
---
+++ src/app/lib/helpers/unset.test.ts
@@ -0,0 +1,27 @@
+import { unset } from "./unset";
+
+describe("unset", () => {
+	it("should return false if the target is not an object", () => {
+		expect(unset([], "a.b.c")).toBe(false);
+	});
+
+	it("should return false if the path is not a string", () => {
+		expect(unset({}, 123 as any)).toBe(false);
+	});
+
+	it("should not do anything if the object is not an object", () => {
+		expect(unset([], "a.b.c")).toBe(false);
+	});
+
+	it("should work with a string or array as path", () => {
+		const object = { a: { b: { c: 7 } } };
+
+		unset(object, "a.b.c");
+
+		expect(object).toEqual({ a: { b: {} } });
+
+		unset(object, "a.b.c");
+
+		expect(object).toEqual({ a: { b: {} } });
+	});
+});
