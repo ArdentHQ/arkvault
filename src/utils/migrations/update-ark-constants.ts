16ee51145e639f25155933430d7e40886cf8e619
Shahin Safaraliyev <shahin@ardenthq.com>
2025-04-04 21:13:57 +0400

    refactor: remove `ARK` package

--- src/utils/migrations/update-ark-constants.ts
+++
@@ -1,8 +0,0 @@
-
-export const updateArkConstants = ({ data }) => {
-	// for (const manifest of Object.values<Networks.NetworkManifest>(data.networks.ark)) {
-	// 	if (manifest.constants.epoch === undefined) {
-	// 		manifest.constants.epoch = ARK.manifest.networks[manifest.id].constants.epoch;
-	// 	}
-	// }
-};
