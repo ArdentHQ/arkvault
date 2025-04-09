a4f55f2c40476b3e8d0fd63a1d713688e56ef60b
goga-m <goga.mamoulasvili@gmail.com>
2025-04-09 05:50:43 +0200

    wip

--- scripts/install-local-sdk.js
+++ scripts/install-local-sdk.js
@@ -14,7 +14,6 @@ if (!sdkPath) {
 	return;
 }
 
-packageJson.dependencies["@ardenthq/sdk"] = join(sdkPath, "packages/sdk");
 packageJson.dependencies["@ardenthq/sdk-intl"] = join(sdkPath, "packages/intl");
 packageJson.dependencies["@ardenthq/sdk-profiles"] = join(sdkPath, "packages/profiles");
 
