4ce0bf0b030943e82a3746e6132ce9b3b2ea0c06
Shahin Safaraliyev <shahin@ardenthq.com>
2025-04-10 18:02:33 +0400

    refactor: refresh wallet in every n seconds

--- src/app/services/Cache.ts
+++ src/app/services/Cache.ts
@@ -28,7 +28,7 @@ export class Cache {
 			result = await value();
 		}
 
-		this.store[key] = { expires_at: DateTime.make().addSeconds(ttl ?? this.ttl), value: result };
+		this.store[key] = { expires_at: DateTime.make().addSeconds(ttl ?? this.ttl).toUNIX(), value: result };
 
 		return result;
 	}
