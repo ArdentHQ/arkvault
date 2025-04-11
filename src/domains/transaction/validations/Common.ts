04194c9791ed539f04e6185ebed691bc3d61d5b3
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-04-11 09:23:16 -0600

    wip

--- src/domains/transaction/validations/Common.ts
+++ src/domains/transaction/validations/Common.ts
@@ -1,10 +1,8 @@
 import { Networks } from "@ardenthq/sdk";
 import { TFunction } from "@/app/i18n/react-i18next.contracts";
-import { configManager } from "@ardenthq/sdk-mainsail";
 import { calculateGasFee } from "@/domains/transaction/components/InputFee/InputFee";
 import { TransactionFees } from "@/types";
-
-import { formatUnits } from "@ardenthq/sdk-mainsail";
+import { formatUnits, configManager } from "@ardenthq/sdk-mainsail";
 import { BigNumber } from "@/app/lib/helpers";
 
 export const common = (t: TFunction) => ({
