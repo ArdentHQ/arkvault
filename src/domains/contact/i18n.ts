393cf613b75923b277bfa01d8b56e5ef84c2495c
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-04-14 02:31:29 -0600

    chore: add missing periods (#1157)

--- src/domains/contact/i18n.ts
+++ src/domains/contact/i18n.ts
@@ -5,7 +5,7 @@ export const translations = {
 			"It looks like you don't have any Contacts yet. After adding new Contacts, they can be viewed here.",
 		NO_CONTACTS_FOUND: `Your search query "{{query}}" does not match any Contacts.`,
 		SEARCH_PLACEHOLDER: "Enter Contact Name",
-		SUBTITLE: "Manage your frequent Contacts",
+		SUBTITLE: "Manage your frequent Contacts.",
 		TITLE: "Contacts",
 	},
 
