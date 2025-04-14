72f919e45b109d30dbc3e71e8f1a02370c41623b
Shahin Safaraliyev <shahin@ardenthq.com>
2025-04-14 16:58:33 +0400

    wip

--- src/domains/transaction/components/TransactionDetail/TransactionAddresses/TransactionAddresses.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionAddresses/TransactionAddresses.tsx
@@ -55,7 +55,7 @@ export const TransactionAddresses = ({
 			</div>
 
 			{interactedWith && ( <div className="mt-3 flex items-center justify-between space-x-2 sm:justify-start sm:space-x-0">
-				<DetailTitle className={labelClassName}>{t("COMMON.INTERACTED")}</DetailTitle>
+				<DetailTitle className={labelClassName}>{t("COMMON.CONTRACT")}</DetailTitle>
 				<Address
 					truncateOnTable
 					address={interactedWith}
