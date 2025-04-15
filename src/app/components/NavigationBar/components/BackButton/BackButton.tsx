74e8a7f3f0481f51a383a8e691e11b607a6c2b38
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-04-14 19:48:53 -0500

    refactor: back button

--- src/app/components/NavigationBar/components/BackButton/BackButton.tsx
+++ src/app/components/NavigationBar/components/BackButton/BackButton.tsx
@@ -19,7 +19,7 @@ const StyledBackButton = ({
 			"flex w-9 items-center border-b border-b-theme-secondary-300 bg-theme-secondary-100 transition-colors duration-200 dark:border-b-theme-dark-700 dark:bg-theme-dark-950",
 			cn({
 				"cursor-not-allowed text-theme-secondary-500 dark:text-theme-secondary-800": disabled,
-				"text-theme-secondary-500 hover:bg-theme-primary-200 hover:text-theme-primary-700 focus:rounded focus:outline-none focus:ring-2 focus:ring-inset focus:ring-theme-primary-400 dark:text-theme-dark-50 hover:dark:bg-theme-dark-700":
+				"text-theme-secondary-500 hover:bg-theme-primary-200 hover:text-theme-primary-700 focus:rounded-xs focus:outline-hidden focus:ring-2 focus:ring-inset focus:ring-theme-primary-400 dark:text-theme-dark-50 dark:hover:bg-theme-dark-700":
 					!disabled,
 			}),
 			props.className,
