fc414dfae07f7db70762e6e1f6c9feeee6923165
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-04-15 23:27:54 -0500

    fix: navbar icons

--- src/app/components/Notifications/NotificationsDropdown.tsx
+++ src/app/components/Notifications/NotificationsDropdown.tsx
@@ -32,7 +32,7 @@ export const NotificationsDropdown = ({ profile }: { profile: Contracts.IProfile
 								data-testid="NavigationBar__buttons--notifications"
 								className="text-theme-secondary-700 hover:text-theme-primary-700 dark:text-theme-dark-200 dark:hover:text-theme-dark-50"
 							>
-								<Icon name="Bell" size="lg" className="p-1" />
+								<Icon name="Bell" size="lg" className="m-0 p-1" />
 								{hasUnread && <Dot />}
 							</Button>
 						</Tooltip>
--- src/app/components/ServerStatusIndicator/ServerStatusIndicator.tsx
+++ src/app/components/ServerStatusIndicator/ServerStatusIndicator.tsx
@@ -49,7 +49,7 @@ export const ServerStatusIndicator = ({ profile }: { profile: Contracts.IProfile
 								data-testid="NavigationBar__buttons--server-status"
 								className="text-theme-secondary-700 hover:text-theme-primary-700 dark:text-theme-dark-200 dark:hover:text-theme-dark-50"
 							>
-								<Icon name="Cloud" size="lg" className="p-1" />
+								<Icon name="Cloud" size="lg" className="m-0 p-1" />
 								<div className="absolute top-0 right-0 flex items-center justify-center rounded-full p-1 transition-all duration-100 ease-linear">
 									<StatusDot status={status.value} />
 								</div>
