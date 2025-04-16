c9ca9d125e745e1fcdcb92a4e896bcd22edab51f
goga-m <goga.mamoulasvili@gmail.com>
2025-04-16 12:16:06 +0200

    wip

--- src/app/lib/mainsail/crypto/index.ts
+++ src/app/lib/mainsail/crypto/index.ts
@@ -1,5 +1,4 @@
 export * as Constants from "./constants";
-export * as Errors from "./errors";
 export * as Identities from "./identities/index";
 export * as Interfaces from "./interfaces/index";
 export * as Managers from "./managers/index";
