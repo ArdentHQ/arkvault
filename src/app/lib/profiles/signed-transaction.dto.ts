decf54f9a1109abbf0a4c4132c3b83077a7d6d0f
patricio0312rev <patricio0312rev@users.noreply.github.com>
2025-04-23 16:40:58 +0000

    style: resolve style guide violations

--- src/app/lib/profiles/signed-transaction.dto.ts
+++ src/app/lib/profiles/signed-transaction.dto.ts
@@ -7,7 +7,7 @@ import { container } from "./container.js";
 import { Identifiers } from "./container.models.js";
 import { IExchangeRateService, IReadWriteWallet } from "./contracts.js";
 import { ExtendedTransactionRecipient } from "./transaction.dto.js";
-import { DateTime } from '@/app/lib/intl';
+import { DateTime } from "@/app/lib/intl";
 
 export class ExtendedSignedTransactionData {
 	readonly #data: Contracts.SignedTransactionData;
