318cde475e11a150ff915e78eb775b39c362f222
goga-m <goga.mamoulasvili@gmail.com>
2025-04-28 15:38:08 +0200

    wip

--- src/app/lib/mainsail/client.service.ts
+++ src/app/lib/mainsail/client.service.ts
@@ -181,16 +181,20 @@ export class ClientService extends Services.AbstractClientService {
 	public override async usernames(addresses: string[]): Promise<Collections.UsernameDataCollection> {
 		try {
 			let data;
+			console.log({ UsernamesAbi, addresses })
+
 			try {
 				data = encodeFunctionData({
 					abi: UsernamesAbi.abi,
 					args: [addresses],
 					functionName: "getUsernames",
 				});
+				console.log({ data });
 			} catch (encodeError) {
 				throw new Error(`Failed to encode function data: ${(encodeError as Error).message}`);
 			}
 
+			console.log({ data })
 			const response = await this.evmCall({
 				data: data,
 				to: wellKnownContracts.username,
@@ -198,11 +202,13 @@ export class ClientService extends Services.AbstractClientService {
 
 			let decoded;
 			try {
+				console.log({ response })
 				decoded = decodeFunctionResult({
 					abi: UsernamesAbi.abi,
 					data: response.result,
 					functionName: "getUsernames",
 				});
+				console.log({ decoded })
 			} catch (decodeError) {
 				throw new Error(`Failed to decode function result: ${(decodeError as Error).message}`);
 			}
