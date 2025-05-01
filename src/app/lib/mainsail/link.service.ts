d4330cc0cc8d6d6aa8dac6edf138c6452f7dba36
goga-m <goga.mamoulasvili@gmail.com>
2025-05-01 13:06:43 +0200

    wip

--- src/app/lib/mainsail/link.service.ts
+++ src/app/lib/mainsail/link.service.ts
@@ -2,29 +2,30 @@ import { formatString } from "@/app/lib/helpers";
 import queryString from "query-string";
 import { IContainer } from "@/app/lib/sdk/container.contracts";
 import { ConfigRepository } from "@/app/lib/sdk/config";
-import { randomNetworkHostFromConfig } from "@/app/lib/sdk/helpers";
+import { randomHost, randomNetworkHostFromConfig } from "@/app/lib/sdk/helpers";
 
 export class LinkService {
-	readonly #configRepository: ConfigRepository;
-
-	public constructor(config: ConfigRepository) {
-		this.#configRepository = config
-	}
-
 	public block(id: string): string {
-		return this.#buildURL(this.#configRepository.get("network.explorer.block"), id);
+		return this.#buildURL("blocks/{0}", id);
 	}
 
 	public transaction(id: string): string {
-		return this.#buildURL(this.#configRepository.get("network.explorer.transaction"), id);
+		return this.#buildURL("transactions/{0}", id);
 	}
 
 	public wallet(id: string): string {
-		return this.#buildURL(this.#configRepository.get("network.explorer.wallet"), id);
+		return this.#buildURL("addresses/{0}", id);
 	}
 
 	#buildURL(schema: string, id: string): string {
-		const { host, query } = randomNetworkHostFromConfig(this.#configRepository, "explorer");
+		// @TODO: Remove hardcoded values.
+		//const { host, query } = randomNetworkHostFromConfig(this.#configRepository, "explorer");
+		const { host, query } = randomHost([
+			{
+				host: "https://explorer-evm-test.mainsailhq.com",
+				type: "explorer",
+			},
+		], "explorer");
 
 		const url = `${host.replace(/\/$/, "")}/${formatString(schema, id)}`;
 
