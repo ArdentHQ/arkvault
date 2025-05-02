494022608143334c17154ff0859cbf730a2a12ca
goga-m <goga.mamoulasvili@gmail.com>
2025-05-02 15:10:46 +0200

    wip

--- src/app/lib/mainsail/index.ts
+++ src/app/lib/mainsail/index.ts
@@ -1,34 +1,3 @@
-import { bundle, Coins } from "@/app/lib/sdk";
-
-import { AddressService } from "./address.service";
-import { ClientService } from "./client.service";
-import { ServiceProvider } from "./coin.provider";
-import { ConfirmedTransactionData } from "./confirmed-transaction.dto";
-import { FeeService } from "./fee.service";
-import { KnownWalletService } from "./known-wallet.service";
-import { LedgerService } from "./ledger.service";
-import { manifest } from "./manifest";
-import { MessageService } from "./message.service";
-import { PrivateKeyService } from "./private-key.service";
-import { PublicKeyService } from "./public-key.service";
-import { SignedTransactionData } from "./signed-transaction.dto";
-import { TransactionService } from "./transaction.service";
-import { WalletData } from "./wallet.dto";
-import { WIFService } from "./wif.service";
-import { LinkService } from "./link.service";
-import { SignatoryService } from "./signatory.service";
-
 export { configManager } from "./crypto/managers/config.js";
 export * from "./helpers/format-units";
 export * from "./helpers/parse-units";
-
-export const Mainsail: Coins.CoinBundle = bundle({
-	dataTransferObjects: {
-		ConfirmedTransactionData,
-		SignedTransactionData,
-		WalletData,
-	},
-	manifest,
-	serviceProvider: ServiceProvider,
-	services: {},
-});
