55598d8e89c5358f87cf7751cc51b4cc67f52916
Shahin Safaraliyev <shahin@ardenthq.com>
2025-05-05 21:41:55 +0400

    test: enable portfolio domain tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -247,13 +247,13 @@ Object.defineProperty(window, "$zopim", {
 
 const originalHasInstance = Uint8Array[Symbol.hasInstance];
 
-Object.defineProperty(Uint8Array, Symbol.hasInstance, {
-	value(potentialInstance: unknown) {
-		if (this === Uint8Array) {
-			return Object.prototype.toString.call(potentialInstance) === "[object Uint8Array]";
-		}
-		return originalHasInstance.call(this, potentialInstance);
-	},
-});
+// Object.defineProperty(Uint8Array, Symbol.hasInstance, {
+// 	value(potentialInstance: unknown) {
+// 		if (this === Uint8Array) {
+// 			return Object.prototype.toString.call(potentialInstance) === "[object Uint8Array]";
+// 		}
+// 		return originalHasInstance.call(this, potentialInstance);
+// 	},
+// });
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
