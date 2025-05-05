478e680d78a6f1615bda864e841eaf807f31c8da
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-05-05 15:48:37 -0600

    wip

--- src/domains/transaction/components/AddRecipient/AddRecipient.test.tsx
+++ src/domains/transaction/components/AddRecipient/AddRecipient.test.tsx
@@ -8,7 +8,16 @@ import { Route } from "react-router-dom";
 import { BigNumber } from "@/app/lib/helpers";
 import { AddRecipient } from "./AddRecipient";
 import { buildTranslations } from "@/app/i18n/helpers";
-import { env, getDefaultProfileId, MNEMONICS, render, screen, waitFor, within } from "@/utils/testing-library";
+import {
+	env,
+	getDefaultProfileId,
+	MNEMONICS,
+	render,
+	screen,
+	waitFor,
+	within,
+	fixUInt8ArrayIssue,
+} from "@/utils/testing-library";
 
 const translations = buildTranslations();
 
@@ -226,6 +235,8 @@ describe("AddRecipient", () => {
 	});
 
 	it("should show zero amount if wallet has zero or insufficient balance", async () => {
+		const restoreUInt8Patch = fixUInt8ArrayIssue();
+
 		const emptyProfile = await env.profiles().create("Empty");
 
 		const emptyWallet = await emptyProfile.walletFactory().fromMnemonicWithBIP39({
@@ -248,6 +259,8 @@ describe("AddRecipient", () => {
 		await waitFor(() => expect(screen.getByTestId("AddRecipient__amount")).toHaveValue("0"));
 
 		expect(container).toMatchSnapshot();
+
+		restoreUInt8Patch();
 	});
 
 	it("should hide available balance if fee > balance", async () => {
