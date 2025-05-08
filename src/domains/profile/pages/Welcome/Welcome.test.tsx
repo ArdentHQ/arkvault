45e57fe0722468e65fd2ad99d52d4fffec284e04
Shahin Safaraliyev <shahin@ardenthq.com>
2025-05-08 15:22:02 +0400

    wip

--- src/domains/profile/pages/Welcome/Welcome.test.tsx
+++ src/domains/profile/pages/Welcome/Welcome.test.tsx
@@ -122,7 +122,7 @@ describe("Welcome with deeplink", () => {
 	});
 
 	it("should navigate to vote page", async () => {
-		const mockDelegateName = vi.spyOn(env.delegates(), "findByUsername").mockReturnValue(profile.wallets().first());
+		const mockDelegateName = vi.spyOn(env.validators(), "findByUsername").mockReturnValue(profile.wallets().first());
 		const toastWarningSpy = vi.spyOn(toasts, "warning").mockImplementation(vi.fn());
 		const historyPushMock = vi.spyOn(history, "push");
 		const route =
@@ -194,7 +194,7 @@ describe("Welcome with deeplink", () => {
 			.spyOn(passwordProtectedProfile.password(), "get")
 			.mockReturnValue(getDefaultPassword());
 
-		const mockDelegateName = vi.spyOn(env.delegates(), "findByUsername").mockReturnValue(profile.wallets().first());
+		const mockDelegateName = vi.spyOn(env.validators(), "findByUsername").mockReturnValue(profile.wallets().first());
 
 		render(
 			<Route path="/">
@@ -591,7 +591,7 @@ describe("Welcome with deeplink", () => {
 	});
 
 	it("should not navigate when clicking multiple times", async () => {
-		const mockDelegateName = vi.spyOn(env.delegates(), "findByUsername").mockReturnValue(profile.wallets().first());
+		const mockDelegateName = vi.spyOn(env.validators(), "findByUsername").mockReturnValue(profile.wallets().first());
 		const mockProfiles = vi.spyOn(env.profiles(), "values").mockReturnValue([profile]);
 		const mockUsesPassword = vi.spyOn(profile, "usesPassword").mockReturnValue(true);
 
