7c5f75e844a5bc07c5471c4cbdd6e1ca42e79ab8
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-05-08 11:36:17 -0600

    wip

--- src/domains/transaction/components/Transactions/Transactions.test.tsx
+++ src/domains/transaction/components/Transactions/Transactions.test.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/require-await */
+
 import { Contracts } from "@/app/lib/profiles";
 import userEvent from "@testing-library/user-event";
 import { createHashHistory } from "history";
