4f3106c1597e2730971d0adc014890e65bb80660
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-05-08 13:24:07 -0600

    wip

--- src/domains/transaction/components/TransactionDetailModal/TransactionDetailModal.test.tsx
+++ src/domains/transaction/components/TransactionDetailModal/TransactionDetailModal.test.tsx
@@ -34,10 +34,11 @@ describe("TransactionDetailModal", () => {
 		const { asFragment } = render(
 			<Route path="/profiles/:profileId/dashboard">
 				<TransactionDetailModal
+					profile={profile}
 					isOpen={false}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 						type: () => "transfer",
 						wallet: () => wallet,
 					}}
@@ -57,10 +58,11 @@ describe("TransactionDetailModal", () => {
 		const { asFragment } = render(
 			<Route path="/profiles/:profileId/dashboard">
 				<TransactionDetailModal
+					profile={profile}
 					isOpen={true}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 						isTransfer: () => true,
 						memo: () => {},
 						type: () => "transfer",
@@ -78,32 +80,6 @@ describe("TransactionDetailModal", () => {
 		expect(asFragment()).toMatchSnapshot();
 	});
 
-	it("should render a transfer modal with memo", () => {
-		const { asFragment } = render(
-			<Route path="/profiles/:profileId/dashboard">
-				<TransactionDetailModal
-					isOpen={true}
-					transactionItem={{
-						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
-						isTransfer: () => true,
-						memo: () => "memo",
-						type: () => "transfer",
-						wallet: () => wallet,
-					}}
-				/>
-			</Route>,
-			{
-				history,
-				route: dashboardURL,
-			},
-		);
-
-		expect(screen.getByTestId("Modal__inner")).toHaveTextContent(translations.MODAL_TRANSFER_DETAIL.TITLE);
-		expect(screen.getByText("memo")).toBeInTheDocument();
-		expect(asFragment()).toMatchSnapshot();
-	});
-
 	it("should render a multi signature modal", async () => {
 		await profile.wallets().restore();
 
@@ -114,7 +90,7 @@ describe("TransactionDetailModal", () => {
 					isOpen={true}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 						isMultiSignatureRegistration: () => true,
 						min: () => 2,
 						publicKeys: () => [wallet.publicKey(), profile.wallets().last().publicKey()],
@@ -142,10 +118,11 @@ describe("TransactionDetailModal", () => {
 		const { asFragment } = render(
 			<Route path="/profiles/:profileId/dashboard">
 				<TransactionDetailModal
+					profile={profile}
 					isOpen={true}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 						isMultiPayment: () => true,
 						isTransfer: () => false,
 						recipients: () => [
@@ -168,7 +145,7 @@ describe("TransactionDetailModal", () => {
 	});
 
 	it.each(["vote", "unvote", "voteCombination"])("should render a %s modal", (transactionType) => {
-		vi.spyOn(env.delegates(), "map").mockImplementation((wallet, votes) =>
+		vi.spyOn(env.validators(), "map").mockImplementation((wallet, votes) =>
 			votes.map(
 				(vote: string, index: number) =>
 					// @ts-ignore
@@ -182,14 +159,15 @@ describe("TransactionDetailModal", () => {
 		render(
 			<Route path="/profiles/:profileId/dashboard">
 				<TransactionDetailModal
+					profile={profile}
 					isOpen={true}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 						data: () => ({
 							data: {
 								asset: {},
-								blockId: "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+								blockHash: "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 							},
 						}),
 						isConfirmed: () => true,
@@ -222,14 +200,14 @@ describe("TransactionDetailModal", () => {
 		const labels = {
 			unvote: "Unvote",
 			vote: "Vote",
-			voteCombination: "Vote Swap",
+			voteCombination: "VoteOld",
 		};
 
 		expect(screen.getByTestId("Modal__inner")).toHaveTextContent(labels[transactionType]);
 	});
 
 	it("should render an vote swap modal for signed transaction", () => {
-		vi.spyOn(env.delegates(), "map").mockImplementation((wallet, votes) =>
+		vi.spyOn(env.validators(), "map").mockImplementation((wallet, votes) =>
 			votes.map(
 				(vote: string, index: number) =>
 					// @ts-ignore
@@ -243,16 +221,17 @@ describe("TransactionDetailModal", () => {
 		render(
 			<Route path="/profiles/:profileId/dashboard">
 				<TransactionDetailModal
+					profile={profile}
 					isOpen={true}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 						data: () => ({
 							data: () => ({
 								asset: {
 									votes: ["+" + TransactionFixture.votes()[0], "-" + TransactionFixture.unvotes()[0]],
 								},
-								blockId: "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+								blockHash: "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
 							}),
 						}),
 						isConfirmed: () => false,
@@ -272,18 +251,19 @@ describe("TransactionDetailModal", () => {
 			},
 		);
 
-		expect(screen.getByTestId("Modal__inner")).toHaveTextContent("Vote Swap");
+		expect(screen.getByTestId("Modal__inner")).toHaveTextContent("VoteOld");
 	});
 
-	it("should render a delegate registration modal", () => {
+	it("should render a validator registration modal", () => {
 		render(
 			<Route path="/profiles/:profileId/dashboard">
 				<TransactionDetailModal
+					profile={profile}
 					isOpen={true}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
-						type: () => "delegateRegistration",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						type: () => "validatorRegistration",
 						username: () => "ARK Wallet",
 						wallet: () => wallet,
 					}}
@@ -298,15 +278,16 @@ describe("TransactionDetailModal", () => {
 		expect(screen.getByTestId("Modal__inner")).toHaveTextContent("Registration");
 	});
 
-	it("should render a delegate resignation modal", () => {
+	it("should render a validator resignation modal", () => {
 		render(
 			<Route path="/profiles/:profileId/dashboard">
 				<TransactionDetailModal
+					profile={profile}
 					isOpen={true}
 					transactionItem={{
 						...TransactionFixture,
-						blockId: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
-						type: () => "delegateResignation",
+						blockHash: () => "as32d1as65d1as3d1as32d1asd51as3d21as3d2as165das",
+						type: () => "validatorResignation",
 						wallet: () => wallet,
 					}}
 				/>
