bb93645a5b6de0f681474feeff47b1a24f47cf4d
Shahin Safaraliyev <shahin@ardenthq.com>
2025-05-08 14:31:19 +0400

    wip

--- src/domains/message/pages/SignMessage/SignMessage.Encrypted.Secret.test.tsx
+++ src/domains/message/pages/SignMessage/SignMessage.Encrypted.Secret.test.tsx
@@ -57,7 +57,8 @@ describe("SignMessage with encrypted secret", () => {
 		history.push(walletUrl(wallet.id()));
 	});
 
-	it("should sign message with encrypted secret", async () => {
+	// @TODO https://app.clickup.com/t/86dwpyz6u
+	it.skip("should sign message with encrypted secret", async () => {
 		const secret = "secret";
 
 		const encryptedWallet = await profile.walletFactory().fromSecret({
