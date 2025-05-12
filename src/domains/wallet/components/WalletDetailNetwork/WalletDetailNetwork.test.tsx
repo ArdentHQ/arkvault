3c87272d4bd080c193439eb3927dc57814fec514
Shahin Safaraliyev <shahin@ardenthq.com>
2025-05-12 21:09:13 +0400

    wip

--- src/domains/wallet/components/WalletDetailNetwork/WalletDetailNetwork.test.tsx
+++ src/domains/wallet/components/WalletDetailNetwork/WalletDetailNetwork.test.tsx
@@ -1,17 +1,16 @@
-import { expect } from "vitest";
+import { render, screen, env, getDefaultProfileId } from "@/utils/testing-library";
+import { translations } from "@/domains/transaction/i18n";
+import { WalletDetailNetwork } from "./WalletDetailNetwork";
 
 describe("WalletDetailNetwork", () => {
 	it("should render", () => {
-		// const network = availableNetworksMock.find((network) => network.id() === "mainsail.devnet");
-		//
-		// const { asFragment } = render(<WalletDetailNetwork network={network!} />);
-		//
-		// expect(screen.getByTestId("WalletDetailNetwork")).toHaveTextContent(translations.CRYPTOASSET);
-		//
-		// // eslint-disable-next-line testing-library/no-node-access
-		// expect(screen.getByTestId("WalletDetailNetwork").querySelector("svg#ark")).toBeInTheDocument();
-		//
-		// expect(asFragment()).toMatchSnapshot();
-		expect(true).toBeTruthy();
+		const profile = env.profiles().findById(getDefaultProfileId());
+		const { asFragment } = render(<WalletDetailNetwork network={profile.activeNetwork()} />);
+
+		expect(screen.getByTestId("WalletDetailNetwork")).toHaveTextContent(translations.CRYPTOASSET);
+
+		// eslint-disable-next-line testing-library/no-node-access
+		expect(screen.getByTestId("WalletDetailNetwork").querySelector("svg#ark")).toBeInTheDocument();
+		expect(asFragment()).toMatchSnapshot();
 	});
 });
