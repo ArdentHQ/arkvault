8ab7f844c1ac4a69c4e509aa21829dc19fc43444
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-05-13 07:19:01 -0500

    test: side panel new props

--- src/app/components/SidePanel/SidePanel.test.tsx
+++ src/app/components/SidePanel/SidePanel.test.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { SidePanel } from "./SidePanel";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
+import { Icon } from '@/app/components/Icon';
 
 describe("SidePanel", () => {
-	it("should render string header", () => {
+	it("should render title", () => {
 		render(
-			<SidePanel open={true} onOpenChange={vi.fn()} header="side panel header">
+			<SidePanel open={true} onOpenChange={vi.fn()} title="side panel header">
 				{" "}
 				panel body{" "}
 			</SidePanel>,
@@ -15,15 +16,49 @@ describe("SidePanel", () => {
 		expect(screen.getByText("side panel header")).toBeInTheDocument();
 	});
 
-	it("should render component header", () => {
+	it("should render title icon", () => {
 		render(
-			<SidePanel open={true} onOpenChange={vi.fn()} header={<div>side panel header</div>}>
+			<SidePanel open={true} onOpenChange={vi.fn()} title="side panel header" titleIcon={<Icon name="test" data-testid="test-icon" />}>
 				{" "}
 				panel body{" "}
 			</SidePanel>,
 		);
 
-		expect(screen.getByText("side panel header")).toBeInTheDocument();
+		expect(screen.getByTestId("test-icon")).toBeInTheDocument();
+	});
+
+	it("should render subtitle", () => {
+		render(
+			<SidePanel open={true} onOpenChange={vi.fn()} title="side panel header" subtitle="side panel subtitle">
+				{" "}
+				panel body{" "}
+			</SidePanel>,
+		);
+
+		expect(screen.getByText("side panel subtitle")).toBeInTheDocument();
+	});
+
+	it("should display steps if hasSteps is true", () => {
+		render(
+			<SidePanel open={true} onOpenChange={vi.fn()} title="side panel header" hasSteps={true} totalSteps={3} activeStep={1}>
+				{" "}
+				panel body{" "}
+			</SidePanel>,
+		);
+
+		expect(screen.getAllByTestId("SidePanelStyledStep")).toHaveLength(3);
+		expect(screen.getAllByTestId("SidePanelStyledStep")[0]).toHaveClass("bg-theme-warning-300");
+	});
+
+	it("should not display steps if hasSteps is false", () => {
+		render(
+			<SidePanel open={true} onOpenChange={vi.fn()} title="side panel header" hasSteps={false} totalSteps={5} activeStep={3}>
+				{" "}
+				panel body{" "}
+			</SidePanel>,
+		);
+
+		expect(screen.queryAllByTestId("SidePanelStyledStep")).toHaveLength(0);
 	});
 
 	it("should render body", () => {
