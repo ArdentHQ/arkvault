db4aa4d3bab4296952fb93a8df8455ce74c87074
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-05-13 06:52:58 -0500

    refactor: create address side panel implementation

---
+++ src/domains/portfolio/components/CreateWallet/CreateAddressSidePanel.blocks.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Icon, ThemeIcon } from "@/app/components/Icon";
+import { TFunction } from "i18next";
+import { useTranslation } from "react-i18next";
+
+export enum CreateStep {
+	WalletOverviewStep = 1,
+	ConfirmPassphraseStep = 2,
+	EncryptPasswordStep = 3,
+	SuccessStep = 4,
+}
+
+interface StepHeaderConfig {
+	title: string;
+	subtitle?: string;
+	titleIcon?: React.ReactNode;
+}
+
+export const useCreateStepHeaderConfig = (step: CreateStep): StepHeaderConfig => {
+	const { t } = useTranslation();
+
+	switch (step) {
+		case CreateStep.WalletOverviewStep: {
+			return {
+				title: t("WALLETS.PAGE_CREATE_WALLET.PASSPHRASE_STEP.TITLE"),
+				titleIcon: (
+					<ThemeIcon lightIcon="YourPassphraseLight" darkIcon="YourPassphraseDark" dimensions={[24, 24]} />
+				),
+			};
+		}
+
+		case CreateStep.ConfirmPassphraseStep: {
+			return {
+				subtitle: t("WALLETS.PAGE_CREATE_WALLET.PASSPHRASE_CONFIRMATION_STEP.SUBTITLE"),
+				title: t("WALLETS.PAGE_CREATE_WALLET.PASSPHRASE_CONFIRMATION_STEP.TITLE"),
+				titleIcon: <Icon name="ConfirmYourPassphrase" dimensions={[24, 24]} />,
+			};
+		}
+
+		case CreateStep.EncryptPasswordStep: {
+			return {
+				title: t("WALLETS.PAGE_IMPORT_WALLET.ENCRYPT_PASSWORD_STEP.TITLE"),
+				titleIcon: (
+					<ThemeIcon
+						lightIcon="WalletEncryptionLight"
+						darkIcon="WalletEncryptionDark"
+						dimensions={[24, 24]}
+					/>
+				),
+			};
+		}
+
+		case CreateStep.SuccessStep: {
+			return {
+				subtitle: t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.SUBTITLE"),
+				title: t("WALLETS.PAGE_CREATE_WALLET.PROCESS_COMPLETED_STEP.TITLE"),
+				titleIcon: (
+					<Icon
+						name="Completed"
+						dimensions={[24, 24]}
+						className="text-theme-success-100 dark:text-theme-success-900"
+						data-testid="icon-Completed"
+					/>
+				),
+			};
+		}
+
+		default: {
+			return { title: "" };
+		}
+	}
+};
