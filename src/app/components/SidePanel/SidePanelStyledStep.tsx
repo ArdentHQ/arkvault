01327f685e831680c36662423118703afc8c7ec7
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-05-13 06:50:45 -0500

    feat: side panel styled step component

---
+++ src/app/components/SidePanel/SidePanelStyledStep.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import cn from "classnames";
+
+export const SidePanelStyledStep = ({ isActive, ...props }: React.HTMLProps<HTMLLIElement> & { isActive: boolean }) => (
+	<li
+		{...props}
+		className={twMerge(
+			"h-1 flex-1 transition-colors duration-300",
+			cn({
+				"bg-theme-primary-100 dark:bg-theme-secondary-800": !isActive,
+				"bg-theme-warning-300": isActive,
+			}),
+			props.className,
+		)}
+	/>
+);
