c5e0760463e0b34213b3f994984b12daad184a16
goga-m <goga.mamoulasvili@gmail.com>
2025-05-14 12:24:41 +0200

    wip

--- src/app/lib/profiles/profile.repository.ts
+++ src/app/lib/profiles/profile.repository.ts
@@ -76,7 +76,7 @@ export class ProfileRepository implements IProfileRepository {
 			throw new Error(`The profile [${name}] already exists.`);
 		}
 
-		const result: IProfile = ProfileFactory.fromName(name, env);
+		const result: IProfile = ProfileFactory.fromName(name, this.#env);
 
 		this.push(result);
 
