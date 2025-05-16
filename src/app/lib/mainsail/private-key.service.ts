fa8a47effa10e1d0fbf4a9a384dd04330b8649c6
shahin-hq <132887516+shahin-hq@users.noreply.github.com>
2025-05-16 14:38:30 +0400

    refactor: restore `bip39` checks (#1234)

--- src/app/lib/mainsail/private-key.service.ts
+++ src/app/lib/mainsail/private-key.service.ts
@@ -2,18 +2,24 @@
 /* eslint-disable */
 import { Services } from "@/app/lib/sdk";
 import { PrivateKey as BasePrivateKey } from "./crypto/identities/private-key";
+import { BIP39 } from "@ardenthq/arkvault-crypto";
+import { abort_if, abort_unless } from "@/app/lib/helpers";
 
 export class PrivateKeyService {
 	public async fromMnemonic(
 		mnemonic: string,
 		options?: Services.IdentityOptions,
 	): Promise<Services.PrivateKeyDataTransferObject> {
+		abort_unless(BIP39.compatible(mnemonic), "The given value is not BIP39 compliant.");
+
 		return {
 			privateKey: BasePrivateKey.fromPassphrase(mnemonic),
 		};
 	}
 
 	public async fromSecret(secret: string): Promise<Services.PrivateKeyDataTransferObject> {
+		abort_if(BIP39.compatible(secret), "The given value is BIP39 compliant. Please use [fromMnemonic] instead.");
+
 		return {
 			privateKey: BasePrivateKey.fromPassphrase(secret),
 		};
--- src/app/lib/mainsail/public-key.service.ts
+++ src/app/lib/mainsail/public-key.service.ts
@@ -1,16 +1,21 @@
 import { Services, Exceptions } from "@/app/lib/sdk";
 
 import { PublicKey } from "@arkecosystem/typescript-crypto";
-import { Bls } from "@ardenthq/arkvault-crypto";
+import { BIP39, Bls } from "@ardenthq/arkvault-crypto";
+import { abort_if, abort_unless } from "@/app/lib/helpers";
 
 export class PublicKeyService {
 	public fromMnemonic(mnemonic: string): Services.PublicKeyDataTransferObject {
+		abort_unless(BIP39.compatible(mnemonic), "The given value is not BIP39 compliant.");
+
 		return {
 			publicKey: PublicKey.fromPassphrase(mnemonic).publicKey,
 		};
 	}
 
 	public fromSecret(secret: string): Services.PublicKeyDataTransferObject {
+		abort_if(BIP39.compatible(secret), "The given value is BIP39 compliant. Please use [fromMnemonic] instead.");
+
 		return {
 			publicKey: PublicKey.fromPassphrase(secret).publicKey,
 		};
