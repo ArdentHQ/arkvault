d3b748476ab92ea5856e36464c5c22e01ac51850
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2025-05-16 17:23:04 +0200

    refactor: simplify http client (#1237)

--- src/app/services/index.tsx
+++ src/app/services/index.tsx
@@ -1,5 +1,5 @@
-import { HttpClient } from "./HttpClient";
+import { Http } from "@/app/lib/mainsail";
 import { ToastService } from "./ToastService";
 
-export const httpClient = new HttpClient(10);
+export const httpClient = new Http.HttpClient(10);
 export const toasts = new ToastService();
