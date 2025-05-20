e8718b2b34e9bd261e61bd00da992f351774c6df
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-05-20 08:52:00 -0600

    wip

--- src/domains/transaction/validations/ValidatorRegistration.test.tsx
+++ src/domains/transaction/validations/ValidatorRegistration.test.tsx
@@ -2,13 +2,16 @@ import { Contracts } from "@/app/lib/profiles";
 import { validatorRegistration } from "./ValidatorRegistration";
 import { env, getMainsailProfileId } from "@/utils/testing-library";
 import { requestMock, server } from "@/tests/mocks/server";
-
+import { Networks } from "@/app/lib/mainsail";
 let profile: Contracts.IProfile;
 let translationMock: any;
+let network: Networks.Network;
 
 describe("Register validator validation", () => {
 	beforeAll(async () => {
 		profile = env.profiles().findById(getMainsailProfileId());
+
+		network = profile.wallets().first().network();
 
 		await env.profiles().restore(profile);
 
@@ -18,26 +21,31 @@ describe("Register validator validation", () => {
 	});
 
 	it("should validate BLS public key", async () => {
-		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile.wallets().first());
+		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile, network);
 
 		await expect(
 			validate.pattern(
 				"a08058db53e2665c84a40f5152e76dd2b652125a6079130d4c315e728bcf4dd1dfb44ac26e82302331d61977d3141118",
 			),
-		).resolves.toBe(true);
+		).toBe(true);
 	});
 
 	it("should fail to validate BLS public key", async () => {
-		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile.wallets().first());
+		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile, network);
 
-		await expect(validate.pattern("test")).resolves.not.toBe(true);
+		await expect(validate.pattern("test")).not.toBe(true);
 	});
 
 	it("should pass if public key hasn't used", async () => {
-		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile.wallets().first());
+		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile, network);
 
 		// Emulate public key hasn't used
-		server.use(requestMock(`https://dwallets-evm.mainsailhq.com/api/wallets*`, { meta: { count: 0 } }));
+		server.use(
+			requestMock(
+				`https://dwallets-evm.mainsailhq.com/api?attributes.validatorPublicKey=a08058db53e2665c84a40f5152e76dd2b652125a6079130d4c315e728bcf4dd1dfb44ac26e82302331d61977d3141118`,
+				{ meta: { count: 0 } },
+			),
+		);
 
 		await expect(
 			validate.unique(
@@ -47,10 +55,15 @@ describe("Register validator validation", () => {
 	});
 
 	it("should fail if public key has used", async () => {
-		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile.wallets().first());
+		const { validate } = validatorRegistration(translationMock).validatorPublicKey(profile, network);
 
 		// Emulate public key has used
-		server.use(requestMock(`https://dwallets-evm.mainsailhq.com/api/wallets*`, { meta: { count: 1 } }));
+		server.use(
+			requestMock(
+				`https://dwallets-evm.mainsailhq.com/api?attributes.validatorPublicKey=a08058db53e2665c84a40f5152e76dd2b652125a6079130d4c315e728bcf4dd1dfb44ac26e82302331d61977d3141118`,
+				{ meta: { count: 1 } },
+			),
+		);
 
 		await expect(
 			validate.unique(
