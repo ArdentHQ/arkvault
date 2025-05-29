d69d58b78349228566c68ac0cd042b59978582cd
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-05-29 12:41:08 -0600

    address dropdown tests

--- src/domains/profile/components/SelectAddressDropdown/SelectAddressDropdown.test.tsx
+++ src/domains/profile/components/SelectAddressDropdown/SelectAddressDropdown.test.tsx
@@ -37,184 +37,81 @@ describe("SelectAddressDropdown", () => {
 		expect(container).toMatchSnapshot();
 	});
 
-	it("should render without user icon", () => {
-		const { container } = render(
-			<SelectAddressDropdown showUserIcon={false} wallets={wallets} profile={profile} />,
-		);
-
-		expect(container).toMatchSnapshot();
-	});
-
-	it("should render without a wallet avatar", () => {
-		render(<SelectAddressDropdown showWalletAvatar={false} wallets={wallets} profile={profile} />);
-
-		expect(screen.queryByTestId("Avatar")).not.toBeInTheDocument();
-	});
-
 	it("should render with wallet name by default", () => {
 		const displayNameSpy = vi.spyOn(wallet, "displayName").mockReturnValue("test");
 
-		render(
-			<SelectAddressDropdown
-				wallets={wallets}
-				profile={profile}
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-			/>,
-		);
+		render(<SelectAddressDropdown wallets={wallets} profile={profile} wallet={wallet} />);
 
-		expect(screen.getByTestId("Address__alias")).toBeInTheDocument();
+		expect(screen.getByText("test")).toBeInTheDocument();
 
 		displayNameSpy.mockRestore();
 	});
 
-	it("should render without wallet name", () => {
-		render(
-			<SelectAddressDropdown
-				showWalletName={false}
-				wallets={wallets}
-				profile={profile}
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-			/>,
+	it("should render invalid", () => {
+		const { container } = render(
+			<SelectAddressDropdown isInvalid wallets={wallets} profile={profile} wallet={wallet} />,
 		);
 
-		expect(screen.queryByTestId("Address__alias")).not.toBeInTheDocument();
-	});
+		expect(screen.getAllByTestId("Input__error")[0]).toBeVisible();
 
-	it("should render invalid", () => {
-		const { container } = render(<SelectAddressDropdown isInvalid wallets={wallets} profile={profile} />);
-
-		expect(screen.getByTestId("Input__error")).toBeVisible();
 		expect(container).toMatchSnapshot();
 	});
 
 	it("should render with preselected address", () => {
-		const { container } = render(
-			<SelectAddressDropdown
-				wallets={wallets}
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-				profile={profile}
-			/>,
-		);
+		const { container } = render(<SelectAddressDropdown wallets={wallets} wallet={wallet} profile={profile} />);
 
-		expect(screen.getByTestId("SelectAddressDropdown__input")).toHaveValue(wallet.address());
+		expect(screen.getByTestId("select-list__input")).toHaveValue(wallet.address());
 
 		expect(container).toMatchSnapshot();
 	});
 
 	it("should open and close wallets modal", async () => {
-		render(
-			<SelectAddressDropdown
-				wallets={wallets}
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-				profile={profile}
-			/>,
-		);
+		render(<SelectAddressDropdown wallets={wallets} wallet={wallet} profile={profile} />);
 
-		expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
+		expect(screen.queryByTestId("SelectDropdown__option--0")).not.toBeInTheDocument();
 
-		await userEvent.click(screen.getByTestId("SelectAddressDropdown__wrapper"));
+		await userEvent.click(screen.getByTestId("SelectDropdown__input"));
 
-		expect(screen.getByTestId("Modal__inner")).toBeInTheDocument();
+		expect(screen.getByTestId("SelectDropdown__option--0")).toBeInTheDocument();
 
-		await userEvent.click(screen.getByTestId("Modal__close-button"));
+		await userEvent.keyboard("{Escape}");
 
 		await waitFor(() => {
-			expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
+			expect(screen.queryByTestId("SelectDropdown__option--0")).not.toBeInTheDocument();
 		});
 	});
 
 	it("should not open if disabled", async () => {
-		render(
-			<SelectAddressDropdown
-				wallets={wallets}
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-				profile={profile}
-				disabled={true}
-			/>,
-		);
+		render(<SelectAddressDropdown wallets={wallets} wallet={wallet} profile={profile} disabled={true} />);
 
-		expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
+		expect(screen.queryByTestId("SelectDropdown__option--0")).not.toBeInTheDocument();
 
-		await userEvent.click(screen.getByTestId("SelectAddressDropdown__wrapper"));
+		await userEvent.click(screen.getByTestId("SelectDropdown__input"));
 
-		expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
-	});
-
-	it("should select address from wallets modal", async () => {
-		render(
-			<SelectAddressDropdown
-				wallets={wallets}
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-				profile={profile}
-			/>,
-		);
-
-		expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
-
-		await userEvent.click(screen.getByTestId("SelectAddressDropdown__wrapper"));
-
-		await waitFor(() => {
-			expect(screen.getByTestId("Modal__inner")).toBeInTheDocument();
-		});
-
-		const firstAddress = screen.getByTestId("SearchWalletListItem__select-0");
-
-		await userEvent.click(firstAddress);
-
-		await waitFor(() => {
-			expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
-		});
-
-		expect(screen.getByTestId("SelectAddressDropdown__input")).toHaveValue(wallets[0].address());
-	});
-
-	it("should not open wallets modal if disabled", async () => {
-		render(
-			<SelectAddressDropdown
-				wallets={wallets}
-				disabled
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-				profile={profile}
-			/>,
-		);
-
-		expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
-
-		await userEvent.click(screen.getByTestId("SelectAddressDropdown__wrapper"));
-
-		await waitFor(() => {
-			expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
-		});
+		expect(screen.queryByTestId("SelectDropdown__option--0")).not.toBeInTheDocument();
 	});
 
 	it("should call onChange prop if provided", async () => {
 		const onChange = vi.fn();
 
-		render(
-			<SelectAddressDropdown
-				wallets={wallets}
-				onChange={onChange}
-				wallet={{ address: wallet.address(), network: wallet.network() }}
-				profile={profile}
-			/>,
-		);
+		render(<SelectAddressDropdown wallets={wallets} onChange={onChange} wallet={wallet} profile={profile} />);
 
-		expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
+		expect(screen.queryByTestId("SelectDropdown__option--0")).not.toBeInTheDocument();
 
-		await userEvent.click(screen.getByTestId("SelectAddressDropdown__wrapper"));
+		await userEvent.click(screen.getByTestId("SelectDropdown__input"));
 
 		await waitFor(() => {
-			expect(screen.getByTestId("Modal__inner")).toBeInTheDocument();
+			expect(screen.getByTestId("SelectDropdown__option--0")).toBeInTheDocument();
 		});
 
-		const firstAddress = screen.getByTestId("SearchWalletListItem__select-0");
+		const firstAddress = screen.getByTestId("SelectDropdown__option--0");
 
 		await userEvent.click(firstAddress);
 
 		await waitFor(() => {
-			expect(screen.queryByTestId("Modal__inner")).not.toBeInTheDocument();
+			expect(screen.queryByTestId("SelectDropdown__option--0")).not.toBeInTheDocument();
 		});
 
-		expect(onChange).toHaveBeenCalledWith(wallets[0].address());
+		expect(onChange).toHaveBeenCalledWith(wallets[0]);
 	});
 });
