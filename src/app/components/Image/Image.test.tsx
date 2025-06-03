3d26485d4cb26dfd82e1f6293d42175920a44959
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-06-02 19:58:38 -0500

    test: dim theme

--- src/app/components/Image/Image.test.tsx
+++ src/app/components/Image/Image.test.tsx
@@ -18,6 +18,14 @@ describe("Image", () => {
 		expect(container).toBeInTheDocument();
 		expect(asFragment()).toMatchSnapshot();
 	});
+	
+	it("should render in dim mode", () => {
+		const useThemeMock = vi.spyOn(theme, "shouldUseDimColors").mockReturnValue(true);
+		const { container } = renderWithoutRouter(<Image name="GenericError" domain="error" />);
+
+		expect(container).toBeInTheDocument();
+		useThemeMock.mockRestore();
+	});
 
 	it("should render in dark mode", () => {
 		const useThemeMock = vi.spyOn(theme, "shouldUseDarkColors").mockReturnValue(true);
