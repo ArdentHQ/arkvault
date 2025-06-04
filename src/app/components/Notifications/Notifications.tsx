a9bf63c9af7512f49fcad3951f6029728e8236ad
patricio0312rev <patricio0312rev@users.noreply.github.com>
2025-06-04 18:21:16 +0000

    style: resolve style guide violations

--- src/app/components/Notifications/Notifications.tsx
+++ src/app/components/Notifications/Notifications.tsx
@@ -24,7 +24,7 @@ export const Notifications = ({ profile, onTransactionClick, hideDropdown }: Not
 			className="dark:bg-theme-dark-900 dim:bg-theme-dim-700 w-full sm:w-[35rem]"
 			data-testid="NotificationsWrapper"
 		>
-			<div className="flex items-center justify-between px-6 py-4 bg-theme-secondary-100 dim:bg-theme-dim-950 rounded-t-xl dark:bg-black">
+			<div className="bg-theme-secondary-100 dim:bg-theme-dim-950 flex items-center justify-between rounded-t-xl px-6 py-4 dark:bg-black">
 				<div className="text-theme-secondary-700 dark:text-theme-dark-200 dim:text-theme-dim-200 text-sm leading-[17px] font-semibold">
 					Notification
 				</div>
@@ -35,8 +35,8 @@ export const Notifications = ({ profile, onTransactionClick, hideDropdown }: Not
 					<EmptyBlock className="py-4">
 						<span>{t("COMMON.NOTIFICATIONS.EMPTY")}</span>
 					</EmptyBlock>
-					<div className="w-full mt-8 mb-2">
-						<Image name="EmptyNotifications" className="w-full mx-auto sm:w-64" />
+					<div className="mt-8 mb-2 w-full">
+						<Image name="EmptyNotifications" className="mx-auto w-full sm:w-64" />
 					</div>
 				</div>
 			)}
