eb7421c6e6864064c9bfd4dafd1adf2ac62f2501
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-06-03 23:28:26 -0500

    refactor: dim theme support for back button

--- src/app/components/NavigationBar/components/BackButton/BackButton.tsx
+++ src/app/components/NavigationBar/components/BackButton/BackButton.tsx
@@ -16,10 +16,10 @@ const StyledBackButton = ({
 	<button
 		{...props}
 		className={twMerge(
-			"border-b-theme-secondary-300 bg-theme-secondary-100 dark:border-b-theme-dark-700 dark:bg-theme-dark-950 flex w-9 items-center border-b transition-colors duration-200",
+			"border-b-theme-secondary-300 bg-theme-secondary-100 dark:border-b-theme-dark-700 dark:bg-theme-dark-950 dim:bg-theme-dim-950 dim:border-b-theme-dim-700 flex w-9 items-center border-b transition-colors duration-200",
 			cn({
 				"text-theme-secondary-500 dark:text-theme-secondary-800 cursor-not-allowed": disabled,
-				"text-theme-secondary-500 hover:bg-theme-primary-200 hover:text-theme-primary-700 focus:ring-theme-primary-400 dark:text-theme-dark-50 dark:hover:bg-theme-dark-700 focus:rounded focus:ring-2 focus:outline-hidden focus:ring-inset":
+				"text-theme-secondary-500 hover:bg-theme-primary-200 hover:text-theme-primary-700 focus:ring-theme-primary-400 dark:text-theme-dark-50 dark:hover:bg-theme-dark-700 dim:text-theme-dim-navy-600 dim-hover:text-theme-dim-500 cursor-pointer focus:rounded focus:ring-2 focus:outline-hidden focus:ring-inset":
 					!disabled,
 			}),
 			props.className,
