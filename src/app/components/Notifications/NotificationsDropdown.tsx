1c559099efa7c97fff48c0e1ae5632101b433e35
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-06-03 23:29:03 -0500

    refactor: dim theme support for navbar buttons

--- src/app/components/Notifications/NotificationsDropdown.tsx
+++ src/app/components/Notifications/NotificationsDropdown.tsx
@@ -30,9 +30,9 @@ export const NotificationsDropdown = ({ profile }: { profile: Contracts.IProfile
 								variant="transparent"
 								size="icon"
 								data-testid="NavigationBar__buttons--notifications"
-								className="text-theme-secondary-700 dark:text-theme-dark-200 dark:hover:text-theme-dark-50 hover:text-theme-primary-700"
+								className="text-theme-secondary-700 dark:text-theme-dark-200 dark:hover:text-theme-dark-50 hover:text-theme-primary-700 dim:text-theme-dim-200 dim-hover:text-theme-dim-50"
 							>
-								<Icon name="Bell" size="lg" className="m-0 p-1" />
+								<Icon name="Bell" size="lg" className="p-1 m-0" />
 								{hasUnread && <Dot />}
 							</Button>
 						</Tooltip>
