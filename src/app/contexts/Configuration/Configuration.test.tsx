f1ad97d840fe7b503ca7d03c946131e151aa606c
goga-m <goga-m@users.noreply.github.com>
2025-06-06 12:00:04 +0000

    style: resolve style guide violations

--- src/app/contexts/Configuration/Configuration.test.tsx
+++ src/app/contexts/Configuration/Configuration.test.tsx
@@ -19,17 +19,15 @@ describe("Configuration Context", () => {
 	});
 
 	it("should throw without provider", () => {
-		const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => { });
+		const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {});
 
 		let error;
 		try {
 			renderHook(() => useConfiguration());
-		} catch (e) {
-			error = e;
+		} catch (error_) {
+			error = error_;
 		}
-		expect(error).toEqual(
-			new Error("[useConfiguration] Component not wrapped within a Provider")
-		);
+		expect(error).toEqual(new Error("[useConfiguration] Component not wrapped within a Provider"));
 		expect(() => {
 			renderHook(() => useConfiguration());
 		}).toThrow("[useConfiguration] Component not wrapped within a Provider");
