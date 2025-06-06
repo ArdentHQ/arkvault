260d3c2cbf9bc5b693a6c9a02eb1f5a8f3c3d5ee
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-06-06 07:10:27 -0700

    refactor: dim theme support for mobile card

--- src/app/components/Table/Mobile/MobileSection.tsx
+++ src/app/components/Table/Mobile/MobileSection.tsx
@@ -8,8 +8,12 @@ interface MobileSectionProps extends HTMLAttributes<HTMLDivElement> {
 
 export const MobileSection: FC<MobileSectionProps> = ({ className, title, children, ...props }) => (
 	<div className={twMerge("flex w-36 flex-col gap-2", className)} {...props}>
-		<span className="text-theme-secondary-700 dark:text-theme-secondary-500 text-sm font-semibold">{title}</span>
+		<span className="text-theme-secondary-700 dark:text-theme-secondary-500 dim:text-theme-dim-200 text-sm font-semibold">
+			{title}
+		</span>
 
-		<div className="text-theme-secondary-900 dark:text-theme-secondary-200 text-sm font-semibold">{children}</div>
+		<div className="text-theme-secondary-900 dark:text-theme-secondary-200 dim:text-theme-dim-50 text-sm font-semibold">
+			{children}
+		</div>
 	</div>
 );
