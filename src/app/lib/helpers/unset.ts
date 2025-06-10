b0de0f6448f3df56a1f9748740626539f4bd6164
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-06-10 12:47:11 -0600

    wip

--- src/app/lib/helpers/unset.ts
+++ src/app/lib/helpers/unset.ts
@@ -2,7 +2,7 @@ import dot from "dot-prop";
 import { isObject } from "./is-object.js";
 import { isString } from "./is-string.js";
 
-export const unset = <T>(object: T, path: string | string[]): boolean => {
+export const unset = <T>(object: T, path: string): boolean => {
 	if (!isObject(object) || !isString(path)) {
 		return false;
 	}
