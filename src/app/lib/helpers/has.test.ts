d23adb7aefeccf19d79c8c79da4287c39e86d174
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-06-10 12:33:10 -0600

    coverage for has helper

--- src/app/lib/helpers/has.test.ts
+++ src/app/lib/helpers/has.test.ts
@@ -3,11 +3,19 @@ import { has } from "./has";
 
 describe("has", () => {
 	it("should return false if the target is not an object", () => {
+		expect(has(null, "a.b.c")).toBe(false);
+		expect(has(undefined, "a.b.c")).toBe(false);
+		expect(has(1, "a.b.c")).toBe(false);
+		expect(has("string", "a.b.c")).toBe(false);
 		expect(has([], "a.b.c")).toBe(false);
 	});
 
 	it("should return false if the path is not a string", () => {
-		expect(has({}, "123")).toBe(false);
+		expect(has({}, null as any)).toBe(false);
+		expect(has({}, undefined as any)).toBe(false);
+		expect(has({}, 123 as any)).toBe(false);
+		expect(has({}, [] as any)).toBe(false);
+		expect(has({}, {} as any)).toBe(false);
 	});
 
 	it("should not do anything if the object is not an object", () => {
