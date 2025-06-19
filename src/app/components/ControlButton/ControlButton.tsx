c63a975a676047132e790095dc7aeeb2abcfeded
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-06-19 11:24:42 -0500

    refactor: dim theme support for control group

--- src/app/components/ControlButton/ControlButton.tsx
+++ src/app/components/ControlButton/ControlButton.tsx
@@ -10,13 +10,13 @@ const ControlButtonStyled = ({
 	<button
 		{...properties}
 		className={twMerge(
-			"text-theme-secondary-700 disabled:text-theme-secondary-400 dark:text-theme-secondary-500 dark:disabled:text-theme-secondary-700 relative flex cursor-pointer items-center justify-center py-2 font-semibold transition-colors duration-200 focus:outline-hidden disabled:cursor-not-allowed",
+			"text-theme-secondary-700 disabled:text-theme-secondary-400 dark:text-theme-secondary-500 dark:disabled:text-theme-secondary-700 dim:text-theme-dim-500 dim:disabled:text-theme-secondary-600 relative flex cursor-pointer items-center justify-center py-2 font-semibold transition-colors duration-200 focus:outline-hidden disabled:cursor-not-allowed",
 			cn({
-				"hover:bg-theme-primary-100 hover:text-theme-primary-700 dark:hover:bg-theme-secondary-800 dark:hover:text-white":
+				"hover:bg-theme-primary-100 hover:text-theme-primary-700 dark:hover:bg-theme-secondary-800 dim-hover:bg-theme-dim-700 dim-hover:text-theme-dim-50 dark:hover:text-white":
 					disabled || noBorder,
-				"hover:border-b-theme-primary-400 hover:text-theme-primary-400 [&.active]:border-b-theme-primary-600 [&.active]:text-theme-primary-600 [&.active]:data-focus-visible-added:rounded":
+				"hover:border-b-theme-primary-400 hover:text-theme-primary-400 dim-hover:border-b-theme-dim-navy-600 dim-hover:text-theme-dim-navy-600 [&.active]:border-b-theme-primary-600 [&.active]:text-theme-primary-600 [&.active]:data-focus-visible-added:rounded":
 					!noBorder && !disabled,
-				"hover:text-theme-primary-400": !disabled,
+				"hover:text-theme-primary-400 dim-hover:text-theme-dim-navy-600": !disabled,
 				"mx-0.5 px-2.5": !noBorder,
 				"rounded px-2 py-1.5": noBorder,
 			}),
@@ -41,7 +41,7 @@ export const ControlButton = ({ isChanged, children, className, ...properties }:
 							{ "-mr-2.5": properties.noBorder },
 						)}
 					>
-						<div className="bg-theme-primary-500 h-2 w-2 rounded-full" />
+						<div className="bg-theme-primary-500 dim:bg-theme-dim-navy-600 h-2 w-2 rounded-full" />
 					</div>
 				)}
 				{children}
