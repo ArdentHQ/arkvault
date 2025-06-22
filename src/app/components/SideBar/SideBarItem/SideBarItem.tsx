908bff7c4e4060b0cd9daea06d169f327ea6abf9
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-06-22 07:27:01 -0500

    refactor: dim theme support for side bar

--- src/app/components/SideBar/SideBarItem/SideBarItem.tsx
+++ src/app/components/SideBar/SideBarItem/SideBarItem.tsx
@@ -21,8 +21,8 @@ export const SideBarItem = withRouter(
 				className={cn(
 					"group relative flex h-12 items-center rounded-lg px-5 transition-all",
 					isActive
-						? "bg-theme-secondary-200 text-theme-primary-600 dark:bg-theme-dark-950 dark:text-theme-dark-50"
-						: "hover:bg-theme-secondary-200 hover:text-theme-secondary-800 dark:hover:bg-theme-dark-950 dark:hover:text-theme-dark-50",
+						? "bg-theme-secondary-200 text-theme-primary-600 dark:bg-theme-dark-950 dark:text-theme-dark-50 dim:bg-theme-dim-950 dim:text-theme-dim-50"
+						: "hover:bg-theme-secondary-200 hover:text-theme-secondary-800 dark:hover:bg-theme-dark-950 dark:hover:text-theme-dark-50 dim-hover:bg-theme-dim-950 dim-hover:text-theme-dim-50",
 				)}
 			>
 				<span className="font-semibold">{label}</span>
