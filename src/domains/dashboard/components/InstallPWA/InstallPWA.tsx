2453c54094d07e38d0b5bad54522269d181996c5
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-06-25 19:35:09 -0500

    refactor: dim theme for pwa banner

--- src/domains/dashboard/components/InstallPWA/InstallPWA.tsx
+++ src/domains/dashboard/components/InstallPWA/InstallPWA.tsx
@@ -20,19 +20,19 @@ export const InstallPWA = () => {
 			{showInstallBanner && (
 				<div
 					data-testid="InstallPWA"
-					className="bg-theme-primary-50 text-theme-secondary-900 dark:bg-theme-secondary-800 fixed bottom-0 left-1/2 z-50 flex w-full -translate-x-1/2 flex-col items-stretch overflow-hidden px-8 sm:flex-row sm:px-0 md:mb-4 md:max-w-xl md:rounded-xl"
+					className="bg-theme-primary-50 text-theme-secondary-900 dark:bg-theme-secondary-800 dim:bg-theme-dim-800 fixed bottom-0 left-1/2 z-50 flex w-full -translate-x-1/2 flex-col items-stretch overflow-hidden px-8 sm:flex-row sm:px-0 md:mb-4 md:max-w-xl md:rounded-xl"
 				>
 					<button
 						data-testid="InstallPWA__close"
 						type="button"
 						onClick={hideInstallBanner}
-						className="text-theme-primary-600 dark:hover:bg-theme-secondary-900 hover:bg-theme-primary-100 hover:text-theme-primary-700 flex shrink-0 items-center justify-center p-4"
+						className="text-theme-primary-600 dark:hover:bg-theme-secondary-900 hover:bg-theme-primary-100 hover:text-theme-primary-700 dim-hover:bg-theme-dim-900 dim-hover:text-theme-navy-600 flex shrink-0 cursor-pointer items-center justify-center p-4"
 					>
 						<Icon name="Cross" size="md" />
 					</button>
 
 					<div className="mb-4 sm:mb-0 sm:py-4">
-						<div className="border-theme-secondary-300 dark:border-theme-secondary-700 w-full border-b sm:h-full sm:w-auto sm:border-l" />
+						<div className="border-theme-secondary-300 dark:border-theme-secondary-700 dim:border-theme-dim-700 w-full border-b sm:h-full sm:w-auto sm:border-l" />
 					</div>
 
 					<div className="text-theme-text mx-auto flex max-w-60 grow flex-col justify-center text-center sm:max-w-full sm:px-4 sm:text-left">
