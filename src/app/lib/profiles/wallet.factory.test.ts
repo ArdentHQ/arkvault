cd70a762984403f928301483e6f98b4640538335
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-07-09 09:06:57 -0600

    test wallet factory

---
+++ src/app/lib/profiles/wallet.factory.test.ts
@@ -0,0 +1,31 @@
+import { describe, vi, expect, beforeEach, it, afterEach } from "vitest";
+import { IProfile, IWalletFactory } from "./contracts.js";
+import { Wallet } from "./wallet.js";
+import { env } from "@/app/lib/mainsail/env.js";
+
+let profile: IProfile;
+let subject: IWalletFactory;
+
+describe("WalletFactory", () => {
+	beforeEach(async () => {
+		profile = await env.profiles().create("test profile");
+		subject = profile.walletFactory();
+	});
+
+	afterEach(() => {
+		env.profiles().forget(profile.id());
+		vi.restoreAllMocks();
+	});
+
+	describe("generate", () => {
+		it("should generate a wallet", async () => {
+			const { mnemonic, wallet } = await subject.generate({
+				locale: "english",
+				wordCount: 12,
+			});
+
+			expect(mnemonic.split(" ")).toHaveLength(12);
+			expect(wallet).toBeInstanceOf(Wallet);
+		});
+	});
+});
