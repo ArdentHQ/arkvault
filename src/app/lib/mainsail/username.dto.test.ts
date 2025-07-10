7fa64db0f855f02caed55b0a9226939202d1d8af
goga-m <goga.mamoulasvili@gmail.com>
2025-07-10 17:49:32 +0200

    wip

--- src/app/lib/mainsail/username.dto.test.ts
+++ src/app/lib/mainsail/username.dto.test.ts
@@ -1,25 +1,25 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
-import { UsernameData } from "./username.dto"
+import { describe, it, expect } from "vitest";
+import { UsernameData } from "./username.dto";
 
 describe("UsernameData", () => {
 	const data = {
 		address: "0x125b484e51Ad990b5b3140931f3BD8eAee85Db23",
-		username: "test"
-	}
+		username: "test",
+	};
 
 	it("#address", async () => {
-		expect(new UsernameData(data).address()).toEqual(data.address)
+		expect(new UsernameData(data).address()).toEqual(data.address);
 	});
 
 	it("#username", async () => {
-		expect(new UsernameData(data).username()).toEqual(data.username)
+		expect(new UsernameData(data).username()).toEqual(data.username);
 	});
 
 	it("#toObject", async () => {
-		expect(new UsernameData(data).toObject()).toEqual(data)
+		expect(new UsernameData(data).toObject()).toEqual(data);
 	});
 
 	it("#raw", async () => {
-		expect(new UsernameData(data).raw()).toEqual(data)
+		expect(new UsernameData(data).raw()).toEqual(data);
 	});
 });
