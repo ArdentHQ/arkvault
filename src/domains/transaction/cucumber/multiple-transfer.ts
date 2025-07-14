76e76c6c9f7bf37db36610e5c73c7e5894c453c7
Shahin Safaraliyev <shahin@ardenthq.com>
2025-07-14 23:25:18 +0400

    test: enable transaction e2e tests

--- src/domains/transaction/cucumber/multiple-transfer.ts
+++ src/domains/transaction/cucumber/multiple-transfer.ts
@@ -31,7 +31,7 @@ cucumber(
 			await t.typeText(amountInput, "10", { replace: true });
 			await t.typeText(
 				Selector("[data-testid=SelectDropdown__input]").nth(0),
-				"D7JJ4ZfkJDwDCwuwzhtbCFapBUCWU3HHGP",
+				"0xcd15953dD076e56Dc6a5bc46Da23308Ff3158EE6",
 				{
 					paste: true,
 				},
@@ -56,7 +56,7 @@ cucumber(
 		mockRequest(
 			{
 				method: "POST",
-				url: "https://ark-test.arkvault.io/api/transactions",
+				url: "https://dwallets-evm.mainsailhq.com/tx/api/transactions",
 			},
 			{
 				data: {
@@ -70,7 +70,42 @@ cucumber(
 		mockRequest(
 			{
 				method: "GET",
-				url: "https://ark-test.arkvault.io/api/transactions/ee4af162f9744d6c1bb2be3b87a489bca98b621b2bd712bd03fce07bd9ae3521",
+				url: "https://dwallets-evm.mainsailhq.com/api/transactions/76a164b1c07d5ecde843c12c78576cc34f92dcd44ed2efa0834c1f956c44ea8d",
+			},
+			{
+				"data": {
+					"blockHash": "05b124023ddd656c8a95664eb61846cc0f4e204341a0d86db325771077e7f002",
+					"confirmations": 1,
+					"data": "",
+					"from": "0x659A76be283644AEc2003aa8ba26485047fd1BFB",
+					"gas": "21000",
+					"gasPrice": "100000000000",
+					"hash": "76a164b1c07d5ecde843c12c78576cc34f92dcd44ed2efa0834c1f956c44ea8d",
+					"nonce": "3",
+					"senderPublicKey": "0311b11b0dea8851d49af7c673d7032e37ee12307f9bbd379b64bbdac6ca302e84",
+					"signature": "cd1b35240b0c1303392e4dc3e1fc83b9da7b74e5c96b99d1ae207c7c9d5480d868ecf4235298c6438f9c0ea9a8274082ebf051d86ff353ae1fb4fffe86cad91101",
+					"to": "0x47ea9bAa16edd859C1792933556c4659A647749C",
+					"value": "2000000000000000000",
+					"timestamp": "1752502567204",
+					"receipt": {
+						"gasRefunded": 0,
+						"gasUsed": 21000,
+						"status": 1
+					}
+				}
+			}
+		),
+		mockRequest(
+			{
+				method: "GET",
+				url: "https://dwallets-evm.mainsailhq.com/api/blocks/05b124023ddd656c8a95664eb61846cc0f4e204341a0d86db325771077e7f002",
+			},
+			{}
+		),
+		mockRequest(
+			{
+				method: "GET",
+				url: "https://dwallets-evm.mainsailhq.com/api/transactions?page=1&limit=20&from=0x659A76be283644AEc2003aa8ba26485047fd1BFB",
 			},
 			{
 				data: {},
@@ -85,7 +120,7 @@ cucumber(
 		"When she attempts to send a multipay transaction with an invalid mnemonic": async (t: TestController) => {
 			await t.click(Selector("span").withText(translations.TRANSACTION.MULTIPLE));
 			await t.typeText(amountInput, "10", { replace: true });
-			await t.typeText(recipientInput, "D7JJ4ZfkJDwDCwuwzhtbCFapBUCWU3HHGP", {
+			await t.typeText(recipientInput, "0xcd15953dD076e56Dc6a5bc46Da23308Ff3158EE6", {
 				paste: true,
 			});
 			await t.pressKey("tab");
@@ -106,34 +141,18 @@ cucumber(
 		"And the send button is disabled": async (t: TestController) => {
 			await t.expect(sendButton.hasAttribute("disabled")).ok();
 		},
-	},
-	[
-		mockRequest(
-			{
-				method: "POST",
-				url: "https://ark-test.arkvault.io/api/transactions",
-			},
-			{
-				data: {
-					accept: ["transaction-id"],
-					broadcast: ["transaction-id"],
-					excess: [],
-					invalid: [],
-				},
-			},
-		),
-	],
+	}
 );
 cucumber("@multipayTransaction-notClearValues", {
 	...preSteps,
 	"When she enters multipay details in the transaction form": async (t: TestController) => {
 		await t.click(Selector("span").withText(translations.TRANSACTION.MULTIPLE));
 		await t.typeText(amountInput, "10", { replace: true });
-		await t.typeText(recipientInput, "DReUcXWdCz2QLKzHM9NdZQE7fAwAyPwAmd", {
+		await t.typeText(recipientInput, "0xcd15953dD076e56Dc6a5bc46Da23308Ff3158EE6", {
 			paste: true,
 		});
 		await t.click(Selector("button").withText(translations.TRANSACTION.ADD_RECIPIENT));
-		await t.typeText(recipientInput, "D7JJ4ZfkJDwDCwuwzhtbCFapBUCWU3HHGP", {
+		await t.typeText(recipientInput, "0xA46720D11Bc8408411Cbd45057EeDA6d32D2Af54", {
 			paste: true,
 			replace: true,
 		});
@@ -157,7 +176,7 @@ cucumber("@multipayTransaction-singleField", {
 	...preSteps,
 	"When she enters details into the single transaction form": async (t: TestController) => {
 		await t.typeText(amountInput, "10", { replace: true });
-		await t.typeText(recipientInput, "DReUcXWdCz2QLKzHM9NdZQE7fAwAyPwAmd", {
+		await t.typeText(recipientInput, "0xcd15953dD076e56Dc6a5bc46Da23308Ff3158EE6", {
 			paste: true,
 		});
 
