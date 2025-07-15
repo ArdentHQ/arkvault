2f4c109b959591473e4452340f37b77b6fc5fbde
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-07-15 01:01:26 -0500

    fix: dim theme support for transaction confirmations

--- src/domains/transaction/components/TransactionDetail/TransactionConfirmations/TransactionConfirmations.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionConfirmations/TransactionConfirmations.tsx
@@ -19,15 +19,15 @@ export const TransactionConfirmations = ({
 	const { t } = useTranslation();
 	const { status } = useMultiSignatureStatus({ transaction, wallet: transaction.wallet() });
 
-	if (confirmations && confirmations > 1 && !transaction.isSuccess()) {
+	if (confirmations && confirmations > 1 && transaction.isSuccess()) {
 		return (
 			<div
 				data-testid="TransactionFailedAlert"
-				className="border-theme-danger-200 bg-theme-danger-50 dark:border-theme-danger-info-border dim:border-theme-success-500 dim:bg-theme-success-900 rounded-xl border py-2 max-sm:text-sm sm:py-4 sm:leading-5 dark:bg-transparent"
+				className="border-theme-danger-200 bg-theme-danger-50 dark:border-theme-danger-info-border dim:border-theme-danger-400 dim:bg-theme-dim-900 rounded-xl border py-2 max-sm:text-sm sm:py-4 sm:leading-5 dark:bg-transparent"
 			>
 				<div className="mb-2 flex items-center space-x-3 px-3 sm:mb-4 sm:px-6">
-					<div className="text-theme-danger-700 dark:text-theme-danger-info-border dim:text-theme-danger-info-border flex items-center space-x-2">
-						<Icon name="CircleMinus" size="lg" className="h-5" />
+					<div className="text-theme-danger-700 dark:text-theme-danger-info-border dim:text-theme-danger-400 flex items-center space-x-2">
+						<Icon name="CircleCross" size="lg" className="h-5" />
 						<p className="font-semibold">{t("COMMON.ALERT.FAILED")}</p>
 					</div>
 
@@ -36,12 +36,12 @@ export const TransactionConfirmations = ({
 						className="text-theme-danger-200 dark:text-theme-secondary-800 dim:text-theme-secondary-800 h-5"
 					/>
 
-					<p className="text-theme-secondary-700 dark:text-theme-secondary-500 dim:text-theme-secondary-500 font-semibold">
+					<p className="text-theme-secondary-700 dark:text-theme-secondary-500 dim:text-theme-dim-200 font-semibold">
 						<span>{t("TRANSACTION.CONFIRMATIONS_COUNT", { count: confirmations })} </span>
 					</p>
 				</div>
 
-				<p className="border-theme-danger-200 text-theme-secondary-700 dark:border-theme-secondary-800 dark:text-theme-secondary-500 dim:text-theme-secondary-500 dim:border-theme-secondary-800 border-t px-3 pt-2 font-semibold sm:px-6 sm:pt-4">
+				<p className="border-theme-danger-200 text-theme-secondary-700 dark:border-theme-secondary-800 dark:text-theme-secondary-500 dim:text-theme-dim-200 dim:border-theme-danger-400 border-t px-3 pt-2 font-semibold sm:px-6 sm:pt-4">
 					{t("TRANSACTION.TRANSACTION_EXECUTION_ERROR")}
 				</p>
 			</div>
