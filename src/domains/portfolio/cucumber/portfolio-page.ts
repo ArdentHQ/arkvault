eb5e4272af481b627e40c115e9e86650d624ff48
Shahin Safaraliyev <shahin@ardenthq.com>
2025-07-16 21:24:32 +0400

    wip

---
+++ src/domains/portfolio/cucumber/portfolio-page.ts
@@ -0,0 +1,30 @@
+import { Selector } from "testcafe";
+
+import { cucumber, mockRequest, scrollToBottom, visitWelcomeScreen } from "../../../utils/e2e-utils";
+import { goToProfile } from "../../profile/e2e/common";
+
+let count: any;
+
+const preSteps = {
+	"Given Alice is signed into a profile": async (t: TestController) => {
+		await visitWelcomeScreen(t);
+		await goToProfile(t);
+	},
+};
+cucumber("@portfolioPage-loadMore", {
+	...preSteps,
+	"When she selects to view more transactions": async (t: TestController) => {
+		await t.expect(Selector("[data-testid=TransactionTable]").exists).ok();
+		await t.expect(Selector("[data-testid=transactions__fetch-more-button]").exists).ok();
+		count = await Selector("[data-testid=TransactionTable] [data-testid=TableRow]").count;
+		await scrollToBottom();
+		await t.click(Selector("[data-testid=transactions__fetch-more-button]"));
+	},
+	"Then the transaction count is increased": async (t: TestController) => {
+		await t.expect(Selector("[data-testid=TransactionTable] [data-testid=TableRow]").count).gt(count);
+	},
+}, [
+	mockRequest("https://dwallets-evm.mainsailhq.com/api/transactions?page=1&limit=30&orderBy=timestamp:desc&to=0xcd15953dD076e56Dc6a5bc46Da23308Ff3158EE6", {
+		tr
+	})
+]);
