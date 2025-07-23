b3decab5288310fcc67b85dc818729ec987d4444
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2025-07-23 12:48:27 +0200

    feat: add alpha logo (#1386)

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { images } from "@/app/assets/images";
+import { useTranslation } from "react-i18next";
+import cn from "classnames";
+import { NavbarVariant } from "@/types";
+import { Tooltip } from "@/app/components/Tooltip";
+import { Image } from "@/app/components/Image";
 
 const { ARKVaultLogo } = images.common;
 
@@ -8,5 +13,60 @@ export const Logo = ({ height }: { height?: number }) => (
 		<ARKVaultLogo height={height} />
 	</span>
 );
+
+export const LogoAlpha = ({
+	height,
+	onClick,
+	variant,
+}: {
+	height?: number;
+	onClick?: () => void;
+	variant?: NavbarVariant | "default";
+}) => {
+	const { t } = useTranslation();
+
+	return (
+		<>
+			{variant === "logo-only" && (
+				<Tooltip content={t("COMMON.ALPHA_RELEASE")}>
+					<div>
+						<Image name="ARKVaultLogoAlpha" />
+					</div>
+				</Tooltip>
+			)}
+
+			{variant !== "logo-only" && (
+				<Tooltip content={t("COMMON.ALPHA_RELEASE")}>
+					<div className="flex items-center space-x-[2px]">
+						<button
+							data-testid="NavigationBarLogo--button"
+							type="button"
+							className={cn(
+								"bg-theme-primary-600 focus:ring-theme-primary-400 dark:bg-theme-dark-navy-500 my-auto flex cursor-pointer items-center justify-center text-white outline-hidden focus:ring-2 focus:outline-hidden",
+								{
+									"h-6 w-6 rounded rounded-r-none": variant === "default",
+								},
+							)}
+							onClick={onClick}
+						>
+							<Logo height={height} />
+						</button>
+						<div
+							className={cn(
+								"dark:bg-theme-dark-navy-900 dark:text-theme-dark-navy-300 dim:bg-theme-dim-navy-900 dim:text-theme-dim-navy-300 bg-theme-navy-100 text-theme-navy-600 flex cursor-pointer items-center px-2 py-[0.313rem] text-center text-xs leading-[0.938rem] font-semibold",
+								{
+									"h-6 rounded rounded-l-none": variant === "default",
+								},
+							)}
+							onClick={onClick}
+						>
+							{t("COMMON.ALPHA")}
+						</div>
+					</div>
+				</Tooltip>
+			)}
+		</>
+	);
+};
 
 Logo.displayName = "Logo";
