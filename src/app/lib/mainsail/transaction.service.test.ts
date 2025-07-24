dab3ef31d149475f419075653862d922389f92a9
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-07-24 16:33:00 -0600

    wip

--- src/app/lib/mainsail/transaction.service.test.ts
+++ src/app/lib/mainsail/transaction.service.test.ts
@@ -138,6 +138,72 @@ describe("TransactionService", () => {
 		expect(result).toHaveProperty("serialized");
 	});
 
+	it("should call builder chain and return SignedTransactionData for vote", async () => {
+		server.use(
+			requestMock("https://test1.com/wallets/0x659A76be283644AEc2003aa8ba26485047fd1BFB", {
+				data: {},
+			}),
+		);
+
+		const input = {
+			data: {
+				votes: [{ id: "659A76be283644AEc2003aa8ba26485047fd1BFB" }],
+			},
+			gasLimit: BigNumber.make(21000),
+			gasPrice: BigNumber.make(20000000000),
+			signatory,
+		} as any;
+
+		const result = await transactionService.vote(input);
+		expect(result).toBeDefined();
+		expect(result).toHaveProperty("data");
+		expect(result).toHaveProperty("serialized");
+	});
+
+	it("should call builder chain and return SignedTransactionData for multiPayment", async () => {
+		server.use(
+			requestMock("https://test1.com/wallets/0x659A76be283644AEc2003aa8ba26485047fd1BFB", {
+				data: {},
+			}),
+		);
+
+		const input = {
+			data: {
+				payments: [{ amount: "1", to: "0x0000000000000000000000000000000000000000" }],
+			},
+			gasLimit: BigNumber.make(21000),
+			gasPrice: BigNumber.make(20000000000),
+			signatory,
+		} as any;
+
+		const result = await transactionService.multiPayment(input);
+		expect(result).toBeDefined();
+		expect(result).toHaveProperty("data");
+		expect(result).toHaveProperty("serialized");
+	});
+
+	it("should call builder chain and return SignedTransactionData for usernameRegistration", async () => {
+		server.use(
+			requestMock("https://test1.com/wallets/0x659A76be283644AEc2003aa8ba26485047fd1BFB", {
+				data: {},
+			}),
+		);
+
+		const input = {
+			data: {
+				username: "testuser",
+			},
+			gasLimit: BigNumber.make(21000),
+			gasPrice: BigNumber.make(20000000000),
+			signatory,
+		} as any;
+
+		const result = await transactionService.usernameRegistration(input);
+		expect(result).toBeDefined();
+		expect(result).toHaveProperty("data");
+		expect(result).toHaveProperty("serialized");
+	});
+
 	it("should throw error when transfer input is missing gasPrice", async () => {
 		const input = {
 			data: { amount: "100", to: "test-address" },
