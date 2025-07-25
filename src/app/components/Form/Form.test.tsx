5fcd903294b45cef4265cd98f07256219423f78a
goga-m <goga.mamoulasvili@gmail.com>
2025-07-25 10:56:34 +0200

    wip

--- src/app/components/Form/Form.test.tsx
+++ src/app/components/Form/Form.test.tsx
@@ -3,9 +3,11 @@ import { useForm } from "react-hook-form";
 
 import { renderHook, waitFor } from "@testing-library/react";
 import { Form } from "./Form";
-import { fireEvent, render, screen } from "@/utils/testing-library";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@/utils/testing-library";
 
 describe("Form", () => {
+	const formSubmitButtonId = "form-submit-button"
 	it("should render with provider", async () => {
 		const { result: form } = renderHook(() => useForm());
 		const onSubmit = vi.fn();
@@ -13,26 +15,29 @@ describe("Form", () => {
 		render(
 			<Form context={form.current} onSubmit={onSubmit}>
 				<input name="name" ref={form.current.register()} defaultValue="test" />
+				<button type="submit" data-testid={formSubmitButtonId} />
 			</Form>,
 		);
 
-		expect(screen.getByTestId("Form")).toBeInTheDocument();
+		expect(screen.getByTestId(formSubmitButtonId)).toBeInTheDocument();
 
-		fireEvent.submit(screen.getByTestId("Form"));
+		await userEvent.click(screen.getByTestId(formSubmitButtonId));
 
 		await waitFor(() => expect(onSubmit).toHaveBeenCalledWith({ name: "test" }, expect.anything()));
 	});
 
-	it("should render without submit handler", () => {
+	it("should render without submit handler", async () => {
 		const { result: form } = renderHook(() => useForm());
 
 		render(
 			<Form context={form.current}>
 				<input name="name" ref={form.current.register()} defaultValue="test" />
+				<button type="submit" data-testid={formSubmitButtonId} />
 			</Form>,
 		);
 
-		fireEvent.submit(screen.getByTestId("Form"));
+
+		await userEvent.click(screen.getByTestId(formSubmitButtonId));
 
 		expect(screen.getByTestId("Form")).toBeInTheDocument();
 	});
