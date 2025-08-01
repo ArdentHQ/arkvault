6fff9a13abdda22b68614674f00fbaa11f21c387
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-08-01 10:48:11 -0600

    add proper transaction types

--- src/app/lib/mainsail/client.service.ts
+++ src/app/lib/mainsail/client.service.ts
@@ -296,12 +296,12 @@ export class ClientService {
 		}
 
 		const transactionTypeMap: Record<string, string | undefined> = {
-			delegateRegistration: TransactionTypes.RegisterValidator,
-			delegateResignation: TransactionTypes.ResignValidator,
 			multiPayment: TransactionTypes.MultiPayment,
 			transfer: TransactionTypes.Transfer,
 			usernameRegistration: TransactionTypes.RegisterUsername,
 			usernameResignation: TransactionTypes.ResignUsername,
+			validatorRegistration: TransactionTypes.RegisterValidator,
+			validatorResignation: TransactionTypes.ResignValidator,
 			vote: [trimHexPrefix(TransactionTypes.Vote), trimHexPrefix(TransactionTypes.Unvote)].join(","),
 		};
 
