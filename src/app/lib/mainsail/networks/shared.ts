5699f97eeafb83bdc6f93f5b4d793c1d8f97dc3f
Shahin Safaraliyev <shahin@ardenthq.com>
2025-08-05 19:27:51 +0400

    fix: improve table filtering

--- src/app/lib/mainsail/networks/shared.ts
+++ src/app/lib/mainsail/networks/shared.ts
@@ -14,6 +14,7 @@ export const transactions: Networks.NetworkManifestTransactions = {
 		"validatorRegistration",
 		"usernameRegistration",
 		"usernameResignation",
+		"updateValidator",
 		"validatorResignation",
 		"multiPayment",
 		"transfer",
