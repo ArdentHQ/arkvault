4c64fac634f58306988a6f6cfbc8fce3349a1c3b
Shahin <132887516+shahin-hq@users.noreply.github.com>
2025-08-06 14:40:50 +0400

    feat: implement address name edit for addresses panel (#1433)

--- src/domains/portfolio/components/AddressesSidePanel/AddressRow.test.tsx
+++ src/domains/portfolio/components/AddressesSidePanel/AddressRow.test.tsx
@@ -25,9 +25,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={false}
+				usesManageMode={false}
 				toggleAddress={vi.fn()}
 				isSelected={false}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -40,9 +41,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={false}
+				usesManageMode={false}
 				toggleAddress={vi.fn()}
 				isSelected={false}
+				onEdit={vi.fn()}
 			/>,
 			"xs",
 		);
@@ -56,9 +58,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={true}
+				usesManageMode={true}
 				toggleAddress={vi.fn()}
 				isSelected={false}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -73,9 +76,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={onDelete}
-				usesDeleteMode={true}
+				usesManageMode={true}
 				toggleAddress={vi.fn()}
 				isSelected={false}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -89,9 +93,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={false}
+				usesManageMode={false}
 				toggleAddress={vi.fn()}
 				isSelected={true}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -106,9 +111,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={false}
+				usesManageMode={false}
 				toggleAddress={toggleAddress}
 				isSelected={true}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -124,9 +130,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={false}
+				usesManageMode={false}
 				toggleAddress={toggleAddress}
 				isSelected={true}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -142,9 +149,10 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={false}
+				usesManageMode={false}
 				toggleAddress={toggleAddress}
 				isSelected={true}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -159,11 +167,12 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={onDelete}
-				usesDeleteMode={true}
+				usesManageMode={true}
 				toggleAddress={vi.fn()}
 				isSelected={false}
 				isSingleView={false}
 				deleteContent={<div>Delete content</div>}
+				onEdit={vi.fn()}
 			/>,
 		);
 
@@ -177,13 +186,85 @@ describe("AddressRow", () => {
 				profile={profile}
 				wallet={wallet}
 				onDelete={vi.fn()}
-				usesDeleteMode={false}
+				usesManageMode={false}
 				toggleAddress={vi.fn()}
 				isSelected={true}
 				isSingleView={true}
+				onEdit={vi.fn()}
 			/>,
 		);
 
 		expect(screen.getByTestId("AddressRow--radio")).toBeInTheDocument();
+	});
+
+	it("should should render editContent", () => {
+		render(
+			<AddressRow
+				profile={profile}
+				wallet={wallet}
+				onDelete={vi.fn()}
+				usesManageMode={true}
+				toggleAddress={vi.fn()}
+				isSelected={false}
+				isSingleView={false}
+				editContent={<div>Edit content</div>}
+				onEdit={vi.fn()}
+			/>,
+		);
+
+		expect(screen.getByText("Edit content")).toBeInTheDocument();
+	});
+
+	it("should should trigger onEdit", async () => {
+		const onEdit = vi.fn();
+
+		render(
+			<AddressRow
+				profile={profile}
+				wallet={wallet}
+				onDelete={vi.fn()}
+				usesManageMode={true}
+				toggleAddress={vi.fn()}
+				isSelected={false}
+				onEdit={onEdit}
+			/>,
+		);
+
+		expect(screen.getByTestId("dropdown__toggle")).toBeInTheDocument();
+
+		await userEvent.click(screen.getByTestId("dropdown__toggle"));
+		await expect(screen.findByTestId("dropdown__option--0")).resolves.toBeInTheDocument();
+
+		await userEvent.click(screen.getByTestId("dropdown__option--0"));
+		expect(onEdit).toHaveBeenCalled();
+	});
+
+	it("should should trigger `Open in Explorer` option", async () => {
+		const windowOpen = vi.spyOn(window, "open").mockImplementation(vi.fn());
+		const explorerLinkSpy = vi.spyOn(wallet, "explorerLink").mockReturnValue("https://mainsail-scan.com/address");
+
+		render(
+			<AddressRow
+				profile={profile}
+				wallet={wallet}
+				onDelete={vi.fn()}
+				usesManageMode={true}
+				toggleAddress={vi.fn()}
+				isSelected={false}
+				onEdit={vi.fn()}
+			/>,
+		);
+
+		expect(screen.getByTestId("dropdown__toggle")).toBeInTheDocument();
+
+		await userEvent.click(screen.getByTestId("dropdown__toggle"));
+		await expect(screen.findByTestId("dropdown__option--1")).resolves.toBeInTheDocument();
+
+		await userEvent.click(screen.getByTestId("dropdown__option--1"));
+
+		expect(windowOpen).toHaveBeenCalledWith(new URL("https://mainsail-scan.com/address").toString(), "_blank");
+
+		windowOpen.mockRestore();
+		explorerLinkSpy.mockRestore();
 	});
 });
---
+++ src/domains/portfolio/components/AddressesSidePanel/UpdateAddressName.tsx
@@ -0,0 +1,98 @@
+import { Contracts } from "@/app/lib/profiles";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { useEnvironmentContext } from "@/app/contexts";
+import React, { useMemo } from "react";
+import { alias } from "@/domains/wallet/validations";
+import { Form, FormField, FormLabel } from "@/app/components/Form";
+import { Input } from "@/app/components/Input";
+import { Button } from "@/app/components/Button";
+import { Divider } from "@/app/components/Divider";
+
+interface UpdateAddressNameProperties {
+	onAfterSave: () => void;
+	onCancel: () => void;
+	profile: Contracts.IProfile;
+	wallet: Contracts.IReadWriteWallet;
+}
+
+interface UpdateAddressNameState {
+	name: string;
+}
+
+export const UpdateAddressName = ({ onAfterSave, onCancel, profile, wallet }: UpdateAddressNameProperties) => {
+	const getDefaultValues = (): UpdateAddressNameState => ({
+		name: wallet.alias() as string,
+	});
+
+	const form = useForm<UpdateAddressNameState>({
+		defaultValues: getDefaultValues(),
+		mode: "onChange",
+	});
+
+	const { formState, register } = form;
+	const { isValid, errors, isDirty, dirtyFields } = formState;
+
+	const { t } = useTranslation();
+	const { persist } = useEnvironmentContext();
+
+	const isChanged = useMemo(() => isDirty && Object.keys(dirtyFields).length > 0, [isDirty, dirtyFields]);
+
+	const aliasValidation = alias({ profile, t, walletAddress: wallet.address() });
+
+	const onSubmit = async ({ name }: UpdateAddressNameState) => {
+		wallet.mutator().alias(name);
+		await persist();
+
+		onAfterSave();
+	};
+
+	return (
+		<Form
+			context={form}
+			onSubmit={onSubmit}
+			className="bg-theme-secondary-100 border-theme-secondary-300 dark:bg-theme-dark-800 dark:border-theme-dark-700 dim:bg-theme-dim-800 dim:border-theme-dim-700 rounded-b border-b px-4 py-3 sm:rounded-b-lg sm:border-b-0 sm:px-6 sm:py-4"
+		>
+			<FormField name="name">
+				<FormLabel>{t("COMMON.NAME")}</FormLabel>
+				<div className="relative">
+					<Input
+						autoFocus
+						errorMessage={errors.name?.message}
+						isInvalid={!isValid}
+						data-testid="UpdateWalletName__input"
+						ref={register(aliasValidation)}
+					/>
+				</div>
+			</FormField>
+
+			<div className="mt-4 flex w-full items-center justify-end leading-[18px] sm:leading-5">
+				<Button
+					data-testid="UpdateWalletName__cancel"
+					size="icon"
+					variant="transparent"
+					onClick={onCancel}
+					className="text-theme-primary-600 dark:text-theme-primary-400 dim:text-theme-dim-navy-400 px-2 py-[3px] leading-5"
+				>
+					{t("COMMON.CANCEL")}
+				</Button>
+
+				<Divider
+					type="vertical"
+					className="border-theme-secondary-400 dark:border-theme-dark-600 dim:border-theme-dim-600 mx-3"
+				/>
+
+				<Button
+					type="submit"
+					size="icon"
+					variant="transparent"
+					data-testid="UpdateWalletName__submit"
+					disabled={!isValid || !isChanged}
+					className="text-theme-primary-600 dark:text-theme-primary-400 dim:text-theme-dim-navy-400 px-2 py-[3px] leading-5"
+				>
+					{t("COMMON.SAVE")}
+				</Button>
+			</div>
+		</Form>
+	);
+};
