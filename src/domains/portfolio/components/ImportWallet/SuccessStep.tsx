4612debb9fc7300ac192522db3f82f7b8b5278b6
goga-m <goga.mamoulasvili@gmail.com>
2025-08-11 16:30:37 +0200

    wip

--- src/domains/portfolio/components/ImportWallet/SuccessStep.tsx
+++ src/domains/portfolio/components/ImportWallet/SuccessStep.tsx
@@ -25,7 +25,7 @@ export const SuccessStep = ({
 
 	return (
 		<section data-testid="ImportWallet__success-step">
-			<div className="space-y-4">
+			<div className="space-y-4 -mx-3 sm:mx-0">
 				<DetailWrapper label={t("COMMON.IMPORTED")}>
 					<div className="mb-3 flex w-full items-center justify-between leading-5 sm:mb-0 sm:justify-start">
 						<DetailTitle> {t("COMMON.ADDRESS")} </DetailTitle>
