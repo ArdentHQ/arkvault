ab463fbb9df239dba69b74f080d7a02d436d0258
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-08-18 03:05:11 -0500

    refactor: pending persisted json type

--- src/app/lib/mainsail/pending-transaction.contract.ts
+++ src/app/lib/mainsail/pending-transaction.contract.ts
@@ -18,3 +18,27 @@ export interface UnconfirmedTransactionsResponse {
     results: UnconfirmedTransaction[];
     totalCount: number;
 }
+
+export interface PendingPersistedJSON {
+    network: number;
+    nonce: string;
+    gasPrice: string;
+    gas: number;
+    gasLimit?: number;
+    to: string;
+    value: string;
+    data: string;
+    v: number;
+    r: string;
+    s: string;
+    senderPublicKey: string;
+    from: string;
+    hash: string;
+    createdAt: string;
+    decimals?: number;
+    meta?: {
+        address?: string;
+        networkId?: string;
+        explorerLink?: string;
+    };
+}
