29d2f38fdd53fb7ce5badf05d23ea7ff7fd1315a
shahin-hq <shahin-hq@users.noreply.github.com>
2025-08-20 09:51:15 +0000

    style: resolve style guide violations

--- src/domains/transaction/hooks/use-pending-transactions.test.tsx
+++ src/domains/transaction/hooks/use-pending-transactions.test.tsx
@@ -5,33 +5,36 @@ import { ExtendedSignedTransactionData } from "@/app/lib/profiles/signed-transac
 import { usePendingTransactions } from "./use-pending-transactions";
 
 const signedTransactionData = {
-	"identifier": "d91057d3b535e43e890c794e2142803a54cd070edd3006e74ffd17dd18165f22",
-	"serialized": "",
-	"signedData": {
-		"data": "36a94134000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000077364667364663300000000000000000000000000000000000000000000000000",
-		"from": "0xA5cc0BfEB09742C5e4C610f2EBaaB82Eb142Ca10",
-		"gasLimit": "21592",
-		"gasPrice": "5000000000",
-		"hash": "d91057d3b535e43e890c794e2142803a54cd070edd3006e74ffd17dd18165f22",
-		"nonce": "187",
-		"r": "c88273028bf96f212eee0d20dc0a72aff1d3670ac2f49a8bc648a15a4366631c",
-		"senderPublicKey": "022a40ea35d53eedf0341ffa17574fca844d69665ce35f224e9a6b1385575044fd",
-		"s": "7073c8d6d2530fdfeafd4ac4a72f0976f33284253e3b97de70b6779b70d7a2f3",
-		"value": "0",
-		"to": "0x2c1DE3b4Dbb4aDebEbB5dcECAe825bE2a9fc6eb6",
-		"username": "sdfsdf3",
-		"v": 1
-	}
+	identifier: "d91057d3b535e43e890c794e2142803a54cd070edd3006e74ffd17dd18165f22",
+	serialized: "",
+	signedData: {
+		data: "36a94134000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000077364667364663300000000000000000000000000000000000000000000000000",
+		from: "0xA5cc0BfEB09742C5e4C610f2EBaaB82Eb142Ca10",
+		gasLimit: "21592",
+		gasPrice: "5000000000",
+		hash: "d91057d3b535e43e890c794e2142803a54cd070edd3006e74ffd17dd18165f22",
+		nonce: "187",
+		r: "c88273028bf96f212eee0d20dc0a72aff1d3670ac2f49a8bc648a15a4366631c",
+		s: "7073c8d6d2530fdfeafd4ac4a72f0976f33284253e3b97de70b6779b70d7a2f3",
+		senderPublicKey: "022a40ea35d53eedf0341ffa17574fca844d69665ce35f224e9a6b1385575044fd",
+		to: "0x2c1DE3b4Dbb4aDebEbB5dcECAe825bE2a9fc6eb6",
+		username: "sdfsdf3",
+		v: 1,
+		value: "0",
+	},
 };
 
 const secondSignedTransactionHash = "e82068d4c535e43e890c794e2142803a54cd070edd3006e74ffd17dd18165f33";
 
 let wallet: Contracts.IReadWriteWallet;
 
-const createMockTransaction = (wallet: Contracts.IReadWriteWallet, signedData = {}): DTO.ExtendedSignedTransactionData => {
+const createMockTransaction = (
+	wallet: Contracts.IReadWriteWallet,
+	signedData = {},
+): DTO.ExtendedSignedTransactionData => {
 	const signedTransaction = new SignedTransactionData().configure(
 		{ ...signedTransactionData.signedData, ...signedData },
-		signedTransactionData.serialized
+		signedTransactionData.serialized,
 	);
 
 	return new ExtendedSignedTransactionData(signedTransaction, wallet);
@@ -67,7 +70,7 @@ describe("usePendingTransactions", () => {
 		expect(result.current.pendingTransactions).toHaveLength(1);
 		expect(result.current.pendingTransactions[0]).toEqual({
 			transaction: signedTransactionData,
-			walletAddress: wallet.address()
+			walletAddress: wallet.address(),
 		});
 	});
 
@@ -88,7 +91,9 @@ describe("usePendingTransactions", () => {
 		});
 
 		expect(result.current.pendingTransactions).toHaveLength(2);
-		expect(result.current.pendingTransactions[0].transaction.signedData.hash).toBe(signedTransactionData.signedData.hash);
+		expect(result.current.pendingTransactions[0].transaction.signedData.hash).toBe(
+			signedTransactionData.signedData.hash,
+		);
 		expect(result.current.pendingTransactions[1].transaction.signedData.hash).toBe(secondSignedTransactionHash);
 	});
 
@@ -108,7 +113,9 @@ describe("usePendingTransactions", () => {
 
 		// Should only have one transaction (duplicate replaced)
 		expect(result.current.pendingTransactions).toHaveLength(1);
-		expect(result.current.pendingTransactions[0].transaction.signedData.hash).toBe(signedTransactionData.signedData.hash);
+		expect(result.current.pendingTransactions[0].transaction.signedData.hash).toBe(
+			signedTransactionData.signedData.hash,
+		);
 	});
 
 	it("should remove a pending transaction by hash", async () => {
@@ -153,7 +160,9 @@ describe("usePendingTransactions", () => {
 		});
 
 		expect(result.current.pendingTransactions).toHaveLength(1);
-		expect(result.current.pendingTransactions[0].transaction.signedData.hash).toBe(signedTransactionData.signedData.hash);
+		expect(result.current.pendingTransactions[0].transaction.signedData.hash).toBe(
+			signedTransactionData.signedData.hash,
+		);
 	});
 
 	it("should handle error when adding transaction fails", async () => {
@@ -166,8 +175,8 @@ describe("usePendingTransactions", () => {
 			},
 			hash: () => "test-hash",
 			wallet: () => ({
-				address: () => "test-address"
-			})
+				address: () => "test-address",
+			}),
 		} as unknown as DTO.ExtendedSignedTransactionData;
 
 		act(() => {
