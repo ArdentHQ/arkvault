a52cbcb31abc0fab000de80b87123715a49f2e57
shahin-hq <shahin-hq@users.noreply.github.com>
2025-09-02 15:33:43 +0000

    style: resolve style guide violations

--- src/domains/transaction/components/TransactionDetail/TransactionConfirmations/TransactionConfirmations.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionConfirmations/TransactionConfirmations.tsx
@@ -52,10 +52,7 @@ export const TransactionConfirmations = ({
 				{transaction.data().receipt().error() && (
 					<p className="border-theme-danger-200 text-theme-secondary-700 dark:border-theme-secondary-800 dark:text-theme-secondary-500 dim:text-theme-dim-200 dim:border-theme-danger-400 border-t px-3 pt-2 font-semibold sm:px-6 sm:pt-4">
 						{t("TRANSACTION.TRANSACTION_EXECUTION_ERROR_WITH_MESSAGE", {
-							error: transaction
-								.data()
-								.receipt()
-								.prettyError()
+							error: transaction.data().receipt().prettyError(),
 						})}
 					</p>
 				)}
