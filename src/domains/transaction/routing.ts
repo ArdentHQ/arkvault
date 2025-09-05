44180ddf6577f4dc9635a7b7148f5fb9b9148cbc
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-09-05 12:37:35 -0600

    Update routing.ts

--- src/domains/transaction/routing.ts
+++ src/domains/transaction/routing.ts
@@ -1,11 +1,9 @@
-import { ComponentType } from "react";
 import { ProfilePaths } from "@/router/paths";
 import { RouteItem } from "@/router/router.types";
 import preloadLazy from "@/utils/preload-lazy";
 
 const SendValidatorResignation = preloadLazy(() => import("./pages/SendValidatorResignation"));
 const SendRegistration = preloadLazy(() => import("./pages/SendRegistration"));
-const SendTransfer = preloadLazy(() => import("./pages/SendTransfer") as Promise<{ default: ComponentType<unknown> }>);
 const SendVote = preloadLazy(() => import("./pages/SendVote"));
 const SendUsernameResignation = preloadLazy(() => import("./pages/SendUsernameResignation"));
 
