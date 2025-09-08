82f530a24b7c5cdd8e4f7f61f143ecee85cb2d3a
patricio0312rev <patricio0312rev@users.noreply.github.com>
2025-09-08 20:00:43 +0000

    style: resolve style guide violations

--- src/app/lib/mainsail/confirmed-transaction.dto.test.ts
+++ src/app/lib/mainsail/confirmed-transaction.dto.test.ts
@@ -22,7 +22,7 @@ describe("ConfirmedTransactionData", () => {
 			gasPrice: 10000000,
 			hash: "test_hash",
 			nonce: 1,
-			receipt: { status: 1, gasUsed: 0.01 },
+			receipt: { gasUsed: 0.01, status: 1 },
 			senderPublicKey: "somePublicKey",
 			timestamp: new Date().getTime(),
 			to: "recipient_address",
