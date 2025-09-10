92f94a35c05a3946759f1441249cb9cbe120adf1
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-09-10 00:23:54 -0500

    fix: format units

--- src/domains/transaction/components/TransactionDetail/TransactionGas/TransactionGas.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionGas/TransactionGas.tsx
@@ -5,6 +5,7 @@ import { MobileCard } from "@/app/components/Table/Mobile/MobileCard";
 import { MobileSection } from "@/app/components/Table/Mobile/MobileSection";
 import { useTranslation } from "react-i18next";
 import cn from "classnames";
+import { formatNumber } from "@/app/lib/helpers/format-number";
 
 export const TransactionGas = ({ gasUsed, gasLimit }: { gasUsed: number | null; gasLimit: number }) => {
 	const { t } = useTranslation();
@@ -12,19 +13,19 @@ export const TransactionGas = ({ gasUsed, gasLimit }: { gasUsed: number | null; 
 	return (
 		<DetailWrapper>
 			<MobileCard className="sm:hidden">
-				<div className="bg-theme-secondary-100 dim:bg-theme-dim-950 flex h-10 w-full items-center justify-between px-4 dark:bg-black">
+				<div className="flex justify-between items-center px-4 w-full h-10 bg-theme-secondary-100 dim:bg-theme-dim-950 dark:bg-black">
 					<div className="text-theme-secondary-700 dark:text-theme-dark-200 dark:hover:text-theme-dark-50 text-sm leading-[17px] font-semibold">
 						{t("TRANSACTION.GAS_INFORMATION")}
 					</div>
 				</div>
 
-				<div className="flex w-full flex-col gap-4 px-4 pt-3 pb-4">
+				<div className="flex flex-col gap-4 px-4 pt-3 pb-4 w-full">
 					<MobileSection title={t("TRANSACTION.GAS_LIMIT")} className="w-full">
-						{gasLimit}
+						{formatNumber(gasLimit)}
 					</MobileSection>
 
 					<Divider
-						className="border-theme-secondary-300 dark:border-theme-secondary-800 dim:border-theme-dim-700 my-0"
+						className="my-0 border-theme-secondary-300 dark:border-theme-secondary-800 dim:border-theme-dim-700"
 						dashed
 					/>
 
@@ -34,7 +35,7 @@ export const TransactionGas = ({ gasUsed, gasLimit }: { gasUsed: number | null; 
 							"text-theme-secondary-500 dim:text-theme-dim-200": !gasUsed,
 						})}
 					>
-						{gasUsed ?? t("COMMON.NOT_AVAILABLE")}
+						{gasUsed ? formatNumber(gasUsed) : t("COMMON.NOT_AVAILABLE")}
 					</MobileSection>
 				</div>
 			</MobileCard>
@@ -44,21 +45,21 @@ export const TransactionGas = ({ gasUsed, gasLimit }: { gasUsed: number | null; 
 					{t("TRANSACTION.GAS_INFORMATION")}
 				</Label>
 
-				<div className="mt-2 flex w-full justify-between gap-2 sm:justify-start">
+				<div className="flex gap-2 justify-between mt-2 w-full sm:justify-start">
 					<DetailLabelText className="min-w-36">{t("TRANSACTION.GAS_LIMIT")}</DetailLabelText>
-					<div className="text-sm leading-[17px] font-semibold sm:text-base sm:leading-5">{gasLimit}</div>
+					<div className="text-sm leading-[17px] font-semibold sm:text-base sm:leading-5">{formatNumber(gasLimit)}</div>
 				</div>
 
 				<DetailDivider />
 
-				<div className="flex w-full justify-between gap-2 sm:justify-start">
+				<div className="flex gap-2 justify-between w-full sm:justify-start">
 					<DetailLabelText className="min-w-36">{t("TRANSACTION.GAS_USAGE")}</DetailLabelText>
 					<div
-						className={cn("text-sm leading-[17px] font-semibold sm:text-base sm:leading-5", {
+						className={cn("text-sm font-semibold leading-[17px] sm:text-base sm:leading-5", {
 							"text-theme-secondary-500 dim:text-theme-dim-200": !gasUsed,
 						})}
 					>
-						{gasUsed ?? t("COMMON.NOT_AVAILABLE")}
+						{gasUsed ? formatNumber(gasUsed) : t("COMMON.NOT_AVAILABLE")}
 					</div>
 				</div>
 			</div>
--- src/domains/transaction/components/TransactionDetail/TransactionGas/TransactionGast.test.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionGas/TransactionGast.test.tsx
@@ -12,7 +12,7 @@ describe("TransactionGas", () => {
 	it("should render with gas limit and gas used", () => {
 		render(<TransactionGas gasLimit={21000} gasUsed={0.01} />);
 
-		expect(screen.getAllByText("21000")).toHaveLength(2);
+		expect(screen.getAllByText("21,000")).toHaveLength(2);
 		expect(screen.getAllByText("0.01")).toHaveLength(2);
 	});
 
