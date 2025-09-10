9fa33803945e7fda7846023b6757e62082a0fb2e
patricio0312rev <patricio0312rev@users.noreply.github.com>
2025-09-10 05:21:47 +0000

    style: resolve style guide violations

--- src/domains/transaction/utils.ts
+++ src/domains/transaction/utils.ts
@@ -48,17 +48,17 @@ export const isContractDeployment = (transaction: DTO.RawTransactionData) =>
 
 export const withAbortPromise =
 	(signal?: AbortSignal, callback?: () => void) =>
-		<T>(promise: Promise<T>) =>
-			new Promise<T>((resolve, reject) => {
-				if (signal) {
-					signal.addEventListener("abort", () => {
-						callback?.();
-						reject("ERR_ABORT");
-					});
-				}
+	<T>(promise: Promise<T>) =>
+		new Promise<T>((resolve, reject) => {
+			if (signal) {
+				signal.addEventListener("abort", () => {
+					callback?.();
+					reject("ERR_ABORT");
+				});
+			}
 
-				return promise.then(resolve).catch(reject);
-			});
+			return promise.then(resolve).catch(reject);
+		});
 
 /*
  * Get subtitle for authentication step
