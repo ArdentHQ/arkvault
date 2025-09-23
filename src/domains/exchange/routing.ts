14d9901f16813fd044516669b3badb640d49004a
Alfonso Bribiesca <alfonsobries@gmail.com>
2025-09-23 12:55:58 -0600

    remove exchange view page

--- src/domains/exchange/routing.ts
+++ src/domains/exchange/routing.ts
@@ -2,15 +2,9 @@ import { ProfilePaths } from "@/router/paths";
 import { RouteItem } from "@/router/router.types";
 import preloadLazy from "@/utils/preload-lazy";
 
-const ExchangeView = preloadLazy(() => import("./pages/ExchangeView"));
 const Exchange = preloadLazy(() => import("./pages/Exchange"));
 
 export const ExchangeRoutes: RouteItem[] = [
-	{
-		component: ExchangeView,
-		exact: true,
-		path: ProfilePaths.ExchangeView,
-	},
 	{
 		component: Exchange,
 		exact: true,
