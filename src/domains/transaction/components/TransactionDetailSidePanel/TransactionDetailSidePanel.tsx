b0504b93151d756712e304bf6edd578fda7b1f13
Shahin Safaraliyev <shahin@ardenthq.com>
2025-10-13 19:13:10 +0400

    refactor: review step paddings

--- src/domains/transaction/components/TransactionDetailSidePanel/TransactionDetailSidePanel.tsx
+++ src/domains/transaction/components/TransactionDetailSidePanel/TransactionDetailSidePanel.tsx
@@ -62,7 +62,7 @@ export const TransactionDetailContent = ({
 			<TransactionId transaction={transaction} isConfirmed={isConfirmed} />
 
 			<div className={cn("mt-6 space-y-3 sm:space-y-4", containerClassname)}>
-				<DetailPadded className="flex-1-mx-3 flex-1 sm:ml-0">
+				<DetailPadded className="flex-1 sm:ml-0">
 					<TransactionAddresses
 						explorerLink={transaction.explorerLink()}
 						profile={profile}
@@ -79,12 +79,12 @@ export const TransactionDetailContent = ({
 					/>
 				</DetailPadded>
 
-				<DetailPadded className="flex-1-mx-3 flex-1 sm:ml-0">
+				<DetailPadded className="flex-1 sm:ml-0">
 					{!isVoteTransaction && <TransactionType transaction={transaction} />}
 					{isVoteTransaction && <VoteTransactionType votes={votes} unvotes={unvotes} showValidator />}
 				</DetailPadded>
 
-				<DetailPadded className="flex-1-mx-3 flex-1 sm:ml-0">
+				<DetailPadded className="flex-1 sm:ml-0">
 					<TransactionSummary
 						labelClassName={labelClassName}
 						transaction={transaction}
@@ -93,7 +93,7 @@ export const TransactionDetailContent = ({
 					/>
 				</DetailPadded>
 
-				<DetailPadded className="flex-1-mx-3 flex-1 sm:ml-0">
+				<DetailPadded className="flex-1 sm:ml-0">
 					<TransactionDetails
 						isConfirmed={isConfirmed}
 						transaction={transaction}
@@ -101,7 +101,7 @@ export const TransactionDetailContent = ({
 					/>
 				</DetailPadded>
 
-				<DetailPadded className="flex-1-mx-3 flex-1 sm:ml-0">
+				<DetailPadded className="flex-1 sm:ml-0">
 					<DetailLabel>{t("TRANSACTION.CONFIRMATIONS")}</DetailLabel>
 					<div className="mt-2 px-3 sm:px-0">
 						<TransactionConfirmations
