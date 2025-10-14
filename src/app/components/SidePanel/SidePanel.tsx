47c4e51d03026c46362c8ffcc82a8152a618bf4a
Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2025-10-14 16:38:39 -0500

    fix: adjust minimized height on settings pages

--- src/app/components/SidePanel/SidePanel.tsx
+++ src/app/components/SidePanel/SidePanel.tsx
@@ -22,6 +22,7 @@ import { SIDE_PANEL_TRANSITION_DURATION, usePanels } from "@/app/contexts/Panels
 import { useLocalStorage } from "usehooks-ts";
 import { useTranslation } from "react-i18next";
 import { useBreakpoint } from "@/app/hooks";
+import { useLocation } from "react-router";
 
 interface SidePanelProps {
 	children: React.ReactNode;
@@ -92,6 +93,9 @@ const SidePanelContent = ({
 	const popStateHandlerRef = useRef<() => void>(() => {});
 	const { isMinimized, toggleMinimize } = usePanels();
 	const { isSmAndAbove } = useBreakpoint();
+	const location = useLocation();
+
+	const isSettingRoute = location.pathname.includes("/settings");
 
 	const [minimizedHintHasShown, persistMinimizedHint] = useLocalStorage("minimized-hint", false);
 	const [shake, setShake] = useState(false);
@@ -223,7 +227,7 @@ const SidePanelContent = ({
 						/>
 						<FloatingOverlay
 							className={cn("transition-all duration-300", {
-								"z-40 pointer-events-none": isMinimized,
+								"pointer-events-none z-40": isMinimized,
 								"z-50": !isMinimized,
 							})}
 							lockScroll={!isMinimized}
@@ -244,8 +248,10 @@ const SidePanelContent = ({
 											className,
 											{
 												"animate-shake": shake,
+												"sm:top-0": isMinimized,
 												"top-0": !isMinimized,
-												"top-[-56px] sm:top-0": isMinimized,
+												"top-[-56px]": !isSettingRoute && isMinimized,
+												"top-[-68px]": isSettingRoute && isMinimized,
 											},
 										)}
 									>
@@ -254,7 +260,7 @@ const SidePanelContent = ({
 											className={cn(
 												"navy-scroll bg-theme-background text-theme-text flex h-dvh w-full flex-col shadow-[0_15px_35px_0px_rgba(33,34,37,0.08)] transition-colors duration-300",
 												{
-													"border-theme-secondary-300 dark:border-theme-dark-700 dim:border-theme-dim-700 overflow-hidden rounded-tl-sm sm:rounded-tl-xl border-t border-l border-r sm:border-r-0 rounded-tr-sm sm:rounded-tr-none":
+													"border-theme-secondary-300 dark:border-theme-dark-700 dim:border-theme-dim-700 overflow-hidden rounded-tl-sm rounded-tr-sm border-t border-r border-l sm:rounded-tl-xl sm:rounded-tr-none sm:border-r-0":
 														isMinimized,
 													"border-transparent": !isMinimized,
 												},
@@ -263,7 +269,7 @@ const SidePanelContent = ({
 										>
 											<div className="relative">
 												<div className="bg-theme-background">
-													<div className="flex relative flex-col">
+													<div className="relative flex flex-col">
 														<div
 															onClick={isMinimized ? () => toggleMinimize() : undefined}
 															className={cn(
@@ -278,7 +284,7 @@ const SidePanelContent = ({
 																},
 															)}
 														>
-															<div className="flex gap-2 items-center">
+															<div className="flex items-center gap-2">
 																{titleIcon && (
 																	<div
 																		className={cn(
@@ -298,7 +304,7 @@ const SidePanelContent = ({
 																		{
 																			"text-base leading-5 md:text-lg md:leading-[21px]":
 																				!isMinimized,
-																			"text-base leading-5 truncate": isMinimized,
+																			"truncate text-base leading-5": isMinimized,
 																		},
 																	)}
 																>
@@ -306,14 +312,14 @@ const SidePanelContent = ({
 																</h2>
 															</div>
 
-															<div className="flex flex-row gap-3 items-center">
+															<div className="flex flex-row items-center gap-3">
 																{minimizeable && (
 																	<div
 																		className={cn(
-																			"bg-transparent rounded transition-all duration-100 ease-linear text-theme-secondary-700 dark:text-theme-secondary-200 dark:hover:bg-theme-primary-500 hover:bg-theme-primary-800 dim:text-theme-dim-200 dim:bg-transparent dim-hover:bg-theme-dim-navy-500 dim-hover:text-white hover:text-white dark:bg-transparent dark:hover:text-white",
+																			"text-theme-secondary-700 dark:text-theme-secondary-200 dark:hover:bg-theme-primary-500 hover:bg-theme-primary-800 dim:text-theme-dim-200 dim:bg-transparent dim-hover:bg-theme-dim-navy-500 dim-hover:text-white rounded bg-transparent transition-all duration-100 ease-linear hover:text-white dark:bg-transparent dark:hover:text-white",
 																			{
-																				"w-5 h-5": isMinimized,
-																				"w-6 h-6": !isMinimized,
+																				"h-5 w-5": isMinimized,
+																				"h-6 w-6": !isMinimized,
 																			},
 																		)}
 																	>
@@ -351,8 +357,8 @@ const SidePanelContent = ({
 																				variant="transparent"
 																				size="md"
 																				className={cn("p-0", {
-																					"w-5 h-5": isMinimized,
-																					"w-6 h-6": !isMinimized,
+																					"h-5 w-5": isMinimized,
+																					"h-6 w-6": !isMinimized,
 																				})}
 																				onClick={(e) => {
 																					e.stopPropagation();
@@ -371,10 +377,10 @@ const SidePanelContent = ({
 
 																<div
 																	className={cn(
-																		"bg-transparent rounded transition-all duration-100 ease-linear text-theme-secondary-700 dark:text-theme-secondary-200 dark:hover:bg-theme-primary-500 hover:bg-theme-primary-800 dim:text-theme-dim-200 dim:bg-transparent dim-hover:bg-theme-dim-navy-500 dim-hover:text-white hover:text-white dark:bg-transparent dark:hover:text-white",
+																		"text-theme-secondary-700 dark:text-theme-secondary-200 dark:hover:bg-theme-primary-500 hover:bg-theme-primary-800 dim:text-theme-dim-200 dim:bg-transparent dim-hover:bg-theme-dim-navy-500 dim-hover:text-white rounded bg-transparent transition-all duration-100 ease-linear hover:text-white dark:bg-transparent dark:hover:text-white",
 																		{
-																			"w-5 h-5": isMinimized,
-																			"w-6 h-6": !isMinimized,
+																			"h-5 w-5": isMinimized,
+																			"h-6 w-6": !isMinimized,
 																		},
 																	)}
 																>
@@ -387,8 +393,8 @@ const SidePanelContent = ({
 																			toggleOpen();
 																		}}
 																		className={cn("p-0", {
-																			"w-5 h-5": isMinimized,
-																			"w-6 h-6": !isMinimized,
+																			"h-5 w-5": isMinimized,
+																			"h-6 w-6": !isMinimized,
 																		})}
 																	>
 																		<Icon name="Cross" />
@@ -398,7 +404,7 @@ const SidePanelContent = ({
 														</div>
 
 														{hasSteps && (
-															<ul className="flex flex-row w-full">
+															<ul className="flex w-full flex-row">
 																{[...Array(totalSteps).keys()].map((index) => (
 																	<SidePanelStyledStep
 																		key={index}
@@ -413,7 +419,7 @@ const SidePanelContent = ({
 
 											<div
 												ref={scrollContainerRef}
-												className="flex overflow-y-auto flex-col flex-1 gap-4 px-6 py-4"
+												className="flex flex-1 flex-col gap-4 overflow-y-auto px-6 py-4"
 												data-testid="SidePanel__content"
 												inert={isMinimized}
 											>
@@ -429,7 +435,7 @@ const SidePanelContent = ({
 												<div
 													data-testid="SidePanel__footer"
 													className={cn(
-														"flex flex-col px-6 py-4 w-full border-t bg-theme-background border-theme-secondary-300 dark:border-theme-dark-700 dim:border-theme-dim-700",
+														"bg-theme-background border-theme-secondary-300 dark:border-theme-dark-700 dim:border-theme-dim-700 flex w-full flex-col border-t px-6 py-4",
 														{ "shadow-footer-side-panel": isScrolled },
 													)}
 												>
