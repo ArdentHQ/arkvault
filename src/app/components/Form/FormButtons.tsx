30647dfd605fc7fafaacd9b536c6976fea8af1e5
shahin-hq <shahin-hq@users.noreply.github.com>
2025-10-29 12:28:25 +0000

    style: resolve style guide violations

--- src/app/components/Form/FormButtons.tsx
+++ src/app/components/Form/FormButtons.tsx
@@ -11,7 +11,7 @@ const FormButtonsWrapper = ({
 	<div
 		{...props}
 		className={twMerge(
-			"[.modal-footer_&]:mt-4 [.modal-footer_&]:px-6 [.modal-footer_&]:sm:px-0 [.modal-footer_&]:border-none [.modal-footer_&]:sm:pt-0",
+			"[.modal-footer_&]:mt-4 [.modal-footer_&]:border-none [.modal-footer_&]:px-6 [.modal-footer_&]:sm:px-0 [.modal-footer_&]:sm:pt-0",
 			"bg-theme-background dim:bg-theme-dim-950 dark:bg-theme-dark-950 border-theme-secondary-300 dark:border-theme-dark-700 dim:border-theme-dim-700 fixed inset-x-0 bottom-0 flex gap-3 border-t px-8 py-3 sm:relative sm:inset-auto sm:mt-6 sm:justify-end sm:bg-transparent sm:p-0 sm:pt-6 sm:shadow-none sm:dark:bg-transparent sm:dark:shadow-none [&>button]:flex-1 sm:[&>button]:flex-none",
 			cn({
 				"mb-14 sm:mb-0": showMobileNavigation,
