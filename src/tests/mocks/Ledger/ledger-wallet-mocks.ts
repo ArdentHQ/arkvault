427e1f90b95e394c876f22e8aae261b50c512406
goga-m <goga.mamoulasvili@gmail.com>
2025-10-31 15:24:15 +0100

    wip

--- src/tests/mocks/Ledger/ledger-wallet-mocks.ts
+++ src/tests/mocks/Ledger/ledger-wallet-mocks.ts
@@ -1,8 +1,6 @@
-import { MAINSAIL_MNEMONICS } from "@/utils/testing-library";
 import { Contracts } from "@/app/lib/profiles";
 import { minVersionList } from "@/app/contexts";
 import { WalletData } from "@/app/lib/mainsail/wallet.dto";
-import { BigNumber } from "@/app/lib/helpers";
 
 export const createLedgerMocks = (wallet: Contracts.IReadWriteWallet, publicKeyPaths: Map<string, string>) => {
 	const isEthBasedAppSpy = vi.spyOn(wallet.ledger(), "isEthBasedApp").mockResolvedValue(true);
@@ -19,6 +17,7 @@ export const createLedgerMocks = (wallet: Contracts.IReadWriteWallet, publicKeyP
 			publicKey: wallet.publicKey(),
 		}),
 	});
+
 	const extendedPublicKeySpy = vi
 		.spyOn(wallet.ledger(), "getExtendedPublicKey")
 		.mockResolvedValue(wallet.publicKey()!);
@@ -30,34 +29,6 @@ export const createLedgerMocks = (wallet: Contracts.IReadWriteWallet, publicKeyP
 			publicKeySpy.mockRestore();
 			scanSpy.mockRestore();
 			extendedPublicKeySpy.mockRestore();
-		},
-	};
-};
-
-export const createTransactionMocks = async (wallet: Contracts.IReadWriteWallet) => {
-	const signatory = await wallet.signatoryFactory().fromSigningKeys({ key: MAINSAIL_MNEMONICS[0] });
-	const hash = await wallet.transaction().signTransfer({
-		data: {
-			amount: 1,
-			to: wallet.profile().wallets().last().address(),
-		},
-		gasLimit: BigNumber.make(1),
-		gasPrice: BigNumber.make(1),
-		nonce: BigNumber.make(1).toString(),
-		signatory,
-	});
-
-	const signatorySpy = vi.spyOn(wallet.signatoryFactory(), "fromSigningKeys").mockResolvedValue(signatory);
-	const signSpy = vi.spyOn(wallet.transaction(), "signTransfer").mockResolvedValue(hash);
-	const broadcastSpy = vi
-		.spyOn(wallet.transaction(), "broadcast")
-		.mockResolvedValue({ accepted: [hash], errors: [] });
-
-	return {
-		restoreAll: () => {
-			signatorySpy.mockRestore();
-			signSpy.mockRestore();
-			broadcastSpy.mockRestore();
 		},
 	};
 };
