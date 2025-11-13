1a28a19ddda2f6c29efad8e4d96aca803dd397f9
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2025-11-13 15:30:31 +0100

    fix: correct border radius on minimized sidepanel (#1621)

--- src/app/components/SidePanel/SidePanel.tsx
+++ src/app/components/SidePanel/SidePanel.tsx
@@ -256,7 +256,7 @@ const SidePanelContent = ({
 											ref={scrollRef}
 										>
 											<div className="relative">
-												<div className="bg-theme-background">
+												<div className="bg-theme-background rounded-tl-sm rounded-tr-sm sm:rounded-tl-xl sm:rounded-tr-none">
 													<div className="relative flex flex-col">
 														<div
 															onClick={isMinimized ? () => toggleMinimize() : undefined}
