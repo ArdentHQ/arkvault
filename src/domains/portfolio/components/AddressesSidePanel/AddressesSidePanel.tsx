05a12b7842ddc3eeaa0d2c127eb0092fc7369a2d
goga-m <goga.mamoulasvili@gmail.com>
2025-11-26 16:13:21 +0100

    wip

--- src/domains/portfolio/components/AddressesSidePanel/AddressesSidePanel.tsx
+++ src/domains/portfolio/components/AddressesSidePanel/AddressesSidePanel.tsx
@@ -150,7 +150,6 @@ export const AddressesSidePanel = ({
 			return true;
 		}
 
-		console.log({ hdAccountToDelete, walletAccountName: wallet.accountName() });
 		if (!!hdAccountToDelete && hdAccountToDelete === wallet.accountName()) {
 			return true;
 		}
