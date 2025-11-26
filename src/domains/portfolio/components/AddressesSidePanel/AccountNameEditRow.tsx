45f140b019bbe490c91b81650f8ee2ec574fa329
goga-m <goga.mamoulasvili@gmail.com>
2025-11-26 11:24:04 +0100

    wip

--- src/domains/portfolio/components/AddressesSidePanel/AccountNameEditRow.tsx
+++ src/domains/portfolio/components/AddressesSidePanel/AccountNameEditRow.tsx
@@ -28,24 +28,28 @@ export const AccountNameEditRow = ({
 		<div className="border-theme-primary-200 dark:border-theme-dark-700 dim:border-theme-dim-700 my-1 rounded-lg border">
 			<div className="px-4 py-3">
 				<div className="flex items-center justify-between">
-					<div className="no-ligatures text-theme-primary-600 border-theme-primary-200 dark:text-theme-secondary-200 dim:text-theme-dim-100 dark:border-theme-dark-700 dim:border-theme-dim-700 inline-block truncate overflow-hidden rounded border bg-transparent px-1 py-0.5 text-xs leading-[15px] font-semibold uppercase">
+					<div className="no-ligatures text-theme-secondary-700 border-theme-secondary-300 dark:text-theme-secondary-200 dim:text-theme-dim-100 dark:border-theme-dark-700 dim:border-theme-dim-700 inline-block truncate overflow-hidden rounded border bg-transparent px-1 py-0.5 text-xs leading-[15px] font-semibold uppercase">
 						{accountName}
 					</div>
 					<div className="flex items-center">
-						<Button
-							variant="transparent"
-							size="md"
-							className="h-6 w-6 p-0"
-							onClick={() => {
-								setIsEditing(true);
-							}}
-						>
-							<Icon name="Pencil" className="h-4 w-4" />
-						</Button>
+						<div className="text-theme-secondary-700 dark:text-theme-secondary-200 dark:hover:bg-theme-primary-500 hover:bg-theme-primary-800 dim:text-theme-dim-200 dim:bg-transparent dim-hover:bg-theme-dim-navy-500 dim-hover:text-white h-6 w-6 rounded bg-transparent transition-all duration-100 ease-linear hover:text-white dark:bg-transparent dark:hover:text-white" >
+							<Button
+								variant="transparent"
+								size="md"
+								className="h-6 w-6 p-0"
+								onClick={() => {
+									setIsEditing(true);
+								}}
+							>
+								<Icon name="Pencil" className="h-4 w-4" />
+							</Button>
+						</div>
 						<Divider type="vertical" />
-						<Button variant="transparent" size="md" className="h-6 w-6 p-0">
-							<Icon name="Trash" className="h-4 w-4" />
-						</Button>
+						<div className="text-theme-secondary-700 dark:text-theme-secondary-200 dark:hover:bg-theme-primary-500 hover:bg-theme-primary-800 dim:text-theme-dim-200 dim:bg-transparent dim-hover:bg-theme-dim-navy-500 dim-hover:text-white h-6 w-6 rounded bg-transparent transition-all duration-100 ease-linear hover:text-white dark:bg-transparent dark:hover:text-white" >
+							<Button variant="transparent" size="md" className="h-6 w-6 p-0">
+								<Icon name="Trash" className="h-4 w-4" />
+							</Button>
+						</div>
 					</div>
 				</div>
 			</div>
