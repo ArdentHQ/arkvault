d5bf72f21e18fe324eed17a20d24f8e70324201a
Shahin Safaraliyev <shahin@ardenthq.com>
2025-11-26 16:59:18 +0400

    wip

--- src/domains/transaction/components/SendRegistrationSidePanel/SendRegistrationSidePanel.tsx
+++ src/domains/transaction/components/SendRegistrationSidePanel/SendRegistrationSidePanel.tsx
@@ -338,9 +338,9 @@ export const SendRegistrationSidePanel = ({
 			);
 		}
 
-		if (activeTab === REVIEW_STEP) {
-			const isContractDeployment = registrationType === "contractDeployment";
+		const isContractDeployment = registrationType === "contractDeployment";
 
+		if (activeTab === REVIEW_STEP) {
 			return (
 				<ThemeIcon
 					lightIcon={isContractDeployment ? "ReviewContractDeploymentLight": "DocumentView"}
@@ -366,23 +366,12 @@ export const SendRegistrationSidePanel = ({
 			return <ThemeIcon lightIcon="Mnemonic" darkIcon="Mnemonic" dimIcon="Mnemonic" dimensions={[24, 24]} />;
 		}
 
-		if (registrationType === "contractDeployment") {
-			return (
-				<ThemeIcon
-					dimensions={[24, 24]}
-					lightIcon="SendContractDeploymentLight"
-					darkIcon="SendContractDeploymentDark"
-					dimIcon="SendContractDeploymentDim"
-				/>
-			);
-		}
-
 		return (
 			<ThemeIcon
 				dimensions={[24, 24]}
-				lightIcon="SendTransactionLight"
-				darkIcon="SendTransactionDark"
-				dimIcon="SendTransactionDim"
+				lightIcon={isContractDeployment ? "SendContractDeploymentLight" : "SendTransactionLight"}
+				darkIcon={isContractDeployment ? "SendContractDeploymentDark" : "SendTransactionDark"}
+				dimIcon={isContractDeployment ? "SendContractDeploymentDim" : "SendTransactionDim"}
 			/>
 		);
 	};
