2109b0b056cf68d5ee5de588303f503e3a37b6ff
Shahin Safaraliyev <shahin@ardenthq.com>
2025-11-27 17:40:09 +0400

    refactor: show bytecode in transaction details

--- src/domains/transaction/components/TransactionDetail/TransactionType/TransactionType.tsx
+++ src/domains/transaction/components/TransactionDetail/TransactionType/TransactionType.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { useTransactionTypes } from "@/domains/transaction/hooks/use-transaction-types";
@@ -6,6 +6,9 @@ import { DetailLabelText, DetailWrapper } from "@/app/components/DetailWrapper";
 import { Label } from "@/app/components/Label";
 import { DTO } from "@/app/lib/profiles";
 import cn from "classnames";
+import { TruncateEnd } from "@/app/components/TruncateEnd";
+import { Divider } from "@/app/components/Divider";
+import { Button } from "@/app/components/Button";
 
 const validatorPublickey = (transaction: DTO.ExtendedSignedTransactionData | DTO.ExtendedConfirmedTransactionData) => {
 	try {
@@ -23,6 +26,8 @@ export const TransactionType = ({
 }) => {
 	const { t } = useTranslation();
 
+	const [showFullBytecode, setShowFullByteCode] = useState(false);
+
 	const { getLabel } = useTransactionTypes();
 
 	const isValidatorRegistrationOrResignation =
@@ -32,6 +37,12 @@ export const TransactionType = ({
 		"min-w-24": !isValidatorRegistrationOrResignation,
 		"min-w-[138px]": isValidatorRegistrationOrResignation,
 	});
+
+	let bytecode: string|undefined;
+
+	if(transaction.type().startsWith("0x")) {
+		bytecode = transaction.isConfirmed() ? transaction.data().data.data : transaction.data().data().data;
+	}
 
 	return (
 		<div data-testid="TransactionType">
@@ -54,6 +65,31 @@ export const TransactionType = ({
 						</div>
 					)}
 
+					{bytecode && (
+						<div className="flex w-full justify-between sm:justify-start">
+							<DetailLabelText className="min-w-auto sm:min-w-24">{t("COMMON.BYTECODE")}</DetailLabelText>
+
+							<div className="flex items-center">
+								<div className="min-w-0 no-ligatures text-theme-secondary-900 dark:text-theme-secondary-200 dim:text-theme-dim-50 truncate text-sm leading-[17px] font-semibold sm:text-base sm:leading-5">
+									<TruncateEnd text={bytecode} maxChars={9} showTooltip={false} />
+								</div>
+								<div className="h-5 leading-[17px] sm:leading-5">
+									<Divider type="vertical" size="md" />
+								</div>
+
+								<Button
+									onClick={() => setShowFullByteCode(!showFullBytecode)}
+									variant="transparent"
+									data-testid="ContractDeploymentForm--ShowFullByteCode"
+									className="text-theme-navy-600 decoration-theme-navy-600 p-0 text-sm leading-[17px] underline decoration-dashed decoration-1 underline-offset-4 sm:text-base sm:leading-5"
+								>
+									{showFullBytecode ? t("COMMON.HIDE") : t("TRANSACTION.VIEW_FULL")}
+								</Button>
+							</div>
+						</div>
+
+					)}
+
 					{(transaction.isValidatorRegistration() || transaction.isUpdateValidator()) && (
 						<div className="flex w-full justify-between sm:justify-start">
 							<DetailLabelText className={labelClassName}>{t("COMMON.PUBLIC_KEY")}</DetailLabelText>
@@ -63,6 +99,17 @@ export const TransactionType = ({
 							</div>
 						</div>
 					)}
+				</div>
+				<div
+					className={cn(
+						"border-theme-secondary-300 dark:border-theme-dark-700 dim:border-theme-dim-700 max-h-0 overflow-y-scroll border-t text-sm leading-5 opacity-0 transition-all sm:text-base sm:leading-7",
+						{
+							"mt-3 -mb-3 max-h-64 pt-3 opacity-100 sm:-mx-6 sm:mt-5 sm:-mb-1 sm:px-6 sm:pt-4":
+							showFullBytecode,
+						},
+					)}
+				>
+					{bytecode}
 				</div>
 			</DetailWrapper>
 		</div>
