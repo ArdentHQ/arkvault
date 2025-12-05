727911a15667c24d9d25c7158a09bd1e52c45492
George Mamoulasvili <22020168+goga-m@users.noreply.github.com>
2025-12-05 16:34:55 +0100

    fix: correct alignment in tx table addressing column (#1662)

--- src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowAddressing.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionRowAddressing.tsx
@@ -47,7 +47,7 @@ const FormattedAddress = ({ alias, address }: { alias?: string; address: string 
 	const { t } = useTranslation();
 
 	return (
-		<div className="flex grow items-center justify-between space-x-4 md:px-4 lg:px-0 xl:min-w-36">
+		<div className="flex grow items-center justify-between space-x-4 lg:px-0 xl:min-w-36">
 			<Tooltip content={address} wrapperClass="grow">
 				<div className="grow" data-testid="TransactionRowAddressing__address-container">
 					<Address
