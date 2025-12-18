9cb06ff7d3c68a13835943378f625b7bfae135fa
Shahin Safaraliyev <shahin@ardenthq.com>
2025-12-18 14:13:34 +0400

    refactor: disable menu items when no addresses added

--- src/utils/url-validation.test.ts
+++ src/utils/url-validation.test.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/require-await */
-import { isKnownPath, isAllowedUrl } from "./url-validation";
+import { isKnownPath, isAllowedUrl, isDisabledUrl } from "./url-validation";
 import { ProfilePaths } from "@/router/paths";
+import { env, getMainsailProfileId } from "@/utils/testing-library";
+import { generatePath } from "react-router-dom";
 
 describe("Url validation", () => {
 	it("#isKnownPath", async () => {
@@ -12,5 +14,22 @@ describe("Url validation", () => {
 		expect(isAllowedUrl(ProfilePaths.Welcome)).toBe(true);
 		expect(isAllowedUrl(ProfilePaths.ImportProfile)).toBe(true);
 		expect(isAllowedUrl(ProfilePaths.CreateProfile)).toBe(true);
+	});
+
+	it("#isDisabledUrl", async () => {
+		const emptyProfile = await env.profiles().create("empty profile");
+
+		await env.profiles().restore(emptyProfile);
+		await emptyProfile.sync();
+
+		expect(isDisabledUrl(
+			generatePath(ProfilePaths.Tokens, {profileId: emptyProfile.id()}),
+			env
+		)).toBe(emptyProfile.id());
+
+		expect(isDisabledUrl(
+			generatePath(ProfilePaths.Tokens, {profileId: getMainsailProfileId()}),
+			env
+		)).toBe(false);
 	});
 });
