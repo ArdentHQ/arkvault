4dcb8a31e5f62edccf37f8454aa67d848e8640bc
Shahin Safaraliyev <shahin@ardenthq.com>
2025-12-18 14:06:43 +0400

    refactor: disable menu items when no addresses added

--- src/router/UrlValidationMiddleware.test.tsx
+++ src/router/UrlValidationMiddleware.test.tsx
@@ -30,6 +30,23 @@ describe("UrlValidationMiddleware", () => {
 		expect(parameters.navigate).toHaveBeenCalledWith("/");
 	});
 
+	it("should redirect to dashboard when no addresses added", async () => {
+		const emptyProfile = await env.profiles().create("empty profile");
+
+		await env.profiles().restore(emptyProfile);
+		await emptyProfile.sync();
+
+		const location = {
+			pathname: `/profiles/${emptyProfile.id()}/votes`,
+		};
+
+		const redirect = vi.fn();
+		const parameters = { env, location, navigate: vi.fn(), redirect };
+
+		expect(subject.handler(parameters)).toBe(true);
+		expect(parameters.navigate).toHaveBeenCalledWith(`/profiles/${emptyProfile.id()}/dashboard`);
+	});
+
 	it("should validate profile in url", () => {
 		const location = {
 			pathname: `/profiles/${getMainsailProfileId()}/dashboard`,
