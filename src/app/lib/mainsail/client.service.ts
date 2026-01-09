b199f243ade83b1e0e809d4df6953d606da560d2
shahin-hq <shahin-hq@users.noreply.github.com>
2026-01-09 08:26:31 +0000

    style: resolve style guide violations

--- src/app/lib/mainsail/client.service.ts
+++ src/app/lib/mainsail/client.service.ts
@@ -67,11 +67,7 @@ export class ClientService {
 	}
 
 	public async tokenAddresses(query: Services.WalletTokensQuery): Promise<WalletTokenCollection> {
-		const response = await this.#client.tokens().tokenAddresses(
-			query.addresses,
-			query.page,
-			query.limit
-		);
+		const response = await this.#client.tokens().tokenAddresses(query.addresses, query.page, query.limit);
 
 		const walletTokens = response.data.map((tokenAddresses: TokenAddressesData) => {
 			const token = new TokenDTO({
