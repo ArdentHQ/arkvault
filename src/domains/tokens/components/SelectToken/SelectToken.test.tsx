69e269f56a1f24dc56f418267f8283ec9304724f
goga-m <goga.mamoulasvili@gmail.com>
2026-01-12 19:40:40 +0100

    wip

--- src/domains/tokens/components/SelectToken/SelectToken.test.tsx
+++ src/domains/tokens/components/SelectToken/SelectToken.test.tsx
@@ -1,17 +1,15 @@
-import { describe, it } from "vitest"
+import { describe, it } from "vitest";
 import { render } from "@/utils/testing-library";
 import { SelectToken } from "./SelectToken";
 
 describe("SelectToken", () => {
 	it("should render", async () => {
 		const { asFragment } = render(<SelectToken tokens={[{ name: "test" }]} />);
-		expect(asFragment()).toMatchSnapshot()
-
+		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should render multiple ", async () => {
-		const { asFragment } = render(<SelectToken tokens={[{ name: "token1", }, { name: "token1", }]} />);
-		expect(asFragment()).toMatchSnapshot()
-
+		const { asFragment } = render(<SelectToken tokens={[{ name: "token1" }, { name: "token1" }]} />);
+		expect(asFragment()).toMatchSnapshot();
 	});
 });
