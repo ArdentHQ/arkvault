8114db9cc120c8a749b71cbeb099a7f7cb1a71e7
goga-m <goga.mamoulasvili@gmail.com>
2026-01-12 19:35:02 +0100

    wip

---
+++ src/domains/tokens/components/SelectToken/SelectToken.test.tsx
@@ -0,0 +1,18 @@
+import { describe, beforeAll, it } from "vitest"
+import { env, getMainsailProfileId, render, screen, waitFor } from "@/utils/testing-library";
+import { Contracts } from "@/app/lib/profiles";
+import { SelectToken } from "./SelectToken";
+
+let profile: Contracts.IProfile;
+
+describe("SelectToken", () => {
+	beforeAll(async () => {
+		profile = env.profiles().findById(getMainsailProfileId());
+	});
+
+	it("should render", async () => {
+		const { asFragment } = render(<SelectToken tokens={[{ name: "test" }]} />);
+		expect(asFragment()).toMatchSnapshot()
+
+	});
+});
