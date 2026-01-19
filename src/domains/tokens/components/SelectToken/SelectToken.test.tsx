5d0c13880d087afd443327a54729030d5a85fe1f
goga-m <goga.mamoulasvili@gmail.com>
2026-01-19 13:16:50 +0100

    wip

--- src/domains/tokens/components/SelectToken/SelectToken.test.tsx
+++ src/domains/tokens/components/SelectToken/SelectToken.test.tsx
@@ -10,20 +10,33 @@ describe("SelectToken", () => {
 	});
 
 	it("should render multiple ", () => {
-		const { asFragment } = render(<SelectToken tokens={[{ label: "token1", value: "token1" }, { label: "token2", value: "token2" }]} />);
+		const { asFragment } = render(
+			<SelectToken
+				tokens={[
+					{ label: "token1", value: "token1" },
+					{ label: "token2", value: "token2" },
+				]}
+			/>,
+		);
 		expect(asFragment()).toMatchSnapshot();
 	});
 
 	it("should change selection ", async () => {
-		const onChangeMock = vi.fn()
-		render(<SelectToken tokens={[{ label: "token1", value: "token1" }, { label: "token2", value: "token2" }]} onChange={onChangeMock} />);
-
+		const onChangeMock = vi.fn();
+		render(
+			<SelectToken
+				tokens={[
+					{ label: "token1", value: "token1" },
+					{ label: "token2", value: "token2" },
+				]}
+				onChange={onChangeMock}
+			/>,
+		);
 
 		await userEvent.clear(screen.getByTestId("SelectDropdown__input"));
 		await userEvent.type(screen.getByTestId("SelectDropdown__input"), "toke");
 
-
-		await userEvent.click(screen.getByTestId("select-list__input"))
-		expect(onChangeMock).toHaveBeenCalled()
+		await userEvent.click(screen.getByTestId("select-list__input"));
+		expect(onChangeMock).toHaveBeenCalled();
 	});
 });
