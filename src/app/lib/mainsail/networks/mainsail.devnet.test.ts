9ae194e9a3ac1e483cbecb31567455fcaccf4f86
goga-m <goga.mamoulasvili@gmail.com>
2026-01-29 10:26:22 +0100

    wip

--- src/app/lib/mainsail/networks/mainsail.devnet.test.ts
+++ src/app/lib/mainsail/networks/mainsail.devnet.test.ts
@@ -34,7 +34,7 @@ describe("Mainsail Devnet Network Configuration", () => {
 	it("should have correct constants", () => {
 		expect(network.constants).toEqual({
 			epoch: "2023-12-21T00:00:00.000Z",
-			slip44: 111,
+			slip44: 1,
 			slip44Eth: 60,
 			slip44Legacy: 1,
 		});
