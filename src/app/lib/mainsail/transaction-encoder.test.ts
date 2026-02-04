055edc6b3da32aa23cef76c8d923bf0646859288
goga-m <goga.mamoulasvili@gmail.com>
2026-02-04 12:35:35 +0100

    wip

--- src/app/lib/mainsail/transaction-encoder.test.ts
+++ src/app/lib/mainsail/transaction-encoder.test.ts
@@ -2,31 +2,52 @@ import { describe, beforeAll, it } from "vitest";
 import { env, getMainsailProfileId } from "@/utils/testing-library";
 import { Contracts } from "@/app/lib/profiles";
 import { TransactionEncoder } from "./transaction-encoder";
+import { WalletTokenDTO } from "../profiles/wallet-token.dto";
+import { TokenDTO } from "../profiles/token.dto";
+import { WalletToken } from "../profiles/wallet-token";
+import Fixtures from "@/tests/fixtures/coins/mainsail/devnet/tokens.json";
 
 let profile: Contracts.IProfile;
 
 describe("TransactionEncoder", () => {
+	let walletTokenDTO: WalletTokenDTO;
+	let tokenDTO: TokenDTO;
+	let walletToken: WalletToken;
+
 	beforeAll(async () => {
+		const fixtureData = Fixtures.ByContractAddress.data;
+		const walletTokenData = Fixtures.ByWalletAddress.data[0];
+
 		profile = env.profiles().findById(getMainsailProfileId());
+
+		walletTokenDTO = new WalletTokenDTO(walletTokenData);
+		tokenDTO = new TokenDTO(fixtureData);
+		walletToken = new WalletToken({
+			network: profile.activeNetwork(),
+			profile,
+			token: tokenDTO,
+			walletToken: walletTokenDTO,
+		});
+		vi.spyOn(profile.tokens().selected(), "items").mockReturnValue([walletToken])
 	});
 
 	const tokenEncodedData = {
-		data: "0xa9059cbb000000000000000000000000cd15953dd076e56dc6a5bc46da23308ff3158ee60000000000000000000000000000000000000000000000000000000000000064",
-		to: "0xcd15953dD076e56Dc6a5bc46Da23308Ff3158EE6",
+		data: "0xa9059cbb000000000000000000000000deb478251073157e400c3d8d2ed92a85c958f9fa0000000000000000000000000000000000000000000000056bc75e2d63100000",
+		to: "0xdeb478251073157e400c3d8d2ed92a85c958f9fa",
 	};
 
 	it("should encode token transfer", async () => {
-		const encoder = new TransactionEncoder(profile.activeNetwork());
-		const address = profile.wallets().first().address();
+		const encoder = new TransactionEncoder(profile, profile.activeNetwork());
+		const address = walletToken.token().address()
 
 		expect(
-			encoder.tokenTransfer(address, { recipients: [{ address, amount: 100 }], senderAddress: address }),
+			encoder.tokenTransfer(address, { recipients: [{ address, amount: 100 }], senderAddress: address, tokenContractAddress: address }),
 		).toEqual(tokenEncodedData);
 	});
 
 	it("should get token transfer if token info is provided", async () => {
-		const encoder = new TransactionEncoder(profile.activeNetwork());
-		const address = profile.wallets().first().address();
+		const encoder = new TransactionEncoder(profile, profile.activeNetwork());
+		const address = walletToken.token().address()
 
 		expect(
 			encoder.byType(
