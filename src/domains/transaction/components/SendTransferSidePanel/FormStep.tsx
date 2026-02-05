399e469416d04017a670f1e18fd6837f5d13c9b3
goga-m <goga.mamoulasvili@gmail.com>
2026-02-05 11:15:11 +0100

    refactor: remove deprecated code

--- src/domains/transaction/components/SendTransferSidePanel/FormStep.tsx
+++ src/domains/transaction/components/SendTransferSidePanel/FormStep.tsx
@@ -3,28 +3,14 @@ import { Contracts } from "@/app/lib/profiles";
 import React, { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import cn from "classnames";
 import { FormField, FormLabel } from "@/app/components/Form";
-import { useBreakpoint } from "@/app/hooks";
 import { AddRecipient } from "@/domains/transaction/components/AddRecipient";
 import { RecipientItem } from "@/domains/transaction/components/RecipientList/RecipientList.contracts";
-import { StepHeader } from "@/app/components/StepHeader";
-import { ThemeIcon, Icon } from "@/app/components/Icon";
+import { Icon } from "@/app/components/Icon";
 import { Button } from "@/app/components/Button";
-import { twMerge } from "tailwind-merge";
 import { WalletCapabilities } from "@/domains/portfolio/lib/wallet.capabilities";
 import { SelectAddressDropdown } from "@/domains/profile/components/SelectAddressDropdown";
 import { useActiveNetwork } from "@/app/hooks/use-active-network";
-
-const QRCodeButton = ({ ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
-	<button
-		{...props}
-		className={twMerge(
-			"border-theme-primary-100 text-theme-secondary-700 hover:border-theme-primary-100 hover:bg-theme-primary-100 hover:text-theme-primary-600 focus:ring-theme-primary-400 dark:border-theme-secondary-800 dark:text-theme-secondary-500 dark:hover:border-theme-secondary-800 dark:hover:bg-theme-secondary-800 dim:text-theme-secondary-200 dim:border-theme-dim-700 dim-hover:border-theme-dim-700 dim-hover:bg-theme-dim-700 dim-hover:text-theme-dim-50 mt-auto flex w-full cursor-pointer items-center space-x-2 rounded border-2 px-5 py-3 transition-colors duration-300 focus:ring-2 focus:outline-hidden sm:w-auto sm:py-5 dark:hover:text-white",
-			props.className,
-		)}
-	/>
-);
 
 export const FormStep = ({
 	network,
@@ -33,8 +19,6 @@ export const FormStep = ({
 	deeplinkProps,
 	onScan,
 	onChange,
-	// @TODO: always hide header once the side panel is fully implemented
-	hideHeader = false,
 	isTokenTransfer,
 }: {
 	network: Networks.Network;
@@ -43,12 +27,9 @@ export const FormStep = ({
 	deeplinkProps: Record<string, string>;
 	onScan?: () => void;
 	onChange?: ({ sender }: { sender?: Contracts.IReadWriteWallet }) => void;
-	hideHeader?: boolean;
 	isTokenTransfer?: boolean;
 }) => {
 	const { t } = useTranslation();
-
-	const { isXs } = useBreakpoint();
 
 	const { setValue, getValues, unregister } = useFormContext();
 
@@ -91,43 +72,7 @@ export const FormStep = ({
 
 	return (
 		<section data-testid="SendTransfer__form-step">
-			{!hideHeader && (
-				<StepHeader
-					title={t("TRANSACTION.PAGE_TRANSACTION_SEND.FORM_STEP.TITLE", { ticker: network.ticker() })}
-					titleIcon={
-						<ThemeIcon
-							dimensions={[24, 24]}
-							lightIcon="SendTransactionLight"
-							darkIcon="SendTransactionDark"
-							dimIcon="SendTransactionDim"
-						/>
-					}
-					subtitle={t("TRANSACTION.PAGE_TRANSACTION_SEND.FORM_STEP.DESCRIPTION")}
-					extra={
-						!isXs && (
-							<div className="hidden sm:flex sm:h-full sm:align-bottom">
-								<QRCodeButton
-									className="group"
-									type="button"
-									onClick={onScan}
-									data-testid="QRCodeModalButton"
-								>
-									<Icon size="lg" name="QRCode" />
-									<span className="text-base leading-5 font-semibold">
-										{t("TRANSACTION.PAGE_TRANSACTION_SEND.FORM_STEP.SCAN")}
-									</span>
-								</QRCodeButton>
-							</div>
-						)
-					}
-				/>
-			)}
-
-			<div
-				className={cn("space-y-4", {
-					"pt-4": !hideHeader,
-				})}
-			>
+			<div className="space-y-4">
 				<FormField name="senderAddress">
 					<div data-testid="sender-address">
 						<div className="mb-2 flex items-center justify-between">
