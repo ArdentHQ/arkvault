96a920759aead58c951b52f23b92ea66c003a116
Shahin Safaraliyev <shahin@ardenthq.com>
2026-02-18 19:39:07 +0400

    wip

--- src/app/lib/mainsail/signed-transaction.dto.test.ts
+++ src/app/lib/mainsail/signed-transaction.dto.test.ts
@@ -223,7 +223,7 @@ describe("SignedTransactionData", () => {
 		it("should return `undefined` for tokens when `isTokenTransfer` is false", () => {
 			const tokenTransferMock = vi
 				.spyOn(TransactionTypeIdentifierMock.TransactionTypeIdentifier, "isTokenTransfer")
-				.mockReturnValue(true);
+				.mockReturnValue(false);
 
 			transaction.configure(
 				{
@@ -241,7 +241,7 @@ describe("SignedTransactionData", () => {
 		it("should return `undefined` for tokens when `tokens` is not present", () => {
 			const tokenTransferMock = vi
 				.spyOn(TransactionTypeIdentifierMock.TransactionTypeIdentifier, "isTokenTransfer")
-				.mockReturnValue(false);
+				.mockReturnValue(true);
 
 			transaction.configure(
 				{
