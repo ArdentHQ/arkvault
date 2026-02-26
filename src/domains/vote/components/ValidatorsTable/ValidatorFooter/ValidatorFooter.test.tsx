1070f17185a58a074c6e43e3ddedae4e02301b9f
Shahin <132887516+shahin-hq@users.noreply.github.com>
2026-02-26 15:25:50 +0400

    refactor: clean up stake amount logic (#1755)

--- src/domains/vote/components/ValidatorsTable/ValidatorFooter/ValidatorFooter.test.tsx
+++ src/domains/vote/components/ValidatorsTable/ValidatorFooter/ValidatorFooter.test.tsx
@@ -1,15 +1,12 @@
-import { Contracts, ReadOnlyWallet } from "@/app/lib/profiles";
+import { Contracts } from "@/app/lib/profiles";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 
 import { ValidatorFooter } from "./ValidatorFooter";
-import { VoteValidatorProperties } from "@/domains/vote/components/ValidatorsTable/ValidatorsTable.contracts";
 import { translations as voteTranslations } from "@/domains/vote/i18n";
-import { data } from "@/tests/fixtures/coins/mainsail/devnet/validators.json";
 import { env, getMainsailProfileId, render, screen, waitFor } from "@/utils/testing-library";
 
 let wallet: Contracts.IReadWriteWallet;
-let validator: Contracts.IReadOnlyWallet;
 
 const continueButton = () => screen.getByTestId("ValidatorTable__continue-button");
 
@@ -17,19 +14,6 @@ describe("ValidatorFooter", () => {
 	beforeAll(() => {
 		const profile = env.profiles().findById(getMainsailProfileId());
 		wallet = profile.wallets().values()[0];
-
-		validator = new ReadOnlyWallet(
-			{
-				address: data[0].address,
-				explorerLink: "",
-				governanceIdentifier: "address",
-				isResignedValidator: false,
-				isValidator: true,
-				publicKey: data[0].publicKey,
-				username: data[0].username,
-			},
-			profile,
-		);
 	});
 
 	it("should render", () => {
@@ -66,33 +50,5 @@ describe("ValidatorFooter", () => {
 		await userEvent.hover(screen.getByTestId("ValidatorTable__continue--wrapper"));
 
 		expect(baseElement).toHaveTextContent(voteTranslations.VALIDATOR_TABLE.TOOLTIP.SELECTED_VALIDATOR);
-	});
-
-	it("should disable continue button with tooltip if there is at least 1 empty amount field when network requires vote amount", async () => {
-		const selectedValidator: VoteValidatorProperties[] = [
-			{
-				amount: 0,
-				validatorAddress: validator.address(),
-			},
-		];
-
-		vi.spyOn(wallet.network(), "votesAmountMinimum").mockReturnValue(2);
-
-		const { baseElement } = render(
-			<ValidatorFooter
-				selectedWallet={wallet}
-				availableBalance={wallet.balance()}
-				selectedVotes={selectedValidator}
-				selectedUnvotes={[]}
-				maxVotes={wallet.network().maximumVotesPerTransaction()}
-				onContinue={vi.fn()}
-			/>,
-		);
-
-		expect(continueButton()).toBeDisabled();
-
-		await userEvent.hover(screen.getByTestId("ValidatorTable__continue--wrapper"));
-
-		expect(baseElement).toHaveTextContent(voteTranslations.VALIDATOR_TABLE.TOOLTIP.INVALID_AMOUNT);
 	});
 });
