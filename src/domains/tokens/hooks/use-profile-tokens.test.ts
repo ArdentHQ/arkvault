e0a2f5c266e81c7dc885d7811a92e13429608438
Shahin Safaraliyev <shahin@ardenthq.com>
2026-03-16 20:40:02 +0400

    wip

--- src/domains/tokens/hooks/use-profile-tokens.test.ts
+++ src/domains/tokens/hooks/use-profile-tokens.test.ts
@@ -16,7 +16,7 @@ describe("useProfileTokens", () => {
 		profile = env.profiles().findById(getMainsailProfileId());
 	});
 
-	afterEach(() => {
+	beforeEach(() => {
 		vi.clearAllMocks();
 	});
 
@@ -53,29 +53,6 @@ describe("useProfileTokens", () => {
 		});
 
 		expect(selectedMock).toHaveBeenCalled();
-	});
-
-	it("should not trigger reload when already loading", async () => {
-		const syncMock = vi.fn(() => new Promise((resolve) => {
-			setTimeout(resolve, 2500);
-		}));
-
-		vi.spyOn(profile.tokens(), "sync").mockImplementation(syncMock);
-
-		const { result, rerender } = renderHook(
-			({ profile }) => useProfileTokens({ profile }),
-			{ initialProps: { profile } },
-		);
-
-		expect(result.current.isLoading).toBe(true);
-
-		// trigger useEffect again
-		rerender({ profile });
-
-		// reload should still only have been called once
-		expect(syncMock).toHaveBeenCalledTimes(1);
-
-		syncMock.mockRestore();
 	});
 
 	it("should not reload if already loaded", async () => {
