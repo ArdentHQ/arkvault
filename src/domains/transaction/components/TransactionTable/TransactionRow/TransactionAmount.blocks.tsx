10df4452e4870dcb49cf4167b0388d939fe55736
Shahin Safaraliyev <shahin@ardenthq.com>
2026-03-17 12:14:31 +0400

    fix: use token symbol for contract deployments

--- src/domains/transaction/components/TransactionTable/TransactionRow/TransactionAmount.blocks.tsx
+++ src/domains/transaction/components/TransactionTable/TransactionRow/TransactionAmount.blocks.tsx
@@ -60,8 +60,7 @@ export const TransactionTotalLabel = ({
 	const { t } = useTranslation();
 
 	const token = transaction.token();
-	const currency =
-		transaction.isTokenTransfer() && token ? token.token().displaySymbol() : transaction.wallet().currency();
+	const currency = showTicker && token ? token.token().displaySymbol() : transaction.wallet().currency();
 
 	const { returnedAmount, total } = useTransactionTotal(transaction);
 
