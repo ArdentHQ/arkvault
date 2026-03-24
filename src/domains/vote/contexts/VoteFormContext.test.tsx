117d9b737ab69405f8798efa47f41e2753600dd1
Shahin Safaraliyev <shahin@ardenthq.com>
2026-03-24 19:58:53 +0400

    test: add coverage for `vote` domain

--- src/domains/vote/contexts/VoteFormContext.test.tsx
+++ src/domains/vote/contexts/VoteFormContext.test.tsx
@@ -12,34 +12,41 @@ import {
 } from "@/utils/testing-library";
 import { useVoteFormContext, VoteFormProvider } from "./VoteFormContext";
 import * as ReactRouter from "react-router";
+import userEvent from "@testing-library/user-event";
+import { waitFor } from "@testing-library/react";
 
 const Component = () => {
-	const { isLoading, votes, unvotes } = useVoteFormContext();
+	const { isLoading, votes, unvotes, showSendVotePanel, setShowSendVotePanel } = useVoteFormContext();
 
 	return (
 		<>
 			{isLoading && <div data-testid="loading">loading validators</div>}
-			{!isLoading && Array.isArray(votes) && (
+			{votes.length > 0 && (
 				<div data-testid="votes">
 					{votes.map((vote) => (
 						<div key={vote.wallet?.address()}>{vote.wallet?.address()}</div>
 					))}
 				</div>
 			)}
-			{!isLoading && Array.isArray(unvotes) && (
+			{unvotes.length > 0 && (
 				<div data-testid="unvotes">
 					{unvotes.map((unvote) => (
 						<div key={unvote.wallet?.address()}>{unvote.wallet?.address()}</div>
 					))}
 				</div>
 			)}
+
+			<button data-testid="toggle-vote-panel" onClick={() => setShowSendVotePanel(!showSendVotePanel)}>
+				toggle vote panel
+			</button>
+
+			{showSendVotePanel && <div>vote panel</div>}
 		</>
 	);
 };
 
 let profile: Contracts.IProfile;
 let wallet: Contracts.IReadWriteWallet;
-// let _useSearchParamsMock;
 
 describe("VoteFormContext", () => {
 	beforeAll(async () => {
@@ -54,7 +61,6 @@ describe("VoteFormContext", () => {
 	});
 
 	beforeEach(() => {
-		// const _useSearchParamsMock = vi
 		vi.spyOn(ReactRouter, "useSearchParams").mockReturnValue([new URLSearchParams(), vi.fn()]);
 	});
 
@@ -87,5 +93,30 @@ describe("VoteFormContext", () => {
 
 		await expect(screen.findByTestId("votes")).resolves.toBeVisible();
 		await expect(screen.findByTestId("unvotes")).resolves.toBeVisible();
+	});
+
+	it("should reset form when vote panel closed", async () => {
+		const route =
+			"?method=vote&coin=Mainsail&validator=test&vote=0xcd15953dD076e56Dc6a5bc46Da23308Ff3158EE6&unvote=0xAa6d78a89706b744eDD2894CE30BeCE77Ab0F753";
+
+		render(
+			<VoteFormProvider profile={profile} wallet={wallet} network={profile.activeNetwork()}>
+				<Component />
+			</VoteFormProvider>,
+			{
+				route,
+			},
+		);
+
+		await expect(screen.findByTestId("votes")).resolves.toBeVisible();
+		await expect(screen.findByTestId("unvotes")).resolves.toBeVisible();
+
+		await userEvent.click(screen.getByTestId("toggle-vote-panel"));
+
+		await waitFor(() => {
+			expect(screen.queryByTestId("votes")).not.toBeInTheDocument();
+		});
+
+		expect(screen.queryByTestId("unvotes")).not.toBeInTheDocument();
 	});
 });
