8ba94f87dbdbca8fec6cf1e058378597eebfdcc5
Shahin Safaraliyev <shahin@ardenthq.com>
2026-03-31 17:23:31 +0400

    refactor: add coverage for `profile` domain

--- src/domains/profile/components/SelectRecipient/SelectRecipient.test.tsx
+++ src/domains/profile/components/SelectRecipient/SelectRecipient.test.tsx
@@ -4,7 +4,6 @@ import React from "react";
 
 import { SelectRecipient } from "./SelectRecipient";
 import { env, getMainsailProfileId, render, screen, waitFor } from "@/utils/testing-library";
-import * as useWalletAliasHook from "@/app/hooks/use-wallet-alias";
 
 let profile: Contracts.IProfile;
 
