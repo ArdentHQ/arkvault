b3cccea994f123ca179041814c7cef9923e61e95
Shahin Safaraliyev <shahin@ardenthq.com>
2026-04-02 20:27:03 +0400

    refactor: add coverage for `wallets` domain

--- src/domains/wallet/components/WalletActionsModals/WalletActionsModals.test.tsx
+++ src/domains/wallet/components/WalletActionsModals/WalletActionsModals.test.tsx
@@ -1,9 +1,11 @@
 import { Contracts } from "@/app/lib/profiles";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import React from "react";
 import { WalletActionsModals } from "./WalletActionsModals";
 import * as envHooks from "@/app/hooks/env";
 import { env, getMainsailProfileId, render, syncValidators } from "@/utils/testing-library";
+import userEvent from "@testing-library/user-event";
+import { expect } from "vitest";
 
 describe("WalletActionsModals", () => {
 	let profile: Contracts.IProfile;
@@ -40,6 +42,35 @@ describe("WalletActionsModals", () => {
 		await expect(screen.findByTestId("UpdateWalletName__input")).resolves.toBeInTheDocument();
 
 		expect(asFragment).toMatchSnapshot();
+	});
+
+	it("should trigger `onUpdateWallet`", async () => {
+		const onUpdateWalletMock = vi.fn();
+		const setActiveModalMock = vi.fn();
+
+		render(
+			<WalletActionsModals
+				wallets={[wallet]}
+				activeModal={"wallet-name"}
+				onUpdateWallet={onUpdateWalletMock}
+				setActiveModal={setActiveModalMock}
+			/>,
+		);
+
+		const walletInputName = "UpdateWalletName__input";
+
+		const getInput = screen.getByTestId(walletInputName);
+
+		expect(getInput).toBeInTheDocument();
+
+		const user = userEvent.setup();
+		await user.clear(getInput);
+		await user.paste("hello-123");
+
+		await user.click(screen.getByTestId("UpdateWalletName__submit"));
+
+		expect(onUpdateWalletMock).toHaveBeenCalled();
+		expect(setActiveModalMock).toHaveBeenCalled();
 	});
 
 	it("should render `delete-wallet` modal", async () => {
