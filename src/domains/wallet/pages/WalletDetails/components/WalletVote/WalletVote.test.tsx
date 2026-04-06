2d3cf171ca0af60b601cbf2c196cb095e0724aaa
Shahin Safaraliyev <shahin@ardenthq.com>
2026-04-06 16:45:35 +0400

    refactor: add coverage for `wallets` domain

--- src/domains/wallet/pages/WalletDetails/components/WalletVote/WalletVote.test.tsx
+++ src/domains/wallet/pages/WalletDetails/components/WalletVote/WalletVote.test.tsx
@@ -105,7 +105,7 @@ describe("#WalletVote", async () => {
 			const ledgerMock = vi.spyOn(wallet, "isLedger").mockReturnValue(true);
 
 			const { asFragment } = render(
-				<WalletVote wallet={wallet} onButtonClick={vi.fn()} votes={votes} isLoadingVotes={false} />,
+				<WalletVote wallets={[wallet]} wallet={wallet} onButtonClick={vi.fn()} votes={votes} isLoadingVotes={false} />,
 			);
 
 			await expect(screen.findByTestId("WalletVote")).resolves.toBeVisible();
