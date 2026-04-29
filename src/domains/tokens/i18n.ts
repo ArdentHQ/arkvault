0b1116ebc43423a723abe52615b704bed5881a56
goga-m <goga-m@users.noreply.github.com>
2026-04-29 11:20:51 +0000

    style: resolve style guide violations

--- src/domains/tokens/i18n.ts
+++ src/domains/tokens/i18n.ts
@@ -15,12 +15,11 @@ export const translations = {
 	CONFIRMATION_MESSAGE:
 		"Please mind that selecting another address will not save the changes you have made. Do you want to continue?",
 	EMPTY_TOKENS: "It looks like you don't have any Tokens yet. After adding new Tokens, they can be viewed here.",
-	TOKEN_TRANSFERS_EMPTY_TEXT:
-		"No token transfers have been found for your selected tokens.",
 	ENTER_TOKEN_NAME: "Enter Token Name",
 	HIDE_DUST: "Hide Dust",
 	PAGE_SUBTITLE: "View, manage, and organize the tokens tied to your address",
 	PAGE_TITLE: "Tokens",
 	SELECT_TOKEN: "Select Token",
 	TOKEN_INFORMATION: "Token Information",
+	TOKEN_TRANSFERS_EMPTY_TEXT: "No token transfers have been found for your selected tokens.",
 };
