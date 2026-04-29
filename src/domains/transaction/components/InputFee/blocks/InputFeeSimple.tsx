3cfea1fa7bc7398cbd968e7ebe189421596f6bce
Shahin Safaraliyev <shahin@ardenthq.com>
2026-04-30 03:09:36 +0400

    fix: send form hover colors

--- src/domains/transaction/components/InputFee/blocks/InputFeeSimple.tsx
+++ src/domains/transaction/components/InputFee/blocks/InputFeeSimple.tsx
@@ -46,7 +46,8 @@ export const InputFeeSimple: React.FC<InputFeeSimpleProperties> = ({
 							<div className="flex items-center justify-between p-3 sm:flex-col sm:items-start sm:justify-start sm:space-y-2">
 								<div
 									className={cn("text-sm leading-[17px] sm:leading-5", {
-										"group-hover:text-theme-secondary-900 dark:group-hover:text-theme-dark-50 group-hover:dim:text-theme-dim-50": !isSelected,
+										"group-hover:text-theme-secondary-900 dark:group-hover:text-theme-dark-50 group-hover:dim:text-theme-dim-50":
+											!isSelected,
 										"text-theme-navy-700 dark:text-theme-dark-50 dim:text-theme-dim-50": isSelected,
 									})}
 								>
