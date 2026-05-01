8c38466d1478b5d4246e45fb0b3521407c4998d7
Shahin Safaraliyev <shahin@ardenthq.com>
2026-05-01 16:43:51 +0400

    fix: divider issue in various modals

--- src/domains/profile/components/ResetProfile/ResetProfile.tsx
+++ src/domains/profile/components/ResetProfile/ResetProfile.tsx
@@ -49,16 +49,18 @@ export const ResetProfile = ({ isOpen, profile, onClose, onCancel, onReset }: Re
 		>
 			<Alert>{t("PROFILE.MODAL_RESET_PROFILE.DESCRIPTION")}</Alert>
 
-			<FormButtons>
-				<Button variant="secondary" onClick={onCancel} data-testid="ResetProfile__cancel-button">
-					{t("COMMON.CANCEL")}
-				</Button>
+			<div className="modal-footer">
+				<FormButtons>
+					<Button variant="secondary" onClick={onCancel} data-testid="ResetProfile__cancel-button">
+						{t("COMMON.CANCEL")}
+					</Button>
 
-				<Button type="submit" onClick={handleReset} data-testid="ResetProfile__submit-button" variant="danger">
-					<Icon name="ArrowRotateRight" />
-					<span>{t("COMMON.RESET")}</span>
-				</Button>
-			</FormButtons>
+					<Button type="submit" onClick={handleReset} data-testid="ResetProfile__submit-button" variant="danger">
+						<Icon name="ArrowRotateRight" />
+						<span>{t("COMMON.RESET")}</span>
+					</Button>
+				</FormButtons>
+			</div>
 		</Modal>
 	);
 };
