34fbd099b69c0aa4f93dd02d25555b07dce3ad59
shahin-hq <shahin-hq@users.noreply.github.com>
2026-05-06 09:16:09 +0000

    style: resolve style guide violations

--- src/domains/tokens/components/SelectToken/SelectToken.test.tsx
+++ src/domains/tokens/components/SelectToken/SelectToken.test.tsx
@@ -103,11 +103,11 @@ describe("SelectToken", () => {
 	});
 
 	it("should display native token when `option.data` is `undefined`", async () => {
-		render(<SelectToken wallet={wallet} tokens={[{ data: undefined, label: "ARK", value: "ARK" }, ...tokens, ]} />);
+		render(<SelectToken wallet={wallet} tokens={[{ data: undefined, label: "ARK", value: "ARK" }, ...tokens]} />);
 
 		await userEvent.clear(screen.getByTestId("SelectDropdown__input"));
 
-		await expect(screen.findByTestId('SelectDropdown__option--0')).resolves.toBeInTheDocument();
+		await expect(screen.findByTestId("SelectDropdown__option--0")).resolves.toBeInTheDocument();
 
 		await expect(screen.findByText("95.27653252325068 ARK")).resolves.toBeInTheDocument();
 	});
