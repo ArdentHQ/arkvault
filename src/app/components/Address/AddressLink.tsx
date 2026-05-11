1d922c473e0a5e6ba43bab756de6b7bd58f9bc91
goga-m <goga.mamoulasvili@gmail.com>
2026-05-11 17:07:28 +0200

    wip

--- src/app/components/Address/AddressLink.tsx
+++ src/app/components/Address/AddressLink.tsx
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge";
 import { MiddleTruncate } from "@/app/components/MiddleTruncate";
 
 export const AddressLabel = ({ children, className }: { children: string; className?: string }) => (
-		<div className={twMerge("no-ligatures text-theme-secondary-900 dark:text-theme-text", className)}>
-			<MiddleTruncate text={children} />
-		</div>
-	);
+	<div className={twMerge("no-ligatures text-theme-secondary-900 dark:text-theme-text", className)}>
+		<MiddleTruncate text={children} />
+	</div>
+);
