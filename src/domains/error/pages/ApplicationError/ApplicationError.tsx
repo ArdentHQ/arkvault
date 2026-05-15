34c97a35ebf82f7ac9112723992fc4ad1149d641
Shahin Safaraliyev <shahin@ardenthq.com>
2026-05-15 19:27:21 +0400

    chore: upgrade minor & patch versions

--- src/domains/error/pages/ApplicationError/ApplicationError.tsx
+++ src/domains/error/pages/ApplicationError/ApplicationError.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Trans, useTranslation } from "react-i18next";
+import { FallbackProps } from "react-error-boundary";
 
 import { Button } from "@/app/components/Button";
 import { Image } from "@/app/components/Image";
@@ -8,14 +9,12 @@ import { useTheme } from "@/app/hooks";
 import { TextArea } from "@/app/components/TextArea";
 import { ClipboardButton } from "@/app/components/Clipboard/ClipboardButton";
 
-interface ApplicationErrorProps {
-	error?: Error;
-}
-
-export const ApplicationError = ({ error }: ApplicationErrorProps) => {
+export const ApplicationError = ({ error }: Partial<FallbackProps>) => {
 	const { t } = useTranslation();
 
 	const { theme } = useTheme();
+
+	const formattedError = error as {message: string} | undefined;
 
 	return (
 		<main className={theme} data-testid="Main">
@@ -28,23 +27,23 @@ export const ApplicationError = ({ error }: ApplicationErrorProps) => {
 					<div data-testid="ApplicationError__text" className="mt-8">
 						<h2 className="text-2xl font-bold capitalize">{t("ERROR.APPLICATION.TITLE")}</h2>
 						<p className="text-theme-secondary-text">{t("ERROR.APPLICATION.DESCRIPTION")}</p>
-						{error && <p className="text-theme-secondary-text">{t("ERROR.APPLICATION.HELP_TEXT")}</p>}
+						{formattedError && <p className="text-theme-secondary-text">{t("ERROR.APPLICATION.HELP_TEXT")}</p>}
 					</div>
 
-					{error && (
+					{formattedError && (
 						<div className="mx-auto mt-8 max-w-md">
 							<TextArea
 								data-testid="ErrorStep__errorMessage"
 								className="py-4"
 								initialHeight={70}
-								defaultValue={error.message}
+								defaultValue={formattedError.message}
 								disabled
 							/>
 						</div>
 					)}
 
 					<div className="mx-auto mt-8 flex max-w-md items-center justify-center space-x-4">
-						{error && <ClipboardButton data={String(error.message)}>{t("COMMON.COPY")}</ClipboardButton>}
+						{formattedError && <ClipboardButton data={String(formattedError.message)}>{t("COMMON.COPY")}</ClipboardButton>}
 
 						<Button data-testid="ApplicationError__button--reload" onClick={() => window.location.reload()}>
 							{t("ERROR.APPLICATION.RELOAD")}
