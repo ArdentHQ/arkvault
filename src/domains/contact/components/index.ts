4c081781354a5f01b10e79e15c85e46715d7d843
Shahin Safaraliyev <shahin@ardenthq.com>
2026-05-19 19:24:55 +0400

    wip

--- src/domains/contact/components/index.ts
+++ src/domains/contact/components/index.ts
@@ -1,3 +1,3 @@
-export * from "./CreateContact/CreateContact";
-export * from "./DeleteContact/DeleteContact";
-export * from "./UpdateContact/UpdateContact";
+export * from "./CreateContact";
+export * from "./DeleteContact";
+export * from "./UpdateContact";
