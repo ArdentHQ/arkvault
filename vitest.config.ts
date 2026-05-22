5ec3bdb452f008938c3c42c9065c6e7c264c02a7
Shahin Safaraliyev <shahin@ardenthq.com>
2026-05-22 14:23:17 +0400

    wip

--- vitest.config.ts
+++ vitest.config.ts
@@ -44,7 +44,7 @@ export default defineConfig(async (env) => {
 				},
 				coverage: {
 					include: process.env.COVERAGE_INCLUDE_PATH
-						? process.env.COVERAGE_INCLUDE_PATH.split(",")
+						? process.env.COVERAGE_INCLUDE_PATH.split(",").map(toCoverageGlob)
 						: ["src/**/*.{ts,tsx}"],
 					exclude: [
 						"**/build/*",
@@ -67,7 +67,6 @@ export default defineConfig(async (env) => {
 						"src/utils/test-plugins.ts",
 						"src/utils/test-helpers.ts",
 						"**/*.test.{ts,tsx}",
-						"**/*.svg",
 					],
 					provider: "istanbul",
 					reporter: ["json", "lcov", "text", "clover", "html"],
