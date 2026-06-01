d842321679c7d3a767dda8f8027b103c1f576d23
goga-m <22020168+goga-m@users.noreply.github.com>
2026-06-01 17:34:25 +0000

    style: resolve style guide violations

--- src/app/components/Address/WalletAddress.tsx
+++ src/app/components/Address/WalletAddress.tsx
@@ -10,33 +10,31 @@ interface Properties {
 	showCopyButton?: boolean;
 }
 
-export const WalletAddress = ({ walletName, address, showCopyButton }: Properties) => {
-	return (
-		<div className="flex w-full min-w-0 items-center gap-2 overflow-hidden whitespace-nowrap">
-			{walletName && (
-				<span data-testid="WalletAddress__alias" className="text-base font-semibold text-theme-text">
-					<TruncateEnd text={walletName} maxChars={16} showTooltip={walletName.length > 16} />
-				</span>
-			)}
+export const WalletAddress = ({ walletName, address, showCopyButton }: Properties) => (
+	<div className="flex w-full min-w-0 items-center gap-2 overflow-hidden whitespace-nowrap">
+		{walletName && (
+			<span data-testid="WalletAddress__alias" className="text-base font-semibold text-theme-text">
+				<TruncateEnd text={walletName} maxChars={16} showTooltip={walletName.length > 16} />
+			</span>
+		)}
 
-			<div className="min-w-0 grow">
-				<MiddleTruncation
-					data-testid="WalletAddress__address"
-					className={cn("font-semibold", {
-						"text-base text-theme-secondary-500 dim:text-theme-dim-200 dark:text-theme-secondary-700":
-							walletName,
-						"text-base text-theme-text": !walletName,
-					})}
-				>
-					{address}
-				</MiddleTruncation>
-			</div>
-
-			{showCopyButton && (
-				<div className="shrink-0">
-					<CopyButton value={address} />
-				</div>
-			)}
+		<div className="min-w-0 grow">
+			<MiddleTruncation
+				data-testid="WalletAddress__address"
+				className={cn("font-semibold", {
+					"text-base text-theme-secondary-500 dim:text-theme-dim-200 dark:text-theme-secondary-700":
+						walletName,
+					"text-base text-theme-text": !walletName,
+				})}
+			>
+				{address}
+			</MiddleTruncation>
 		</div>
-	);
-};
+
+		{showCopyButton && (
+			<div className="shrink-0">
+				<CopyButton value={address} />
+			</div>
+		)}
+	</div>
+);
