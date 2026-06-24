9866647cc0245107ba4e8c69b2cba2cdeb01924d
Shahin Safaraliyev <shahin@ardenthq.com>
2026-06-24 19:50:47 +0400

    wip

---
+++ src/domains/transaction/components/SendTransferSidePanel/BatchTransfer/ApproveStep.tsx
@@ -0,0 +1,129 @@
+import { Contracts } from "@/app/lib/profiles";
+import React, { useEffect, useState } from "react";
+import { useFormContext } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+
+import { useActiveProfile, useValidation } from "@/app/hooks";
+import { DetailTitle, DetailWrapper } from "@/app/components/DetailWrapper";
+import { Amount } from "@/app/components/Amount";
+import { ExchangeCurrencyAmount } from "@/domains/transaction/components/SendTransferSidePanel/SendTransferSidepanel.blocks";
+import { FormField, FormLabel } from "@/app/components/Form";
+import { FeeField } from "@/domains/transaction/components/FeeField";
+import { getFeeType } from "@/domains/transaction/components/SendTransferSidePanel/utils";
+import { buildTransferData } from "@/domains/transaction/components/SendTransferSidePanel/SendTransfer.helpers";
+import { AuthenticationStep } from "@/domains/transaction/components/AuthenticationStep";
+import { useTransferDetails } from "@/domains/transaction/components/SendTransferSidePanel/BatchTransfer/BatchTranfer.blocks";
+import { TruncatedContractAddress } from "@/domains/transaction/components/ContractAddressHint/ContractAddressHint";
+
+interface ApproveStepProperties {
+	wallet: Contracts.IReadWriteWallet;
+}
+
+export const ApproveStep = ({ wallet }: ApproveStepProperties) => {
+	const { t } = useTranslation();
+
+	const { register, getValues } = useFormContext();
+	const { recipients, tokenContractAddress } = getValues();
+
+	const profile = useActiveProfile();
+
+	const { amount, convertedAmount, walletToken, exchangeTicker } = useTransferDetails({
+		profile,
+		recipients,
+		tokenContractAddress,
+		wallet,
+	});
+
+	const { common: commonValidation } = useValidation();
+
+	const nativeTokenBalance = wallet.balance();
+
+	useEffect(() => {
+		register("gasPrice", commonValidation.gasPrice(nativeTokenBalance, getValues, wallet.network()));
+		register("gasLimit", commonValidation.gasLimit(nativeTokenBalance, getValues, wallet.network()));
+	}, [commonValidation, register, nativeTokenBalance.toString()]);
+
+	const [feeTransactionData, setFeeTransactionData] = useState<Record<string, any> | undefined>();
+
+	useEffect(() => {
+		const updateFeeTransactionData = () => {
+			const transferData = buildTransferData({
+				recipients,
+			});
+
+			setFeeTransactionData(transferData);
+		};
+
+		void updateFeeTransactionData();
+	}, [recipients]);
+
+	const network = profile.activeNetwork();
+
+	return (
+		<section data-testid="BatchTransfer__approve-step">
+			<div className="space-y-3 sm:space-y-4">
+				<DetailWrapper label={t("COMMON.DETAILS")} className="rounded-xl">
+					<div className="space-y-3">
+						<div className="flex items-center justify-between space-x-2 sm:justify-start sm:space-x-0">
+							<DetailTitle className="w-auto sm:min-w-40 sm:pr-6">{t("COMMON.TOKEN")}</DetailTitle>
+
+							<div className="whitespace-normal break-all text-sm font-semibold leading-[17px] sm:text-base sm:leading-5">
+								{walletToken.token().name()}
+							</div>
+						</div>
+
+						<div className="flex items-center justify-between space-x-2 sm:justify-start sm:space-x-0">
+							<DetailTitle className="w-auto sm:min-w-40 sm:pr-6">Approval Amount</DetailTitle>
+
+							<div className="flex flex-1 flex-row items-center justify-end gap-2 sm:w-full sm:justify-start">
+								<Amount
+									ticker={walletToken.token().displaySymbol()}
+									value={amount}
+									decimals={walletToken.token().decimals()}
+									className="whitespace-normal break-all text-sm font-semibold md:text-base"
+								/>
+								<ExchangeCurrencyAmount
+									isTestnet={wallet.network().isTest()}
+									convertedAmount={convertedAmount}
+									exchangeTicker={exchangeTicker}
+								/>
+							</div>
+						</div>
+
+						<div className="flex items-center justify-between space-x-2 sm:justify-start sm:space-x-0">
+							<DetailTitle className="w-auto sm:min-w-40 sm:pr-6">{t("COMMON.CONTRACT")}</DetailTitle>
+
+							<div className="whitespace-normal break-all text-sm font-semibold leading-[17px] sm:text-base sm:leading-5">
+								<TruncatedContractAddress
+									token={walletToken}
+									link={wallet.link().wallet(walletToken.token().address())}
+								/>
+							</div>
+						</div>
+					</div>
+				</DetailWrapper>
+
+				<div className="border-t border-theme-secondary-300 px-3 pt-6 dim:border-theme-dim-700 dark:border-theme-dark-700 sm:border-none sm:px-0 sm:pt-0">
+					<FormField name="fee" disableStateHints>
+						<FormLabel
+							textClassName="text-sm leading-[17px] sm:text-base sm:leading-5"
+							label={t("TRANSACTION.TRANSACTION_FEE")}
+						/>
+
+						<FeeField
+							type={getFeeType(recipients?.length)}
+							data={{
+								...feeTransactionData,
+								recipientsCount: recipients.length,
+							}}
+							network={network}
+							profile={profile}
+						/>
+					</FormField>
+				</div>
+
+				<AuthenticationStep wallet={wallet!} noHeading />
+			</div>
+		</section>
+	);
+};
---
+++ src/domains/transaction/components/SendTransferSidePanel/BatchTransfer/ReviewStep.tsx
@@ -0,0 +1,109 @@
+import { Contracts } from "@/app/lib/profiles";
+import React, { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { TransactionAddresses } from "@/domains/transaction/components/TransactionDetail";
+import { useActiveProfile } from "@/app/hooks";
+import { DetailTitle, DetailWrapper } from "@/app/components/DetailWrapper";
+import { Amount } from "@/app/components/Amount";
+import { ExchangeCurrencyAmount } from "@/domains/transaction/components/SendTransferSidePanel/SendTransferSidepanel.blocks";
+import { Label } from "@/app/components/Label";
+import { useTransferDetails } from "@/domains/transaction/components/SendTransferSidePanel/BatchTransfer/BatchTranfer.blocks";
+
+interface ReviewStepProperties {
+	wallet: Contracts.IReadWriteWallet;
+}
+
+export const ReviewStep = ({ wallet }: ReviewStepProperties) => {
+	const { t } = useTranslation();
+
+	const { unregister, getValues } = useFormContext();
+	const { recipients, tokenContractAddress } = getValues();
+
+	const profile = useActiveProfile();
+
+	useEffect(() => {
+		unregister("mnemonic");
+	}, [unregister]);
+
+	const { amount, convertedAmount, walletToken, exchangeTicker } = useTransferDetails({
+		profile,
+		recipients,
+		tokenContractAddress,
+		wallet,
+	});
+
+	return (
+		<section data-testid="BatchTransfer__review-step">
+			<div className="-mx-3 mt-4 space-y-3 sm:mx-0 sm:space-y-4">
+				<TransactionAddresses
+					senderAddress={wallet.address()}
+					recipients={recipients}
+					profile={profile}
+					network={wallet.network()}
+					labelClassName="w-14 sm:min-w-[168px] sm:pr-6"
+					isMultiPayment={recipients.length > 1}
+				/>
+
+				<div className="space-y-3 sm:space-y-4">
+					<div className="mx-0">
+						<DetailWrapper label={t("COMMON.TRANSACTION_SUMMARY")} className="rounded-xl">
+							<div className="flex flex-col gap-3">
+								<div
+									className="flex items-center justify-between space-x-2 sm:justify-start sm:space-x-0"
+									data-testid="AmountSection"
+								>
+									<DetailTitle className="w-auto sm:min-w-44 sm:pr-6">
+										{t("COMMON.AMOUNT")}
+									</DetailTitle>
+
+									<div className="flex flex-1 flex-row items-center justify-end gap-2 sm:w-full sm:justify-start">
+										<Amount
+											ticker={walletToken.token().displaySymbol()}
+											value={amount}
+											decimals={walletToken.token().decimals()}
+											className="whitespace-normal break-all text-sm font-semibold md:text-base"
+										/>
+										<ExchangeCurrencyAmount
+											isTestnet={wallet.network().isTest()}
+											convertedAmount={convertedAmount}
+											exchangeTicker={exchangeTicker}
+										/>
+									</div>
+								</div>
+							</div>
+						</DetailWrapper>
+					</div>
+
+					<div className="mx-0">
+						<DetailWrapper label={t("COMMON.TRANSACTION_STEPS")} className="rounded-xl">
+							<div className="flex flex-col gap-3">
+								<div className="flex items-center justify-between space-x-2 sm:justify-start sm:space-x-0">
+									<DetailTitle className="w-44 sm:min-w-44 sm:pr-6">Approve Contract</DetailTitle>
+
+									<div className="flex flex-1 flex-row items-center justify-end gap-2 sm:w-full sm:justify-start">
+										<Label color="secondary" size="xs">
+											{t("COMMON.AWAITING")}
+										</Label>
+									</div>
+								</div>
+							</div>
+
+							<div className="mt-3 flex flex-col gap-3">
+								<div className="flex items-center justify-between space-x-2 sm:justify-start sm:space-x-0">
+									<DetailTitle className="w-44 sm:min-w-44 sm:pr-6">Multiple Transfer</DetailTitle>
+
+									<div className="flex flex-1 flex-row items-center justify-end gap-2 sm:w-full sm:justify-start">
+										<Label color="secondary" size="xs">
+											{t("COMMON.AWAITING")}
+										</Label>
+									</div>
+								</div>
+							</div>
+						</DetailWrapper>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
