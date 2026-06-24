b3a9e7b84d96443011b75241ccacff911e569345
Shahin Safaraliyev <shahin@ardenthq.com>
2026-06-24 19:50:54 +0400

    wip

---
+++ src/domains/transaction/components/SendTransferSidePanel/BatchTransfer/BatchTranfer.blocks.tsx
@@ -0,0 +1,49 @@
+import { Contracts } from "@/app/lib/profiles";
+import { useExchangeRate } from "@/app/hooks/use-exchange-rate";
+import { WalletToken } from "@/app/lib/profiles/wallet-token";
+import { BigNumber } from "@/app/lib/helpers";
+import { RecipientItem } from "@/domains/transaction/components/RecipientList/RecipientList.contracts";
+
+interface TransferDetailsProperties {
+	profile: Contracts.IProfile;
+	wallet: Contracts.IReadWriteWallet;
+	recipients: RecipientItem[];
+	tokenContractAddress: string;
+}
+
+export const calculateTotalAmount = (recipients: RecipientItem[]) => {
+	let amount = BigNumber.make(0);
+
+	for (const recipient of recipients) {
+		amount = amount.plus(BigNumber.make(recipient.amount ?? 0));
+	}
+
+	return amount;
+}
+
+export const useTransferDetails = ({
+	profile,
+	wallet,
+	recipients,
+	tokenContractAddress,
+}: TransferDetailsProperties) => {
+	const amount = calculateTotalAmount(recipients);
+
+	const walletToken = wallet
+		.tokens()
+		.values()
+		.find((token) => token.token().address() === tokenContractAddress) as WalletToken;
+
+	const ticker = walletToken.token().displaySymbol();
+	const exchangeTicker = profile.settings().get<string>(Contracts.ProfileSetting.ExchangeCurrency) as string;
+	const { convert } = useExchangeRate({ exchangeTicker, profile, ticker });
+
+	const convertedAmount =  wallet.network().isTest() ? 0 : convert(amount.toFixed());
+
+	return {
+		amount,
+		convertedAmount,
+		exchangeTicker,
+		walletToken,
+	};
+};
