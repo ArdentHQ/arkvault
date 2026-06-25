6e1f430af6f55b581e7d9fa32cb0c93fb2dddf20
goga-m <goga.mamoulasvili@gmail.com>
2026-06-25 16:28:51 +0200

    wip

--- src/app/components/ServerStatusIndicator/ServerStatusIndicator.tsx
+++ src/app/components/ServerStatusIndicator/ServerStatusIndicator.tsx
@@ -50,7 +50,7 @@ export const ServerStatusIndicator = ({ profile }: { profile: Contracts.IProfile
 					</NavigationButtonWrapper>
 				}
 			>
-				<>
+				<div className="sm:max-w-[32rem]">
 					<div className="flex w-full items-center justify-between rounded-t-xl bg-theme-secondary-100 px-8 py-4 dark:bg-black sm:w-128">
 						<div className="text-sm font-semibold text-theme-secondary-500">
 							{t("COMMON.NETWORK_STATUS")}
@@ -65,7 +65,7 @@ export const ServerStatusIndicator = ({ profile }: { profile: Contracts.IProfile
 						</div>
 						<div>{status.label}</div>
 					</div>
-				</>
+				</div>
 			</Dropdown>
 		</div>
 	);
