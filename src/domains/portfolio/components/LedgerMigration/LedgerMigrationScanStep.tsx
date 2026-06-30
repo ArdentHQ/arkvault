3d00f28a05ca8df3f21822e4c9f5b44cd1124ea3
goga-m <goga.mamoulasvili@gmail.com>
2026-06-30 13:23:52 +0200

    wip

--- src/domains/portfolio/components/LedgerMigration/LedgerMigrationScanStep.tsx
+++ src/domains/portfolio/components/LedgerMigration/LedgerMigrationScanStep.tsx
@@ -12,6 +12,8 @@ import { LedgerMigrator } from "@/app/lib/mainsail/ledger.migrator";
 import { LedgerData } from "@/app/contexts";
 import { BigNumber } from "@/app/lib/helpers";
 
+const dustAmount = 0.001;
+
 const MigrateToOneCheckbox = ({
 	onChange,
 	isDisabled,
@@ -77,7 +79,7 @@ export const MigrationLedgerScanStep = ({
 				network={network}
 				onSelect={async (ledgerAddresses) => {
 					const withBalance = ledgerAddresses.filter((address) =>
-						BigNumber.make(address.balance ?? 0).isGreaterThan(0),
+						BigNumber.make(address.balance ?? 0).isGreaterThanOrEqualTo(dustAmount),
 					);
 					setSelectedLedgerAddresses(withBalance);
 					handleSelectedAddresses(withBalance, shouldMigrateToOne);
