c35a4cebc36f7b430c97cc23c8d0bb52ed83903a
shahin-hq <132887516+shahin-hq@users.noreply.github.com>
2026-06-30 10:45:49 +0000

    style: resolve style guide violations

--- src/domains/transaction/components/SendTransferSidePanel/BatchTransfer/BatchTranfer.blocks.tsx
+++ src/domains/transaction/components/SendTransferSidePanel/BatchTransfer/BatchTranfer.blocks.tsx
@@ -11,9 +11,7 @@ import {
 	TransactionStepLabel,
 	TransferStatus,
 } from "@/domains/transaction/components/SendTransferSidePanel/BatchTransfer/TransactionStepLabel";
-import {
-	BatchTransferTabStep
-} from "@/domains/transaction/components/SendTransferSidePanel/BatchTransfer/BatchTransferTabs.contracts";
+import { BatchTransferTabStep } from "@/domains/transaction/components/SendTransferSidePanel/BatchTransfer/BatchTransferTabs.contracts";
 import { Button } from "@/app/components/Button";
 
 interface TransferDetailsProperties {
@@ -101,7 +99,13 @@ interface ActionsProperties {
 	isNextDisabled: boolean;
 }
 
-export const BatchTransferActions = ({ activeTab, isConfirmed, handleBack, handleNext, isNextDisabled }: ActionsProperties) => {
+export const BatchTransferActions = ({
+	activeTab,
+	isConfirmed,
+	handleBack,
+	handleNext,
+	isNextDisabled,
+}: ActionsProperties) => {
 	const { t } = useTranslation();
 
 	if (activeTab === BatchTransferTabStep.SummaryStep && !isConfirmed) {
