799fb1544da21ee7905b871024027a7436a1295f
goga-m <goga.mamoulasvili@gmail.com>
2026-07-01 11:39:44 +0200

    refactor: move dust amount to constant

--- src/app/lib/mainsail/networks/mainsail.devnet.test.ts
+++ src/app/lib/mainsail/networks/mainsail.devnet.test.ts
@@ -33,6 +33,7 @@ describe("Mainsail Devnet Network Configuration", () => {
 
 	it("should have correct constants", () => {
 		expect(network.constants).toEqual({
+			dustAmount: "0.001",
 			epoch: "2023-12-21T00:00:00.000Z",
 			slip44: 1,
 			slip44Eth: 60,
