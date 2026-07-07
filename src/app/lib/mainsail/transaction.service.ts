e21d891e9f0bddd5eb71468dc266bd195726f618
Shahin Safaraliyev <shahin@ardenthq.com>
2026-07-07 15:09:35 +0400

    refactor: implement sending approve contract and batch transfer txs with ledger

--- src/app/lib/mainsail/transaction.service.ts
+++ src/app/lib/mainsail/transaction.service.ts
@@ -177,6 +177,8 @@ export class TransactionService {
 			builder.addRecipient(payment.to, BigInt(amount.toFixed(0)));
 		}
 
+		builder.build();
+
 		await this.#sign(input, builder);
 
 		return new SignedTransactionData().configure(
