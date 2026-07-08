04d17676a19e6c3caf39e09ae0c0c37a745ab6b3
goga-m <goga-m@users.noreply.github.com>
2026-07-08 12:23:10 +0000

    style: resolve style guide violations

--- src/domains/setting/pages/General/General.tsx
+++ src/domains/setting/pages/General/General.tsx
@@ -286,7 +286,7 @@ export const GeneralSettings: React.FC = () => {
 										) {
 											exchangeRateCache.flush();
 										}
-                    
+
 										setValue("exchangeCurrency", exchangeCurrency?.value, {
 											shouldDirty: true,
 											shouldValidate: true,
