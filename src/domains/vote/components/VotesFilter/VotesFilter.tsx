6a5eb68f1a2470bced9ac83709d2f6d07914d2a6
goga-m <goga.mamoulasvili@gmail.com>
2026-07-27 18:45:45 +0200

    wip

--- src/domains/vote/components/VotesFilter/VotesFilter.tsx
+++ src/domains/vote/components/VotesFilter/VotesFilter.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import { FilterProperties } from "./VotesFilter.contracts";
 import { Checkbox } from "@/app/components/Checkbox";
-import { Dropdown } from "@/app/components/Dropdown";
+import { DropdownRoot, DropdownToggle, DropdownContent } from "@/app/components/SimpleDropdown";
 import { Tooltip } from "@/app/components/Tooltip";
 import { Button } from "@/app/components/Button";
 
@@ -17,66 +17,64 @@ export const VotesFilter = ({
 	const { t } = useTranslation();
 
 	return (
-		<div {...properties} className="mr-6 hidden md:block">
-			<Dropdown
-				data-testid="VotesFilter"
-				variant="votesFilter"
-				placement="bottom-end"
-				toggleContent={
+		<div {...properties} data-testid="VotesFilter" className="mr-6 hidden md:block">
+			<DropdownRoot>
+				<DropdownToggle>
 					<Button variant="secondary" size="sm" icon="Funnel" className="h-8">
 						<span className="text-base font-semibold">{t("COMMON.TYPE")}</span>
 					</Button>
-				}
-			>
-				<div className="flex w-56 flex-col items-start space-y-5 px-10 py-7 text-theme-secondary-700 dim:text-theme-dim-200 dark:text-theme-secondary-200">
-					<label
-						className="flex h-5 cursor-pointer items-center space-x-3 rounded-md"
-						data-testid="VotesFilter__option--all"
-					>
-						<Checkbox
-							name="all"
-							checked={selectedOption === "all"}
-							onChange={() => onChange?.("all")}
-							onKeyDown={(event) => {
-								/* istanbul ignore next -- @preserve */
-								if (event.key === "Enter" || event.key === " ") {
-									onChange?.("all");
-								}
-							}}
-						/>
-						<span className="text-base font-medium">{t("VOTE.FILTERS.ALL")}</span>
-					</label>
-
-					<Tooltip
-						placement="bottom-start"
-						content={t("VOTE.FILTERS.NO_VOTES")}
-						disabled={totalCurrentVotes > 0}
-					>
+				</DropdownToggle>
+				<DropdownContent>
+					<div className="flex w-56 flex-col items-start space-y-5 px-10 py-7 text-theme-secondary-700 dim:text-theme-dim-200 dark:text-theme-secondary-200">
 						<label
-							className={cn("flex h-5 items-center space-x-3 rounded-md", {
-								"cursor-pointer": totalCurrentVotes,
-								"text-theme-secondary-500 dim:text-theme-dim-700 dark:text-theme-secondary-700":
-									!totalCurrentVotes,
-							})}
-							data-testid="VotesFilter__option--current"
+							className="flex h-5 cursor-pointer items-center space-x-3 rounded-md"
+							data-testid="VotesFilter__option--all"
 						>
 							<Checkbox
-								disabled={totalCurrentVotes === 0}
-								name="current"
-								checked={selectedOption === "current"}
-								onChange={() => onChange?.("current")}
+								name="all"
+								checked={selectedOption === "all"}
+								onChange={() => onChange?.("all")}
 								onKeyDown={(event) => {
 									/* istanbul ignore next -- @preserve */
 									if (event.key === "Enter" || event.key === " ") {
-										onChange?.("current");
+										onChange?.("all");
 									}
 								}}
 							/>
-							<span className="text-base font-medium">{t("VOTE.FILTERS.CURRENT_VOTES")}</span>
+							<span className="text-base font-medium">{t("VOTE.FILTERS.ALL")}</span>
 						</label>
-					</Tooltip>
-				</div>
-			</Dropdown>
+
+						<Tooltip
+							placement="bottom-start"
+							content={t("VOTE.FILTERS.NO_VOTES")}
+							disabled={totalCurrentVotes > 0}
+						>
+							<label
+								className={cn("flex h-5 items-center space-x-3 rounded-md", {
+									"cursor-pointer": totalCurrentVotes,
+									"text-theme-secondary-500 dim:text-theme-dim-700 dark:text-theme-secondary-700":
+										!totalCurrentVotes,
+								})}
+								data-testid="VotesFilter__option--current"
+							>
+								<Checkbox
+									disabled={totalCurrentVotes === 0}
+									name="current"
+									checked={selectedOption === "current"}
+									onChange={() => onChange?.("current")}
+									onKeyDown={(event) => {
+										/* istanbul ignore next -- @preserve */
+										if (event.key === "Enter" || event.key === " ") {
+											onChange?.("current");
+										}
+									}}
+								/>
+								<span className="text-base font-medium">{t("VOTE.FILTERS.CURRENT_VOTES")}</span>
+							</label>
+						</Tooltip>
+					</div>
+				</DropdownContent>
+			</DropdownRoot>
 		</div>
 	);
 };
